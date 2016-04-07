2730748c9614614a98207375656a9d6c27f9c630
ilyabo <Ilya Boyandin>
2016-04-07 23:32:54 +0200

    moved WithState from helpers to client_api

--- dist/client/client_api.js
+++ dist/client/client_api.js
@@ -3,6 +3,27 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.withState = exports.WithState = undefined;
+
+var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
+
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+
+var _assign = require('babel-runtime/core-js/object/assign');
+
+var _assign2 = _interopRequireDefault(_assign);
+
+var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require('babel-runtime/helpers/inherits');
+
+var _inherits3 = _interopRequireDefault(_inherits2);
 
 var _from = require('babel-runtime/core-js/array/from');
 
@@ -16,7 +37,9 @@ var _createClass2 = require('babel-runtime/helpers/createClass');
 
 var _createClass3 = _interopRequireDefault(_createClass2);
 
-var _helpers = require('./helpers');
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -81,6 +104,58 @@ var ClientApi = function () {
 
 exports.default = ClientApi;
 
+var WithState = exports.WithState = function (_Component) {
+  (0, _inherits3.default)(WithState, _Component);
 
-ClientApi.withState = _helpers.withState;
-ClientApi.WithState = _helpers.WithState;
+  function WithState(props) {
+    (0, _classCallCheck3.default)(this, WithState);
+
+    var _this2 = (0, _possibleConstructorReturn3.default)(this, (0, _getPrototypeOf2.default)(WithState).call(this, props));
+
+    _this2.state = {};
+    return _this2;
+  }
+
+  (0, _createClass3.default)(WithState, [{
+    key: 'render',
+    value: function render() {
+      var _this3 = this;
+
+      var handlers = this.props.handlers;
+
+      var child = this.props.children;
+
+      var props = (0, _assign2.default)({}, this.state);
+
+      var _loop = function _loop(act) {
+        var prop = handlers[act];
+        props[act] = function (v) {
+          if (typeof child.props[act] === 'function') {
+            child.props[act](v);
+          }
+          _this3.setState((0, _defineProperty3.default)({}, prop, v));
+        };
+      };
+
+      for (var act in handlers) {
+        _loop(act);
+      }
+      return _react2.default.cloneElement(child, props);
+    }
+  }]);
+  return WithState;
+}(_react.Component);
+
+WithState.propTypes = {
+  children: _react.PropTypes.object.isRequired
+};
+
+var withState = exports.withState = function withState(handlers, renderChildren) {
+  return function () {
+    return _react2.default.createElement(
+      WithState,
+      { handlers: handlers },
+      renderChildren()
+    );
+  };
+};
--- src/client/client_api.js
+++ src/client/client_api.js
@@ -1,4 +1,4 @@
-import { withState, WithState } from './helpers';
+import React, { Component, PropTypes } from 'react'
 
 export default class ClientApi {
   constructor({ syncedStore, storyStore }) {
@@ -44,6 +44,42 @@ export default class ClientApi {
   
 }
 
-export const withState = withState;
-export const WithState = WithState;
+
+export class WithState extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    let { handlers } = this.props;
+    let child = this.props.children;
+
+    let props = Object.assign({}, this.state);
+
+    for (let act in handlers) {
+      let prop = handlers[ act ];
+      props[ act ] = (v) => {
+        if (typeof(child.props[ act ]) === 'function')  {
+          child.props[ act ](v);
+        }
+        this.setState({ [ prop ] : v });
+      }
+    }
+    return React.cloneElement(child, props);
+  }
+
+}
+
+
+WithState.propTypes = {
+  children: PropTypes.object.isRequired
+};
+
+
+
+export const withState = (handlers, renderChildren) => () => (
+  <WithState handlers={ handlers }>{ renderChildren() }</WithState>
+);
 
