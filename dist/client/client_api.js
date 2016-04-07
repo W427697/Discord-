4fa1afd1bcc2f12b2a02af8b0b84b18e216f4eb1
ilyabo <Ilya Boyandin>
2016-04-07 23:04:29 +0200

    added WithState to the client API

--- dist/client/client_api.js
+++ dist/client/client_api.js
@@ -1,20 +1,44 @@
-"use strict";
+'use strict';
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _from = require("babel-runtime/core-js/array/from");
+var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
+
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+
+var _assign = require('babel-runtime/core-js/object/assign');
+
+var _assign2 = _interopRequireDefault(_assign);
+
+var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
+
+var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+
+var _possibleConstructorReturn2 = require('babel-runtime/helpers/possibleConstructorReturn');
+
+var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
+
+var _inherits2 = require('babel-runtime/helpers/inherits');
+
+var _inherits3 = _interopRequireDefault(_inherits2);
+
+var _from = require('babel-runtime/core-js/array/from');
 
 var _from2 = _interopRequireDefault(_from);
 
-var _classCallCheck2 = require("babel-runtime/helpers/classCallCheck");
+var _classCallCheck2 = require('babel-runtime/helpers/classCallCheck');
 
 var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
 
-var _createClass2 = require("babel-runtime/helpers/createClass");
+var _createClass2 = require('babel-runtime/helpers/createClass');
 
 var _createClass3 = _interopRequireDefault(_createClass2);
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -26,10 +50,11 @@ var ClientApi = function () {
 
     this._syncedStore = syncedStore;
     this._storyStore = storyStore;
+    this.WithState = WithState;
   }
 
   (0, _createClass3.default)(ClientApi, [{
-    key: "storiesOf",
+    key: 'storiesOf',
     value: function storiesOf(kind, m) {
       var _this = this;
 
@@ -47,7 +72,7 @@ var ClientApi = function () {
       return { add: add };
     }
   }, {
-    key: "action",
+    key: 'action',
     value: function action(name) {
       var syncedStore = this._syncedStore;
 
@@ -66,7 +91,7 @@ var ClientApi = function () {
         // Remove events from the args. Otherwise, it creates a huge JSON string.
 
         if (args[0] && args[0].constructor && /Synthetic/.test(args[0].constructor.name)) {
-          args[0] = "[" + args[0].constructor.name + "]";
+          args[0] = '[' + args[0].constructor.name + ']';
         }
 
         actions = [{ name: name, args: args }].concat(actions.slice(0, 4));
@@ -78,3 +103,49 @@ var ClientApi = function () {
 }();
 
 exports.default = ClientApi;
+
+var WithState = function (_Component) {
+  (0, _inherits3.default)(WithState, _Component);
+
+  function WithState(props) {
+    (0, _classCallCheck3.default)(this, WithState);
+
+    var _this2 = (0, _possibleConstructorReturn3.default)(this, (0, _getPrototypeOf2.default)(WithState).call(this, props));
+
+    _this2.state = {};
+    return _this2;
+  }
+
+  (0, _createClass3.default)(WithState, [{
+    key: 'render',
+    value: function render() {
+      var _this3 = this;
+
+      var handlers = this.props.handlers;
+
+      var child = this.props.children;
+
+      var props = (0, _assign2.default)({}, this.state);
+
+      var _loop = function _loop(act) {
+        var prop = handlers[act];
+        props[act] = function (v) {
+          if (typeof child.props[act] === 'function') {
+            child.props[act](v);
+          }
+          _this3.setState((0, _defineProperty3.default)({}, prop, v));
+        };
+      };
+
+      for (var act in handlers) {
+        _loop(act);
+      }
+      return _react2.default.cloneElement(child, props);
+    }
+  }]);
+  return WithState;
+}(_react.Component);
+
+WithState.propTypes = {
+  children: _react.PropTypes.object.isRequired
+};
--- src/client/client_api.js
+++ src/client/client_api.js
@@ -1,7 +1,10 @@
+import React, { Component, PropTypes } from 'react'
+
 export default class ClientApi {
   constructor({ syncedStore, storyStore }) {
     this._syncedStore = syncedStore;
     this._storyStore = storyStore;
+    this.WithState = WithState;
   }
 
   storiesOf(kind, m) {
@@ -40,3 +43,36 @@ export default class ClientApi {
     };
   }
 }
+
+
+class WithState extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    let { handlers } = this.props;
+    let child = this.props.children;
+
+    let props = Object.assign({}, this.state);
+
+    for (let act in handlers) {
+      let prop = handlers[ act ];
+      props[ act ] = (v) => {
+        if (typeof(child.props[ act ]) === 'function')  {
+          child.props[ act ](v);
+        }
+        this.setState({ [ prop ] : v });
+      }
+    }
+    return React.cloneElement(child, props);
+  }
+
+}
+
+
+WithState.propTypes = {
+  children: PropTypes.object.isRequired
+};
