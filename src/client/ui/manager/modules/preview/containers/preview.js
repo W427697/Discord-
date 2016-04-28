8aad8f596b79a0d1263be21301bcf7e677d7972d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-04-28 13:09:07 +0530

    Update comments.

--- src/client/ui/manager/modules/preview/containers/preview.js
+++ src/client/ui/manager/modules/preview/containers/preview.js
@@ -3,10 +3,10 @@ import { useDeps, compose, composeAll } from 'mantra-core';
 
 export const composer = ({ context }, onData) => {
   const { reduxStore } = context();
+  // Here we are sure that, Redux store initialize with the dataId.
+  // So that's why don't need to subscribe here.
   const state = reduxStore.getState();
   const url = `iframe.html?dataId=${state.core.dataId}`;
-  // Here we are sure that, Redux store initialize with teh dataId.
-  // So that's why don't need to subscribe here.
   onData(null, { url });
 };
 
