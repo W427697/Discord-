43477657a80d9d3981f19e3c1bc94c092076b918
Tomas Trescak <tomi.trescak@gmail.com>
2016-05-03 16:44:02 +1000

    Added typings

--- lib/storybook.d.ts
+++ lib/storybook.d.ts
@@ -1,8 +1,10 @@
+declare var module: any; // dangerous
+
 declare module "@kadira/storybook" {
   interface Story {
       add (storyName: string, callback: Function): Story;
   }
 
   export function storiesOf(name: string, module: any): Story;
-  export function action(name: string): void;
+  export function action(name: string, ...params: any[]): Function;
 }
