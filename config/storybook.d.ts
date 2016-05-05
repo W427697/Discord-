ea35e472dd8970d77d61d6dbaf72700090f8cc42
Tomas Trescak <tomi.trescak@gmail.com>
2016-05-05 12:11:46 +1000

    Updated the type definitions to reflect the module structure.

--- config/storybook.d.ts
+++ config/storybook.d.ts
@@ -1,10 +1,8 @@
 declare var module: any; // dangerous
 
-declare module "@kadira/storybook" {
-  interface Story {
-      add (storyName: string, callback: Function): Story;
-  }
-
-  export function storiesOf(name: string, module: any): Story;
-  export function action(name: string, ...params: any[]): Function;
+interface Story {
+    add (storyName: string, callback: Function): Story;
 }
+
+export function storiesOf(name: string, module: any): Story;
+export function action(name: string, ...params: any[]): Function;
