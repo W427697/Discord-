de8caf857fd601687b5821d97617559afa45cef0
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-05-07 03:55:55 +0530

    Implment the logic partially. Still mantra-core needs to provide the support for actions inside actions.

--- src/client/manager/modules/shortcuts/actions/shortcuts.js
+++ src/client/manager/modules/shortcuts/actions/shortcuts.js
@@ -1,10 +1,20 @@
 import { types } from './';
+import { features } from '../../../../libs/key_events';
 
 export default {
-  handleEvent({ reduxStore }, event) {
-    reduxStore.dispatch({
-      type: types.HANDLE_EVENT,
-      event,
-    });
+  handleEvent({ reduxStore }, actions, event) {
+    switch (event) {
+      case features.NEXT_STORY:
+        actions.api.jumpToStory(1);
+        break;
+      case features.PREV_STORY:
+        actions.api.jumpToStory(-1);
+        break;
+      default:
+        reduxStore.dispatch({
+          type: types.HANDLE_EVENT,
+          event,
+        });
+    }
   },
 };
