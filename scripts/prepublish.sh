6016901e4b02b9017eeaca4a0e5cb9b2bf76db12
dcpdev <developer@kroger.com>
2016-05-10 15:50:12 -0400

    Fix broken prepublish npm script; Add shebang to scripts for linux so manager is added to dist; Fix 'react-hmre' removal bug;

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 echo "> Start transpiling ES2015"
 echo ""
 rm -rf ./dist
 ./node_modules/.bin/babel --ignore __tests__,manager --plugins "transform-runtime" ./src --out-dir ./dist
-if [[ -z $DEV_BUILD ]]; then
+if [ -z $DEV_BUILD ]; then
   ./node_modules/.bin/webpack --config scripts/webpack.manager.conf.js
 fi
 echo ""
