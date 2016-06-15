fe90c5570c2b401c0ad3d44ef53d7d47fef76f9c
Muhammed Thanish <mnmtanish@gmail.com>
2016-06-15 08:30:44 +0530

    Refactor webpack config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -3,11 +3,7 @@ const path = require('path');
 module.exports = {
   module: {
     loaders: [
-      {
-        test: /\.css?$/,
-        loaders: ['style', 'raw'],
-        include: path.resolve(__dirname, '../'),
-      },
+      { test: /\.css?$/, loaders: ['style', 'raw'], include: path.resolve(__dirname, '../') },
     ],
   },
 };
