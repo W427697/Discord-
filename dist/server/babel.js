e5e137f48b1edb023ee1e6c66e4b5f90ed4429df
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2016-08-01 14:43:59 +0530

    Add support for stage-0.

--- dist/server/babel.js
+++ dist/server/babel.js
@@ -3,7 +3,7 @@
 module.exports = {
   babelrc: false,
   cacheDirectory: true,
-  presets: ['babel-preset-es2015', 'babel-preset-es2016', 'babel-preset-react'].map(require.resolve),
+  presets: ['babel-preset-es2015', 'babel-preset-es2016', 'babel-preset-stage-0', 'babel-preset-react'].map(require.resolve),
   plugins: ['babel-plugin-syntax-trailing-function-commas', 'babel-plugin-transform-class-properties', 'babel-plugin-transform-object-rest-spread'].map(require.resolve).concat([[require.resolve('babel-plugin-transform-runtime'), {
     helpers: false,
     polyfill: false,
--- dist/server/babel.prod.js
+++ dist/server/babel.prod.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   babelrc: false,
-  presets: ['babel-preset-es2015', 'babel-preset-es2016', 'babel-preset-react'].map(require.resolve),
+  presets: ['babel-preset-es2015', 'babel-preset-es2016', 'babel-preset-stage-0', 'babel-preset-react'].map(require.resolve),
   plugins: ['babel-plugin-syntax-trailing-function-commas', 'babel-plugin-transform-class-properties', 'babel-plugin-transform-object-rest-spread', 'babel-plugin-transform-react-constant-elements'].map(require.resolve).concat([[require.resolve('babel-plugin-transform-runtime'), {
     helpers: false,
     polyfill: false,
