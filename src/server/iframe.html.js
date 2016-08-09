d7b3b396df7ec85bec9e71ae9391da31519a23a9
Stephen Wyatt Bush <stephen.bush@airbnb.com>
2016-08-09 14:42:54 -0700

    Add lang="en" to html element
    Adds lang="en" to html elements in both the index and iframe

--- src/server/iframe.html.js
+++ src/server/iframe.html.js
@@ -1,7 +1,7 @@
 export default function (headHtml) {
   return `
     <!DOCTYPE html>
-    <html>
+    <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script>
