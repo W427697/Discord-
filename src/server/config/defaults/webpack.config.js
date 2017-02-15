cfdb8a28499b146e1ff7a6e3b1be8834b7b39f53
Curtis Steckel <steckel@inscriptive.com>
2017-02-14 21:49:36 -0800

    Work throguh webpage 1 to 2 migration steps

--- src/server/config/defaults/webpack.config.js
+++ src/server/config/defaults/webpack.config.js
@@ -4,53 +4,54 @@ import { includePaths } from '../utils';
 // Add a default custom config which is similar to what React Create App does.
 module.exports = (storybookBaseConfig) => {
   const newConfig = { ...storybookBaseConfig };
-  newConfig.module.loaders = [
-    ...storybookBaseConfig.module.loaders,
+  newConfig.module.rules = [
+    ...storybookBaseConfig.module.rules,
     {
       test: /\.css?$/,
       include: includePaths,
-      loaders: [
-        require.resolve('style-loader'),
-        `${require.resolve('css-loader')}?importLoaders=1`,
-        require.resolve('postcss-loader'),
+      use: [
+        'style-loader',
+        'css-loader?importLoaders=1',
+        'postcss-loader',
       ],
-    },
-    {
-      test: /\.json$/,
-      include: includePaths,
-      loader: require.resolve('json-loader'),
     },
     {
       test: /\.(ico|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2)(\?.*)?$/,
       include: includePaths,
-      loader: require.resolve('file-loader'),
-      query: {
-        name: 'static/media/[name].[hash:8].[ext]',
-      },
+      use: [
+        {
+          loader: 'file-loader',
+          query: {
+            name: 'static/media/[name].[hash:8].[ext]',
+          },
+        },
+      ]
     },
     {
       test: /\.(mp4|webm|wav|mp3|m4a|aac|oga)(\?.*)?$/,
       include: includePaths,
-      loader: require.resolve('url-loader'),
-      query: {
-        limit: 10000,
-        name: 'static/media/[name].[hash:8].[ext]',
-      },
+      use: [{
+        loader: 'url-loader',
+        query: {
+          limit: 10000,
+          name: 'static/media/[name].[hash:8].[ext]',
+        },
+      }]
     },
   ];
 
-  newConfig.postcss = () => {
-    return [
-      autoprefixer({
-        browsers: [
-          '>1%',
-          'last 4 versions',
-          'Firefox ESR',
-          'not ie < 9',
-        ],
-      }),
-    ];
-  };
+  // newConfig.postcss = () => {
+  //   return [
+  //     autoprefixer({
+  //       browsers: [
+  //         '>1%',
+  //         'last 4 versions',
+  //         'Firefox ESR',
+  //         'not ie < 9',
+  //       ],
+  //     }),
+  //   ];
+  // };
 
   newConfig.resolve.alias = {
     ...storybookBaseConfig.resolve.alias,
--- src/server/config/utils.js
+++ src/server/config/utils.js
@@ -26,21 +26,21 @@ export const nodePaths = (process.env.NODE_PATH || '')
 export function loadEnv(options = {}) {
   const defaultNodeEnv = options.production ? 'production' : 'development';
   const env = {
-    NODE_ENV: process.env.NODE_ENV || defaultNodeEnv,
+    NODE_ENV: JSON.stringify(process.env.NODE_ENV || defaultNodeEnv),
     // This is to support CRA's public folder feature.
     // In production we set this to dot(.) to allow the browser to access these assests
     // even when deployed inside a subpath. (like in GitHub pages)
     // In development this is just empty as we always serves from the root.
-    PUBLIC_URL: options.production ? '.' : '',
+    PUBLIC_URL: JSON.stringify(options.production ? '.' : ''),
   };
 
   Object.keys(process.env)
     .filter(name => /^STORYBOOK_/.test(name))
     .forEach((name) => {
-      env[name] = process.env[name];
+      env[name] = JSON.stringify(process.env[name]);
     });
 
   return {
-    'process.env': JSON.stringify(env),
+    'process.env': env,
   };
 }
