42328de95389f040a2e5738504f6aace4d62fe37
Michael Shilman <shilman@lab80.co>
2017-06-09 01:34:53 +1200

    Added addon-options to test-cra kitchen sink

--- examples/test-cra/.storybook/config.js
+++ examples/test-cra/.storybook/config.js
@@ -1,4 +1,16 @@
 import { configure } from '@storybook/react';
+import { setOptions } from '@storybook/addon-options';
+
+setOptions({
+  name: 'Test-CRA',
+  url: 'https://github.com/storybooks/storybook/tree/master/examples/test-cra',
+  goFullScreen: false,
+  showLeftPanel: true,
+  showDownPanel: true,
+  showSearchBox: false,
+  downPanelInRight: true,
+  sortStoriesByKind: false,
+})
 
 function loadStories() {
   require('../src/stories');
