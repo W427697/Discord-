493a1674b546620d8da94c35d6a26f6eecef6fb7
Michael Shilman <shilman@lab80.co>
2017-06-09 01:20:04 +1200

    Simple notes example

--- examples/test-cra/src/stories/index.js
+++ examples/test-cra/src/stories/index.js
@@ -5,6 +5,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import { WithNotes } from '@storybook/addon-notes';
 
 import Button from './Button';
 import Welcome from './Welcome';
@@ -13,7 +14,11 @@ import ComponentWithRef from './ComponentWithRef';
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with text', () =>
+    <WithNotes notes={'A very simple button'}>
+      <Button onClick={action('clicked')}>Hello Button</Button>
+    </WithNotes>
+  )
   .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
 
 storiesOf('ComponentWithRef', module).add('basic', () => <ComponentWithRef />);
