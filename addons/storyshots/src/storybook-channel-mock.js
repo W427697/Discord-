6358e0392223d6cd3982ed095fab63e680e34283
Norbert de Langen <ndelangen@me.com>
2017-06-09 21:49:13 +0200

    Add mocking of all decorators in storyshots

--- addons/storyshots/src/storybook-channel-mock.js
+++ addons/storyshots/src/storybook-channel-mock.js
@@ -8,3 +8,46 @@ export default function createChannel() {
 
   return new Channel({ transport });
 }
+
+try {
+  jest.mock('@storybook/addon-options', () => ({
+    setOptions: jest.fn(),
+  }));
+} catch (error) {
+  /* didn't need to mock it */
+}
+try {
+  jest.mock('@storybook/addon-actions', () => ({
+    action: jest.fn(),
+  }));
+} catch (error) {
+  /* didn't need to mock it */
+}
+try {
+  jest.mock('@storybook/addon-links', () => ({
+    linkTo: jest.fn(),
+  }));
+} catch (error) {
+  /* didn't need to mock it */
+}
+try {
+  jest.mock('@storybook/addon-notes', () => ({
+    WithNotes: ({ children }) => children,
+  }));
+} catch (error) {
+  /* didn't need to mock it */
+}
+try {
+  jest.mock('@storybook/addon-knobs', () => ({
+    withKnobs: a => a(),
+    text: jest.fn(),
+    number: jest.fn(),
+  }));
+} catch (error) {
+  /* didn't need to mock it */
+}
+try {
+  jest.mock('@storybook/addon-centered', () => a => a());
+} catch (error) {
+  /* didn't need to mock it */
+}
--- examples/cra-storybook/.storybook/config.js
+++ examples/cra-storybook/.storybook/config.js
@@ -3,13 +3,13 @@ import { setOptions } from '@storybook/addon-options';
 import infoAddon from '@storybook/addon-info';
 
 setOptions({
-  name: 'Test-CRA',
+  name: 'Storybook',
   url: 'https://github.com/storybooks/storybook/tree/master/examples/test-cra',
   goFullScreen: false,
   showLeftPanel: true,
   showDownPanel: true,
   showSearchBox: false,
-  downPanelInRight: true,
+  downPanelInRight: false,
   sortStoriesByKind: false,
 })
 
