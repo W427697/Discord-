fd258949d8561f8f4635fafed646041f6d239b02
Oleg Proskurin <regx@usul.su>
2017-06-29 20:27:22 +0300

    Setup stories

--- examples/cra-kitchen-sink/src/stories/index.js
+++ examples/cra-kitchen-sink/src/stories/index.js
@@ -91,6 +91,62 @@ storiesOf('Button', module)
     () => <Button>click the "?" in top right for info</Button>
   );
 
+
+storiesOf('Buttons Guide#', module)
+  .addDecorator(withKnobs)
+  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>)
+  .add('with notes', () =>
+    <WithNotes notes={'A very simple button'}>
+      <Button>Check my notes in the notes panel</Button>
+    </WithNotes>
+  )
+  .add('with knobs', () => {
+    const name = text('Name', 'Storyteller');
+    const age = number('Age', 70, { range: true, min: 0, max: 90, step: 5 });
+    const fruits = {
+      apple: 'Apple',
+      banana: 'Banana',
+      cherry: 'Cherry',
+    };
+    const fruit = select('Fruit', fruits, 'apple');
+    const dollars = number('Dollars', 12.5);
+
+    // NOTE: color picker is currently broken
+    const backgroundColor = color('background', '#ffff00');
+    const items = array('Items', ['Laptop', 'Book', 'Whiskey']);
+    const otherStyles = object('Styles', {
+      border: '3px solid #ff00ff',
+      padding: '10px',
+    });
+    const nice = boolean('Nice', true);
+
+    // NOTE: put this last because it currently breaks everything after it :D
+    const birthday = date('Birthday', new Date('Jan 20 2017'));
+
+    const intro = `My name is ${name}, I'm ${age} years old, and my favorite fruit is ${fruit}.`;
+    const style = { backgroundColor, ...otherStyles };
+    const salutation = nice ? 'Nice to meet you!' : 'Leave me alone!';
+
+    return (
+      <div style={style}>
+        <p>{intro}</p>
+        <p>My birthday is: {new Date(birthday).toLocaleDateString()}</p>
+        <p>My wallet contains: ${dollars.toFixed(2)}</p>
+        <p>In my backpack, I have:</p>
+        <ul>
+          {items.map(item => <li key={item}>{item}</li>)}
+        </ul>
+        <p>{salutation}</p>
+      </div>
+    );
+  })
+  .addWithInfo(
+    'with some info',
+    'Use the [info addon](https://github.com/storybooks/storybook/tree/master/addons/info) with its painful API.',
+    () => <Button>click the "?" in top right for info</Button>
+  );
+
 storiesOf('App', module).add('full app', () => <App />);
 
 storiesOf('Centered Button', module)
