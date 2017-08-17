a0647e4c2b2ccb1ba7456358c7359df83f40ab1e
Dominic Chambers <dominic.chambers@babylonhealth.com>
2017-08-17 13:18:06 +0100

    Allow promise to be returned from 'add()'

--- addons/storyshots/src/test-bodies.js
+++ addons/storyshots/src/test-bodies.js
@@ -1,11 +1,11 @@
 import renderer from 'react-test-renderer';
 import shallow from 'react-test-renderer/shallow';
 
-export const snapshotWithOptions = options => ({ story, context }) => {
-  const storyElement = story.render(context);
-  const tree = renderer.create(storyElement, options).toJSON();
-  expect(tree).toMatchSnapshot();
-};
+export const snapshotWithOptions = options => ({ story, context }) =>
+  Promise.resolve(story).then(storyVal => storyVal.render(context)).then(storyElement => {
+    const tree = renderer.create(storyElement, options).toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 
 export const snapshot = snapshotWithOptions({});
 
