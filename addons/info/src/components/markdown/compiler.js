079bae9c2ada8f6ef83cc52d33991d05350c15c0
Oleg Proskurin <regx@usul.su>
2017-08-24 22:27:34 +0300

    Get rid of remark plagins

--- addons/info/src/components/markdown/compiler.js
+++ addons/info/src/components/markdown/compiler.js
@@ -1,9 +1,9 @@
 import unified from 'unified';
 import remarkParse from 'remark-parse';
 import remarkReact from 'remark-react';
-import remarkGithub from 'remark-github'; // note: uncomment when issue will be solved
-import remarkEmoji from 'remark-emoji';
-import remarkHighlight from 'remark-highlight.js';
+// import remarkGithub from 'remark-github'; // note: uncomment when issue will be solved
+// import remarkEmoji from 'remark-emoji';
+// import remarkHighlight from 'remark-highlight.js';
 
 import { options as defaultOptions } from './defaults';
 
@@ -12,11 +12,11 @@ let options = defaultOptions;
 function setupProcessor() {
   return unified()
     .use(remarkParse)
-    .use(remarkGithub, {
-      repository: options.repository,
-    })
-    .use(remarkEmoji)
-    .use(remarkHighlight)
+    // .use(remarkGithub, {
+    //   repository: options.repository,
+    // })
+    // .use(remarkEmoji)
+    // .use(remarkHighlight)
     .use(remarkReact, { remarkReactComponents: options.components, sanitize: false });
 }
 
