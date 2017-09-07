49203b45efcc0618f8c2333a0352864cfdd77838
Dominic Chambers <dominic.chambers@babylonhealth.com>
2017-09-07 04:41:13 +0100

    Wrap stories in promises

--- examples/es6-promise-kitchen-sink/src/stories/index.js
+++ examples/es6-promise-kitchen-sink/src/stories/index.js
@@ -9,5 +9,9 @@ import { Button, Welcome } from '@storybook/react/demo';
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('with text', () =>
+    Promise.resolve(<Button onClick={action('clicked')}>Hello Button</Button>)
+  )
+  .add('with some emoji', () =>
+    Promise.resolve(<Button onClick={action('clicked')}>😀 😎 👍 💯</Button>)
+  );
