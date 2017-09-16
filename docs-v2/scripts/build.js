8a9621d0cb85adbf5534daae55528bf87e8a0cc5
igor <davydkin.igor@gmail.com>
2017-09-16 17:01:22 +0300

    Move git scripts to build.js

--- docs-v2/scripts/build.js
+++ docs-v2/scripts/build.js
@@ -1,9 +1,25 @@
+/* eslint-disable no-console */
 const exec = require('child_process').exec;
+const fs = require('fs-extra');
+const packageJson = require('../package.json');
 const generateSitemap = require('./tasks/sitemap');
+
+function handleProcessClose(build, resolve, reject, message) {
+  build.on('close', code => (code === 0 ? resolve() : reject(new Error(message))));
+}
+
+const version = packageJson.version;
+const prettifiedVersion = version.replace(/\./g, '-');
+const outputDir = 'public';
+const docsRepo = process.env.DOCS_REPO;
+
+if (!docsRepo) {
+  throw new Error('DOCS_REPO env parameter is no defined');
+}
 
 const sitemapReady = generateSitemap().then(() => console.log('🗺 ', 'Sitemap generated'));
 
-/* 
+/*
  * This script runs the command 'next build' in node production mode
  * If succesfull we proceed with 'next export'
  * We pipe all the output of the process directly into the output of this script's output
@@ -12,26 +28,64 @@ Promise.all([sitemapReady])
   .then(
     () =>
       new Promise((resolve, reject) => {
-        const build = exec('NODE_END="production" next build');
+        const build = exec(`git clone ${docsRepo} ./${outputDir} -v`);
         build.stdout.pipe(process.stdout);
-        build.on(
-          'close',
-          code => (code === 0 ? resolve() : reject(new Error('🛑 build step failed')))
-        );
+        handleProcessClose(build, resolve, reject, '🛑 git-clone step failed');
       })
   )
   .then(
     () =>
       new Promise((resolve, reject) => {
-        const build = exec('NODE_END="production" next export');
+        const build = exec('next build');
         build.stdout.pipe(process.stdout);
-        build.stderr.pipe(process.stdout);
-        build.on(
-          'close',
-          code => (code === 0 ? resolve() : reject(new Error('🛑 export step failed')))
-        );
+        handleProcessClose(build, resolve, reject, '🛑 build step failed');
       })
   )
+  .then(
+    () =>
+      new Promise((resolve, reject) => {
+        const build = exec(`next export -o ${outputDir}`);
+        build.stdout.pipe(process.stdout);
+        build.stderr.pipe(process.stdout);
+        handleProcessClose(build, resolve, reject, '🛑 export step failed');
+      })
+  )
+  .then(
+    () =>
+      new Promise((resolve, reject) => {
+        const build = exec(`next export -o ${outputDir}/${prettifiedVersion}`);
+        build.stdout.pipe(process.stdout);
+        build.stderr.pipe(process.stdout);
+        handleProcessClose(build, resolve, reject, '🛑 export to version step failed');
+      })
+  )
+  .then(
+    () =>
+      new Promise((resolve, reject) => {
+        const build = exec(`cd ${outputDir} && git add . -v`);
+        build.stdout.pipe(process.stdout);
+        handleProcessClose(build, resolve, reject, '🛑 git-add step failed');
+      })
+  )
+  .then(
+    () =>
+      new Promise((resolve, reject) => {
+        const build = exec(
+          `cd ${outputDir} && git commit -a --message="Update docs version to ${version}" -v`
+        );
+        build.stdout.pipe(process.stdout);
+        handleProcessClose(build, resolve, reject, '🛑 git-commit step failed');
+      })
+  )
+  .then(
+    () =>
+      new Promise((resolve, reject) => {
+        const build = exec(`cd ${outputDir} && git push -v`);
+        build.stdout.pipe(process.stdout);
+        handleProcessClose(build, resolve, reject, '🛑 git-push step failed');
+      })
+  )
+  .then(() => fs.remove(outputDir))
   .catch(error => {
     // we wait a bit to let the stderr be printed
     setTimeout(() => console.log(error), 500);
