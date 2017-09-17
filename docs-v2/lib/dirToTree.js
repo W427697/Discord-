7b2990ec12f8089765a104dd8424cf5bdcb628fd
igor <davydkin.igor@gmail.com>
2017-09-17 13:57:28 +0300

    Make sitemap generation windows friendly

--- docs-v2/lib/dirToTree.js
+++ docs-v2/lib/dirToTree.js
@@ -6,7 +6,10 @@ const unified = require('unified');
 const remarkParse = require('remark-parse');
 const mdHelpers = require('remark-helpers');
 
-const parser = markdown => unified().use(remarkParse).parse(markdown);
+const parser = markdown =>
+  unified()
+    .use(remarkParse)
+    .parse(markdown);
 
 const findTitle = list =>
   // get first main heading and return plain text
@@ -19,7 +22,7 @@ const findTitle = list =>
 const createFile = (stat, fileName, workingDir, baseDir) => {
   const name = path.basename(fileName, path.extname(fileName));
   const routeFile = name.replace('index', '');
-  const routeBase = workingDir.replace(baseDir, '') || path.sep;
+  const routeBase = workingDir.replace(baseDir, '').replace(/\\/, '/') || path.posix.sep;
 
   return fs
     .readFileAsync(
@@ -35,30 +38,33 @@ const createFile = (stat, fileName, workingDir, baseDir) => {
       name,
       title,
       isFile: true,
-      route: path.join(routeBase, routeFile),
+      route: path.posix.join(routeBase, routeFile),
     }));
 };
 
 const createDirectory = (items, localWorkingDir, baseDir) => ({
-  route: localWorkingDir.replace(baseDir, ''),
+  route: localWorkingDir.replace(baseDir, '').replace(/\\/, '/'),
   length: items.length,
   files: items,
 });
 
 const readDir = (workingDir, baseDir) =>
-  fs.readdirAsync(workingDir).filter(n => !n.match(/^[._]/)).map(fileName =>
-    fs.statAsync(path.join(workingDir, fileName)).then(stat => {
-      if (stat.isFile()) {
-        return createFile(stat, fileName, workingDir, baseDir);
-      }
-      if (stat.isDirectory()) {
-        const localWorkingDir = path.join(workingDir, fileName);
-        return readDir(localWorkingDir, baseDir).then(items =>
-          createDirectory(items, localWorkingDir, baseDir)
-        );
-      }
-      return undefined;
-    })
-  );
+  fs
+    .readdirAsync(workingDir)
+    .filter(n => !n.match(/^[._]/))
+    .map(fileName =>
+      fs.statAsync(path.join(workingDir, fileName)).then(stat => {
+        if (stat.isFile()) {
+          return createFile(stat, fileName, workingDir, baseDir);
+        }
+        if (stat.isDirectory()) {
+          const localWorkingDir = path.join(workingDir, fileName);
+          return readDir(localWorkingDir, baseDir).then(items =>
+            createDirectory(items, localWorkingDir, baseDir)
+          );
+        }
+        return undefined;
+      })
+    );
 
 module.exports = readDir;
