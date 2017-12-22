d7547a11b851fbe82d62181316f4728da70435d2
Rahul <Primigenus@users.noreply.github.com>
2017-12-22 12:18:00 -0800

    Add missing lang attribute
    Accessibility tests warn that the HTML element requires a lang attribute. See: https://dequeuniversity.com/rules/axe/2.4/html-has-lang?application=AxeChrome
    
    Ideally the lang attribute's value would be set to whatever language the user is using for Storybook, but that would require adding configuration, so this seems like a short term fix that addresses the immediate issue.

--- app/react/src/server/iframe.html.js
+++ app/react/src/server/iframe.html.js
@@ -58,7 +58,7 @@ export default function({ assets, publicPath, headHtml }) {
 
   return `
     <!DOCTYPE html>
-    <html>
+    <html lang="en">
       <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
