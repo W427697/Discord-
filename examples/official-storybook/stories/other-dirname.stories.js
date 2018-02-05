6cfe71e3ce2ba91e71df17419af2553a3f529915
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-02-05 08:47:39 -0500

    remove BASE from error story

--- examples/official-storybook/stories/other-dirname.stories.js
+++ examples/official-storybook/stories/other-dirname.stories.js
@@ -9,7 +9,7 @@ storiesOf(`Other|${base}/Dirname Example`, module)
   .add('story 1', () => <BaseButton label="Story 1" />)
   .add('story 2', () => <BaseButton label="Story 2" />);
 
-storiesOf(`Other|${base}/Error Overlay Example`, module)
+storiesOf('Other|Error Overlay Example', module)
   .add('Storybook has a React errorOverlay when a component throws an error', () => (
     <button
       onClick={() => {
