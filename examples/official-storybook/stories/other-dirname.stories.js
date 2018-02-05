68254e9e0039b030a3a1c162dabf49359e6d2325
tsiq-swyx <shawn.wang@twosigmaiq.com>
2018-02-04 20:57:53 -0500

    add error overlay example to official-storybook

--- examples/official-storybook/stories/other-dirname.stories.js
+++ examples/official-storybook/stories/other-dirname.stories.js
@@ -8,3 +8,17 @@ import BaseButton from '../components/BaseButton';
 storiesOf(`Other|${base}/Dirname Example`, module)
   .add('story 1', () => <BaseButton label="Story 1" />)
   .add('story 2', () => <BaseButton label="Story 2" />);
+
+storiesOf(`Other|${base}/Error Overlay Example`, module)
+  .add('Storybook has a React errorOverlay when a component throws an error', () => (
+    <button
+      onClick={() => {
+        throw new Error('example error');
+      }}
+    >
+      Test Error Overlay
+    </button>
+  ))
+  .add('Storybook has an error', () => {
+    throw new Error('example error');
+  });
