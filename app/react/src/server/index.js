9977f8020445e93a0b75b51c05faf24f865d6d1f
igor <davydkin.igor@gmail.com>
2018-02-08 08:05:55 +0200

    Pass errorOverlayMiddleware as a parameter to the "buildDev"

--- app/react/src/server/index.js
+++ app/react/src/server/index.js
@@ -1,5 +1,6 @@
 import { buildDev } from '@storybook/core/server';
 import path from 'path';
+import errorOverlayMiddleware from 'react-dev-utils/errorOverlayMiddleware';
 import packageJson from '../../package.json';
 import getBaseConfig from './config/webpack.config';
 import loadConfig from './config';
@@ -8,5 +9,6 @@ buildDev({
   packageJson,
   getBaseConfig,
   loadConfig,
+  appMiddleware: [errorOverlayMiddleware],
   defaultFavIcon: path.resolve(__dirname, 'public/favicon.ico'),
 });
