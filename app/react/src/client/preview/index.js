3367c2820bdd236834dbc4d9d80b5e5b67e5e5f1
Tom Coleman <tom@thesnail.org>
2018-04-05 21:46:37 +1000

    POC: render stories *inside* the app

--- app/react/src/client/preview/index.js
+++ app/react/src/client/preview/index.js
@@ -1,8 +1,15 @@
+import React from 'react';
 import { start } from '@storybook/core/client';
 
 import render from './render';
 
-const { clientApi, configApi, forceReRender } = start(render);
+// This is a total hack for a POC
+export const el = React.createRef();
+
+// start() normally renders immediately to #root
+const { clientApi, configApi, forceReRender } = start(
+  (context, force) => console.log(el.current) || (el.current && render(context, force, el.current))
+);
 
 export const {
   storiesOf,
--- examples/cra-kitchen-sink/src/.storybook/config.js
+++ examples/cra-kitchen-sink/src/.storybook/config.js
@@ -23,10 +23,10 @@ setAddon(infoAddon);
 
 function loadStories() {
   // put welcome screen at the top of the list so it's the first one displayed
-  require('../src/stories/welcome');
+  require('../stories/welcome');
 
   // automatically import all story js files that end with *.stories.js
-  const req = require.context('../src/stories', true, /\.stories\.js$/);
+  const req = require.context('../stories', true, /\.stories\.js$/);
   req.keys().forEach(filename => req(filename));
 }
 
