78ef5f300583ff18b6c7c884be62671aa3519166
Zarema Khalilova <zarema.khalilova@gmail.com>
2018-06-15 22:07:41 +0300

    Update stories for hyperapp example

--- examples/hyperapp-kitchen-sink/src/stories/index.stories.js
+++ examples/hyperapp-kitchen-sink/src/stories/index.stories.js
@@ -1,5 +1,14 @@
 import { storiesOf } from '@storybook/hyperapp';
+import { action } from '@storybook/addon-actions';
+import { withLinks } from '@storybook/addon-links';
 import { h } from 'hyperapp';
 import Welcome from '../components/Welcome';
+import Button from '../components/Button';
 
-storiesOf('Welcome', module).add('welcome', () => <Welcome />);
+storiesOf('Welcome', module)
+  .addDecorator(withLinks)
+  .add('welcome', () => <Welcome />);
+
+storiesOf('Demo', module).add('button', () => (
+  <Button onclick={() => action('button-click')}>Click me</Button>
+));
