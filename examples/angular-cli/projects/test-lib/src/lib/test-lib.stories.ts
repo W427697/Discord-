ff7b2cf5637928faf4b16924f54ceead41aa710b
Rob Halff <rob.halff@gmail.com>
2018-06-15 14:41:30 +0200

    remove decorator

--- examples/angular-cli/projects/test-lib/src/lib/test-lib.stories.ts
+++ examples/angular-cli/projects/test-lib/src/lib/test-lib.stories.ts
@@ -1,8 +1,6 @@
 import { storiesOf } from '@storybook/angular';
 import { TestLibComponent } from './test-lib.component';
 
-storiesOf('Projects|TestLib', module)
-  .addDecorator('test-lib.component')
-  .add('TestLib', () => ({
-    component: TestLibComponent,
-  }));
+storiesOf('Projects|TestLib', module).add('TestLib', () => ({
+  component: TestLibComponent,
+}));
