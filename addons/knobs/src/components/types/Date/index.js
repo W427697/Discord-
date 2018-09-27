5fbab788f147b3c6abf127ee06c9e8a0feb5d453
Norbert de Langen <ndelangen@me.com>
2018-09-27 20:10:44 +0200

    REPLACE react-datetime with native date/time pickers

--- addons/knobs/src/components/types/Date/index.js
+++ addons/knobs/src/components/types/Date/index.js
@@ -1,36 +1,71 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import Datetime from 'react-datetime';
-import insertCss from 'insert-css';
-import style from './styles';
 
-const customStyle = `
-  .rdt input {
-    outline: 0;
-    width: 100%;
-    border: 1px solid #f7f4f4;
-    border-radius: 2px;
-    font-size: 11px;
-    padding: 5px;
-    color: #555;
-    display: table-cell;
-    box-sizing: border-box;
-  }
-`;
+const styles = {
+  display: 'table-cell',
+  boxSizing: 'border-box',
+  verticalAlign: 'middle',
+  height: '25px',
+  width: '100%',
+  outline: 'none',
+  border: '1px solid #f7f4f4',
+  borderRadius: 2,
+  fontSize: 11,
+  padding: '5px',
+  color: '#444',
+  flex: 1,
+};
 
-insertCss(style);
-insertCss(customStyle);
+const formatDate = date => {
+  const year = date.getFullYear();
+  const month = `0${date.getMonth() + 1}`.slice(-2);
+  const day = `0${date.getDate()}`.slice(-2);
 
-const DateType = ({ knob, onChange }) => (
-  <div>
-    <Datetime
-      id={knob.name}
-      value={knob.value ? new Date(knob.value) : null}
-      type="date"
-      onChange={date => onChange(date.valueOf())}
-    />
-  </div>
-);
+  return `${year}-${month}-${day}`;
+};
+const formatTime = date => {
+  const hours = `0${date.getHours()}`.slice(-2);
+  const minutes = `0${date.getMinutes()}`.slice(-2);
+
+  return `${hours}:${minutes}`;
+};
+
+const DateType = ({ knob, onChange }) => {
+  const { name } = knob;
+  const date = new Date(knob.value);
+
+  return name ? (
+    <div style={{ display: 'flex' }}>
+      <input
+        style={styles}
+        type="date"
+        id={`${name}date`}
+        value={formatDate(date)}
+        onChange={e => {
+          const [year, month, day] = e.target.value.split('-');
+          const result = new Date(date);
+          result.setFullYear(parseInt(year, 10));
+          result.setMonth(parseInt(month, 10) - 1);
+          result.setDate(parseInt(day, 10));
+          onChange(result);
+        }}
+      />
+      <input
+        style={styles}
+        type="time"
+        id={`${name}time`}
+        value={formatTime(date)}
+        onChange={e => {
+          const [hours, minutes] = e.target.value.split(':');
+          const result = new Date(date);
+          result.setHours(parseInt(hours, 10));
+          result.setMinutes(parseInt(minutes, 10));
+          onChange(result);
+        }}
+      />
+    </div>
+  ) : null;
+};
 
 DateType.defaultProps = {
   knob: {},
@@ -40,12 +75,14 @@ DateType.defaultProps = {
 DateType.propTypes = {
   knob: PropTypes.shape({
     name: PropTypes.string,
-    value: PropTypes.number,
+    value: PropTypes.string,
   }),
   onChange: PropTypes.func,
 };
 
-DateType.serialize = value => String(value);
-DateType.deserialize = value => parseFloat(value);
+DateType.serialize = value =>
+  console.log('serialize input:', value) || new Date(value).getTime() || new Date().getTime();
+DateType.deserialize = value =>
+  console.log('deserialize input:', value) || new Date(value).getTime() || new Date().getTime();
 
 export default DateType;
