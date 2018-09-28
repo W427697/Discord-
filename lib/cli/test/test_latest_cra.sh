766bea666a162db03e78fcda5960f4e13b4d632b
Norbert de Langen <ndelangen@me.com>
2018-09-28 10:09:58 +0200

    sad

--- lib/cli/test/test_latest_cra.sh
+++ lib/cli/test/test_latest_cra.sh
@@ -10,9 +10,20 @@ cd cra-fixtures
 
 npm install expo-cli -g
 
+echo "---------------------------------------------"
+echo "create-react-app react-scripts-latest-fixture"
+echo "---------------------------------------------"
 ../../node_modules/.bin/npx create-react-app react-scripts-latest-fixture
+echo ""
 
+echo "---------------------------------------------"
+echo "expo init react-native-scripts-latest-fixture"
+echo "---------------------------------------------"
 ../../node_modules/.bin/npx expo init react-native-scripts-latest-fixture
+echo ""
 
 cd ..
+echo "------------------------------"
+echo "./run_tests.sh -f cra-fixtures"
+echo "------------------------------"
 ./run_tests.sh -f cra-fixtures
