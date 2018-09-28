bdcffa6f1fb5a48fac27166012cc65c4bc128cee
Norbert de Langen <ndelangen@me.com>
2018-09-28 09:25:42 +0200

    work damnit

--- lib/cli/test/test_latest_cra.sh
+++ lib/cli/test/test_latest_cra.sh
@@ -8,6 +8,8 @@ rm -rfd cra-fixtures
 mkdir cra-fixtures
 cd cra-fixtures
 
+npm install expo-cli -g
+
 ../../node_modules/.bin/npx create-react-app react-scripts-latest-fixture
 
 ../../node_modules/.bin/npx expo init react-native-scripts-latest-fixture
