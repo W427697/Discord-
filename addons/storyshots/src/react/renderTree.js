fb4f915997bb74269187b4eb284d27d3fbb4fb81
Tobias Lengsholz <tobias.lengsholz@invision.de>
2018-10-03 20:12:32 +0200

    Add deprecation warning when using the `serlaizer` option

--- addons/storyshots/src/react/renderTree.js
+++ addons/storyshots/src/react/renderTree.js
@@ -1,7 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import reactTestRenderer from 'react-test-renderer';
+import deprecate from 'util-deprecate';
 
 function getRenderedTree(story, context, { renderer, serializer, ...rendererOptions }) {
+  if (serializer) deprecate('The "serializer" option of @storybook/addon-storyshots  has been deprecated. Please use "snapshotSerializers: [<your serializer>]" in the future.');
+
   const storyElement = story.render(context);
   const currentRenderer = renderer || reactTestRenderer.create;
   const tree = currentRenderer(storyElement, rendererOptions);
