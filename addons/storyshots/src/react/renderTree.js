35425e38c84d798cf1bcfdb6a35dcc1742af013e
Tobias Lengsholz <tobias.lengsholz@invision.de>
2018-10-03 20:58:35 +0200

    Use new option in example project

--- addons/storyshots/src/react/renderTree.js
+++ addons/storyshots/src/react/renderTree.js
@@ -1,9 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import reactTestRenderer from 'react-test-renderer';
-import deprecate from 'util-deprecate';
 
 function getRenderedTree(story, context, { renderer, serializer, ...rendererOptions }) {
-  if (serializer) deprecate('The "serializer" option of @storybook/addon-storyshots  has been deprecated. Please use "snapshotSerializers: [<your serializer>]" in the future.');
+  if (serializer) {
+    // eslint-disable-next-line no-console
+    console.warn(
+      'The "serializer" option of @storybook/addon-storyshots  has been deprecated. Please use "snapshotSerializers: [<your serializer>]" in the future.'
+    );
+  }
 
   const storyElement = story.render(context);
   const currentRenderer = renderer || reactTestRenderer.create;
