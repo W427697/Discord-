e0a4af14eea0bd530f215489d2f6d6391c3a678c
Tobias Lengsholz <tobias.lengsholz@invision.de>
2018-10-04 10:03:13 +0200

    Refactor renderTree logic to only have one conditional

--- addons/storyshots/src/react/renderTree.js
+++ addons/storyshots/src/react/renderTree.js
@@ -2,17 +2,18 @@
 import reactTestRenderer from 'react-test-renderer';
 
 function getRenderedTree(story, context, { renderer, serializer, ...rendererOptions }) {
+  const storyElement = story.render(context);
+  const currentRenderer = renderer || reactTestRenderer.create;
+  const tree = currentRenderer(storyElement, rendererOptions);
+
   if (serializer) {
     // eslint-disable-next-line no-console
     console.warn(
       'The "serializer" option of @storybook/addon-storyshots  has been deprecated. Please use "snapshotSerializers: [<your serializer>]" in the future.'
     );
+    return serializer(tree);
   }
-
-  const storyElement = story.render(context);
-  const currentRenderer = renderer || reactTestRenderer.create;
-  const tree = currentRenderer(storyElement, rendererOptions);
-  return serializer ? serializer(tree) : tree;
+  return tree;
 }
 
 export default getRenderedTree;
