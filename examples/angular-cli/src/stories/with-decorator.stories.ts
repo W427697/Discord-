279a083663d54fc25e0cf520131ab578ff3e2a37
klimentru1986 <kliment1986@inbox.ru>
2018-10-06 17:56:58 +0300

    examples

---
+++ examples/angular-cli/src/stories/with-decorator.stories.ts
@@ -0,0 +1,60 @@
+import { NgStory, NgStoryModule, ngBootstrapStoryModule } from '@storybook/angular';
+import { Welcome, Button } from '@storybook/angular/demo';
+import { linkTo } from '@storybook/addon-links';
+
+@NgStory({
+  kind: 'Story with decorator',
+  name: 'to Storybook',
+  parameters: {
+    template: `<storybook-welcome-component (showApp)="showApp()"></storybook-welcome-component>`,
+    props: {
+      showApp: linkTo('Button'),
+    },
+    moduleMetadata: {
+      declarations: [Welcome],
+    },
+  },
+})
+export class Storybook {}
+
+@NgStory({
+  kind: 'Story with decorator',
+  name: 'with text',
+  parameters: {
+    template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
+    props: {
+      text: 'Hello Button',
+      onClick: (event: Event) => {
+        console.log('some bindings work');
+        console.log(event);
+      },
+    },
+    moduleMetadata: {
+      declarations: [Button],
+    },
+  },
+})
+export class WithText {}
+
+@NgStory({
+  kind: 'Story with decorator',
+  name: 'with some emoji',
+  parameters: {
+    template: `<storybook-button-component [text]="text" (onClick)="onClick($event)"></storybook-button-component>`,
+    props: {
+      text: '😀 😎 👍 💯',
+      onClick: () => {},
+    },
+    moduleMetadata: {
+      declarations: [Button],
+    },
+  },
+})
+export class WithSomeEmoji {}
+
+@NgStoryModule({
+  stories: [Storybook, WithText, WithSomeEmoji],
+})
+export class StoryModule {}
+
+ngBootstrapStoryModule(StoryModule);
