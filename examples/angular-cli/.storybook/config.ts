c311bcd900e549156276c2e260ad9c5c1725b1b9
klimentru1986 <kliment1986@inbox.ru>
2018-10-07 07:27:11 +0300

    config for decorator demo

--- examples/angular-cli/.storybook/config.ts
+++ examples/angular-cli/.storybook/config.ts
@@ -8,6 +8,19 @@ setOptions({
   hierarchyRootSeparator: /\|/,
 });
 
+/** configuration with using decorators @NgStoryModule and @NgStory */
+function loadStoriesDecoratorConfig() {
+  // put welcome screen at the top of the list so it's the first one displayed
+  require('../src/stories-decorator-api/main');
+
+  // automatically import all story ts files that end with *.stories.ts
+  const req = require.context('../src/stories-decorator-api', true, /main\.ts$/);
+  req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStoriesDecoratorConfig, module);
+
+/** Configuration */
 function loadStories() {
   // put welcome screen at the top of the list so it's the first one displayed
   require('../src/stories');
