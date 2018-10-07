08b2df58d5dc1b263663f4f0a7349484e217c489
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2018-10-07 17:25:42 +0300

    Fixed keyboard aware view.

--- app/react-native/src/preview/components/OnDeviceUI/addons/wrapper.js
+++ app/react-native/src/preview/components/OnDeviceUI/addons/wrapper.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import { View, ScrollView, KeyboardAvoidingView } from 'react-native';
+import { View, ScrollView, KeyboardAvoidingView, Platform } from 'react-native';
 
 import style from '../style';
 
@@ -22,6 +22,8 @@ export default class Wrapper extends PureComponent {
   render() {
     const { panels, addonSelected } = this.props;
 
+    const keyboardVerticalOffset = Platform.OS === 'ios' ? 60 : 0;
+
     const addonKeys = Object.keys(panels);
 
     return addonKeys.map(id => {
@@ -29,7 +31,11 @@ export default class Wrapper extends PureComponent {
 
       return (
         <View key={id} style={selected ? style.flex : style.invisible}>
-          <KeyboardAvoidingView behavior="padding" style={style.flex}>
+          <KeyboardAvoidingView
+            behavior={Platform.OS === 'android' ? null : 'padding'}
+            keyboardVerticalOffset={keyboardVerticalOffset}
+            style={style.flex}
+          >
             <ScrollView style={style.flex}>{panels[id].render({ active: selected })}</ScrollView>
           </KeyboardAvoidingView>
         </View>
