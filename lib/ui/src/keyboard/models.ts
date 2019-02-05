b73c64fd60033ea0c229d0f5c38c80309eeaa0a0
Jessica Koch <jessicakoch136@icloud.com>
2019-02-04 18:08:52 -0800

    remove unused files

--- lib/ui/src/keyboard/models.ts
+++
@@ -1,9 +0,0 @@
-import { KeyCode } from './keyCodes';
-
-export interface IKeyboardEvent {
-	readonly ctrlKey: boolean;
-	readonly shiftKey: boolean;
-	readonly altKey: boolean;
-	readonly metaKey: boolean;
-	readonly keyCode: KeyCode;
-}
