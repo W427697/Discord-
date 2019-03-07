23c20cfc4db05f2111129b6d553f69a34be6aa48
Michael Shilman <michael@lab80.co>
2019-03-07 15:12:29 +0800

    More jest monkeying

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   cacheDirectory: '.cache/jest',
   clearMocks: true,
   moduleNameMapper: {
-    '\\.(svg|jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
     '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
     '\\.(md)$': '<rootDir>/__mocks__/htmlMock.js',
@@ -17,13 +17,9 @@ module.exports = {
     '<rootDir>/examples/vue-kitchen-sink',
     '<rootDir>/examples/angular-cli',
     '<rootDir>/examples/preact-kitchen-sink',
-  ],
-  roots: [
-    '<rootDir>/addons',
-    '<rootDir>/app',
-    '<rootDir>/lib',
     '<rootDir>/examples/official-storybook',
   ],
+  roots: ['<rootDir>/addons', '<rootDir>/app', '<rootDir>/lib'],
   transform: {
     '^.+\\.jsx?$': '<rootDir>/scripts/babel-jest.js',
     '^.+\\.tsx?$': '<rootDir>/node_modules/ts-jest',
