a4a287daf8b9d71278bf95847de33d26dba3c198
Norbert de Langen <ndelangen@me.com>
2019-03-28 12:25:26 +0100

    WIP

--- lib/config/src/index.ts
+++ lib/config/src/index.ts
@@ -1,6 +1,9 @@
 import path from 'path';
 import fs from 'fs-extra';
 
+import pkgDir from 'pkg-dir';
+import gitDir from 'git-root-dir';
+
 import { preshake } from './preshake';
 import { treeshake } from './treeshake';
 
@@ -28,6 +31,37 @@ interface Input {
   };
   cacheDir: string | undefined | null;
 }
+
+const getConfigPath = async (fileName: string): Promise<string> => {
+  const locations = [
+    async () => process.cwd(), // user's current working directory
+    async () => '.', //
+    pkgDir, // look up to where package.json is, and use that directory
+    gitDir, // look up to where .git is and use that directory
+  ];
+
+  const fullPath = await locations.reduce(
+    async (acc: Promise<string>, l: () => Promise<string>): Promise<string> => {
+      const prevResult = await acc;
+      if (prevResult) {
+        return prevResult;
+      }
+      const location = await l();
+      const p = path.join(location, fileName);
+      const result = (await fs.pathExists(p)) ? p : prevResult;
+      return result;
+    },
+    Promise.resolve('')
+  );
+
+  // if empty string, return undefined
+  return fullPath || undefined;
+};
+
+export const getStorybookConfigPath = async () => {
+  const configFileName = 'storybook.config.js';
+  return getConfigPath(configFileName);
+};
 
 export const splitter = async ({ file, config, cacheDir = './' }: Input): Promise<Result> => {
   const raw = await fs.readFile(file, 'utf8');
