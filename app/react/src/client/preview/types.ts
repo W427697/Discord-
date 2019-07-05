c950778687ef0d59d2e1026c4269b88e88e734ac
Norbert de Langen <ndelangen@me.com>
2019-07-05 21:07:06 +0200

    FIX types

--- app/react/src/client/preview/types.ts
+++ app/react/src/client/preview/types.ts
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
+import { StoryFn } from '@storybook/addons';
 
 export interface ShowErrorArgs {
   title: string;
   description: string;
 }
 
-export type StoryFnReactReturnType = React.ElementType;
+export type StoryFnReactReturnType = ReactElement;
 
+export interface RenderMainArgs {
+  storyFn: StoryFn<ReactElement>;
+  selectedKind: string;
+  selectedStory: string;
+  showMain: () => void;
+  showError: (args: ShowErrorArgs) => void;
+  forceRender: boolean;
+}
 export interface ICollection {
   [p: string]: any;
 }
