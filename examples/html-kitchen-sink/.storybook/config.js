a309dfcfadefb4633f7811e3c22934bbe02b2115
Hypnosphi <Filipp.Riabchun@jetbrains.com>
2019-07-14 18:38:29 +0200

    Fix stories with theming

--- examples/html-kitchen-sink/.storybook/config.js
+++ examples/html-kitchen-sink/.storybook/config.js
@@ -2,14 +2,15 @@ import { load, addParameters, addDecorator } from '@storybook/html';
 import { withA11y } from '@storybook/addon-a11y';
 import withReact from '@storybook/addon-react';
 import { ThemeProvider, themes, convert } from '@storybook/theming';
+import { useParameter } from '@storybook/client-api';
 import React from 'react';
 
 addDecorator(withA11y);
 
-addDecorator((getStory, context) => {
-  const story = getStory(context);
-  const { framework } = context.parameters;
-  if (!framework || !framework.react) {
+addDecorator(getStory => {
+  const story = getStory();
+  const framework = useParameter('framework');
+  if (framework !== 'react') {
     return story;
   }
 
