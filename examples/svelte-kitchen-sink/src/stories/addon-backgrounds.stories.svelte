71f70435ac2abbc462113003e115f322ba80fbe4
rixo <eric@rixo.fr>
2019-08-04 23:31:36 +0200

    Svelte example: fix background example

--- examples/svelte-kitchen-sink/src/stories/addon-backgrounds.stories.svelte
+++ examples/svelte-kitchen-sink/src/stories/addon-backgrounds.stories.svelte
@@ -2,14 +2,12 @@
   import { Meta, Story } from '@storybook/svelte';
   import ButtonView from './views/ButtonView.svelte';
 
-  const parameters = [
-    {
-      backgrounds: [
-        { name: 'light', value: '#eeeeee' },
-        { name: 'dark', value: '#222222', default: true },
-      ],
-    }
-  ]
+  const parameters = {
+    backgrounds: [
+      { name: 'light', value: '#eeeeee' },
+      { name: 'dark', value: '#222222', default: true },
+    ],
+  };
 </script>
 
 <Meta title="Addon|Backgrounds" {parameters} />
