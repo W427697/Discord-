2ce1be02ced47db6a1825ad50d25569f2d01a17e
rixo <eric@rixo.fr>
2019-08-04 03:52:12 +0200

    Svelte example: Add centered example

---
+++ examples/svelte-kitchen-sink/src/stories/addon-centered.stories.svelte
@@ -0,0 +1,17 @@
+<script>
+  import { Meta, Story } from '@storybook/svelte';
+  import Centered from '@storybook/addon-centered/svelte';
+  import { action } from '@storybook/addon-actions';
+
+  import Button from '../components/Button.svelte';
+</script>
+
+<Meta title="Addon|Centered" decorators={[Centered]} />
+
+<Story name="rounded">
+   <Button text="Look, I'm centered!" />
+</Story>
+
+<Story name="with action">
+  <Button on:click={action("Centered and with actions!")} />
+</Story>
