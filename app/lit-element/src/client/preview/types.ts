8215ff66c6c22ef9a99e3ba0d5e3f1bd1f5baeb1
lonyele <skyrabbits1@gmail.com>
2019-08-08 14:03:54 +0900

    feat: basic support for lit-element

---
+++ app/lit-element/src/client/preview/types.ts
@@ -0,0 +1,37 @@
+import { StoryFn, ClientStoryApi, Loadable } from '@storybook/addons';
+
+export type StoryFnLitElementReturnType = string | Node;
+
+export interface ShowErrorArgs {
+  title: string;
+  description: string;
+}
+
+export interface RenderMainArgs {
+  storyFn: () => StoryFn<StoryFnLitElementReturnType>;
+  selectedKind: string;
+  selectedStory: string;
+  showMain: () => void;
+  showError: (args: ShowErrorArgs) => void;
+  forceRender: boolean;
+}
+
+export interface IStorybookStory {
+  name: string;
+  render: () => any;
+}
+
+export interface IStorybookSection {
+  kind: string;
+  stories: IStorybookStory[];
+}
+
+export interface ClientApi extends ClientStoryApi<StoryFnLitElementReturnType> {
+  setAddon(addon: any): void;
+  configure(loader: Loadable, module: NodeModule): void;
+  getStorybook(): IStorybookSection[];
+  clearDecorators(): void;
+  forceReRender(): void;
+  raw: () => any;
+  load: (...args: any[]) => void;
+}
---
+++ app/lit-element/src/server/framework-preset-lit-element.ts
@@ -0,0 +1,21 @@
+import { Configuration } from 'webpack';
+
+export function webpack(config: Configuration) {
+  return {
+    ...config,
+    module: {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: require.resolve('html-loader'),
+            },
+          ],
+        },
+      ],
+    },
+  };
+}
