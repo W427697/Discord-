37217cee3e67006665c486af42a1be4f53dc6987
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-11-06 16:18:48 +0200

    Theme support.

--- addons/ondevice-jsx/src/components/index.tsx
+++ addons/ondevice-jsx/src/components/index.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
-import { View, TouchableOpacity, Clipboard, Text } from 'react-native';
+import { View, TouchableOpacity, Clipboard } from 'react-native';
+import styled from '@emotion/native';
+
+const Label = styled.Text(({ theme }) => ({
+  color: theme.labelColor,
+  fontSize: 12,
+  letterSpacing: 1,
+}));
 
 interface Props {
   storySource: string;
@@ -16,33 +23,19 @@ export default class JSX extends React.PureComponent<Props> {
     const { storySource } = this.props;
 
     return (
-      <View style={style.wrapper}>
+      <View style={{ flex: 1, padding: 10 }}>
         <View style={{ flex: 1 }}>
           {storySource.split('\n').map((item, idx) => (
             // eslint-disable-next-line react/no-array-index-key
-            <Text style={style.text} key={idx}>
-              {item}
-            </Text>
+            <Label key={idx}>{item}</Label>
           ))}
         </View>
         <View style={{ position: 'absolute', right: 5, top: 5 }}>
           <TouchableOpacity onPress={this.onPressCopy}>
-            <Text>COPY</Text>
+            <Label style={{ fontSize: 14 }}>COPY</Label>
           </TouchableOpacity>
         </View>
       </View>
     );
   }
 }
-
-const style = {
-  text: {
-    color: 'rgb(68, 68, 68)',
-    fontSize: 12,
-    letterSpacing: 1,
-  },
-  wrapper: {
-    flex: 1,
-    padding: 10,
-  },
-};
