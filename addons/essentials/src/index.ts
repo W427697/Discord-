641b2fb6011309642030d67523e9040ff082d0c0
Michael Shilman <michael@lab80.co>
2019-12-17 22:50:55 +0800

    Addon-essentials: Automatically add package dependencies

--- addons/essentials/src/index.ts
+++ addons/essentials/src/index.ts
@@ -9,36 +9,57 @@ type PresetOptions = {
   viewport?: any;
 };
 
-let packageJson: any = {};
-if (fs.existsSync('./package.json')) {
-  try {
-    packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
-  } catch (err) {
-    logger.error(`Error reading package.json: ${err.message}`);
-  }
-}
-
-const isInstalled = (addon: string) => {
-  const { dependencies, devDependencies } = packageJson;
-  return (dependencies && dependencies[addon]) || (devDependencies && devDependencies[addon]);
-};
-
 const makeAddon = (key: string) => `@storybook/addon-${key}`;
+const addonsToRegister = ['actions', 'backgrounds', 'links', 'viewport'].map(makeAddon);
+const presetsToRegister = ['knobs'].map(makeAddon);
 
 export function presets(options: PresetOptions = {}) {
-  const presetAddons = ['knobs']
+  const presetAddons = presetsToRegister
     .filter(key => (options as any)[key] !== false)
-    .map(key => makeAddon(key))
-    .filter(addon => !isInstalled(addon))
+    .map(makeAddon)
     .map(addon => `${addon}/preset`);
   return presetAddons;
 }
 
 export function addons(entry: any[] = [], options: PresetOptions = {}) {
-  const registerAddons = ['actions', 'backgrounds', 'links', 'viewport']
+  const registerAddons = addonsToRegister
     .filter(key => (options as any)[key] !== false)
-    .map(key => makeAddon(key))
-    .filter(addon => !isInstalled(addon))
+    .map(makeAddon)
     .map(addon => `${addon}/register`);
+
   return [...entry, ...registerAddons];
 }
+
+async function updatePackageDependencies() {
+  const isInstalled = (packageJson: any, addon: string, version: string) => {
+    const { dependencies, devDependencies } = packageJson;
+    return (
+      (dependencies && dependencies[addon] === version) ||
+      (devDependencies && devDependencies[addon] === version)
+    );
+  };
+  // eslint-disable-next-line global-require
+  const { version: currentVersion } = require('../package.json');
+  try {
+    // FIXME: find-pkg-up
+    const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+    const requiredPackages = [...addonsToRegister, ...presetsToRegister].map(makeAddon);
+    let packageAdded = false;
+    requiredPackages.forEach(addon => {
+      if (!isInstalled(packageJson, addon, currentVersion)) {
+        packageAdded = true;
+        packageJson.devDependencies = {
+          ...packageJson.devDependencies,
+          [addon]: currentVersion,
+        };
+      }
+    });
+    if (packageAdded) {
+      fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
+    }
+  } catch (err) {
+    logger.error(`Error reading package.json: ${err.message}`);
+  }
+}
+
+updatePackageDependencies();
