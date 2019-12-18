d7b66119caf5423d70a497dac4f20d33cb8b855b
Mike Schutte <tmikeschutte@gmail.com>
2019-12-18 12:35:29 -0500

    Storyshots: update render w/ React.createElement

--- addons/storyshots/storyshots-core/src/frameworks/react/renderShallowTree.ts
+++ addons/storyshots/storyshots-core/src/frameworks/react/renderShallowTree.ts
@@ -1,8 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import shallow from 'react-test-renderer/shallow';
 
 function getRenderedTree(story: any, context: any, { renderer, serializer }: any) {
-  const storyElement = story.render();
+  const storyElement = React.createElement(story.render);
   const shallowRenderer = renderer || shallow.createRenderer();
   const tree = shallowRenderer.render(storyElement);
   return serializer ? serializer(tree) : tree;
--- addons/storyshots/storyshots-core/src/frameworks/react/renderTree.ts
+++ addons/storyshots/storyshots-core/src/frameworks/react/renderTree.ts
@@ -1,8 +1,10 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
+import React from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import reactTestRenderer from 'react-test-renderer';
 
 function getRenderedTree(story: any, context: any, { renderer, ...rendererOptions }: any) {
-  const storyElement = story.render();
+  const storyElement = React.createElement(story.render);
   const currentRenderer = renderer || reactTestRenderer.create;
   const tree = currentRenderer(storyElement, rendererOptions);
 
