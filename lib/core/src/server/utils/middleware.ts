8b47f1fbffe2497246257fac034c8ec4d369261a
Kai Roeder <roeder.kai@gmail.com>
2020-02-16 10:58:20 +0100

    prepare lib/core for ts migration

--- lib/core/src/server/utils/middleware.ts
+++ lib/core/src/server/utils/middleware.ts
@@ -5,7 +5,8 @@ export function getMiddleware(configDir) {
   const middlewarePath = path.resolve(configDir, 'middleware.js');
   if (fs.existsSync(middlewarePath)) {
     let middlewareModule = require(middlewarePath); // eslint-disable-line
-    if (middlewareModule.__esModule) { // eslint-disable-line
+    if (middlewareModule.__esModule) {
+      // eslint-disable-line
       middlewareModule = middlewareModule.default;
     }
     return middlewareModule;
