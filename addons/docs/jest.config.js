82203ed0c45ceaeb468fc7e87af262e4ca525df5
Aaron Pool <aaron.pool@cadre5.com>
2020-03-07 00:10:37 -0500

    fix maybe?

--- addons/docs/jest.config.js
+++ addons/docs/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.stories\\.[jt]sx?$': '@storybook/addon-storyshots/injectFileName',
+    '^.+\\.[jt]sx?$': '../../scripts/babel-jest.js',
+    '^.+\\.mdx$': '@storybook/addon-docs/jest-transform-mdx',
+    '^.+\\.vue$': 'vue-jest'
+  }
 };
