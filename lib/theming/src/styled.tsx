950a86c74fb25cdf6b16c117c7a9507018a03aa1
Fabian Hiller <hillerfabian11@gmail.com>
2020-04-15 17:51:52 +0200

    Add custom styled implementation to storybook theming package

---
+++ lib/theming/src/styled.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import type { ElementType } from 'react';
+import emotionStyled, { StyledOptions } from '@emotion/styled';
+import { Context } from './context';
+
+const storybookStyled = (tag: any, options?: StyledOptions) => {
+  const createStyled = emotionStyled(tag, options);
+
+  return (...args: any[]) => {
+    const PrevStyled = createStyled(...args);
+
+    const NewStyled: any = React.forwardRef((props, ref) => (
+      <Context.Consumer>
+        {(theme) => <PrevStyled ref={ref} {...props} theme={theme} />}
+      </Context.Consumer>
+    ));
+
+    NewStyled.withComponent = (nextTag: ElementType, nextOptions?: StyledOptions) => {
+      return storybookStyled(
+        nextTag,
+        nextOptions !== undefined ? { ...(options || {}), ...nextOptions } : options
+        // eslint-disable-next-line no-underscore-dangle
+      )(...PrevStyled.__emotion_styles);
+    };
+
+    return NewStyled;
+  };
+};
+
+const newStyled = storybookStyled.bind(this);
+
+Object.keys(emotionStyled).forEach((tagName) => {
+  newStyled[tagName] = newStyled(tagName);
+});
+
+export const styled = newStyled;
