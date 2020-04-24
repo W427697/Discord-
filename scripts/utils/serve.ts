53b6360238641d5c142da4b1a91271cc6a5d51d2
Norbert de Langen <ndelangen@me.com>
2020-04-24 18:02:12 +0200

    CHANGE serve to TS

--- scripts/utils/serve.ts
+++ scripts/utils/serve.ts
@@ -1,7 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import express from 'express';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import serveStatic from 'serve-static';
 
-export const serve = async (location, port) => {
+export const serve = async (location: string, port: string) => {
   return new Promise((resolve, reject) => {
     const app = express();
 
