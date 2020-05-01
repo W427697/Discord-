19d036fe4a987f79736f2fba3eff923056af8539
tooppaaa <clementdungler@gmail.com>
2020-05-01 19:11:45 +0200

    windows friendly

--- scripts/utils/list-packages.js
+++ scripts/utils/list-packages.js
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 
 export const listOfPackages = () =>
   new Promise((res, rej) => {
-    const command = `./node_modules/.bin/lerna list --json`;
+    const command = `npx lerna list --json`;
     exec(command, (e, result) => {
       if (e) {
         rej(e);
