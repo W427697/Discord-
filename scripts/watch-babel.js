918d58d168a40e1668c8d16a019036d27e817a19
Norbert de Langen <ndelangen@me.com>
2020-05-14 23:55:12 +0200

    CLEANUP

--- scripts/watch-babel.js
+++
@@ -1,9 +0,0 @@
-const { babelify } = require('./compile-babel');
-
-babelify({
-  watch: true,
-  silent: false,
-}).catch((e) => {
-  console.error(e);
-  process.exitCode = 1;
-});
--- scripts/watch-tsc.js
+++
@@ -1,9 +0,0 @@
-const { tscfy } = require('./compile-tsc');
-
-tscfy({
-  watch: true,
-  silent: false,
-}).catch((e) => {
-  console.error(e);
-  process.exitCode = 1;
-});
