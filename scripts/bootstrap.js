725b1d03f0dcea60f4e2046bd047de31491ce7e3
Norbert de Langen <ndelangen@me.com>
2020-05-21 16:34:22 +0200

    remove silent for debugging

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -94,7 +94,7 @@ function run() {
       option: '--build',
       command: () => {
         log.info(prefix, 'prepare');
-        spawn('lerna run prepare -- --silent');
+        spawn('lerna run prepare');
       },
       order: 2,
     }),
