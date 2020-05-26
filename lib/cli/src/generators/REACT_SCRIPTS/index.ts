f535af4519a4bad11154f31c907b28b241d3c752
tooppaaa <clementdungler@gmail.com>
2020-05-26 22:10:47 +0200

    Move ember

--- lib/cli/src/generators/REACT_SCRIPTS/index.ts
+++ lib/cli/src/generators/REACT_SCRIPTS/index.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 import fs from 'fs';
-import { retrievePackageJson, writePackageJson } from '../../helpers';
 
 import baseGenerator, { Generator } from '../generator';
 import { StoryFormat } from '../../project_types';
@@ -9,14 +8,8 @@ const generator: Generator = async (npmOptions, options) => {
   await baseGenerator(npmOptions, options, 'react', {
     extraAddons: ['@storybook/preset-create-react-app'],
     dirname: options.storyFormat === StoryFormat.MDX ? __dirname : undefined,
+    staticDir: fs.existsSync(path.resolve('./public')) ? 'public' : undefined,
   });
-  if (fs.existsSync(path.resolve('./public'))) {
-    const packageJson = await retrievePackageJson();
-    // has a public folder and add support to it.
-    packageJson.scripts.storybook += ' -s public';
-    packageJson.scripts['build-storybook'] += ' -s public';
-    writePackageJson(packageJson);
-  }
 };
 
 export default generator;
