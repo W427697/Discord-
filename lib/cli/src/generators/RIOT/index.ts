5ba3eef437451f9c3caafc7593699f0fc6760e2c
tooppaaa <clementdungler@gmail.com>
2020-05-26 22:37:21 +0200

    Move riot

--- lib/cli/src/generators/RIOT/index.ts
+++ lib/cli/src/generators/RIOT/index.ts
@@ -1,57 +1,11 @@
-import {
-  getVersions,
-  retrievePackageJson,
-  writePackageJson,
-  getBabelDependencies,
-  installDependencies,
-  copyTemplate,
-} from '../../helpers';
-import { Generator } from '../generator';
+import baseGenerator, { Generator } from '../generator';
+import { StoryFormat } from '../../project_types';
 
-const generator: Generator = async (npmOptions, { storyFormat }) => {
-  const [
-    storybookVersion,
-    actionsVersion,
-    linksVersion,
-    addonsVersion,
-    tagLoaderVersion,
-  ] = await getVersions(
-    npmOptions,
-    '@storybook/riot',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addons',
-    'riot-tag-loader'
-  );
-
-  copyTemplate(__dirname, storyFormat);
-
-  const packageJson = await retrievePackageJson();
-
-  packageJson.dependencies = packageJson.dependencies || {};
-  packageJson.devDependencies = packageJson.devDependencies || {};
-
-  const dependencies = [
-    `@storybook/riot@${storybookVersion}`,
-    `@storybook/addon-actions@${actionsVersion}`,
-    `@storybook/addon-links@${linksVersion}`,
-    `@storybook/addons@${addonsVersion}`,
-  ];
-  if (
-    !packageJson.devDependencies['riot-tag-loader'] &&
-    !packageJson.dependencies['riot-tag-loader']
-  )
-    dependencies.push(`riot-tag-loader@${tagLoaderVersion}`);
-
-  packageJson.scripts = packageJson.scripts || {};
-  packageJson.scripts.storybook = 'start-storybook -p 6006';
-  packageJson.scripts['build-storybook'] = 'build-storybook';
-
-  writePackageJson(packageJson);
-
-  const babelDependencies = await getBabelDependencies(npmOptions, packageJson);
-
-  installDependencies({ ...npmOptions, packageJson }, [...dependencies, ...babelDependencies]);
+const generator: Generator = async (npmOptions, options) => {
+  baseGenerator(npmOptions, options, 'riot', {
+    dirname: options.storyFormat === StoryFormat.MDX ? __dirname : undefined,
+    extraPackages: ['riot-tag-loader'],
+  });
 };
 
 export default generator;
