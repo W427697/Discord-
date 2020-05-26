ba37d85223d2563ef651c27089fffe43a826c831
tooppaaa <clementdungler@gmail.com>
2020-05-26 22:37:38 +0200

    Move svelte

--- lib/cli/src/generators/SVELTE/index.ts
+++ lib/cli/src/generators/SVELTE/index.ts
@@ -1,55 +1,11 @@
-import {
-  getVersions,
-  retrievePackageJson,
-  writePackageJson,
-  getBabelDependencies,
-  installDependencies,
-  copyTemplate,
-} from '../../helpers';
-import { Generator } from '../generator';
+import baseGenerator, { Generator } from '../generator';
+import { StoryFormat } from '../../project_types';
 
-const generator: Generator = async (npmOptions, { storyFormat }) => {
-  const [
-    storybookVersion,
-    actionsVersion,
-    linksVersion,
-    addonsVersion,
-    svelte,
-    svelteLoader,
-  ] = await getVersions(
-    npmOptions,
-    '@storybook/svelte',
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-    '@storybook/addons',
-    'svelte',
-    'svelte-loader'
-  );
-
-  copyTemplate(__dirname, storyFormat);
-
-  const packageJson = await retrievePackageJson();
-
-  packageJson.dependencies = packageJson.dependencies || {};
-  packageJson.devDependencies = packageJson.devDependencies || {};
-
-  packageJson.scripts = packageJson.scripts || {};
-  packageJson.scripts.storybook = 'start-storybook -p 6006';
-  packageJson.scripts['build-storybook'] = 'build-storybook';
-
-  writePackageJson(packageJson);
-
-  const babelDependencies = await getBabelDependencies(npmOptions, packageJson);
-
-  installDependencies({ ...npmOptions, packageJson }, [
-    `@storybook/svelte@${storybookVersion}`,
-    `@storybook/addon-actions@${actionsVersion}`,
-    `@storybook/addon-links@${linksVersion}`,
-    `@storybook/addons@${addonsVersion}`,
-    `svelte@${svelte}`,
-    `svelte-loader@${svelteLoader}`,
-    ...babelDependencies,
-  ]);
+const generator: Generator = async (npmOptions, options) => {
+  baseGenerator(npmOptions, options, 'svelte', {
+    dirname: options.storyFormat === StoryFormat.MDX ? __dirname : undefined,
+    extraPackages: ['svelte', 'svelte-loader'],
+  });
 };
 
 export default generator;
