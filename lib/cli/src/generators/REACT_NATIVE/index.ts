6a29fb0e2e88924751ac8ef44523e31f36004d54
tooppaaa <clementdungler@gmail.com>
2020-05-28 23:23:18 +0200

    Move react-native

--- lib/cli/src/generators/REACT_NATIVE/index.ts
+++ lib/cli/src/generators/REACT_NATIVE/index.ts
@@ -1,32 +1,14 @@
 import shell from 'shelljs';
 import chalk from 'chalk';
-import {
-  getVersions,
-  retrievePackageJson,
-  writePackageJson,
-  paddedLog,
-  getBabelDependencies,
-  installDependencies,
-  copyTemplate,
-} from '../../helpers';
+import { retrievePackageJson, paddedLog } from '../../helpers';
 import { NpmOptions } from '../../NpmOptions';
-import { GeneratorOptions } from '../generator';
+import baseGenerator, { GeneratorOptions } from '../generator';
 
-export default async (
+const generator = async (
   npmOptions: NpmOptions,
   installServer: boolean,
-  { storyFormat }: GeneratorOptions
-) => {
-  const [storybookVersion, addonsVersion, actionsVersion, linksVersion] = await getVersions(
-    npmOptions,
-    '@storybook/react-native',
-    '@storybook/addons',
-    '@storybook/addon-actions',
-    '@storybook/addon-links'
-  );
-
-  copyTemplate(__dirname, storyFormat);
-
+  options: GeneratorOptions
+): Promise<void> => {
   // set correct project name on entry files if possible
   const dirname = shell.ls('-d', 'ios/*.xcodeproj').stdout;
 
@@ -47,35 +29,18 @@ export default async (
 
   const packageJson = await retrievePackageJson();
 
-  packageJson.dependencies = packageJson.dependencies || {};
-  packageJson.devDependencies = packageJson.devDependencies || {};
+  const missingReactDom =
+    !packageJson.dependencies['react-dom'] && !packageJson.devDependencies['react-dom'];
+  const reactVersion = packageJson.dependencies.react;
 
-  const devDependencies = [
-    `@storybook/react-native@${storybookVersion}`,
-    `@storybook/addon-actions@${actionsVersion}`,
-    `@storybook/addon-links@${linksVersion}`,
-    `@storybook/addons@${addonsVersion}`,
-  ];
-
-  if (installServer) {
-    devDependencies.push(`@storybook/react-native-server@${storybookVersion}`);
-  }
-
-  if (!packageJson.dependencies['react-dom'] && !packageJson.devDependencies['react-dom']) {
-    if (packageJson.dependencies.react) {
-      const reactVersion = packageJson.dependencies.react;
-      devDependencies.push(`react-dom@${reactVersion}`);
-    }
-  }
-
-  if (installServer) {
-    packageJson.scripts = packageJson.scripts || {};
-    packageJson.scripts.storybook = 'start-storybook -p 7007';
-  }
-
-  writePackageJson(packageJson);
-
-  const babelDependencies = await getBabelDependencies(npmOptions, packageJson);
-
-  installDependencies({ ...npmOptions, packageJson }, [...devDependencies, ...babelDependencies]);
+  await baseGenerator(npmOptions, options, 'react-native', {
+    extraPackages: [
+      missingReactDom && reactVersion && `react-dom@${reactVersion}`,
+      installServer && '@storybook/react-native-server',
+    ].filter(Boolean),
+    addScripts: installServer,
+    addComponents: false, // We copy template-csf as it's wrapped in a storybook folder
+  });
 };
+
+export default generator;
