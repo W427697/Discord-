6137412b11d8b5872523b5c83f0e698416cfdf3e
Rodrigo Faria <rodrigo.faria@cartrack.com>
2020-06-20 14:57:04 +0100

    feat(@storybook/builder): first draft WIP

---
+++ lib/builder/src/index.ts
@@ -0,0 +1,6 @@
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackVirtualModules from 'webpack-virtual-modules';
+
+export { webpack, webpackDevMiddleware, webpackHotMiddleware, webpackVirtualModules };
