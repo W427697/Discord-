94eeff00171f77f9c1c960236c3e6f67d234a479
Michael Shilman <michael@lab80.co>
2020-07-29 18:22:54 +0800

    Merge branch 'next' into 10844-allow-component-argtype-enhancers

--- addons/docs/src/frameworks/common/enhanceArgTypes.ts
+++ addons/docs/src/frameworks/common/enhanceArgTypes.ts
@@ -1,37 +1,18 @@
 import mapValues from 'lodash/mapValues';
-import { StoryArgTypesEnhancer, combineParameters } from '@storybook/client-api';
-import { ArgTypes } from '@storybook/api';
-import { inferArgTypes } from './inferArgTypes';
+import { ArgTypesEnhancer, combineParameters } from '@storybook/client-api';
 import { inferControls } from './inferControls';
+import { normalizeArgTypes } from './normalizeArgTypes';
 
-const isSubset = (kind: string, subset: object, superset: object) => {
-  const keys = Object.keys(subset);
-  // eslint-disable-next-line no-prototype-builtins
-  const overlap = keys.filter((key) => superset.hasOwnProperty(key));
-  return overlap.length === keys.length;
-};
+export const enhanceArgTypes: ArgTypesEnhancer = (context) => {
+  const { __isArgsStory, component, argTypes: userArgTypes = {}, docs = {} } = context.parameters;
+  const { extractArgTypes } = docs;
 
-export const enhanceArgTypes: StoryArgTypesEnhancer = (context) => {
-  const { component, argTypes: userArgTypes = {}, docs = {}, args = {} } = context.parameters;
-  const { extractArgTypes, forceExtractedArgTypes = false } = docs;
+  const normalizedArgTypes = normalizeArgTypes(userArgTypes);
+  const namedArgTypes = mapValues(normalizedArgTypes, (val, key) => ({ name: key, ...val }));
+  const extractedArgTypes = extractArgTypes && component ? extractArgTypes(component) : {};
+  const withArgTypes = combineParameters(extractedArgTypes, namedArgTypes);
 
-  const namedArgTypes = mapValues(userArgTypes, (val, key) => ({ name: key, ...val }));
-  const inferredArgTypes = inferArgTypes(args);
-  let extractedArgTypes: ArgTypes = extractArgTypes && component ? extractArgTypes(component) : {};
-
-  if (
-    !forceExtractedArgTypes &&
-    ((Object.keys(userArgTypes).length > 0 &&
-      !isSubset(context.kind, userArgTypes, extractedArgTypes)) ||
-      (Object.keys(inferredArgTypes).length > 0 &&
-        !isSubset(context.kind, inferredArgTypes, extractedArgTypes)))
-  ) {
-    extractedArgTypes = {};
-  }
-
-  const withArgTypes = combineParameters(inferredArgTypes, extractedArgTypes, namedArgTypes);
-
-  if (context.storyFn.length === 0) {
+  if (!__isArgsStory) {
     return withArgTypes;
   }
 
