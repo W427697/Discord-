5c3ecb13f0e1846c3a544e3e00907edff6b1f46a
Jason Miller <jason@developit.ca>
2020-09-07 14:29:13 -0400

    Add preact/compat aliases by default

--- app/preact/src/server/framework-preset-preact.ts
+++ app/preact/src/server/framework-preset-preact.ts
@@ -1,4 +1,6 @@
+import path from 'path';
 import { TransformOptions } from '@babel/core';
+import { Configuration } from 'webpack'; // eslint-disable-line
 
 export function babelDefault(config: TransformOptions) {
   return {
@@ -7,5 +9,22 @@ export function babelDefault(config: TransformOptions) {
       ...config.plugins,
       [require.resolve('@babel/plugin-transform-react-jsx'), { pragma: 'h' }, 'preset'],
     ],
+  };
+}
+
+export function webpackFinal(config: Configuration) {
+  return {
+    ...config,
+    resolve: {
+      ...config.resolve,
+      // Prefer top-level aliases
+      modules: [path.resolve('node_modules'), ...config.resolve.modules],
+      // Alias react imports to "preact/compat":
+      alias: {
+        ...config.resolve.alias,
+        react: 'preact/compat',
+        'react-dom': 'preact/compat',
+      },
+    },
   };
 }
