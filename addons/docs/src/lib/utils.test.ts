1e5a90458d466a2ec310aa4d29730e0b29e2a33f
Blaine Bublitz <blaine.bublitz@gmail.com>
2020-09-26 19:40:42 -0700

    Addon-docs: Add tests for isUnsafeToSplit

--- addons/docs/src/lib/utils.test.ts
+++ addons/docs/src/lib/utils.test.ts
@@ -1,4 +1,4 @@
-import { createSummaryValue } from './utils';
+import { createSummaryValue, isUnsafeToSplit } from './utils';
 
 describe('createSummaryValue', () => {
   it('creates an object with just summary if detail is not passed', () => {
@@ -16,5 +16,53 @@ describe('createSummaryValue', () => {
     const summary = 'boolean';
     const detail = 'boolean';
     expect(createSummaryValue(summary, detail)).toEqual({ summary });
+  });
+});
+
+describe('isUnsafeToSplit', () => {
+  describe('reports false', () => {
+    it('basic union', () => {
+      const value = '"foo" | "bar" | "baz"';
+      expect(isUnsafeToSplit(value)).toEqual(false);
+    });
+  });
+
+  describe('reports true', () => {
+    it('union including an object', () => {
+      const value = '"foo" | "bar" | { baz: string }';
+      expect(isUnsafeToSplit(value)).toEqual(true);
+    });
+
+    it('union including a function', () => {
+      const value = '"foo" | "bar" | () => void';
+      expect(isUnsafeToSplit(value)).toEqual(true);
+    });
+
+    it('union including an array', () => {
+      const value = '"foo" | "bar" | string[]';
+      expect(isUnsafeToSplit(value)).toEqual(true);
+    });
+
+    it('union including a generic', () => {
+      const value = '"foo" | "bar" | Class<Baz>';
+      expect(isUnsafeToSplit(value)).toEqual(true);
+    });
+  });
+
+  describe('false positives', () => {
+    it('function with an internal union', () => {
+      const value = '() => "foo" | "bar"';
+      expect(isUnsafeToSplit(value)).toEqual(true);
+    });
+
+    it('object with an internal union', () => {
+      const value = '{ foo: "bar" | "baz" }';
+      expect(isUnsafeToSplit(value)).toEqual(true);
+    });
+
+    it('array with an internal union', () => {
+      const value = 'Array<"bar" | "baz">';
+      expect(isUnsafeToSplit(value)).toEqual(true);
+    });
   });
 });
