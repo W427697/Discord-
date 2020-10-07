b1a530261b1267afe9e259907d5830e36dc22f5c
Cameron Yick <cameron.yick@datadoghq.com>
2020-10-06 22:58:55 -0400

    (feat:controls) Add documentation for configuring JSON Tree Viewer for Object Props

--- examples/official-storybook/stories/addon-controls.stories.tsx
+++ examples/official-storybook/stories/addon-controls.stories.tsx
@@ -11,25 +11,42 @@ export default {
   },
 };
 
+const DEFAULT_NESTED_OBJECT = { a: 4, b: { c: 'hello', d: [1, 2, 3] } };
+
 const Template = (args) => <Button {...args} />;
 
 export const Basic = Template.bind({});
 Basic.args = {
   children: 'basic',
-  somethingElse: { a: 2 },
+  somethingElse: DEFAULT_NESTED_OBJECT,
 };
 
 export const Action = Template.bind({});
 Action.args = {
   children: 'hmmm',
   type: 'action',
-  somethingElse: { a: 4 },
+  somethingElse: DEFAULT_NESTED_OBJECT,
 };
 
 export const CustomControls = Template.bind({});
+CustomControls.args = {
+  children: 'hmmm',
+  type: 'action',
+  somethingElse: DEFAULT_NESTED_OBJECT,
+};
+
 CustomControls.argTypes = {
   children: { table: { disable: true } },
   type: { control: { disable: true } },
+  somethingElse: {
+    control: {
+      type: 'object',
+      collapsed: true,
+      displayObjectSize: false,
+      displayDataTypes: false,
+      // See src/controls/types:ObjectConfig for all options
+    },
+  },
 };
 
 export const NoArgs = () => <Button>no args</Button>;
