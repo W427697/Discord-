d44a346619346b1a8056761f6cd6e4e50c0a2ea7
Yann Braga <yannbf@gmail.com>
2020-10-10 18:04:25 +0200

    feat(addon-controls): support query parameters

--- addons/controls/src/components/ControlsPanel.tsx
+++ addons/controls/src/components/ControlsPanel.tsx
@@ -1,8 +1,18 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 import { ArgsTable, NoControlsWarning } from '@storybook/components';
 import { useArgs, useArgTypes, useParameter } from '@storybook/api';
+import { getQueryParams } from '@storybook/client-api';
 
 import { PARAM_KEY } from '../constants';
+
+const controlsValuesFromUrl: Record<string, string> = Object.entries(getQueryParams())
+  .filter((key) => {
+    return key[0]?.includes('control-');
+  })
+  .reduce((acc, [key, value]) => {
+    const argName = key.replace('control-', '');
+    return { ...acc, [argName]: value };
+  }, {});
 
 interface ControlsParameters {
   expanded?: boolean;
@@ -18,6 +28,13 @@ export const ControlsPanel: FC = () => {
     {}
   );
   const hasControls = Object.values(rows).filter((argType) => !!argType?.control).length > 0;
+
+  useEffect(() => {
+    if (Object.keys(controlsValuesFromUrl).length > 0) {
+      updateArgs(controlsValuesFromUrl);
+    }
+  }, []);
+
   return (
     <>
       {(hasControls && isArgsStory) || hideNoControlsWarning ? null : <NoControlsWarning />}
