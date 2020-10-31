aa19454fb17339e3a3288f11c99a8c75d4f05a67
Zack Jackson <zackary.l.jackson@gmail.com>
2020-10-30 23:05:46 -0700

    fix most the things.

--- app/aurelia/src/server/create-fork-ts-checker-plugin.ts
+++ app/aurelia/src/server/create-fork-ts-checker-plugin.ts
@@ -5,6 +5,7 @@ import { Options } from 'ts-loader';
 
 export default function (tsLoaderOptions: Partial<Options>) {
   if (tsLoaderOptions && tsLoaderOptions.configFile) {
+    // this config is busted, read the errors it throws and read the docs / do TS wizardy that i dont understand
     return new ForkTsCheckerWebpackPlugin({
       tsconfig: tsLoaderOptions.configFile,
       async: false,
