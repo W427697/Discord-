6ce9e5126e56593903a50e874c1150c9c501f1f3
Michael Shilman <michael@lab80.co>
2020-11-10 01:26:17 +0800

    Add examples/cra-essentials for benchmark

---
+++ examples/cra-essentials/.storybook/main.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-bench',
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/preset-create-react-app',
+  ],
+  webpackFinal: (config) => {
+    // add monorepo root as a valid directory to import modules from
+    config.resolve.plugins.forEach((p) => {
+      if (Array.isArray(p.appSrcs)) {
+        p.appSrcs.push(path.join(__dirname, '..', '..', '..'));
+      }
+    });
+    return config;
+  },
+};
