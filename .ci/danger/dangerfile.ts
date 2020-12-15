06923d6278e7685a52ef1a9a5d6824f742ccc4fb
Ademola <ooade96@gmail.com>
2020-12-15 12:01:00 +0100

    Switch to the new yarn syntax

--- .ci/danger/dangerfile.ts
+++ .ci/danger/dangerfile.ts
@@ -1,7 +1,7 @@
 import { fail, danger } from 'danger';
 import { execSync } from 'child_process';
 
-execSync('yarn install lodash');
+execSync('yarn add lodash');
 
 const flatten = require('lodash/flatten');
 const intersection = require('lodash/intersection');
