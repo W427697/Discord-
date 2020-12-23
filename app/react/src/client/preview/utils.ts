dd0e8a35d634e8d6650a8806edb5cd3318e4a1d4
Yann Braga <yannbf@gmail.com>
2020-12-23 22:49:34 +0100

    fix(react): set correct decorator order
    - also change the signature of composeStory and expose it

--- app/react/src/client/preview/utils.ts
+++ app/react/src/client/preview/utils.ts
@@ -1,11 +1,12 @@
 // TODO: add client-api as dependency
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defaultDecorateStory, combineParameters } from '@storybook/client-api';
 import { GlobalConfig, Meta, Story, StoryContext } from './types-6-0';
 
-function composeStory<GenericArgs>(
+export function composeStory<GenericArgs>(
+  story: Story<GenericArgs>,
   meta: Meta,
-  globalConfig: GlobalConfig,
-  story: Story<GenericArgs>
+  globalConfig: GlobalConfig = {}
 ) {
   const finalStoryFn = (context: StoryContext) => {
     const { passArgsFirst = true } = context.parameters;
@@ -13,14 +14,14 @@ function composeStory<GenericArgs>(
   };
 
   const combinedDecorators = [
-    ...(globalConfig?.decorators || []),
-    ...(meta?.decorators || []),
     ...(story.decorators || story.story?.decorators || []),
+    ...(meta?.decorators || []),
+    ...(globalConfig?.decorators || []),
   ];
 
   const decorated = defaultDecorateStory(finalStoryFn, combinedDecorators);
 
-  return (extraArgs: Record<string, any>) =>
+  return ((extraArgs: Record<string, any>) =>
     decorated({
       id: '',
       kind: '', // TODO: figure out about id, kind and name.
@@ -37,7 +38,7 @@ function composeStory<GenericArgs>(
         ...story.args,
         ...extraArgs,
       },
-    });
+    })) as Story<GenericArgs>;
 }
 
 export function composeStories<T extends { default: Meta } & { [key: string]: Story }>(
@@ -47,10 +48,10 @@ export function composeStories<T extends { default: Meta } & { [key: string]: St
   const { default: meta, ...stories } = storiesImport;
   const initial: { [key: string]: Story } = {};
   // Compose an object containing all processed stories passed as parameters
-  return Object.entries(stories).reduceRight((storiesMap, [key, value]: [string, Story]) => {
+  return Object.entries(stories).reduceRight((storiesMap, [key, story]: [string, Story]) => {
     // @ts-ignore FIX THIS LATER PLEASE!
     // eslint-disable-next-line no-param-reassign
-    storiesMap[key] = composeStory(meta, globalConfig, value);
+    storiesMap[key] = composeStory(story, meta, globalConfig);
     return storiesMap;
   }, initial);
 }
