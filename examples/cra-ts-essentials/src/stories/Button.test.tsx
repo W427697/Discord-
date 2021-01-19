fbbd0a3dabec079c34d1d181f4300ff8baceccce
Yann Braga <yannbf@gmail.com>
2021-01-19 09:45:02 +0100

    chore(react-ts-example): update tests to the new syntax with setup files
    - globalConfig is now set in setup files, only once!

--- examples/cra-ts-essentials/src/stories/Button.test.tsx
+++ examples/cra-ts-essentials/src/stories/Button.test.tsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { composeStories, composeStory } from '@storybook/react';
 
-import * as globalConfig from '../../.storybook/preview';
 import * as stories from './Button.stories';
 
 // example with composeStories, returns an object with all stories composed with args/decorators
 const { Primary } = composeStories(stories);
 
 // example with composeStory, returns a single story composed with args/decorators
-const Secondary = composeStory(stories.Secondary, stories.default, globalConfig);
+const Secondary = composeStory(stories.Secondary, stories.default);
 
 test('renders primary button', () => {
   const { getByText } = render(<Primary>Hello world</Primary>);
