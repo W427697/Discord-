4ef7e067016986ef788ba36cbc2b9bc9e4667f3a
Gaëtan Maisse <gaetanmaisse@gmail.com>
2021-02-13 18:41:54 +0100

    e2e: remove all exotic options send to yarn commands

--- scripts/run-e2e-config.ts
+++ scripts/run-e2e-config.ts
@@ -16,7 +16,7 @@ const baseAngular: Parameters = {
   name: 'angular',
   version: 'latest',
   generator: [
-    `yarn add @angular/cli@{{version}} --no-lockfile --non-interactive --silent --no-progress`,
+    `yarn add @angular/cli@{{version}}`,
     `yarn ng new {{name}}-{{version}} --routing=true --minimal=true --style=scss --skipInstall=true --strict --packageManager=npm`,
     `cd {{name}}-{{version}}`,
     `yarn install`,
@@ -185,7 +185,7 @@ export const react_in_yarn_workspace: Parameters = {
   generator: [
     'cd {{name}}-{{version}}',
     'echo "{ \\"name\\": \\"workspace-root\\", \\"private\\": true, \\"workspaces\\": [] }" > package.json',
-    `yarn add react react-dom --silent`,
+    `yarn add react react-dom`,
   ].join(' && '),
 };
 
