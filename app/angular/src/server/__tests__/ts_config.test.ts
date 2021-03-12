aedc48d09f51cb0b8151994df51ff269096ae964
Norbert de Langen <ndelangen@me.com>
2021-03-12 14:57:10 +0100

    fix messed up test

--- app/angular/src/server/__tests__/ts_config.test.ts
+++ app/angular/src/server/__tests__/ts_config.test.ts
@@ -1,21 +1,13 @@
+import path from 'path';
 import getTsLoaderOptions from '../ts_config';
 
-// eslint-disable-next-line global-require, jest/no-mocks-import
-jest.mock('fs', () => require('../../../../../__mocks__/fs'));
 jest.mock('path', () => ({
-  resolve: () => 'tsconfig.json',
+  resolve: jest.fn(() => 'tsconfig.json'),
 }));
 jest.mock('@storybook/node-logger');
 
-const setupFiles = (files: any) => {
-  // eslint-disable-next-line no-underscore-dangle, global-require
-  require('fs').__setMockFiles(files);
-};
-
 describe('ts_config', () => {
   it('should return the config with the path to the tsconfig.json', () => {
-    setupFiles({ 'tsconfig.json': '{}' });
-
     const config = getTsLoaderOptions('.foo');
 
     expect(config).toEqual({
@@ -28,8 +20,8 @@ describe('ts_config', () => {
   });
 
   it('should return object with transpileOnly: true when there is no tsconfig.json', () => {
-    setupFiles({});
-
+    // @ts-ignore
+    path.resolve.mockReturnValue(false);
     const config = getTsLoaderOptions('.foo');
 
     expect(config).toEqual({
