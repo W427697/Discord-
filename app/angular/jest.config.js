1be1120018853a6967646d282b4feae105113a13
Kai Röder <roeder.kai@gmail.com>
2021-03-18 22:55:28 +0100

    fix jest error

--- app/angular/jest.config.js
+++ app/angular/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleNameMapper: {
+    '@storybook/node-logger': '<rootDir>/../../lib/node-logger/dist/cjs/index.js',
+  },
 };
