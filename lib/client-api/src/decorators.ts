579c5c5bdb4196d6841df6de5cade0010721b971
Tom Coleman <tom@thesnail.org>
2021-04-09 17:13:16 +1000

    Add some comments

--- lib/client-api/src/decorators.ts
+++ lib/client-api/src/decorators.ts
@@ -16,8 +16,16 @@ const defaultContext: StoryContext = {
   globals: {},
 };
 
+/**
+ * We cannot guarantee that decorators will pass the context to the next decorator in the chain;
+ * To avoid this we could re-bind each decorator with the story context at runtime, but
+ * this has problems in e.g. React -- we could create a new SFC each render (see #12255).
+ * Instead we use a "global" to track the current story context. This is OK as rendering is
+ * syncronous, so we will make it the whole way through the decorator stack before we try
+ * rendering any other story.
+ */
 let currentStoryContext: StoryContext;
-// You cannot override the parameters key, it is fixed
+// You cannot override the parameters key, it is fixed (we should add to this list)
 function overrideStoryContext({ parameters, ...innerContext }: StoryContextUpdate = {}) {
   return { ...currentStoryContext, ...innerContext };
 }
@@ -30,7 +38,8 @@ function overrideStoryContext({ parameters, ...innerContext }: StoryContextUpdat
  * ```
  *
  * This will override the `foo` property on the `innerContext`, which gets
- * merged in with the default context
+ * merged in with the default context, and stored in the `currentStoryContext` "global"
+ * to be read by the next decorator
  */
 export const decorateStory = (storyFn: StoryFn, decorator: DecoratorFunction) => {
   return (innerContext: StoryContext = defaultContext) => {
@@ -44,6 +53,7 @@ export const defaultDecorateStory = (storyFn: StoryFn, decorators: DecoratorFunc
     storyFn(overrideStoryContext(finalInnerStoryContext))
   );
   return (firstStoryContext: StoryContext) => {
+    // Set the initial story context that is passed into each subsequent decorator
     currentStoryContext = firstStoryContext;
     return decorated(firstStoryContext);
   };
