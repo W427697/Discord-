22a02949bc71f2c66aa4432331aa1d787d8198b0
IgorSzyporyn <igor.szyporyn@gmail.com>
2021-06-14 19:16:35 +0200

    fix: added memoization of pure function

--- lib/components/src/hooks/useContentRect.tsx
+++ lib/components/src/hooks/useContentRect.tsx
@@ -1,10 +1,25 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useMemo } from 'react';
 import ResizeObserver from 'resize-observer-polyfill';
 import { useCallbackRef } from './useCallbackRef';
 
+const isStateDirty = (a: UseContentRectBounds, b: UseContentRectBounds) => {
+  return JSON.stringify(a) !== JSON.stringify(b);
+};
+
+export interface UseContentRectBounds {
+  height: number;
+  width: number;
+  left: number;
+  top: number;
+  bottom: number;
+  right: number;
+  x: number;
+  y: number;
+}
+
 export const useContentRect = (ref: any) => {
   const { ref: element, fn: attachRef } = useCallbackRef<any>();
-  const [bounds, setBounds] = useState({
+  const [bounds, setBounds] = useState<UseContentRectBounds>({
     height: 0,
     width: 0,
     left: 0,
@@ -18,8 +33,9 @@ export const useContentRect = (ref: any) => {
   useEffect(() => {
     const onResize = ([entry]: ResizeObserverEntry[]) => {
       const newBounds = entry.target.getBoundingClientRect().toJSON();
+      const isDirty = useMemo(() => isStateDirty(bounds, newBounds), [bounds, newBounds]);
 
-      if (JSON.stringify(bounds) !== JSON.stringify(newBounds)) {
+      if (isDirty) {
         setBounds(newBounds);
       }
     };
