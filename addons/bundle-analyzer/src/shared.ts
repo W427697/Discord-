dccf997a8ff16066fe6e731b39803a671fb82f7c
Norbert de Langen <ndelangen@me.com>
2021-06-17 15:31:33 +0200

    Update addons/bundle-analyzer/src/shared.ts
    Co-authored-by: Yann Braga <yannbf@gmail.com>

--- addons/bundle-analyzer/src/shared.ts
+++ addons/bundle-analyzer/src/shared.ts
@@ -1,4 +1,4 @@
 // addons, panels and events get unique names using a prefix
 export const PARAM_KEY = 'bundle-analyzer';
-export const ADDON_ID = 'storybookjs/bundele-analyzer';
+export const ADDON_ID = 'storybookjs/bundle-analyzer';
 export const PANEL_ID = `${ADDON_ID}/panel`;
