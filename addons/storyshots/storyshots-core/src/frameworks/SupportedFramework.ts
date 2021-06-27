32b70baf19d92af1141341c63fc0a0fda006e86b
IgorSzyporyn <igor.szyporyn@gmail.com>
2021-06-27 15:32:19 +0200

    fix(addon-storyshots): leeching types instead of double declaration

--- addons/storyshots/storyshots-core/src/frameworks/SupportedFramework.ts
+++ addons/storyshots/storyshots-core/src/frameworks/SupportedFramework.ts
@@ -1,12 +1,3 @@
-export type SupportedFramework =
-  | 'angular'
-  | 'html'
-  | 'preact'
-  | 'react'
-  | 'riot'
-  | 'react-native'
-  | 'svelte'
-  | 'vue'
-  | 'vue3'
-  | 'web-components'
-  | 'rax';
+import type { AugmentedWindow } from '@storybook/core-client';
+
+export type SupportedFramework = AugmentedWindow['STORYBOOK_ENV'];
