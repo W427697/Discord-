49cb04e33691b1e6cc9cc62b91a55ade5bd6d448
Norbert de Langen <ndelangen@me.com>
2021-08-02 17:57:33 +0200

    fix linting

--- examples/official-storybook/components/addon-measure/Visualization.js
+++ examples/official-storybook/components/addon-measure/Visualization.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import React, { useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { drawSelectedElement } from '@storybook/addon-measure/dist/cjs/box-model/visualizer';
