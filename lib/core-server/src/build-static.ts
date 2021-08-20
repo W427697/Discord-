ebbc9b6b0246d0ddf5e877c90c1800195f3ed70b
Black-Hole1 <158blackhole@gmail.com>
2021-08-20 11:34:52 +0800

    fix(build): avoid logging an object on compilation errors

--- lib/core-server/src/build-static.ts
+++ lib/core-server/src/build-static.ts
@@ -144,8 +144,11 @@ export async function buildStatic({ packageJson, ...loadOptions }: LoadOptions) 
       configType: 'PRODUCTION',
       cache,
     });
-  } catch (e) {
-    logger.error(e);
+  } catch (error) {
+    if (error instanceof Error) {
+      logger.error(error);
+    }
+
     process.exit(1);
   }
 }
