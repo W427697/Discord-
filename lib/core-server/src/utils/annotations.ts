6890de4a8cafd8c8f7f6b2c5854161dc88d2eb50
Juho Vepsäläinen <bebraw@gmail.com>
2021-09-22 14:46:52 +0300

    chore: Add early type parsing for TS
    There are a couple of open questions still:
    
    1. How to get the project base path
    2. How to know which type parser to apply for which story

--- lib/core-server/src/utils/annotations.ts
+++ lib/core-server/src/utils/annotations.ts
@@ -1,9 +1,49 @@
+import * as path from 'path';
+import ts from 'typescript';
+import * as docGen from 'react-docgen-typescript';
+
 export async function useAnnotations(router: any) {
   router.use(async (req: any, res: any, next: any) => {
     if (!req.path.match(/\.annotations\.json$/)) {
       next();
+
+      return;
     }
 
-    res.end('HERE WE GO');
+    // TODO: How to construct the project base path correctly? This is way too coupled
+    // to this project.
+    const projectBase = path.join(__dirname.split('storybook')[0], 'storybook');
+    const exampleBase = 'examples/official-storybook';
+    const storyFile = req.path.split('.annotations.json')[0];
+    const storyPath = path.join(projectBase, exampleBase, storyFile);
+
+    // The assumption here is that we're parsing React (ts(x)) which probably
+    // isn't correct. How do we know which parser to apply?
+    const documentation = parseReactDocumentation(storyPath);
+
+    // console.log(projectBase, storyPath, documentation);
+
+    // TODO: Which is the right story to test against?
+    res.json(documentation);
   });
+}
+
+// TODO: Push this to the right place (type parsers for different targets)
+function parseReactDocumentation(filePath: string) {
+  // The simplest option
+  // const componentDocs = docGen.parse(filename);
+
+  // react-docgen-typescript-plugin does something along this to allow
+  // customizability
+  const compilerOptions = {
+    jsx: ts.JsxEmit.React,
+    module: ts.ModuleKind.CommonJS,
+    target: ts.ScriptTarget.Latest,
+  };
+  const docgenOptions = {};
+  const tsProgram = ts.createProgram([filePath], compilerOptions);
+  const docGenParser = docGen.withCompilerOptions(compilerOptions, docgenOptions);
+  const componentDocs = docGenParser.parseWithProgramProvider([filePath], () => tsProgram);
+
+  return componentDocs;
 }
