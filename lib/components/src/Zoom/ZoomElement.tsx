1a3f03ba7fe01af71e03adfbbfed42682cf7c1f4
Tomas Nygren <tomasnygren.854@gmail.com>
2021-12-07 13:54:50 +1100

    update zoomElement heigth when a child element mutate using a MutationObserver

--- lib/components/src/Zoom/ZoomElement.tsx
+++ lib/components/src/Zoom/ZoomElement.tsx
@@ -1,4 +1,5 @@
-import React, { ReactElement, useEffect, useState } from 'react';
+/* global MutationObserver */
+import React, { ReactElement, RefObject, useCallback, useEffect, useMemo, useState } from 'react';
 import { styled } from '@storybook/theming';
 import { browserSupportsCssZoom } from './browserSupportsCssZoom';
 
@@ -20,15 +21,51 @@ type ZoomProps = {
   children: ReactElement | ReactElement[];
 };
 
+const useMutationObserver = ({
+  element,
+  options = {},
+  callback,
+}: {
+  element: RefObject<Element>;
+  options: MutationObserverInit;
+  callback: MutationCallback;
+}): void => {
+  const observer = useMemo(
+    () =>
+      new MutationObserver((mutationRecord, mutationObserver) => {
+        callback?.(mutationRecord, mutationObserver);
+      }),
+    [callback]
+  );
+
+  useEffect(() => {
+    if (observer && element?.current) {
+      observer.observe(element.current, options);
+    }
+
+    return () => observer?.disconnect();
+  }, [element, observer, options]);
+};
+
 export function ZoomElement({ scale, children }: ZoomProps) {
-  const componentWrapperRef = React.useRef<HTMLDivElement>(null);
+  const componentWrapperRef = React.useRef(null);
   const [height, setHeight] = useState(0);
+
+  const handleMutations = useCallback(() => {
+    setHeight(componentWrapperRef.current.getBoundingClientRect().height);
+  }, []);
 
   useEffect(() => {
     if (componentWrapperRef.current) {
       setHeight(componentWrapperRef.current.getBoundingClientRect().height);
     }
   }, [scale, componentWrapperRef.current]);
+
+  useMutationObserver({
+    element: componentWrapperRef,
+    options: { subtree: true, childList: true },
+    callback: handleMutations,
+  });
 
   return (
     <ZoomElementWrapper scale={scale} height={height}>
