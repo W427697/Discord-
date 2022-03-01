158701901c0b0e3b623239808ae522581a448435
Norbert de Langen <ndelangen@me.com>
2022-03-01 22:40:09 +0100

    arghh

--- lib/core-server/src/utils/output-stats.ts
+++ lib/core-server/src/utils/output-stats.ts
@@ -3,20 +3,19 @@ import { logger } from '@storybook/node-logger';
 import chalk from 'chalk';
 import fs from 'fs-extra';
 import path from 'path';
-import { Stats } from 'webpack';
 
 export async function outputStats(directory: string, previewStats?: any, managerStats?: any) {
   if (previewStats) {
-    const filePath = await writeStats(directory, 'preview', previewStats as Stats);
+    const filePath = await writeStats(directory, 'preview', previewStats as any);
     logger.info(`=> preview stats written to ${chalk.cyan(filePath)}`);
   }
   if (managerStats) {
-    const filePath = await writeStats(directory, 'manager', managerStats as Stats);
+    const filePath = await writeStats(directory, 'manager', managerStats as any);
     logger.info(`=> manager stats written to ${chalk.cyan(filePath)}`);
   }
 }
 
-export const writeStats = async (directory: string, name: string, stats: Stats) => {
+export const writeStats = async (directory: string, name: string, stats: any) => {
   const filePath = path.join(directory, `${name}-stats.json`);
   const { chunks, ...data } = stats.toJson(); // omit chunks, which is about half of the total data
   await new Promise((resolve, reject) => {
