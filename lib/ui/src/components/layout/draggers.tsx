534cf62cb3f9b2da79991ece16c2741b17b2110b
Luca Cras <luca_cras@hotmail.com>
2022-03-16 16:12:04 +0100

    add prop for reversing the shade of the Handle, and stop overlap with scrollbar & iframe

--- lib/ui/src/components/layout/draggers.tsx
+++ lib/ui/src/components/layout/draggers.tsx
@@ -3,7 +3,7 @@ import { styled } from '@storybook/theming';
 
 export type Axis = 'x' | 'y';
 
-const Handle = styled.div<{ isDragging: boolean; axis: Axis }>(
+const Handle = styled.div<{ isDragging: boolean; axis: Axis; reverse?: boolean }>(
   ({ theme, isDragging }) => ({
     zIndex: 10,
     position: 'absolute',
@@ -17,7 +17,7 @@ const Handle = styled.div<{ isDragging: boolean; axis: Axis }>(
 
     overflow: 'hidden',
     transition:
-      'color 0.2s linear, background-position 0.2s linear, background-size 0.2s linear, background 0.2s linear',
+      'color 0.2s linear, background-size 0.2s linear, background 0.2s linear, background-position 0s linear',
     '&:hover': {
       color: theme.color.secondary,
     },
@@ -28,16 +28,16 @@ const Handle = styled.div<{ isDragging: boolean; axis: Axis }>(
   ({ theme, axis }) =>
     axis === 'x'
       ? {
-          height: '100%',
-          width: theme.layoutMargin,
-          marginLeft: 0,
-        }
+        height: '100%',
+        width: theme.layoutMargin,
+        marginLeft: 0,
+      }
       : {
-          height: theme.layoutMargin,
-          width: '100%',
-          marginTop: 0,
-        },
-  ({ axis, isDragging }) => {
+        height: theme.layoutMargin,
+        width: '100%',
+        marginTop: 0,
+      },
+  ({ axis, isDragging, reverse = false }) => {
     if (axis === 'y') {
       const style = {
         backgroundImage: `radial-gradient(at center center,rgba(0,0,0,0.2) 0%,transparent 70%,transparent 100%)`,
@@ -48,25 +48,25 @@ const Handle = styled.div<{ isDragging: boolean; axis: Axis }>(
       return isDragging
         ? style
         : {
-            ...style,
-            backgroundPosition: '50% 10px',
-            '&:hover': style,
-          };
+          ...style,
+          backgroundPosition: '50% 10px',
+          '&:hover': style,
+        };
     }
     if (axis === 'x') {
       const style = {
         backgroundImage: `radial-gradient(at center center,rgba(0,0,0,0.2) 0%,transparent 70%,transparent 100%)`,
         backgroundSize: '50px 100%',
-        backgroundPosition: '0 50%',
+        backgroundPosition: reverse ? '100% 50%' : '0 50%',
         backgroundRepeat: 'no-repeat',
       };
       return isDragging
         ? style
         : {
-            ...style,
-            backgroundPosition: '10px 50%',
-            '&:hover': style,
-          };
+          ...style,
+          backgroundPosition: '10px 50%',
+          '&:hover': style,
+        };
     }
     return {};
   }
