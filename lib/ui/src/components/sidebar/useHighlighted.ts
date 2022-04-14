f8a275dd56cd8d58c6fc0cd02c1572c6c5c1a27d
Norbert de Langen <ndelangen@me.com>
2022-04-14 14:08:28 +0200

    add preloading to stories getting highlighted in the sidebar

--- lib/ui/src/components/sidebar/useHighlighted.ts
+++ lib/ui/src/components/sidebar/useHighlighted.ts
@@ -8,6 +8,8 @@ import {
   useRef,
   useState,
 } from 'react';
+import { useStorybookApi } from '@storybook/api';
+import { PRELOAD_STORIES } from '@storybook/core-events';
 import { matchesKeyCode, matchesModifiers } from '../../keybinding';
 
 import { CombinedDataset, Highlight, Selection } from './types';
@@ -40,6 +42,7 @@ export const useHighlighted = ({
   const initialHighlight = fromSelection(selected);
   const highlightedRef = useRef<Highlight>(initialHighlight);
   const [highlighted, setHighlighted] = useState<Highlight>(initialHighlight);
+  const api = useStorybookApi();
 
   const updateHighlighted = useCallback(
     (highlight) => {
@@ -109,6 +112,10 @@ export const useHighlighted = ({
         const nextIndex = cycle(highlightable, currentIndex, isArrowUp ? -1 : 1);
         const didRunAround = isArrowUp ? nextIndex === highlightable.length - 1 : nextIndex === 0;
         highlightElement(highlightable[nextIndex], didRunAround);
+
+        if (highlightable[nextIndex].getAttribute('data-nodetype') === 'story') {
+          api.emit(PRELOAD_STORIES, [highlightedRef.current?.itemId]);
+        }
       });
     };
 
