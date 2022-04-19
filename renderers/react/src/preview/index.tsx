7135b6a837b5846ae77177bab6694cd7391e4e35
Michael Shilman <michael@lab80.co>
2022-04-19 11:26:00 +0800

    WIP client-api changes for `@storybook/react`

--- renderers/react/src/preview/index.tsx
+++ renderers/react/src/preview/index.tsx
@@ -19,20 +19,38 @@ const framework = 'react';
 
 const api = start(renderToDOM, { render });
 
+// We need this for v6 compat (in `@storybook/react`? `@storybook/stories-of`?)
 export const storiesOf: ClientApi['storiesOf'] = (kind, m) => {
   return (api.clientApi.storiesOf(kind, m) as ReturnType<ClientApi['storiesOf']>).addParameters({
     framework,
   });
 };
-
-export const configure: ClientApi['configure'] = (...args) => api.configure(framework, ...args);
-export const addDecorator: ClientApi['addDecorator'] = api.clientApi
-  .addDecorator as ClientApi['addDecorator'];
-export type DecoratorFn = Parameters<typeof addDecorator>[0];
-export const addParameters: ClientApi['addParameters'] = api.clientApi
-  .addParameters as ClientApi['addParameters'];
-export const clearDecorators: ClientApi['clearDecorators'] = api.clientApi.clearDecorators;
-export const setAddon: ClientApi['setAddon'] = api.clientApi.setAddon;
+// deprecate but don't remove (in `@storybook/stories-of`? `useForceReRender`? `context.forceReRender`?)
 export const forceReRender: ClientApi['forceReRender'] = api.forceReRender;
-export const getStorybook: ClientApi['getStorybook'] = api.clientApi.getStorybook;
-export const raw: ClientApi['raw'] = api.clientApi.raw;
+
+// move people to the stories field in main.js
+// export a function that throws a well-formatted error?
+export const configure: ClientApi['configure'] = (...args) => api.configure(framework, ...args);
+
+// use preview.js instead
+// - runtime exports throw errors
+// - automigrate converts to exports
+//
+// export const addDecorator: ClientApi['addDecorator'] = api.clientApi
+//   .addDecorator as ClientApi['addDecorator'];
+// export type DecoratorFn = Parameters<typeof addDecorator>[0];
+// export const addParameters: ClientApi['addParameters'] = api.clientApi
+//   .addParameters as ClientApi['addParameters'];
+// export const clearDecorators: ClientApi['clearDecorators'] = api.clientApi.clearDecorators;
+
+// delete this
+// export const setAddon: ClientApi['setAddon'] = api.clientApi.setAddon;
+
+// unintentionally deleted in 6.4
+// export const getStorybook: ClientApi['getStorybook'] = api.clientApi.getStorybook;
+// export const raw: ClientApi['raw'] = api.clientApi.raw;
+
+// To opt out of `features.storyStoreV7`
+// => `features.legacyStoriesOf`?
+// - install `@storybook/stories-of` (autodetect?)
+// storyStoreV7: false detection in v6 projects
