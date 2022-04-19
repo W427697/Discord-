379a42daa51b09f13b2630efaf1c286056d4f2a9
Michael Shilman <michael@lab80.co>
2022-04-20 01:20:04 +0800

    Update preset.ts

--- app/web-components/src/server/preset.ts
+++ app/web-components/src/server/preset.ts
@@ -3,5 +3,4 @@ import type { StorybookConfig } from '@storybook/core-common';
 export const addons: StorybookConfig['addons'] = [
   '@storybook/renderer-web-components',
   require.resolve('./framework-preset-web-components'),
-  require.resolve('./framework-preset-web-components-docs'),
 ];
