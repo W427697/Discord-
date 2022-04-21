2a80955da2691e8407c94cdce1a47076d64c2723
Michael Shilman <michael@lab80.co>
2022-04-21 17:02:12 +0800

    Presets CI fixes

--- presets/svelte-webpack/src/preset.ts
+++ presets/svelte-webpack/src/preset.ts
@@ -1,7 +1,6 @@
 import type { StorybookConfig } from '@storybook/core-common';
 
 export const addons: StorybookConfig['addons'] = [
-  '@storybook/renderer-svelte',
   require.resolve('./framework-preset-svelte'),
   require.resolve('./framework-preset-svelte-docs'),
 ];
