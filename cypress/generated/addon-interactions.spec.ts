e44056b6f362ffffbd2e818e5b94ee4ae27f38ce
Michael Shilman <michael@lab80.co>
2022-04-25 14:30:41 +0800

    Merge branch 'next' into tech/telemetry

--- cypress/generated/addon-interactions.spec.ts
+++ cypress/generated/addon-interactions.spec.ts
@@ -34,4 +34,16 @@ describe('addon-interactions', () => {
   onlyOn('vite_react', () => {
     it('should have interactions', test);
   });
+
+  onlyOn('preact', () => {
+    it('should have interactions', test);
+  });
+
+  onlyOn('svelte', () => {
+    it('should have interactions', test);
+  });
+
+  onlyOn('vue3', () => {
+    it('should have interactions', test);
+  });
 });
