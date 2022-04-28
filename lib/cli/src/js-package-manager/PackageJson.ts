c95403f0e18ddfaaf3c1d04737d986936f606f28
Yann Braga <yannbf@gmail.com>
2022-04-28 17:39:28 +0200

    fix types in js-package-manager

--- lib/cli/src/js-package-manager/PackageJson.ts
+++ lib/cli/src/js-package-manager/PackageJson.ts
@@ -1,4 +1,5 @@
 import type { PackageJson } from '@storybook/core-common';
 
+export type { PackageJson } from '@storybook/core-common';
 export type PackageJsonWithDepsAndDevDeps = PackageJson &
   Required<Pick<PackageJson, 'dependencies' | 'devDependencies'>>;
