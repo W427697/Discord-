4787b4f0081f09de732bcf36f3c3a76675f843f0
Andrey Okonetchnikov <andrey@okonet.dev>
2022-04-28 16:25:15 +0200

    docs(Source): Add types and improve stories

--- lib/blocks/src/Source.stories.tsx
+++ lib/blocks/src/Source.stories.tsx
@@ -1,3 +1,4 @@
+import { ComponentStory } from '@storybook/react';
 import React from 'react';
 import { Source, SourceError } from './Source';
 
@@ -6,25 +7,25 @@ export default {
   component: Source,
 };
 
-export const Loading = (args) => <Source {...args} />;
+export const Loading: ComponentStory<typeof Source> = (args) => <Source {...args} />;
 Loading.args = {
   isLoading: true,
 };
 
-const jsxCode = `
+export const JSX: ComponentStory<typeof Source> = (args) => <Source {...args} />;
+JSX.args = {
+  code: `
 <MyComponent boolProp scalarProp={1} complexProp={{ foo: 1, bar: '2' }}>
   <SomeOtherComponent funcProp={(a) => a.id} />
 </MyComponent>
-`;
-
-export const JSX = (args) => <Source {...args} />;
-JSX.args = {
-  code: jsxCode,
+`,
   language: 'jsx',
   format: false,
 };
 
-const cssCode = `
+export const CSSWithDarkMode: ComponentStory<typeof Source> = (args) => <Source {...args} />;
+CSSWithDarkMode.args = {
+  code: `
 @-webkit-keyframes blinker {
   from { opacity: 1.0; }
   to { opacity: 0.0; }
@@ -36,22 +37,34 @@ const cssCode = `
   -webkit-animation-timing-function: cubic-bezier(.5, 0, 1, 1);
   -webkit-animation-duration: 1.7s;
 }
-`;
-
-export const CSS = (args) => <Source {...args} />;
-CSS.args = {
-  code: cssCode,
+`,
   language: 'css',
   format: false,
+  dark: true,
 };
 
-export const NoStory = (args) => <Source {...args} />;
+export const GraphQLWithFormatting: ComponentStory<typeof Source> = (args) => <Source {...args} />;
+GraphQLWithFormatting.args = {
+  code: `query HeroNameAndFriends($episode: Episode) {
+          hero(episode: $episode) {
+            name
+            friends {
+              name
+            }
+          }
+        }
+`,
+  language: 'graphql',
+  format: true,
+};
+
+export const NoStory: ComponentStory<typeof Source> = (args) => <Source {...args} />;
 NoStory.args = {
   error: SourceError.NO_STORY,
   format: false,
 };
 
-export const SourceUnavailable = (args) => <Source {...args} />;
+export const SourceUnavailable: ComponentStory<typeof Source> = (args) => <Source {...args} />;
 SourceUnavailable.args = {
   error: SourceError.SOURCE_UNAVAILABLE,
   format: false,
