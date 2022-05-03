b7f2cedcba3210710a7dda96e228255b75df54b3
Andrey Okonetchnikov <andrey@okonet.dev>
2022-05-03 15:54:22 +0200

    fix: Update import to blocks

--- app/react/src/client/docs/react-argtypes.stories.tsx
+++ app/react/src/client/docs/react-argtypes.stories.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import mapValues from 'lodash/mapValues';
-import { ArgsTable } from '@storybook/components';
+import { ArgsTable } from '@storybook/blocks';
 import { Args } from '@storybook/api';
 import { inferControls } from '@storybook/store';
 
