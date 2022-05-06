19f2331bd4205e418dc4001bc0c413d45aa07cda
Tom Coleman <tom@chromatic.com>
2022-05-06 16:15:19 +1000

    Factored out `ExternalPreview` and created tests

--- addons/docs/src/blocks/ExternalDocsContainer.tsx
+++ addons/docs/src/blocks/ExternalDocsContainer.tsx
@@ -1,95 +1,11 @@
 import React from 'react';
 
-import { Preview, DocsContextProps } from '@storybook/preview-web';
-import { Path, ModuleExports, StoryIndex, Story } from '@storybook/store';
-import { toId, AnyFramework, ComponentTitle, StoryId, ProjectAnnotations } from '@storybook/csf';
+import { DocsContextProps } from '@storybook/preview-web';
+import { ModuleExport, Story } from '@storybook/store';
+import { AnyFramework, StoryId } from '@storybook/csf';
 
 import { DocsContext } from './DocsContext';
-
-type StoryExport = any;
-type MetaExport = any;
-type ExportName = string;
-
-class ConstantMap<TKey, TValue extends string> {
-  entries = new Map<TKey, TValue>();
-
-  // eslint-disable-next-line no-useless-constructor
-  constructor(private prefix: string) {}
-
-  get(key: TKey) {
-    if (!this.entries.has(key)) {
-      this.entries.set(key, `${this.prefix}${this.entries.size}` as TValue);
-    }
-    return this.entries.get(key);
-  }
-}
-
-class ExternalPreview<TFramework extends AnyFramework> extends Preview<TFramework> {
-  private initialized = false;
-
-  private importPaths = new ConstantMap<MetaExport, Path>('./importPath/');
-
-  private titles = new ConstantMap<MetaExport, ComponentTitle>('title-');
-
-  private exportNames = new ConstantMap<StoryExport, ExportName>('story-');
-
-  public storyIds = new Map<StoryExport, StoryId>();
-
-  private storyIndex: StoryIndex = { v: 4, entries: {} };
-
-  private moduleExportsByImportPath: Record<Path, ModuleExports> = {};
-
-  constructor(public projectAnnotations: ProjectAnnotations) {
-    super();
-  }
-
-  addStoryFromExports(storyExport: StoryExport, meta: MetaExport) {
-    const importPath = this.importPaths.get(meta);
-    const title = meta.title || this.titles.get(meta);
-
-    const exportName = this.exportNames.get(storyExport);
-    const storyId = toId(title, exportName);
-    this.storyIds.set(storyExport, storyId);
-
-    if (!this.moduleExportsByImportPath[importPath])
-      this.moduleExportsByImportPath[importPath] = { default: meta };
-    this.moduleExportsByImportPath[importPath][exportName] = storyExport;
-
-    this.storyIndex.entries[storyId] = {
-      id: storyId,
-      importPath,
-      title,
-      name: 'Name',
-      type: 'story',
-    };
-
-    if (!this.initialized) {
-      return this.initialize({
-        getStoryIndex: () => this.storyIndex,
-        importFn: (path: Path) => {
-          console.log(this.moduleExportsByImportPath, path);
-          return Promise.resolve(this.moduleExportsByImportPath[path]);
-        },
-        getProjectAnnotations: () => this.projectAnnotations,
-      });
-    }
-    // else
-    return this.onStoriesChanged({ storyIndex: this.storyIndex });
-  }
-
-  storyById(storyId: StoryId) {
-    const entry = this.storyIndex.entries[storyId];
-    if (!entry) throw new Error(`Unknown storyId ${storyId}`);
-    const { importPath, title } = entry;
-    const moduleExports = this.moduleExportsByImportPath[importPath];
-    const csfFile = this.storyStore.processCSFFileWithCache<TFramework>(
-      moduleExports,
-      importPath,
-      title
-    );
-    return this.storyStore.storyFromCSFFile({ storyId, csfFile });
-  }
-}
+import { ExternalPreview } from './ExternalPreview';
 
 let preview: ExternalPreview<AnyFramework>;
 
@@ -99,8 +15,8 @@ export const ExternalDocsContainer: React.FC<{ projectAnnotations: any }> = ({
 }) => {
   if (!preview) preview = new ExternalPreview(projectAnnotations);
 
-  let pageMeta: MetaExport;
-  const setMeta = (m: MetaExport) => {
+  let pageMeta: ModuleExport;
+  const setMeta = (m: ModuleExport) => {
     pageMeta = m;
   };
 
@@ -111,9 +27,8 @@ export const ExternalDocsContainer: React.FC<{ projectAnnotations: any }> = ({
     title: 'External',
     name: 'Docs',
 
-    storyIdByModuleExport: (storyExport: StoryExport) => {
-      if (!preview.storyIds.has(storyExport)) preview.addStoryFromExports(storyExport, pageMeta);
-      return preview.storyIds.get(storyExport);
+    storyIdByModuleExport: (storyExport: ModuleExport) => {
+      return preview.storyIdByModuleExport(storyExport, pageMeta);
     },
 
     storyById: (id: StoryId) => {
