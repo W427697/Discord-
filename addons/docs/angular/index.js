b43bb990e56196053a050d049a2b8b4e76ff7599
Rivaldo Cavalcante <rivaldo.cavalcante@cityplumbing.co.uk>
2022-05-11 17:52:42 -0300

    Upgrade @mdx/react to support react 18

--- addons/docs/angular/index.js
+++ addons/docs/angular/index.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-underscore-dangle */
 /* global window */
 
-export const setCompodocJson = (compodocJson) => {
+const setCompodocJson = (compodocJson) => {
   // @ts-ignore
   window.__STORYBOOK_COMPODOC_JSON__ = compodocJson;
 };
+
+module.exports.setCompodocJson = setCompodocJson;
