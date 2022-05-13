b990545b970593ed6499ecb8cff3db085ce5c6c8
Michael Shilman <michael@lab80.co>
2022-05-13 16:19:10 +0800

    Set up projectAnnotations properly for source decorator

--- examples/external-docs/pages/_app.js
+++ examples/external-docs/pages/_app.js
@@ -2,13 +2,12 @@
 import React from 'react';
 import 'nextra-theme-docs/style.css';
 import { ExternalDocsContainer } from '@storybook/addon-docs';
+import { composeConfigs } from '@storybook/store';
 import * as reactAnnotations from '@storybook/react/dist/esm/client/preview/config';
+import * as docsAnnotations from '@storybook/react/dist/esm/client/docs/config';
 import * as previewAnnotations from '../.storybook/preview';
 
-const projectAnnotations = {
-  ...reactAnnotations,
-  ...previewAnnotations,
-};
+const projectAnnotations = composeConfigs([reactAnnotations, docsAnnotations, previewAnnotations]);
 
 export default function Nextra({ Component, pageProps }) {
   return (
