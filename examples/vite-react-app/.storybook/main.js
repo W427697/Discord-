14eb2c15873f306c959683d5551395e09f4d8e1e
Michael Shilman <michael@lab80.co>
2022-05-22 02:45:48 +0800

    Examples: Vite builder now set by framework

--- examples/vite-react-app/.storybook/main.js
+++ examples/vite-react-app/.storybook/main.js
@@ -2,9 +2,6 @@ module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-essentials'],
   framework: '@storybook/react-vite',
-  core: {
-    builder: '@storybook/builder-vite',
-  },
   features: {
     storyStoreV7: true,
   },
