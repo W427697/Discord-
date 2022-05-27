f09fd20a3e3450b4373058d9a252c37342daca12
Norbert de Langen <ndelangen@me.com>
2022-05-27 16:20:31 +0200

    fix

--- presets/preact-webpack/src/index.ts
+++ presets/preact-webpack/src/index.ts
@@ -5,7 +5,7 @@ export const babelDefault: StorybookConfig['babelDefault'] = (config) => {
   return {
     ...config,
     plugins: [
-      ...config.plugins,
+      ...(config.plugins || []),
       [require.resolve('@babel/plugin-transform-react-jsx'), { pragma: 'h' }, 'preset'],
     ],
   };
@@ -17,7 +17,7 @@ export const webpackFinal: StorybookConfig['webpackFinal'] = (config) => {
     resolve: {
       ...config.resolve,
       alias: {
-        ...config.resolve.alias,
+        ...(config.resolve.alias || {}),
         react: path.dirname(require.resolve('preact/compat/package.json')),
         'react-dom/test-utils': path.dirname(require.resolve('preact/test-utils/package.json')),
         'react-dom': path.dirname(require.resolve('preact/compat/package.json')),
