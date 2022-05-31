04dac620012c4be19d86c9a4095dd900ebc750f0
Norbert de Langen <ndelangen@me.com>
2022-05-31 11:25:48 +0200

    fix examples

--- examples/cra-kitchen-sink/.storybook/preview.js
+++ examples/cra-kitchen-sink/.storybook/preview.js
@@ -1,7 +1,7 @@
 export const parameters = {
   options: {
     storySort: (a, b) =>
-      a[1].kind === b[1].kind ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+      a[1].title === b[1].title ? 0 : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
   docs: {
     source: {
