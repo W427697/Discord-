77c3ff7f2f1e492a39d6a1f0465dd2ad345c0412
Andrey Okonetchnikov <andrey@okonet.dev>
2022-06-07 13:32:53 +0200

    Add theme switcher to the storybook and wrap story with a Box

--- lib/blocks/src/primitives/VanillaThemeProvider/VanillaThemeProvider.tsx
+++ lib/blocks/src/primitives/VanillaThemeProvider/VanillaThemeProvider.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
+import { Box } from '..';
 import { darkTheme, lightTheme } from '../theme.css';
 
-export const VanillaThemeProvider = ({ children, theme }) => {
-  return <div className={theme === 'dark' ? darkTheme : lightTheme}>{children}</div>;
+export const VanillaThemeProvider: React.FC<{ theme: 'light' | 'dark' }> = ({
+  children,
+  theme,
+}) => {
+  const className = theme === 'dark' ? darkTheme : lightTheme;
+  return (
+    <Box
+      className={className}
+      css={{
+        padding: 'medium',
+        background: 'background',
+        color: 'text',
+      }}
+    >
+      {children}
+    </Box>
+  );
 };
