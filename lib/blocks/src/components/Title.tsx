ca7a2943292c4ef00322342813d3c459381b2ec7
Andrey Okonetchnikov <andrey@okonet.dev>
2022-06-08 13:41:23 +0200

    Title: Use Text component

--- lib/blocks/src/components/Title.tsx
+++ lib/blocks/src/components/Title.tsx
@@ -1,17 +1,16 @@
-import { styled, Theme } from '@storybook/theming';
-import { withReset } from '@storybook/components';
+import React from 'react';
+import { Text } from '../primitives';
 
-const breakpoint = 600;
-
-export const Title = styled.h1<{}>(withReset, ({ theme }: { theme: Theme }) => ({
-  color: theme.color.defaultText,
-  fontSize: theme.typography.size.m3,
-  fontWeight: theme.typography.weight.black,
-  lineHeight: '32px',
-
-  [`@media (min-width: ${breakpoint}px)`]: {
-    fontSize: theme.typography.size.l1,
-    lineHeight: '36px',
-    marginBottom: '.5rem', // 8px
-  },
-}));
+export const Title: React.FC = (props) => {
+  return (
+    <Text
+      as="h1"
+      css={{
+        color: 'text',
+        fontSize: ['m3', 'l1'],
+        fontWeight: 'black',
+      }}
+      {...props}
+    />
+  );
+};
