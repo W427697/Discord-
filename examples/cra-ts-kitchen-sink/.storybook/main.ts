ea3cbadb46d6d6799ebd81b56dde114e462342a8
Michael Shilman <michael@lab80.co>
2022-06-16 11:21:10 +0800

    Examples: Extract cra-ts-kitchen-sink

--- examples/cra-ts-kitchen-sink/.storybook/main.ts
+++ examples/cra-ts-kitchen-sink/.storybook/main.ts
@@ -28,6 +28,7 @@ module.exports = {
   core: {
     channelOptions: { allowFunction: false, maxDepth: 10 },
     disableTelemetry: true,
+    disableProjectJson: true,
   },
   staticDirs: ['../public'],
   features: {
