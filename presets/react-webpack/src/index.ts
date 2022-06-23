015232a10b6b49450b220251636138b7f8e74afa
Norbert de Langen <ndelangen@me.com>
2022-06-23 09:09:33 +0200

    fix

--- presets/react-webpack/src/index.ts
+++ presets/react-webpack/src/index.ts
@@ -3,8 +3,8 @@ import type { StorybookConfig } from './types';
 export * from './types';
 
 export const addons: StorybookConfig['addons'] = [
-  require.resolve('@storybook/react-webpack5/dist/framework-preset-react'),
-  require.resolve('@storybook/react-webpack5/dist/framework-preset-react-dom-hack'),
-  require.resolve('@storybook/react-webpack5/dist/framework-preset-cra'),
-  require.resolve('@storybook/react-webpack5/dist/framework-preset-react-docs'),
+  require.resolve('@storybook/preset-react-webpack/dist/framework-preset-react'),
+  require.resolve('@storybook/preset-react-webpack/dist/framework-preset-react-dom-hack'),
+  require.resolve('@storybook/preset-react-webpack/dist/framework-preset-cra'),
+  require.resolve('@storybook/preset-react-webpack/dist/framework-preset-react-docs'),
 ];
