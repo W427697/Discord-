b2a18cf256eca1435b63def39bbf3d6f5f9ccd04
Gert Hengeveld <info@ghengeveld.nl>
2022-06-24 09:36:01 +0200

    Pull example stories into their own file

--- addons/interactions/src/components/AccountForm/addon-interactions.stories.tsx
+++ addons/interactions/src/components/AccountForm/addon-interactions.stories.tsx
@@ -1,14 +1,7 @@
 /* eslint-disable jest/no-standalone-expect */
-import { Story as CSF2Story, Meta, ComponentStoryObj } from '@storybook/react';
+import { Meta, ComponentStoryObj } from '@storybook/react';
 import { expect } from '@storybook/jest';
-import {
-  within,
-  waitFor,
-  fireEvent,
-  userEvent,
-  waitForElementToBeRemoved,
-} from '@storybook/testing-library';
-import React from 'react';
+import { within, waitFor, fireEvent, userEvent } from '@storybook/testing-library';
 
 import { AccountForm } from './AccountForm';
 
@@ -26,96 +19,6 @@ export default {
 } as Meta;
 
 type CSF3Story = ComponentStoryObj<typeof AccountForm>;
-
-export const Demo: CSF2Story = (args) => (
-  <button type="button" onClick={() => args.onSubmit('clicked')}>
-    Click
-  </button>
-);
-
-Demo.play = async ({ args, canvasElement }) => {
-  await userEvent.click(within(canvasElement).getByRole('button'));
-  await expect(args.onSubmit).toHaveBeenCalledWith(expect.stringMatching(/([A-Z])\w+/gi));
-  await expect([{ name: 'John', age: 42 }]).toEqual(
-    expect.arrayContaining([
-      expect.objectContaining({ name: 'John' }),
-      expect.objectContaining({ age: 42 }),
-    ])
-  );
-};
-
-export const Exception = Demo.bind({});
-Exception.play = () => Demo.play(undefined as any); // deepscan-disable-line
-Exception.parameters = { chromatic: { disableSnapshot: true } };
-
-export const FindBy: CSF2Story = (args) => {
-  const [isLoading, setIsLoading] = React.useState(true);
-  React.useEffect(() => {
-    setTimeout(() => setIsLoading(false), 500);
-  }, []);
-  return isLoading ? <div>Loading...</div> : <button type="button">Loaded!</button>;
-};
-FindBy.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  await canvas.findByRole('button');
-  await expect(true).toBe(true);
-};
-
-export const WaitFor: CSF2Story = (args) => (
-  <button type="button" onClick={() => setTimeout(() => args.onSubmit('clicked'), 100)}>
-    Click
-  </button>
-);
-WaitFor.play = async ({ args, canvasElement }) => {
-  await userEvent.click(await within(canvasElement).findByText('Click'));
-  await waitFor(async () => {
-    await expect(args.onSubmit).toHaveBeenCalledWith(expect.stringMatching(/([A-Z])\w+/gi));
-    await expect(true).toBe(true);
-  });
-};
-
-export const WaitForElementToBeRemoved: CSF2Story = () => {
-  const [isLoading, setIsLoading] = React.useState(true);
-  React.useEffect(() => {
-    setTimeout(() => setIsLoading(false), 1500);
-  }, []);
-  return isLoading ? <div>Loading...</div> : <button type="button">Loaded!</button>;
-};
-WaitForElementToBeRemoved.play = async ({ canvasElement }) => {
-  const canvas = within(canvasElement);
-  await waitForElementToBeRemoved(await canvas.findByText('Loading...'), { timeout: 2000 });
-  const button = await canvas.findByText('Loaded!');
-  await expect(button).not.toBeNull();
-};
-
-export const WithLoaders: CSF2Story = (args, { loaded: { todo } }) => {
-  return (
-    <button type="button" onClick={args.onSubmit(todo.title)}>
-      Todo: {todo.title}
-    </button>
-  );
-};
-WithLoaders.loaders = [
-  async () => {
-    // long fake timeout
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    return {
-      todo: {
-        userId: 1,
-        id: 1,
-        title: 'delectus aut autem',
-        completed: false,
-      },
-    };
-  },
-];
-WithLoaders.play = async ({ args, canvasElement }) => {
-  const canvas = within(canvasElement);
-  const todoItem = await canvas.findByText('Todo: delectus aut autem');
-  await userEvent.click(todoItem);
-  await expect(args.onSubmit).toHaveBeenCalledWith('delectus aut autem');
-};
 
 export const Standard: CSF3Story = {
   args: { passwordVerification: false },
