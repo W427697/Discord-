8b98d81081012d933b0918b292678279ae363935
Norbert de Langen <ndelangen@me.com>
2022-06-27 14:25:19 +0200

    fix

--- lib/components/src/form/input/input.tsx
+++ lib/components/src/form/input/input.tsx
@@ -2,7 +2,9 @@ import React, { FunctionComponent, forwardRef, HTMLProps, SelectHTMLAttributes }
 import type { Theme, CSSObject } from '@storybook/theming';
 import { styled } from '@storybook/theming';
 
-import TextareaAutoResize, { TextareaAutosizeProps } from 'react-textarea-autosize';
+import type { TextareaAutosizeProps } from 'react-textarea-autosize';
+// @ts-ignore
+import TextareaAutoResize from 'react-textarea-autosize/dist/react-textarea-autosize.esm';
 
 import { Button as StyledButton } from '../../Button/Button';
 
