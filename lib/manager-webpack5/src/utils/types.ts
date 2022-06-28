988982ae0e974c23851425f6f37876178a15affb
Norbert de Langen <ndelangen@me.com>
2022-06-28 10:03:37 +0200

    cleanup

--- lib/manager-webpack5/src/utils/types.ts
+++
@@ -1,6 +0,0 @@
-import type { Ref } from '@storybook/core-common';
-
-export interface ManagerWebpackOptions {
-  entries: string[];
-  refs: Record<string, Ref>;
-}
--- lib/manager-webpack5/typings.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'global';
-declare module '@storybook/semver';
-declare module 'lazy-universal-dotenv';
-declare module '@storybook/theming/paths';
-declare module '@storybook/ui/paths';
-declare module 'better-opn';
-declare module 'open';
-declare module 'x-default-browser';
-declare module '@storybook/ui';
