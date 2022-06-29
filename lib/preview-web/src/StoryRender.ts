aafbfe6f44aceb0a4e96a859d389ff516fd430d1
Gert Hengeveld <info@ghengeveld.nl>
2022-06-29 13:51:12 +0200

    Wait for idle before starting play function

--- lib/preview-web/src/StoryRender.ts
+++ lib/preview-web/src/StoryRender.ts
@@ -45,6 +45,16 @@ function createController(): AbortController {
   } as AbortController;
 }
 
+function createIdlePromise() {
+  return new Promise<void>((resolve) => {
+    try {
+      global.window.requestIdleCallback(resolve, { timeout: 1000 });
+    } catch (e) {
+      setTimeout(resolve, 0);
+    }
+  });
+}
+
 function serializeError(error: any) {
   try {
     const { name = 'Error', message = String(error), stack } = error;
@@ -224,8 +234,8 @@ export class StoryRender<TFramework extends AnyFramework> implements Render<TFra
       };
 
       await this.runPhase(abortSignal, 'rendering', async () => {
-        this.teardownRender =
-          (await this.renderToScreen(renderContext, canvasElement)) || (() => {});
+        const teardown = await this.renderToScreen(renderContext, canvasElement);
+        this.teardownRender = teardown || (() => {});
       });
       this.notYetRendered = false;
       if (abortSignal.aborted) return;
@@ -233,6 +243,7 @@ export class StoryRender<TFramework extends AnyFramework> implements Render<TFra
       if (forceRemount && playFunction) {
         this.disableKeyListeners = true;
         try {
+          await createIdlePromise();
           await this.runPhase(abortSignal, 'playing', async () => {
             await playFunction(renderContext.storyContext);
           });
