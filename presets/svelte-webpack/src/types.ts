88486fbb42871459aeba7854e7a76a2538cb9bb0
Norbert de Langen <ndelangen@me.com>
2022-06-29 08:45:17 +0200

    apply review comments (thanks)

--- presets/svelte-webpack/src/types.ts
+++ presets/svelte-webpack/src/types.ts
@@ -1,15 +1,4 @@
-import type {
-  WebpackConfiguration,
-  StorybookConfig as StorybookConfigBase,
-  TypescriptOptions as BaseTypescriptOptions,
-} from '@storybook/core-webpack';
-
-export type { BuilderResult } from '@storybook/core-webpack';
-
-export type TypescriptOptions = BaseTypescriptOptions;
-
-export type StorybookConfig<TWebpackConfiguration = WebpackConfiguration> =
-  StorybookConfigBase<TWebpackConfiguration>;
+export type { BuilderResult, TypescriptOptions, StorybookConfig } from '@storybook/core-webpack';
 
 export type SvelteOptions = {
   preprocess?: any;
