ac0317e542730857d3d72d2ce020b4524a2502c3
Norbert de Langen <ndelangen@me.com>
2022-06-29 22:28:54 +0200

    swap out assert for a hyper-lightweight alternative. It has what we need in doctrine

--- lib/builder-manager/src/index.ts
+++ lib/builder-manager/src/index.ts
@@ -42,7 +42,7 @@ export const getConfig: ManagerBuilder['getConfig'] = async (options) => {
       aliasPlugin({
         process: require.resolve('rollup-plugin-node-polyfills/polyfills/process-es6.js'),
         util: require.resolve('rollup-plugin-node-polyfills/polyfills/util.js'),
-        assert: require.resolve('rollup-plugin-node-polyfills/polyfills/assert.js'),
+        assert: require.resolve('browser-assert'),
       }),
       globalExternals(definitions),
       pnpPlugin(),
