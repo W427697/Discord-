795c33c89d2d4ee4ebc6fb54534bca7a639d632c
Norbert de Langen <ndelangen@me.com>
2022-07-06 10:30:08 +0200

    add comments to explain the situation with globals, and why we do what we do.

--- lib/ui/src/globals/types.ts
+++ lib/ui/src/globals/types.ts
@@ -1,7 +1,6 @@
 import { ModuleInfo } from '@fal-works/esbuild-plugin-global-externals';
 
-// this is the mapping between what a module name and what this module will be globalized as.
-//
+// Here we map the name of a module to their NAME in the global scope.
 export enum Keys {
   'react' = '__REACT__',
   'react-dom' = '__REACTDOM__',
@@ -14,4 +13,5 @@ export enum Keys {
   '@storybook/addons' = '__STORYBOOKADDONS__',
   '@storybook/client-logger' = '__STORYBOOKCLIENTLOGGER__',
 }
+
 export type Definitions = Required<Record<keyof typeof Keys, Required<ModuleInfo>>>;
