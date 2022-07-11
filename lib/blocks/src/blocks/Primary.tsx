b50523d3643e8fae540caf15ea3e59c6882a534b
Tom Coleman <tom@chromatic.com>
2022-07-11 16:25:18 +1000

    Primary doc block should use `storyById()`

--- lib/blocks/src/blocks/Primary.tsx
+++ lib/blocks/src/blocks/Primary.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, FC } from 'react';
-import { Story } from '@storybook/store';
 
 import { DocsContext } from './DocsContext';
 import { DocsStory } from './DocsStory';
@@ -9,11 +8,8 @@ interface PrimaryProps {
 }
 
 export const Primary: FC<PrimaryProps> = ({ name }) => {
-  const { componentStories: getComponentStories } = useContext(DocsContext);
-  const componentStories = getComponentStories();
-  let story;
-  if (componentStories) {
-    story = name ? componentStories.find((s: Story<any>) => s.name === name) : componentStories[0];
-  }
+  const docsContext = useContext(DocsContext);
+  const storyId = name && docsContext.storyIdByName(name);
+  const story = docsContext.storyById(storyId);
   return story ? <DocsStory {...story} expanded={false} withToolbar /> : null;
 };
