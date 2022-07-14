5ecf3395057b507a6c4d7395f7355d73443a9c40
Ben McCann <322311+benmccann@users.noreply.github.com>
2022-07-14 09:56:57 -0700

    remove @storybook/addon-svelte-csf for now

--- lib/cli/src/generators/SVELTE/index.ts
+++ lib/cli/src/generators/SVELTE/index.ts
@@ -2,7 +2,6 @@ import { baseGenerator, Generator } from '../baseGenerator';
 
 const generator: Generator = async (packageManager, npmOptions, options) => {
   await baseGenerator(packageManager, npmOptions, options, 'svelte', {
-    extraPackages: ['@storybook/addon-svelte-csf'],
     extensions: ['js', 'jsx', 'ts', 'tsx', 'svelte'],
     commonJs: true,
   });
