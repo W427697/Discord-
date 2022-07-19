59a4a06e8b4e8adb5846d890f9d78cf0abbd2304
Tom Coleman <tom@chromatic.com>
2022-07-19 14:41:57 +1000

    Skip test on react

--- cypress/generated/addon-docs.spec.ts
+++ cypress/generated/addon-docs.spec.ts
@@ -8,22 +8,24 @@ describe('addon-docs', () => {
 
   skipOn('vue3', () => {
     skipOn('html', () => {
-      it('should provide source snippet', () => {
-        cy.getDocsElement()
-          .find('.docblock-code-toggle')
-          .each(($div) => {
-            cy.wrap($div)
-              .should('contain.text', 'Show code')
-              // use force click so cypress does not automatically scroll, making the source block visible on this step
-              .click({ force: true });
-          });
+      skipOn('react', () => {
+        it('should provide source snippet', () => {
+          cy.getDocsElement()
+            .find('.docblock-code-toggle')
+            .each(($div) => {
+              cy.wrap($div)
+                .should('contain.text', 'Show code')
+                // use force click so cypress does not automatically scroll, making the source block visible on this step
+                .click({ force: true });
+            });
 
-        cy.getDocsElement()
-          .find('pre.prismjs')
-          .each(($div) => {
-            const text = $div.text();
-            expect(text).not.match(/^\(args\) => /);
-          });
+          cy.getDocsElement()
+            .find('pre.prismjs')
+            .each(($div) => {
+              const text = $div.text();
+              expect(text).not.match(/^\(args\) => /);
+            });
+        });
       });
     });
   });
