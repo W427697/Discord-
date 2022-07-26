36ad7476c5a1672fe105e94e9bb0b4d15c601251
Michael Arestad <marestad@gmail.com>
2022-07-26 14:39:28 -0600

    Initial icon update

--- code/lib/components/src/icon/icon.tsx
+++ code/lib/components/src/icon/icon.tsx
@@ -1,39 +1,54 @@
-import React, { ComponentProps, memo } from 'react';
+import React, { FunctionComponent, ComponentProps, memo } from 'react';
 import { styled } from '@storybook/theming';
-import icons, { IconKey } from './icons';
+import { icons, IconKey } from './icons';
 
-import Svg from './svg';
+const Svg = styled.svg`
+  display: inline-block;
+  shape-rendering: inherit;
+  transform: translate3d(0, 0, 0);
+  vertical-align: middle;
 
-const Path = styled.path({
-  fill: 'currentColor',
-});
+  path {
+    fill: currentColor;
+  }
+`;
 
-export interface IconsProps extends ComponentProps<typeof Svg> {
-  icon?: IconKey;
-  symbol?: IconKey;
+/**
+ * An Icon is a piece of visual element, but we must ensure its accessibility while using it.
+ * It can have 2 purposes:
+ *
+ * - *decorative only*: for example, it illustrates a label next to it. We must ensure that it is ignored by screen readers, by setting `aria-hidden` attribute (ex: `<Icon icon="check" aria-hidden />`)
+ * - *non-decorative*: it means that it delivers information. For example, an icon as only child in a button. The meaning can be obvious visually, but it must have a proper text alternative via `aria-label` for screen readers. (ex: `<Icon icon="print" aria-label="Print this document" />`)
+ */
+export interface IconProps {
+  icon: IconType;
+  symbol?: IconType;
 }
 
-// TODO: if we can resize the 1024 to 20, we can remove the size attributes
-export const Icons = memo<IconsProps>(({ icon, symbol, ...props }) => (
-  <Svg viewBox="0 0 1024 1024" {...props}>
-    {symbol ? <use xlinkHref={`#icon--${symbol}`} /> : <Path d={icons[icon]} />}
-  </Svg>
-));
+export const Icons: FunctionComponent<IconProps> = ({ icon, symbol, ...props }: IconProps) => {
+  return (
+    <Svg viewBox="0 0 14 14" width="14px" height="14px" {...props}>
+      {symbol ? <use xlinkHref={`#icon--${symbol}`} /> : icons[icon]}
+    </Svg>
+  );
+};
+
+export type IconType = keyof typeof icons;
 
 export interface SymbolsProps extends ComponentProps<typeof Svg> {
   icons?: IconKey[];
 }
 
 export const Symbols = memo<SymbolsProps>(({ icons: keys = Object.keys(icons) }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
+  <Svg
+    viewBox="0 0 14 14"
     style={{ position: 'absolute', width: 0, height: 0 }}
     data-chromatic="ignore"
   >
     {keys.map((key: IconKey) => (
       <symbol id={`icon--${key}`} key={key}>
-        <Path d={icons[key]} />
+        {icons[key]}
       </symbol>
     ))}
-  </svg>
+  </Svg>
 ));
