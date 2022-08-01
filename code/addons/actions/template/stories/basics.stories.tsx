4dba874d5d0e4e463d62733b97474588ab017a1d
Tom Coleman <tom@chromatic.com>
2022-08-01 10:41:52 +1000

    Got "test-components" concept working

--- code/addons/actions/template/stories/basics.stories.tsx
+++ code/addons/actions/template/stories/basics.stories.tsx
@@ -1,17 +1,10 @@
-// TODO -- for now react, going to generalise
-import React, { FC } from 'react';
-
 import { action } from '@storybook/addon-actions';
 
-// TODO -- this needs to be a generic component
-const Button: FC<{ onClick: () => void }> = ({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-);
-
 export default {
-  component: Button,
+  component: 'Button',
+  args: {
+    children: 'Click Me!',
+  },
 };
 
 export const BasicExample = {
