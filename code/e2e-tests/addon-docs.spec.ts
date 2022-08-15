8e373f4b5495e83ba20515c13bc681485cbf405c
Michael Shilman <shilman@users.noreply.github.com>
2022-08-15 08:29:52 +0800

    E2E: Conditionally skip vue3 docs snippet tests

--- code/e2e-tests/addon-docs.spec.ts
+++ code/e2e-tests/addon-docs.spec.ts
@@ -1,19 +1,24 @@
+/* eslint-disable jest/no-disabled-tests */
 /* eslint-disable no-await-in-loop */
 import { test, expect } from '@playwright/test';
 import process from 'process';
 import { SbPage } from './util';
 
 const storybookUrl = process.env.STORYBOOK_URL || 'http://localhost:8001';
+const templateName = process.env.STORYBOOK_TEMPLATE_NAME || '';
 
 test.describe('addon-docs', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto(storybookUrl);
   });
 
-  // FIXME: skip html/vue3
   test('should provide source snippet', async ({ page }) => {
-    const sbPage = new SbPage(page);
+    test.skip(
+      /^vue3/.test(templateName),
+      `Skipping ${templateName}, which does not support dynamic source snippets`
+    );
 
+    const sbPage = new SbPage(page);
     await sbPage.navigateToStory('example-button', 'docs');
     const root = sbPage.previewRoot();
     const toggles = root.locator('.docblock-code-toggle');
