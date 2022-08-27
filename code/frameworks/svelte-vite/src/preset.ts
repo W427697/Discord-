46fe3ea3fb96b38aa1591d89d4799cd915d8aa9d
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-08-26 22:48:09 -0400

    Remove duplicate vue and svelte plugins
    It's expected that the original vite.config.js should have one, and we're using that now.

--- code/frameworks/svelte-vite/src/preset.ts
+++ code/frameworks/svelte-vite/src/preset.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import type { StorybookConfig } from '@storybook/builder-vite';
+import { svelteDocgen } from './plugins/svelte-docgen';
 
 export const addons: StorybookConfig['addons'] = ['@storybook/svelte'];
 
@@ -20,25 +21,7 @@ export function readPackageJson(): Record<string, any> | false {
 
 export const viteFinal: StorybookConfig['viteFinal'] = async (config, { presets }) => {
   const { plugins = [] } = config;
-  const svelteOptions = await presets.apply<Record<string, any>>('frameworkOptions');
 
-  // eslint-disable-next-line global-require
-  const { loadSvelteConfig } = require('@sveltejs/vite-plugin-svelte');
-  const csfConfig = { ...loadSvelteConfig(), ...svelteOptions };
-
-  try {
-    // eslint-disable-next-line global-require
-    const csfPlugin = require('./plugins/csf-plugin').default;
-    plugins.push(csfPlugin(csfConfig));
-  } catch (err) {
-    // Not all projects use `.stories.svelte` for stories, and by default 6.5+ does not auto-install @storybook/addon-svelte-csf.
-    // If it's any other kind of error, re-throw.
-    if ((err as NodeJS.ErrnoException).code !== 'MODULE_NOT_FOUND') {
-      throw err;
-    }
-  }
-
-  const { svelteDocgen } = await import('./plugins/svelte-docgen');
   plugins.push(svelteDocgen(config));
 
   return {
