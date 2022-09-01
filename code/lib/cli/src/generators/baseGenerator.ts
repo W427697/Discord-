f968c85fc25b527db2e1fcb814b3e64affa6c73c
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-09-01 16:33:00 -0400

    Don't add babel dependencies for vite projects

--- code/lib/cli/src/generators/baseGenerator.ts
+++ code/lib/cli/src/generators/baseGenerator.ts
@@ -221,7 +221,10 @@ export async function baseGenerator(
     await fse.writeFile(`.storybook/preview-head.html`, previewHead, { encoding: 'utf8' });
   }
 
-  const babelDependencies = addBabel ? await getBabelDependencies(packageManager, packageJson) : [];
+  const babelDependencies =
+    addBabel && builder !== CoreBuilder.Vite
+      ? await getBabelDependencies(packageManager, packageJson)
+      : [];
   const isNewFolder = !files.some(
     (fname) => fname.startsWith('.babel') || fname.startsWith('babel') || fname === 'package.json'
   );
