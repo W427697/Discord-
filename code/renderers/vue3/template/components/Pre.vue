2f9373d8794d1f4bb7f5566115d4e183a805e834
Tom Coleman <tom@chromatic.com>
2022-09-05 17:08:08 +1000

    Add Pre components to svelte and vue3

---
+++ code/renderers/vue3/template/components/Pre.vue
@@ -0,0 +1,31 @@
+<template>
+  <pre data-testid="pre" :style="style">{{ text }}</pre>
+</template>
+
+<script>
+import { reactive, computed } from 'vue';
+
+export default {
+  name: 'my-pre',
+
+  props: {
+    style: {
+      type: Object,
+    },
+    object: {
+      type: Object,
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+
+  setup(props, { emit }) {
+    props = reactive(props);
+    return {
+      text: computed(() => (props.object ? JSON.stringify(props.object, null, 2) : props.text)),
+    };
+  },
+};
+</script>
