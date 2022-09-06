9bfbb871f6a88f9e3f0eda77723d0f63d1c3059b
Tom Coleman <tom@chromatic.com>
2022-09-06 14:48:07 +1000

    Added hooks stories

---
+++ code/lib/store/template/stories/hooks.stories.ts
@@ -0,0 +1,53 @@
+import globalThis from 'global';
+import { PartialStoryFn, PlayFunctionContext, StoryContext } from '@storybook/csf';
+import { useEffect, useState } from '@storybook/client-api';
+import { within, userEvent } from '@storybook/testing-library';
+
+export default {
+  component: globalThis.Components.Button,
+};
+
+export const UseState = {
+  decorators: [
+    (story: PartialStoryFn) => {
+      const [count, setCount] = useState(0);
+
+      return story({
+        args: {
+          children: `Clicked ${count} times`,
+          onClick: () => {
+            setCount(count + 1);
+          },
+        },
+      });
+    },
+  ],
+  play: async ({ canvasElement }: PlayFunctionContext) => {
+    const button = await within(canvasElement).findByText('Clicked 0 times', {}, { timeout: 3000 });
+
+    await userEvent.click(button);
+    await within(canvasElement).findByText('Clicked 1 times', {}, { timeout: 3000 });
+  },
+};
+
+// NOTE: it isn't possible to write a play function for this story, as the
+// useEffect hooked doesn't fire until *after* the story has rendered, which includes
+// the play function running.
+export const UseEffect = {
+  decorators: [
+    (story: PartialStoryFn) => {
+      const [count, setCount] = useState(0);
+
+      useEffect(() => {
+        setCount(1);
+      }, []);
+
+      return story({
+        args: {
+          children: count > 0 ? `useEffect worked!` : `useEffect hasn't worked yet!`,
+          onClick: () => {},
+        },
+      });
+    },
+  ],
+};
