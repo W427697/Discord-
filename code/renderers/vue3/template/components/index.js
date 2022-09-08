3c33ab04d4f509ef0ad2627373a597cff83d8be8
Norbert de Langen <ndelangen@me.com>
2022-09-08 12:11:33 +0200

    Add a Html component for vue3

--- code/renderers/vue3/template/components/index.js
+++ code/renderers/vue3/template/components/index.js
@@ -1,5 +1,6 @@
 import globalThis from 'global';
 
 import Button from './Button.vue';
+import Html from './Html.vue';
 
-globalThis.Components = { Button };
+globalThis.Components = { Button, Html };
