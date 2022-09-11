599efbd270a85fe75226725c4f9da6971c74433b
Michael Shilman <shilman@users.noreply.github.com>
2022-09-11 17:44:27 +0800

    Toolbars: Improved example story

--- code/addons/toolbars/template/stories/globals.stories.ts
+++ code/addons/toolbars/template/stories/globals.stories.ts
@@ -1,11 +1,32 @@
 import globalThis from 'global';
 import { PartialStoryFn, StoryContext } from '@storybook/csf';
 
+const greetingForLocale = (locale: string) => {
+  switch (locale) {
+    case 'es':
+      return 'Hola!';
+    case 'fr':
+      return 'Bonjour !';
+    case 'zh':
+      return '你好!';
+    case 'kr':
+      return '안녕하세요!';
+    case 'en':
+    default:
+      return 'Hello';
+  }
+};
+
 export default {
   component: null,
   decorators: [
-    (storyFn: PartialStoryFn, context: StoryContext) =>
-      storyFn({ component: globalThis.Components.Pre, args: { object: context.globals } }),
+    (storyFn: PartialStoryFn, { globals }: StoryContext) => {
+      const object = {
+        ...globals,
+        caption: `Locale is '${globals.locale}', so I say: ${greetingForLocale(globals.locale)}`,
+      };
+      return storyFn({ component: globalThis.Components.Pre, args: { object: globals } });
+    },
   ],
 };
 
