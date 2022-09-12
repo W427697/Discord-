646077fccb6c968b52869ad139d06d05feb110f7
Tom Coleman <tom@chromatic.com>
2022-09-12 16:47:38 +1000

    Implement form in vue+svelte

--- code/renderers/svelte/template/components/index.js
+++ code/renderers/svelte/template/components/index.js
@@ -2,5 +2,6 @@ import globalThis from 'global';
 
 import Button from './Button.svelte';
 import Pre from './Pre.svelte';
+import Form from './Form.svelte';
 
-globalThis.Components = { Button, Pre };
+globalThis.Components = { Button, Pre, Form };
--- code/renderers/vue3/template/components/index.js
+++ code/renderers/vue3/template/components/index.js
@@ -2,5 +2,6 @@ import globalThis from 'global';
 
 import Button from './Button.vue';
 import Pre from './Pre.vue';
+import Form from './Form.vue';
 
-globalThis.Components = { Button, Pre };
+globalThis.Components = { Button, Pre, Form };
