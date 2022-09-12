06d885a249d98a98b43ff4750e18ee14f18dd375
Michael Shilman <shilman@users.noreply.github.com>
2022-09-12 09:54:18 +0800

    Vue2: Add template components for sandbox

---
+++ code/renderers/vue/template/components/Pre.vue
@@ -0,0 +1,29 @@
+<template>
+  <pre data-testid="pre" :style="style">{{ finalText }}</pre>
+</template>
+
+<script>
+export default {
+  name: 'my-pre',
+
+  props: {
+    // deepscan-disable-next-line
+    style: {
+      type: Object,
+    },
+    object: {
+      type: Object,
+    },
+    text: {
+      type: String,
+      default: '',
+    },
+  },
+
+  computed: {
+    finalText() {
+      return props.object ? JSON.stringify(props.object, null, 2) : props.text;
+    },
+  },
+};
+</script>
