3dbf5f69002b1ed0b44b5183cefec1f90df5ceef
Michael Shilman <shilman@users.noreply.github.com>
2022-09-13 15:08:43 +0800

    CLI/Vue3: Upgrade vue-loader and install `@vue/compiler-sfc`

--- code/lib/cli/src/generators/VUE3/index.ts
+++ code/lib/cli/src/generators/VUE3/index.ts
@@ -3,7 +3,7 @@ import { Generator } from '../types';
 
 const generator: Generator = async (packageManager, npmOptions, options) => {
   await baseGenerator(packageManager, npmOptions, options, 'vue3', {
-    extraPackages: ['vue-loader@^16.0.0'],
+    extraPackages: ['vue-loader@^17.0.0', '@vue/compiler-sfc@^3.2.0'],
   });
 };
 
