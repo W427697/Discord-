48e3af9778b36b930fee4aafb912e0cfb69f6658
Michael Shilman <shilman@users.noreply.github.com>
2022-09-15 00:05:34 +0800

    Vue: Add Form/Html template components

--- code/renderers/vue/template/components/index.js
+++ code/renderers/vue/template/components/index.js
@@ -2,5 +2,7 @@ import globalThis from 'global';
 
 import Button from './Button.vue';
 import Pre from './Pre.vue';
+import Form from './Form.vue';
+import Html from './Html.vue';
 
-globalThis.Components = { Button, Pre };
+globalThis.Components = { Button, Pre, Form, Html };
