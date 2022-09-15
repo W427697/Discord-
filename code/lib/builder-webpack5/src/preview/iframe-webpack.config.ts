127544bee9bb6cb456937b3cc3c06e5fef067b60
Jeppe Reinhold <jeppe@reinhold.is>
2022-09-15 13:52:19 +0200

    ignore missing types

--- code/lib/builder-webpack5/src/preview/iframe-webpack.config.ts
+++ code/lib/builder-webpack5/src/preview/iframe-webpack.config.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import { DefinePlugin, HotModuleReplacementPlugin, ProgressPlugin, ProvidePlugin } from 'webpack';
 import type { Configuration } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-// @ts-ignore
 import CaseSensitivePathsPlugin from 'case-sensitive-paths-webpack-plugin';
 import TerserWebpackPlugin from 'terser-webpack-plugin';
 import VirtualModulePlugin from 'webpack-virtual-modules';
