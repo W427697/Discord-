485758861453a132b375fb268106d98835fa3287
Michael Shilman <shilman@users.noreply.github.com>
2022-09-18 01:11:55 +0800

    Scripts: Prefix junit into base task runner

--- scripts/tasks/chromatic.ts
+++ scripts/tasks/chromatic.ts
@@ -1,4 +1,3 @@
-import fs from 'fs-extra';
 import type { Task } from '../task';
 import { exec } from '../utils/exec';
 
@@ -11,22 +10,15 @@ export const chromatic: Task = {
   async run(templateKey, { sandboxDir, builtSandboxDir, junitFilename }) {
     const tokenEnvVarName = `CHROMATIC_TOKEN_${templateKey.toUpperCase().replace(/\/|-/g, '_')}`;
     const token = process.env[tokenEnvVarName];
-    try {
-      await exec(
-        `npx chromatic \
+
+    await exec(
+      `npx chromatic \
           --exit-zero-on-changes \
           --storybook-build-dir=${builtSandboxDir} \
           --junit-report=${junitFilename} \
           --projectToken=${token}`,
-        { cwd: sandboxDir },
-        { debug: true }
-      );
-    } finally {
-      if (fs.existsSync(junitFilename)) {
-        const junitXml = await (await fs.readFile(junitFilename)).toString();
-        const prefixedXml = junitXml.replace(/classname="(.*)"/g, `classname="${templateKey} $1"`);
-        await fs.writeFile(junitFilename, prefixedXml);
-      }
-    }
+      { cwd: sandboxDir },
+      { debug: true }
+    );
   },
 };
