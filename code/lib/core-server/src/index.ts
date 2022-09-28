2f20085b4512a75ecf2f385ef5f0f3922425f237
Norbert de Langen <ndelangen@me.com>
2022-09-28 13:51:26 +0200

    fix

--- code/lib/core-server/src/index.ts
+++ code/lib/core-server/src/index.ts
@@ -1,6 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./typings.d.ts" />
 
+//
+
 export {
   getPreviewHeadTemplate,
   getPreviewBodyTemplate,
