ceaf69b15f1c85e9aea3c017d84a8de7aa2138ed
Jeppe Reinhold <jeppe@reinhold.is>
2022-10-04 00:51:03 +0200

    allow root dir in vite-builder

--- code/lib/builder-vite/src/vite-config.ts
+++ code/lib/builder-vite/src/vite-config.ts
@@ -88,6 +88,19 @@ export async function pluginConfig(options: ExtendedOptions) {
     mdxPlugin(options),
     injectExportOrderPlugin,
     stripStoryHMRBoundary(),
+    {
+      name: 'vite-plugin-storybook-allow-root-dir',
+      enforce: 'post',
+      config(config) {
+        // if there is NO allow list then Vite allows anything in the root directory
+        // if there is an allow list then Vite only allows anything in the listed directories
+        // add the root directory only if there's an allow list so that we don't end up
+        // disallowing the other directories unless root is already disallowed
+        if (config?.server?.fs?.allow) {
+          config.server.fs.allow.push('.');
+        }
+      },
+    },
   ] as PluginOption[];
 
   // We need the react plugin here to support MDX in non-react projects.
