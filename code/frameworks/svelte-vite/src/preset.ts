f205c1c72bf1ebb3c6f1126931e460b8d08c6648
Jeppe Reinhold <jeppe@reinhold.is>
2022-10-04 01:16:33 +0200

    remove vite-plugin-svelte-kit in config

--- code/frameworks/svelte-vite/src/preset.ts
+++ code/frameworks/svelte-vite/src/preset.ts
@@ -1,6 +1,7 @@
 import path from 'path';
 import fs from 'fs';
 import type { StorybookConfig } from '@storybook/builder-vite';
+import { PluginOption } from 'vite';
 import { svelteDocgen } from './plugins/svelte-docgen';
 
 export const addons: StorybookConfig['addons'] = ['@storybook/svelte'];
@@ -24,8 +25,23 @@ export const viteFinal: StorybookConfig['viteFinal'] = async (config, { presets 
 
   plugins.push(svelteDocgen(config));
 
+  removeSvelteKitPlugin(plugins);
+
   return {
     ...config,
     plugins,
   };
+};
+
+const removeSvelteKitPlugin = (plugins: PluginOption[]) => {
+  plugins.forEach((plugin, index) => {
+    if (plugin && 'name' in plugin && plugin.name === 'vite-plugin-svelte-kit') {
+      // eslint-disable-next-line no-param-reassign -- we explicitly want to mutate the array as stated in Vite docs
+      plugins[index] = undefined;
+    }
+    if (Array.isArray(plugin)) {
+      // recursive, Vite plugins can be nested
+      removeSvelteKitPlugin(plugin);
+    }
+  });
 };
