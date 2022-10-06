fc38e731eb4b99f138d869675842199c977aa6ec
Norbert de Langen <ndelangen@me.com>
2022-10-06 11:38:41 +0200

    flatten the files esbuild outputs, to make loading addons in the manager more robust

--- code/lib/builder-manager/src/utils/files.ts
+++ code/lib/builder-manager/src/utils/files.ts
@@ -1,11 +1,20 @@
 import { writeFile, ensureFile } from 'fs-extra';
+import { join } from 'path';
 import { compilation } from '../index';
 
 export async function readOrderedFiles(addonsDir: string) {
   const files = await Promise.all(
     compilation?.outputFiles?.map(async (file) => {
-      await ensureFile(file.path).then(() => writeFile(file.path, file.contents));
-      return file.path.replace(addonsDir, './sb-addons');
+      // convert deeply nested paths to a single level, also remove special characters
+      const filePath = file.path
+        .replace(addonsDir, '')
+        .replace(/[^a-z0-9\-.]+/g, '-')
+        .replace(/^-/, '/');
+      const location = join(addonsDir, filePath);
+      const url = `./sb-addons${filePath}`;
+
+      await ensureFile(location).then(() => writeFile(location, file.contents));
+      return url;
     }) || []
   );
 
