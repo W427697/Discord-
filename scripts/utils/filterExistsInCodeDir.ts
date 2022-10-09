da590c7a78046d2786cc8e6e58bee4350729f617
Tom Coleman <tom@chromatic.com>
2022-10-10 10:23:07 +1100

    Remove spurious log

--- scripts/utils/filterExistsInCodeDir.ts
+++ scripts/utils/filterExistsInCodeDir.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import { pathExists } from 'fs-extra';
 
 export const codeDir = path.resolve(__dirname, '../../code');
-console.log(codeDir);
 
 // packageDirs of the form `lib/store`
 // paths to check of the form 'template/stories'
