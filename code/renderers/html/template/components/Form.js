a95bb2c1103c89b098ffaddf90fcff783fe53f18
Norbert de Langen <ndelangen@me.com>
2022-10-10 20:28:18 +0200

    fix

--- code/renderers/html/template/components/Form.js
+++ code/renderers/html/template/components/Form.js
@@ -1,27 +1,32 @@
 /* eslint-disable no-undef */
-export const Form = ({ complete, value, setValue, onSubmit }) => {
+export const Form = ({ onSuccess }) => {
   const container = document.createElement('div');
 
-  container.innerHTML = `
-    <form id="interaction-test-form">
-      <label>
-        Enter Value
-        <input type="text" data-testid="value" required />
-      </label>
-      <button type="submit">Submit</button>
-      ${complete ? '<p>Completed!!</p>' : ''}
-    </form>
-  `;
+  const getInnerHTML = ({ complete }) => `
+  <form id="interaction-test-form">
+    <label>
+      Enter Value
+      <input type="text" data-testid="value" required />
+    </label>
+    <button type="submit">Submit</button>
+    ${complete ? '<p>Completed!!</p>' : ''}
+  </form>
+`;
+
+  container.innerHTML = getInnerHTML({ complete: false });
 
   const form = container.querySelector('form');
-  form.onSubmit = (e) => {
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    onSubmit(e);
-  };
 
-  const input = container.querySelector('[data-testid="value"]');
-  input.value = value;
-  input.onInput = (e) => setValue(e.target.value);
+    setTimeout(() => {
+      container.innerHTML = getInnerHTML({ complete: true });
+    }, 500);
+    setTimeout(() => {
+      container.innerHTML = getInnerHTML({ complete: false });
+    }, 1500);
+    onSuccess(e);
+  });
 
   return container;
 };
