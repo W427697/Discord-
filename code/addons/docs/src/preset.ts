58af9712b68cd465dd1e02da3bb164dcece9c4d5
Tom Coleman <tom@chromatic.com>
2022-10-11 21:27:28 +1100

    WIP

--- code/addons/docs/src/preset.ts
+++ code/addons/docs/src/preset.ts
@@ -17,11 +17,23 @@ type BabelParams = {
 };
 function createBabelOptions({ babelOptions, mdxBabelOptions, configureJSX }: BabelParams) {
   const babelPlugins = mdxBabelOptions?.plugins || babelOptions?.plugins || [];
+
+  const filteredBabelPlugins = babelPlugins.filter((p: any) => {
+    const name = Array.isArray(p) ? p[0] : p;
+    if (typeof name === 'string') {
+      return !name.includes('plugin-transform-react-jsx');
+    }
+    return true;
+  });
+
+  console.log(babelPlugins);
+  console.log(filteredBabelPlugins);
+
   const jsxPlugin = [
     require.resolve('@babel/plugin-transform-react-jsx'),
     { pragma: 'React.createElement', pragmaFrag: 'React.Fragment' },
   ];
-  const plugins = configureJSX ? [...babelPlugins, jsxPlugin] : babelPlugins;
+  const plugins = configureJSX ? [...filteredBabelPlugins, jsxPlugin] : babelPlugins;
   return {
     // don't use the root babelrc by default (users can override this in mdxBabelOptions)
     babelrc: false,
