12ac82526bd976075269f9584765704ebfbc3c2d
Tom Coleman <tom@chromatic.com>
2022-10-11 15:15:43 +1100

    Update test-runner.ts

--- scripts/tasks/test-runner.ts
+++ scripts/tasks/test-runner.ts
@@ -15,6 +15,8 @@ export const testRunner: Task = {
     // Using a fixed version to work around core-js problems
     await exec(`yarn add --dev @storybook/test-runner@0.8.1--canary.202.99d82aa.0`, execOptions);
 
+    console.log(await exact(`yarn why core-js`));
+    
     await exec(
       `yarn test-storybook --url http://localhost:${PORT} --junit --index-json`,
       {
