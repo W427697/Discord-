52c64987a5162b6960350e3d4da7d1eaafc6365b
Jeppe Reinhold <jeppe@reinhold.is>
2022-10-20 15:12:32 +0200

    migrate TooltipNote stories to CSF3

--- code/ui/components/src/tooltip/TooltipNote.stories.tsx
+++ code/ui/components/src/tooltip/TooltipNote.stories.tsx
@@ -1,15 +1,33 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
 import { WithTooltip } from './WithTooltip';
-
 import { TooltipNote } from './TooltipNote';
 
-storiesOf('basics/Tooltip/TooltipNote', module)
-  .addDecorator((storyFn) => (
-    <div style={{ height: '300px' }}>
-      <WithTooltip hasChrome={false} placement="top" trigger="click" startOpen tooltip={storyFn()}>
-        <div>Tooltip</div>
-      </WithTooltip>
-    </div>
-  ))
-  .add('default', () => <TooltipNote note="Lorem ipsum dolor" />);
+export default {
+  title: 'Basics/Tooltip/TooltipNote',
+  component: TooltipNote,
+  decorators: [
+    (storyFn) => (
+      <div
+        style={{
+          height: '300px',
+        }}
+      >
+        <WithTooltip
+          hasChrome={false}
+          placement="top"
+          trigger="click"
+          startOpen
+          tooltip={storyFn()}
+        >
+          <div>Tooltip</div>
+        </WithTooltip>
+      </div>
+    ),
+  ],
+};
+
+export const Default = {
+  args: {
+    note: 'This is a note',
+  },
+};
