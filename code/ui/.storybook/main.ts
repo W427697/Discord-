2bc1d71abb5b2379d32051c9bc83b3aa4928be55
Jeppe Reinhold <jeppe@reinhold.is>
2022-10-24 12:54:52 +0200

    remove custom viteFinal

--- code/ui/.storybook/main.ts
+++ code/ui/.storybook/main.ts
@@ -1,4 +1,4 @@
-import vitePluginReact from '@vitejs/plugin-react';
+// import vitePluginReact from '@vitejs/plugin-react';
 import type { PluginOption } from 'vite';
 import type { StorybookConfig } from '../../frameworks/react-vite/dist';
 
@@ -14,9 +14,6 @@ const config: StorybookConfig = {
       files: '*.stories.@(js|jsx|ts|tsx|mdx)',
       titlePrefix: 'Components',
     },
-    // '../components/src/**/*.stories.@(js|jsx|ts|tsx|mdx)',
-
-    // '../../../addons/interactions/**/*.stories.@(ts|tsx|js|jsx|mdx)',
   ],
   addons: [
     '@storybook/addon-links',
@@ -27,42 +24,6 @@ const config: StorybookConfig = {
     name: '@storybook/react-vite',
     options: {},
   },
-  viteFinal: (config) => {
-    return {
-      ...config,
-      optimizeDeps: {
-        ...config.optimizeDeps,
-        include: [
-          ...(config.optimizeDeps?.include ?? []),
-          'react-element-to-jsx-string',
-          'core-js/modules/es.regexp.flags.js',
-          'react-colorful',
-        ],
-      },
-      /*
-      This might look complex but all we're doing is removing the default set of React Vite plugins
-      and adding them back in, but with the `jsxRuntime: 'classic'` option.
-      TODO: When we've upgraded to React 18 all of this shouldn't be necessary anymore
-      */
-      plugins: [...withoutReactPlugins(config.plugins), vitePluginReact({ jsxRuntime: 'classic' })],
-    };
-  },
 };
-
-// recursively remove all plugins from the React Vite plugin
-const withoutReactPlugins = (plugins: PluginOption[] = []) =>
-  plugins.map((plugin) => {
-    if (Array.isArray(plugin)) {
-      return withoutReactPlugins(plugin);
-    }
-    if (
-      plugin &&
-      'name' in plugin &&
-      ['vite:react-jsx', 'vite:react-babel', 'vite:react-refresh'].includes(plugin.name)
-    ) {
-      return false;
-    }
-    return plugin;
-  });
 
 export default config;
