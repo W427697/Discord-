824c6821b9e472509c9fba791011679a035c06a8
Tom Coleman <tom@chromatic.com>
2022-10-27 19:30:11 +1100

    Replace use of CSF in template stories

--- code/addons/toolbars/template/stories/globals.stories.ts
+++ code/addons/toolbars/template/stories/globals.stories.ts
@@ -1,5 +1,5 @@
 import globalThis from 'global';
-import { PartialStoryFn, StoryContext } from '@storybook/csf';
+import { PartialStoryFn, StoryContext } from '@storybook/types';
 
 const greetingForLocale = (locale: string) => {
   switch (locale) {
