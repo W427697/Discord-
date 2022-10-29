a1ca59dabd16cce830f4f7099c5262c16ae50203
Michael Shilman <shilman@users.noreply.github.com>
2022-10-29 12:02:45 +0800

    Fix linting errors

--- code/lib/csf-tools/src/enrichCsf.ts
+++ code/lib/csf-tools/src/enrichCsf.ts
@@ -1,14 +1,16 @@
 /* eslint-disable no-underscore-dangle */
 import * as t from '@babel/types';
-import generate from '@babel/generator';
-import template from '@babel/template';
-import { CsfFile, formatCsf, loadCsf } from './CsfFile';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as generate from '@babel/generator';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import * as template from '@babel/template';
+import type { CsfFile } from './CsfFile';
 
 export const enrichCsf = (csf: CsfFile) => {
   Object.keys(csf._storyExports).forEach((key) => {
     const storyExport = csf.getStoryExport(key);
     const source = extractSource(storyExport);
-    const addParameter = template(`
+    const addParameter = template.default(`
        %%key%%.parameters = { storySource: { source: %%source%% }, ...%%key%%.parameters };
     `)({
       key: t.identifier(key),
@@ -20,6 +22,6 @@ export const enrichCsf = (csf: CsfFile) => {
 
 export const extractSource = (node: t.Node) => {
   const src = t.isVariableDeclarator(node) ? node.init : node;
-  const { code } = generate(src, {});
+  const { code } = generate.default(src, {});
   return code;
 };
