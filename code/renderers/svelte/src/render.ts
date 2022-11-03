fcfc9f3ea391da748c8651555a7644f8295d51de
Jeppe Reinhold <jeppe@reinhold.is>
2022-11-03 14:21:36 +0100

    use domElement as key instead of id

--- code/renderers/svelte/src/render.ts
+++ code/renderers/svelte/src/render.ts
@@ -1,4 +1,3 @@
-import global from 'global';
 import type { Store_RenderContext, ArgsStoryFn } from '@storybook/types';
 import type { SvelteComponentTyped } from 'svelte';
 // eslint-disable-next-line import/no-extraneous-dependencies
@@ -6,25 +5,18 @@ import PreviewRender from '@storybook/svelte/templates/PreviewRender.svelte';
 
 import type { SvelteFramework } from './types';
 
-const componentsByDomElementKey = new Map<string, SvelteComponentTyped>();
+const componentsByDomElement = new Map<Element, SvelteComponentTyped>();
 
-const STORYBOOK_ROOT_ID = 'storybook-root';
-
-function teardown(domElementKey: string) {
-  if (!componentsByDomElementKey.has(domElementKey)) {
+function teardown(domElement: Element) {
+  if (!componentsByDomElement.has(domElement)) {
     return;
   }
   // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- we know it exists because we just checked
-  componentsByDomElementKey.get(domElementKey)!.$destroy();
+  componentsByDomElement.get(domElement)!.$destroy();
 
-  // in docs mode, the element we render to is a child of the element with the story id
-  // TODO: this feels brittle. If we ever change the structure in the <Story /> component, this will break
-  const rootElement =
-    domElementKey === STORYBOOK_ROOT_ID
-      ? global.document.getElementById(domElementKey)
-      : global.document.getElementById(domElementKey).children[0];
-  rootElement.innerHTML = '';
-  componentsByDomElementKey.delete(domElementKey);
+  // eslint-disable-next-line no-param-reassign -- this is on purpose
+  domElement.innerHTML = '';
+  componentsByDomElement.delete(domElement);
 }
 
 export function renderToDOM(
@@ -32,7 +24,6 @@ export function renderToDOM(
     storyFn,
     kind,
     name,
-    id,
     showMain,
     showError,
     storyContext,
@@ -40,13 +31,10 @@ export function renderToDOM(
   }: Store_RenderContext<SvelteFramework>,
   domElement: Element
 ) {
-  // in docs mode we're rendering multiple stories to the DOM, so we need to key by the story id
-  const domElementKey = storyContext.viewMode === 'docs' ? id : STORYBOOK_ROOT_ID;
-
-  const existingComponent = componentsByDomElementKey.get(domElementKey);
+  const existingComponent = componentsByDomElement.get(domElement);
 
   if (forceRemount) {
-    teardown(domElementKey);
+    teardown(domElement);
   }
 
   if (!existingComponent || forceRemount) {
@@ -60,7 +48,7 @@ export function renderToDOM(
         showError,
       },
     }) as SvelteComponentTyped;
-    componentsByDomElementKey.set(domElementKey, createdComponent);
+    componentsByDomElement.set(domElement, createdComponent);
   } else {
     existingComponent.$set({
       storyFn,
@@ -75,7 +63,7 @@ export function renderToDOM(
 
   // teardown the component when the story changes
   return () => {
-    teardown(domElementKey);
+    teardown(domElement);
   };
 }
 
