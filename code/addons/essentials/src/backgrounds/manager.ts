aaa4a210e08687cd81a9b29a84ee3d932bd69d55
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-11-03 16:11:59 -0400

    Re-export all addons from addon-essentials

---
+++ code/addons/essentials/src/backgrounds/manager.ts
@@ -0,0 +1,5 @@
+/* eslint-disable import/export */
+// TODO: We need to configure an eslint-import typescript resolver for export maps to be considered
+
+// @ts-expect-error The generated definition file is empty. https://github.com/egoist/tsup/issues/762
+export * from '@storybook/addon-backgrounds/manager';
--- code/addons/essentials/src/typings.d.ts
+++ code/addons/essentials/src/typings.d.ts
@@ -2,3 +2,4 @@ declare module 'fs';
 
 // TODO: Remove this once addon-docs is generated with tsup
 declare module '@storybook/addon-docs/preview';
+declare module '@storybook/addon-docs/preset';
