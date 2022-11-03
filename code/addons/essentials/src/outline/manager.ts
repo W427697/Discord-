e7dc1c3340c91fe986d223efc0c14ecdb5937032
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-11-03 16:33:13 -0400

    Remove unused eslint disable directives

--- code/addons/essentials/src/outline/manager.ts
+++ code/addons/essentials/src/outline/manager.ts
@@ -1,5 +1,2 @@
-/* eslint-disable import/export */
-// TODO: We need to configure an eslint-import typescript resolver for export maps to be considered
-
 // @ts-expect-error The generated definition file is empty. https://github.com/egoist/tsup/issues/762
 export * from '@storybook/addon-outline/manager';
