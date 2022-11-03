44028bd0d5f9db1c9d855b20753f23a5c91fe0f4
Jeppe Reinhold <jeppe@reinhold.is>
2022-11-03 22:30:21 +0100

    cleanup

--- code/ui/blocks/src/controls/options/CheckOptions.stories.tsx
+++ code/ui/blocks/src/controls/options/CheckOptions.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useArgs } from '@storybook/addons';
 import type { Meta, StoryObj } from '@storybook/react';
 import { OptionsControl } from './Options';
