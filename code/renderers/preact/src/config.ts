36515e65b843b32706116f4b84ad9bf939bfcd31
Kasper Peulen <kasperpeulen@gmail.com>
2022-11-03 13:35:12 +0100

    docs.inlineStories to true for preact as well

--- code/renderers/preact/src/config.ts
+++ code/renderers/preact/src/config.ts
@@ -1,3 +1,5 @@
+import { parameters as docsParams } from './docs/config';
+
 export { renderToDOM, render } from './render';
 
-export const parameters = { framework: 'preact' as const };
+export const parameters = { framework: 'preact' as const, ...docsParams };
