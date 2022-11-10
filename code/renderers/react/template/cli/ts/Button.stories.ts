f6790df49dc8054c6402669959f52b161ca695b0
Kasper Peulen <kasperpeulen@gmail.com>
2022-11-10 16:06:19 +0100

    Add Todos

--- code/renderers/react/template/cli/ts/Button.stories.ts
+++ code/renderers/react/template/cli/ts/Button.stories.ts
@@ -2,13 +2,16 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+// TODO
+// 1. Only one comment about args <- For sure
+// 2. Only one comment above meta
+// 3. And change links to point to 7.0
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
-  // This component will have an automatically generated docsPage entry: https://storybook.js.org/docs/react/writing-docs/docs-page
   tags: ['docsPage'],
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
     backgroundColor: { control: 'color' },
   },
@@ -17,9 +20,8 @@ const meta: Meta<typeof Button> = {
 export default meta;
 type Story = StoryObj<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary: Story = {
-  // More on args: https://storybook.js.org/docs/react/writing-stories/args
   args: {
     primary: true,
     label: 'Button',
