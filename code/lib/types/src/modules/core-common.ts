59ac6d804bd800538c2c633b2c69b57bf4baa5c4
Tom Coleman <tom@chromatic.com>
2022-11-10 14:57:54 +1100

    Update comment to be accurate

--- code/lib/types/src/modules/core-common.ts
+++ code/lib/types/src/modules/core-common.ts
@@ -252,7 +252,7 @@ interface CoreCommon_StoriesSpecifier {
   /**
    * What does the filename of a story file look like?
    * (a glob, relative to directory, no leading `./`)
-   * If unset, we use `** / *.stories.@(mdx|tsx|ts|jsx|js)` (no spaces)
+   * If unset, we use `** / *.@(mdx|stories.@(mdx|tsx|ts|jsx|js))` (no spaces)
    */
   files?: string;
 }
