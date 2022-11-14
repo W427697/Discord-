2a5f3c3fac2f3eba0fe25cac28409b2f0d3c9587
Yann Braga <yannbf@gmail.com>
2022-11-14 09:21:45 +0100

    redo failure

--- scripts/tasks/build.ts
+++ scripts/tasks/build.ts
@@ -9,6 +9,10 @@ export const build: Task = {
     return pathExists(builtSandboxDir);
   },
   async run({ sandboxDir }, { dryRun, debug }) {
+    const result = 1 + 1 === 2;
+    if (result === true) {
+      throw new Error('Oh no!');
+    }
     return exec(`yarn build-storybook --quiet`, { cwd: sandboxDir }, { dryRun, debug });
   },
 };
