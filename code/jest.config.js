7d4801aecbad6d64081179551747ac3df508ce44
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-11-15 23:35:06 -0500

    Disable vue (both 2 and 3) tests for now

--- code/jest.config.js
+++ code/jest.config.js
@@ -4,7 +4,6 @@ module.exports = {
     '<rootDir>/frameworks/!(angular)*',
     '<rootDir>/lib/*',
     '<rootDir>/renderers/!(vue)*',
-    '<rootDir>/renderers/vue3',
     '<rootDir>/ui/*',
   ],
 };
