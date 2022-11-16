08ba91944028d79dbf99e736c5b8a8dffaa35360
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-11-16 12:16:09 -0500

    Re-enable vue tests

--- code/jest.config.js
+++ code/jest.config.js
@@ -3,7 +3,7 @@ module.exports = {
     '<rootDir>/addons/*',
     '<rootDir>/frameworks/!(angular)*',
     '<rootDir>/lib/*',
-    '<rootDir>/renderers/!(vue)*',
+    '<rootDir>/renderers/*',
     '<rootDir>/ui/*',
   ],
 };
