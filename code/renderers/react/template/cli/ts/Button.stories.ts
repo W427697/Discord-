a80620f38d27d77a0e560ff142dc0859e9792bc7
Kasper Peulen <kasperpeulen@gmail.com>
2022-11-17 11:51:50 +0100

    Remove todos

--- code/renderers/react/template/cli/ts/Button.stories.ts
+++ code/renderers/react/template/cli/ts/Button.stories.ts
@@ -2,11 +2,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { Button } from './Button';
 
-// TODO
-// 1. Only one comment about args <- For sure
-// 2. Only one comment above meta
-// 3. And change links to point to 7.0
-
 // More on how to set up stories at: https://storybook.js.org/docs/7.0/react/writing-stories/introduction
 const meta: Meta<typeof Button> = {
   title: 'Example/Button',
