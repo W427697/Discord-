342745bc8514f2978346a6bdb2656395f3fe0cad
Yann Braga <yannbf@gmail.com>
2022-11-18 09:42:37 +0100

    experimental change that should trigger tests for vite templates

--- code/lib/builder-vite/src/vite-config.ts
+++ code/lib/builder-vite/src/vite-config.ts
@@ -24,13 +24,13 @@ export type PluginConfigType = 'build' | 'development';
 const configEnvServe: ConfigEnv = {
   mode: 'development',
   command: 'serve',
-  ssrBuild: false,
+  ssrBuild: true,
 };
 
 const configEnvBuild: ConfigEnv = {
   mode: 'production',
   command: 'build',
-  ssrBuild: false,
+  ssrBuild: true,
 };
 
 // Vite config that is common to development and production mode
