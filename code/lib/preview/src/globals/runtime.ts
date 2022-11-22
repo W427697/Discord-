f1c0ce9c62ac653b964eb3ab3db9fb21fe479dd5
Norbert de Langen <ndelangen@me.com>
2022-11-22 16:51:42 +0100

    fixes

--- code/lib/preview/src/globals/runtime.ts
+++ code/lib/preview/src/globals/runtime.ts
@@ -1,3 +1,4 @@
+/* eslint-disable import/namespace */
 import * as ADDONS from '../modules/addons';
 import * as CHANNEL_POSTMESSAGE from '../modules/channel-postmessage';
 import * as CHANNEL_WEBSOCKET from '../modules/channel-websocket';
@@ -6,6 +7,7 @@ import * as CLIENT_API from '../modules/client-api';
 import * as CLIENT_LOGGER from '../modules/client-logger';
 import * as CORE_CLIENT from '../modules/core-client';
 import * as CORE_EVENTS from '../modules/core-events';
+import * as PREVIEW_API from '../modules/preview-api';
 import * as PREVIEW_WEB from '../modules/preview-web';
 import * as STORE from '../modules/store';
 
@@ -21,6 +23,7 @@ export const values: Required<Record<keyof typeof Keys, any>> = {
   '@storybook/client-logger': CLIENT_LOGGER,
   '@storybook/core-client': CORE_CLIENT,
   '@storybook/core-events': CORE_EVENTS,
+  '@storybook/preview-api': PREVIEW_API,
   '@storybook/preview-web': PREVIEW_WEB,
   '@storybook/store': STORE,
 };
