aef13af799f80bced38dab4e8c8190dff12f7fef
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-11-27 19:40:04 -0500

    Simplify preview globals

--- code/lib/builder-vite/src/vite-config.ts
+++ code/lib/builder-vite/src/vite-config.ts
@@ -10,6 +10,7 @@ import type {
 import viteReact from '@vitejs/plugin-react';
 import externalGlobals from 'rollup-plugin-external-globals';
 import { isPreservingSymlinks, getFrameworkName } from '@storybook/core-common';
+import { globals } from '@storybook/preview/globals';
 import {
   codeGeneratorPlugin,
   injectExportOrderPlugin,
@@ -19,20 +20,6 @@ import {
 import type { ExtendedOptions } from './types';
 
 export type PluginConfigType = 'build' | 'development';
-
-const globals = {
-  '@storybook/addons': '__STORYBOOK_MODULE_ADDONS__',
-  '@storybook/channel-postmessage': '__STORYBOOK_MODULE_CHANNEL_POSTMESSAGE__',
-  '@storybook/channel-websocket': '__STORYBOOK_MODULE_CHANNEL_WEBSOCKET__',
-  '@storybook/channels': '__STORYBOOK_MODULE_CHANNELS__',
-  '@storybook/client-api': '__STORYBOOK_MODULE_CLIENT_API__',
-  '@storybook/client-logger': '__STORYBOOK_MODULE_CLIENT_LOGGER__',
-  '@storybook/core-client': '__STORYBOOK_MODULE_CORE_CLIENT__',
-  '@storybook/core-events': '__STORYBOOK_MODULE_CORE_EVENTS__',
-  '@storybook/preview-web': '__STORYBOOK_MODULE_PREVIEW_WEB__',
-  '@storybook/preview-api': '__STORYBOOK_MODULE_PREVIEW_API__',
-  '@storybook/store': '__STORYBOOK_MODULE_STORE__',
-};
 
 const configEnvServe: ConfigEnv = {
   mode: 'development',
