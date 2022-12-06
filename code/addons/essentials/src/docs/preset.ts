a363f4937754f10f7564dbf62e2e05f3b46419cc
Norbert de Langen <ndelangen@me.com>
2022-12-06 17:32:09 +0100

    change path to export, the export map isn't taken into account by node, looks like..

--- code/addons/essentials/src/docs/preset.ts
+++ code/addons/essentials/src/docs/preset.ts
@@ -3,6 +3,7 @@ export * from '@storybook/addon-docs/dist/preset';
 
 export const mdxLoaderOptions = async (config: any) => {
   // eslint-disable-next-line no-param-reassign
-  config.mdxCompileOptions.providerImportSource = '@storybook/addon-essential/docs/mdx-react-shim';
+  config.mdxCompileOptions.providerImportSource =
+    '@storybook/addon-essential/dist/docs/mdx-react-shim';
   return config;
 };
