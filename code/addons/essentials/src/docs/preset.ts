e8d1c08eb91fb0369bae4b48e162cabda42006ad
Norbert de Langen <ndelangen@me.com>
2022-12-06 18:20:31 +0100

    fix typo

--- code/addons/essentials/src/docs/preset.ts
+++ code/addons/essentials/src/docs/preset.ts
@@ -3,7 +3,6 @@ export * from '@storybook/addon-docs/dist/preset';
 
 export const mdxLoaderOptions = async (config: any) => {
   // eslint-disable-next-line no-param-reassign
-  config.mdxCompileOptions.providerImportSource =
-    '@storybook/addon-essential/dist/docs/mdx-react-shim';
+  config.mdxCompileOptions.providerImportSource = '@storybook/addon-essentials/docs/mdx-react-shim';
   return config;
 };
