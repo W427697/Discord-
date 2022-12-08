ad2232c26b554076e25effc295717acb521160dc
Jeppe Reinhold <jeppe@reinhold.is>
2022-12-08 23:19:37 +0100

    svelte-vite error on sveltekit instead of warn

--- code/frameworks/svelte-vite/src/utils.ts
+++ code/frameworks/svelte-vite/src/utils.ts
@@ -1,6 +1,4 @@
 import type { PluginOption } from 'vite';
-import { deprecate } from '@storybook/node-logger';
-import { withoutVitePlugins } from '@storybook/builder-vite';
 import type { Options } from '@storybook/types';
 
 function checkName(plugin: PluginOption, name: string) {
@@ -23,32 +21,24 @@ export function hasPlugin(plugins: PluginOption[], name: string) {
  * but warns the user that they should use the sveltekit framework instead.
  * Should be removed when we decide to remove support completely for SvelteKit in svelte-vite
  */
-export async function handleSvelteKit(
-  plugins: PluginOption[],
-  options: Options
-): Promise<PluginOption[]> {
-  if (!hasPlugin(plugins, 'vite-plugin-svelte-kit')) {
-    // this is not a SvelteKit project ✅
-    return plugins;
-  }
-
+export async function handleSvelteKit(plugins: PluginOption[], options: Options) {
   /*
   the sveltekit framework uses this svelte-vite framework under the hood
-  so we have to take extra care of only warning when the user is actually using
+  so we have to take extra care of only throwing when the user is actually using
   svelte-vite directly and not just through sveltekit
   */
-
   const frameworkPreset = await options.presets.apply('framework', {}, options);
   const framework = typeof frameworkPreset === 'string' ? frameworkPreset : frameworkPreset.name;
 
-  if (framework === '@storybook/sveltekit') {
-    // this uses @storybook/sveltekit, so everything is fine ✅
-    return plugins;
-  }
+  if (hasPlugin(plugins, 'vite-plugin-svelte-kit') && framework !== '@storybook/sveltekit') {
+    throw new Error(`
+      We've detected a SvelteKit project using the @storybook/svelte-vite framework, which is not supported in Storybook 7.0
+      Please use the @storybook/sveltekit framework instead.
+      You can migrate automatically by running
+      
+      npx sb@next automigrate sveltekitFramework
 
-  // this is a SvelteKit project but doesn't use @storybook/sveltekit, warn user about this and remove vite-plugin-svelte-kit ❌
-  deprecate(
-    'SvelteKit support in @storybook/svelte-vite is deprecated in Storybook 7.0, use @storybook/sveltekit instead.'
-  );
-  return withoutVitePlugins(plugins, ['vite-plugin-svelte-kit']);
+      See https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#sveltekit-needs-the-storybooksveltekit-framework
+      `);
+  }
 }
