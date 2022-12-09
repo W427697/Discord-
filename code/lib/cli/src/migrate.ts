59d0bcc962d6fe7fb71456b2e7b758999a2d10b1
Norbert de Langen <ndelangen@me.com>
2022-12-09 23:36:16 +0100

    Revert "fix migrate command"
    This reverts commit c6f4baba2a22aee6d183683ac7ca7a1f3bec1d64.

--- code/lib/cli/src/migrate.ts
+++ code/lib/cli/src/migrate.ts
@@ -1,10 +1,13 @@
 import { listCodemods, runCodemod } from '@storybook/codemod';
 
-export async function migrate(migration: any, { glob, dryRun, list, rename, logger, parser }: any) {
+export async function migrate(
+  migration: any,
+  { configDir, glob, dryRun, list, rename, logger, parser }: any
+) {
   if (list) {
     listCodemods().forEach((key: any) => logger.log(key));
   } else if (migration) {
-    await runCodemod(migration, { glob, dryRun, logger, rename, parser });
+    await runCodemod(migration, { configDir, glob, dryRun, logger, rename, parser });
   } else {
     throw new Error('Migrate: please specify a migration name or --list');
   }
