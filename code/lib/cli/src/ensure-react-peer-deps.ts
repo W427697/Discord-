0eafd67ffa34bd37d7b842000eb8468a4d9fd3fd
Valentin Palkovic <valentin@chromatic.com>
2022-12-12 15:46:55 +0100

    Edit peer deps react error message

--- code/lib/cli/src/ensure-react-peer-deps.ts
+++ code/lib/cli/src/ensure-react-peer-deps.ts
@@ -8,7 +8,22 @@ export function ensureReactPeerDeps() {
   } catch (e) {
     logger.error(dedent`
       Starting in 7.0, react and react-dom are now required peer dependencies of Storybook.
-      Please install react and react-dom in your project.
+      https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#react-peer-dependencies-required
+
+      It seems, that you haven't run Storybook's CLI to upgrade to the latest version.
+      The upgrade command will install the required peer dependencies for you and will take 
+      care of other important auto migrations as well.
+
+      If you want to upgrade to the latest prerelease version, please run:
+
+      $ npx storybook@next upgrade --prerelease
+
+      Otherwise, please run:
+
+      $ npx storybook upgrade
+
+      If you do not want to use the upgrade commands, 
+      please install react and react-dom in your project manually.
 
       npm:
       $ npm add react react-dom --dev
