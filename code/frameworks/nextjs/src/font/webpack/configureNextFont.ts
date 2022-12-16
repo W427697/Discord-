5c2b48506c816cd886cc92ed6da83c4ba67d291c
Valentin Palkovic <valentin@chromatic.com>
2022-12-16 15:05:58 +0100

    Remove unused plugin

--- code/frameworks/nextjs/src/font/webpack/configureNextFont.ts
+++ code/frameworks/nextjs/src/font/webpack/configureNextFont.ts
@@ -1,8 +1,7 @@
 import type { Configuration } from 'webpack';
-import VirtualLocalFontModulePlugin from './plugin/virtual-local-font-module';
 
 export function configureNextFont(baseConfig: Configuration) {
-  baseConfig.plugins = [...(baseConfig.plugins || []), new VirtualLocalFontModulePlugin()];
+  baseConfig.plugins = [...(baseConfig.plugins || [])];
   baseConfig.resolveLoader = {
     ...baseConfig.resolveLoader,
     alias: {
