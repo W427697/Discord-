190b0d4548fc64552bc1e86825c67b7d77529dc6
Tom Coleman <tom@chromatic.com>
2022-12-21 21:17:42 +1100

    Simulate rendering doc blocks "unattached"
    Via `relativeCsfPaths: []`. This demonstrates the issue from https://github.com/storybookjs/storybook/issues/19964

--- code/ui/blocks/src/blocks/Description.stories.tsx
+++ code/ui/blocks/src/blocks/Description.stories.tsx
@@ -16,8 +16,25 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+export const Default: Story = {};
+
+export const PrimaryStory: Story = {
+  args: {
+    of: '^',
+  },
+};
+
 export const BooleanControlJSDoc: Story = {
   args: {
     of: BooleanControl,
+  },
+};
+
+export const OfUnattached: Story = {
+  args: {
+    of: BooleanControl,
+  },
+  parameters: {
+    relativeCsfPaths: [],
   },
 };
