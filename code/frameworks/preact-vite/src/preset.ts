ebe557a3432d1646bfdd89b302a81d7df587ac38
Klemen Slavič <krof.drakula@gmail.com>
2022-12-23 10:46:31 +0100

    Initial code dump for preact-vite framework combo

---
+++ code/frameworks/preact-vite/src/preset.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/builder-vite';
+// import { hasVitePlugins } from '@storybook/builder-vite';
+import preact from '@preact/preset-vite';
+
+export const core: StorybookConfig['core'] = {
+  builder: '@storybook/builder-vite',
+  renderer: '@storybook/preact',
+};
+
+export const viteFinal: StorybookConfig['viteFinal'] = async (config, { presets }) => {
+  const { plugins = [] } = config;
+
+  // Add preact plugin if not present
+  // FIXME: check vite plugins for presence of preact plugin
+  plugins.push(preact());
+
+  // TODO: Add docgen plugin per issue https://github.com/storybookjs/storybook/issues/19739
+
+  return config;
+};
