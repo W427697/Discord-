e09e8d3d607e9edb5a38af790f753309aa1aea3d
Ian VanSchooten <ian.vanschooten@gmail.com>
2022-12-30 11:39:42 -0500

    CLI: Add prop-types dependency if not using TypeScript

--- code/lib/cli/src/generators/REACT/index.ts
+++ code/lib/cli/src/generators/REACT/index.ts
@@ -1,8 +1,19 @@
+import { detectLanguage } from '../../detect';
+import { SupportedLanguage } from '../../project_types';
 import { baseGenerator } from '../baseGenerator';
 import type { Generator } from '../types';
 
 const generator: Generator = async (packageManager, npmOptions, options) => {
-  await baseGenerator(packageManager, npmOptions, options, 'react');
+  // Add prop-types dependency if not using TypeScript
+  const extraPackages = [];
+  const language = detectLanguage();
+  if (language === SupportedLanguage.JAVASCRIPT) {
+    extraPackages.push('prop-types');
+  }
+
+  await baseGenerator(packageManager, npmOptions, options, 'react', {
+    extraPackages,
+  });
 };
 
 export default generator;
