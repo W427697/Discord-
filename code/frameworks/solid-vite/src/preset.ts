d6055f70eb2fcee58a744704ddc428b0f1ca0457
Mauricio Rivera <mauriver21@gmail.com>
2022-12-31 14:26:50 -0500

    Updated preset with vite-plugin-solid

--- code/frameworks/solid-vite/src/preset.ts
+++ code/frameworks/solid-vite/src/preset.ts
@@ -1,6 +1,18 @@
-import type { StorybookConfig } from '@storybook/builder-vite';
+import { hasVitePlugins, StorybookConfig } from '@storybook/builder-vite';
 
 export const core: StorybookConfig['core'] = {
   builder: '@storybook/builder-vite',
-  renderer: '@storybook/solid',
+  renderer: '@storybook/html',
+};
+
+export const viteFinal: StorybookConfig['viteFinal'] = async (config, { presets }) => {
+  const { plugins = [] } = config;
+
+  // Add solid plugin if not present
+  if (!(await hasVitePlugins(plugins, ['vite-plugin-solid']))) {
+    const { default: solidPlugin } = await import('vite-plugin-solid');
+    plugins.push(solidPlugin());
+  }
+
+  return config;
 };
