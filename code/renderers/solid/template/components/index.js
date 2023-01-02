ce9113aeef3942033ec831d1b1550afc80353c5f
Mauricio Rivera <mauriver21@gmail.com>
2023-01-02 09:57:00 -0500

    Initial Button component as SolidJS template

--- code/renderers/solid/template/components/index.js
+++ code/renderers/solid/template/components/index.js
@@ -1,9 +1,6 @@
 import { global as globalThis } from '@storybook/global';
 
 import { Button } from './Button';
-import { Pre } from './Pre';
-import { Form } from './Form';
-import { Html } from './Html';
 
-globalThis.Components = { Button, Pre, Form, Html };
-globalThis.storybookRenderer = 'html';
+globalThis.Components = { Button };
+globalThis.storybookRenderer = 'solid';
