769b3513304b5216d3b4e416b62f708d67bda94b
Michael Shilman <shilman@users.noreply.github.com>
2023-01-09 11:18:43 +0800

    Telemetry: Add pageStoryCount

--- code/lib/core-server/src/utils/summarizeIndex.ts
+++ code/lib/core-server/src/utils/summarizeIndex.ts
@@ -2,14 +2,22 @@ import type { StoryIndex } from '@storybook/types';
 
 import { STORIES_MDX_TAG, isMdxEntry, AUTODOCS_TAG } from './StoryIndexGenerator';
 
+const PAGE_REGEX = /(page|screen)/i;
+
+export const isPageStory = (storyId: string) => PAGE_REGEX.test(storyId);
+
 export function summarizeIndex(storyIndex: StoryIndex) {
   let storyCount = 0;
+  let pageStoryCount = 0;
   let autodocsCount = 0;
   let storiesMdxCount = 0;
   let mdxCount = 0;
   Object.values(storyIndex.entries).forEach((entry) => {
     if (entry.type === 'story') {
       storyCount += 1;
+      if (isPageStory(entry.title)) {
+        pageStoryCount += 1;
+      }
     } else if (entry.type === 'docs') {
       if (isMdxEntry(entry)) {
         mdxCount += 1;
@@ -22,6 +30,7 @@ export function summarizeIndex(storyIndex: StoryIndex) {
   });
   return {
     storyCount,
+    pageStoryCount,
     autodocsCount,
     storiesMdxCount,
     mdxCount,
