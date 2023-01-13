2e3a5757c0df439ae6b18275097efabb5ba8c412
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-01-13 05:46:47 -0800

    Update code/lib/cli/src/generators/SVELTE/index.ts

--- code/lib/cli/src/generators/SVELTE/index.ts
+++ code/lib/cli/src/generators/SVELTE/index.ts
@@ -3,7 +3,6 @@ import { baseGenerator } from '../baseGenerator';
 import type { Generator } from '../types';
 
 const generator: Generator = async (packageManager, npmOptions, options) => {
-  const extraPackages = options.builder === CoreBuilder.Webpack5 ? ['svelte'] : [];
 
   await baseGenerator(packageManager, npmOptions, options, 'svelte', {
     extensions: ['js', 'jsx', 'ts', 'tsx', 'svelte'],
