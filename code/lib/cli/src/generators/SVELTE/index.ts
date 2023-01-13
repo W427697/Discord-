ef835267e739f25030909524f368037eae87dd25
Jeppe Reinhold <jeppe@reinhold.is>
2023-01-13 15:02:46 +0100

    fix lint

--- code/lib/cli/src/generators/SVELTE/index.ts
+++ code/lib/cli/src/generators/SVELTE/index.ts
@@ -1,9 +1,7 @@
-import { CoreBuilder } from '../../project_types';
 import { baseGenerator } from '../baseGenerator';
 import type { Generator } from '../types';
 
 const generator: Generator = async (packageManager, npmOptions, options) => {
-
   await baseGenerator(packageManager, npmOptions, options, 'svelte', {
     extensions: ['js', 'jsx', 'ts', 'tsx', 'svelte'],
     commonJs: true,
