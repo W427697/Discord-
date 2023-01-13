29959c8c948c1b9b988a4e430a2ea15d1cb0bacd
Tom Coleman <tom@chromatic.com>
2023-01-13 20:03:38 +1100

    Cleanup a bit

--- code/ui/blocks/src/components/Story.stories.tsx
+++ code/ui/blocks/src/components/Story.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import type { StoryObj, Meta } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react';
+import type { ModuleExport } from '@storybook/types';
 import type { StoryProps } from './Story';
 import { Story as StoryComponent, StorySkeleton } from './Story';
 import type { DocsContextProps } from '../blocks';
@@ -8,12 +9,12 @@ import * as ButtonStories from '../examples/Button.stories';
 const preview = __STORYBOOK_PREVIEW__;
 const renderStoryToElement = preview.renderStoryToElement.bind(preview);
 
-// TODO: can't quite figure out types here.
-// type OverriddenStoryProps = StoryProps & {
-//   story: typeof ButtonStories.Primary;
-// };
+type ExtendedStoryProps = Omit<StoryProps, 'story'> & {
+  storyExport: ModuleExport;
+};
 
-const meta: Meta<typeof StoryComponent> = {
+const meta: Meta<ExtendedStoryProps> = {
+  // @ts-expect-error getting too complex with props
   component: StoryComponent,
   parameters: {
     relativeCsfPaths: ['../examples/Button.stories'],
@@ -21,37 +22,42 @@ const meta: Meta<typeof StoryComponent> = {
   args: {
     height: '100px',
     // NOTE: the real story arg is a PreparedStory, which we'll get in the render function below
-    story: ButtonStories.Primary as any,
+    storyExport: ButtonStories.Primary as any,
   },
-  render(args, { loaded }) {
+  render({ storyExport, ...args }, { loaded }) {
     const docsContext = loaded.docsContext as DocsContextProps;
-    const storyId = docsContext.storyIdByModuleExport(args.story);
+    const storyId = docsContext.storyIdByModuleExport(storyExport);
     const story = docsContext.storyById(storyId);
+    // @ts-expect-error getting too complex with props
     return <StoryComponent {...args} story={story} />;
   },
 };
 export default meta;
 
+type Story = StoryObj<typeof meta>;
+
 export const Loading = () => <StorySkeleton />;
 
-export const Inline = {
+export const Inline: Story = {
   args: {
     inline: true,
+    // @ts-expect-error getting too complex with props
     autoplay: false,
     renderStoryToElement,
   },
 };
 
-export const IFrame = {
+export const IFrame: Story = {
   args: {
     inline: false,
   },
 };
 
-export const Autoplay = {
+export const Autoplay: Story = {
   args: {
-    story: ButtonStories.Clicking,
+    storyExport: ButtonStories.Clicking,
     inline: true,
+    // @ts-expect-error getting too complex with props
     autoplay: true,
     renderStoryToElement,
   },
