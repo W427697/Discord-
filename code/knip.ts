3578892ae8239a9b28b4bb480056bd5b2fff2cfd
Lars Kappert <lars@webpro.nl>
2023-01-16 13:20:22 +0100

    Auto-add entry files from package.json#bundler.entries to Knip's config

--- code/knip.ts
+++ code/knip.ts
@@ -1,8 +1,15 @@
+import path from 'node:path';
+import { parseArgs } from 'node:util';
 import { z } from 'zod';
+import { isMatch } from 'picomatch';
 import type { ConfigurationValidator } from 'knip/dist/configuration-validator';
 
 // TODO: Import this type directly from Knip once it's available (and get rid of zod dependency here)
 type Configuration = z.infer<typeof ConfigurationValidator>;
+
+const baseDir = process.cwd();
+const { values } = parseArgs({ strict: false, options: { workspace: { type: 'string' } } });
+const wsArg = values.workspace;
 
 const baseConfig: Configuration = {
   ignore: '**/*.d.ts',
@@ -18,7 +25,6 @@ const baseConfig: Configuration = {
     },
     'frameworks/*': {
       entry: [
-        'src/{index,preset,preview}.{ts,tsx}',
         'src/builders/{build,start}-storybook/index.ts',
         'src/**/docs/{index,config}.{js,ts}',
       ],
@@ -26,27 +32,41 @@ const baseConfig: Configuration = {
       ignore: ['**/__mocks-ng-workspace__', '**/__testfixtures__'],
     },
     'lib/*': {
-      entry: ['src/{index,entry,runtime}.ts'],
+      entry: [],
       project: 'src/**/*.ts',
     },
     'lib/builder-webpack5': {
-      entry: ['src/index.ts', 'src/presets/*.ts'],
+      entry: [],
       project: 'src/**/*.ts',
     },
     'lib/cli': {
-      entry: ['src/generate.ts'],
+      entry: [],
       project: 'src/**/*.ts',
     },
     'ui/manager': {
-      entry: [
-        'scripts/generate-exports-file.ts',
-        'src/index.tsx',
-        'src/globals.ts',
-        'src/runtime.ts',
-      ],
+      entry: ['scripts/generate-exports-file.ts'],
       project: '{scripts,src}/**/*.{js,ts,tsx}',
     },
   },
 };
 
-export default baseConfig;
+// Adds package.json#bundler.entries to each workspace config `entry: []`
+export const addBundlerEntries = async (config: Configuration) => {
+  const wsKeys = config.workspaces ? Object.keys(config.workspaces) : ['.'];
+  const wsMatches = wsArg ? wsKeys.filter((wsKey) => isMatch(wsArg, wsKey)) : wsKeys;
+  wsMatches.forEach((wsKey) => {
+    const wsDir = !wsKey.includes('*') ? wsKey : wsArg && isMatch(wsArg, wsKey) ? wsArg : null;
+    if (wsDir) {
+      // Explicitly no try/catch here to require package.json
+      const manifest = require(path.join(baseDir, wsDir, 'package.json'));
+      const configEntries = config.workspaces ? config.workspaces[wsKey].entry : config.entry;
+      const bundlerEntries = manifest?.bundler?.entries;
+      if (Array.isArray(configEntries) && Array.isArray(bundlerEntries)) {
+        configEntries.push(...manifest.bundler.entries);
+      }
+    }
+  });
+  return config;
+};
+
+export default addBundlerEntries(baseConfig);
