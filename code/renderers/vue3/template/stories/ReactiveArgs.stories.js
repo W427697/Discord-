96fcaa53c29755e186b880228ab14c83c887ecfa
chakir qatab <javachakir@gmail.com>
2023-01-22 13:56:40 +0400

    fix add await to expect

--- code/renderers/vue3/template/stories/ReactiveArgs.stories.js
+++ code/renderers/vue3/template/stories/ReactiveArgs.stories.js
@@ -1,6 +1,6 @@
 import { expect } from '@storybook/jest';
 import { global as globalThis } from '@storybook/global';
-import { within } from '@storybook/testing-library';
+import { within, userEvent } from '@storybook/testing-library';
 import { UPDATE_STORY_ARGS } from '@storybook/core-events';
 import ReactiveArgs from './ReactiveArgs.vue';
 
@@ -21,18 +21,22 @@ export const Reactive = {
     const channel = globalThis.__STORYBOOK_ADDONS_CHANNEL__;
     const canvas = within(canvasElement);
     const reactiveButton = await canvas.getByRole('button');
-    reactiveButton.click(); // click to update the label to increment the count + 1
-    channel.emit(UPDATE_STORY_ARGS, {
+    await expect(reactiveButton).toHaveTextContent('Button 0');
+
+    await userEvent.click(reactiveButton); // click to update the label to increment the count + 1
+    await channel.emit(UPDATE_STORY_ARGS, {
       storyId: id,
       updatedArgs: { label: 'updated' },
     });
-    expect(reactiveButton).toHaveTextContent('updated 1');
-    reactiveButton.click(); // click to update the label to increment the count + 1
-    expect(reactiveButton).toHaveTextContent('updated 2');
-    channel.emit(UPDATE_STORY_ARGS, {
+    await expect(reactiveButton).toHaveTextContent('updated 1');
+
+    await userEvent.click(reactiveButton); // click to update the label to increment the count + 1
+    await expect(reactiveButton).toHaveTextContent('updated 2');
+
+    await channel.emit(UPDATE_STORY_ARGS, {
       storyId: id,
       updatedArgs: { label: 'updated again' },
     });
-    expect(reactiveButton).toHaveTextContent('updated again 3');
+    expect(reactiveButton).toHaveTextContent('updated again 2');
   },
 };
