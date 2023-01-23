1244c06ac8d66ae3caad8bb347b35b4caa8856cf
Mauricio Rivera <mauriver21@gmail.com>
2023-01-23 17:09:23 -0500

    Updated StoryRender.test.ts

--- code/lib/preview-api/src/modules/preview-web/render/StoryRender.test.ts
+++ code/lib/preview-api/src/modules/preview-web/render/StoryRender.test.ts
@@ -114,6 +114,7 @@ describe('StoryRender', () => {
       applyLoaders: jest.fn(),
       unboundStoryFn: jest.fn(),
       playFunction: jest.fn(),
+      prepareContext: jest.fn((ctx) => ctx),
     };
 
     const renderToScreen = jest.fn();
