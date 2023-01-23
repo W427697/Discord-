23bca2dc1c1f24936a1ad7cf1cc403588e921c75
Jeppe Reinhold <jeppe@reinhold.is>
2023-01-23 21:16:25 +0100

    change forReRender story to forceRemount

--- code/lib/store/template/stories/rendering.stories.ts
+++ code/lib/store/template/stories/rendering.stories.ts
@@ -2,6 +2,7 @@ import { global as globalThis } from '@storybook/global';
 import type { PlayFunctionContext } from '@storybook/types';
 import { within, waitFor } from '@storybook/testing-library';
 import { expect } from '@storybook/jest';
+import { FORCE_REMOUNT, RESET_STORY_ARGS, UPDATE_STORY_ARGS } from '@storybook/core-events';
 
 export default {
   component: globalThis.Components.Button,
@@ -9,16 +10,15 @@ export default {
     label: 'Click me',
   },
 };
-// this is the story should be remove it will always fail, because forceReRender Api is removed in V7
-export const ForceReRender = {
+
+export const ForceRemount = {
   play: async ({ canvasElement }: PlayFunctionContext<any>) => {
     const channel = globalThis.__STORYBOOK_ADDONS_CHANNEL__;
     const button = await within(canvasElement).findByRole('button');
     await button.focus();
     await expect(button).toHaveFocus();
-    //  forceRender is not called in V7
-    // By forcing the component to rerender, we reset the focus state
-    await channel.emit('forceReRender');
+    // By forcing the component to remount, we reset the focus state
+    await channel.emit(FORCE_REMOUNT);
     await waitFor(() => expect(button).not.toHaveFocus());
   },
 };
@@ -30,17 +30,16 @@ export const ChangeArgs = {
     await button.focus();
     await expect(button).toHaveFocus();
 
-    // Vue3: https://github.com/storybookjs/storybook/issues/13913
     // Web-components: https://github.com/storybookjs/storybook/issues/19415
     // Preact: https://github.com/storybookjs/storybook/issues/19504
-    if (['vue3', 'web-components', 'html', 'preact'].includes(globalThis.storybookRenderer)) return;
+    if (['web-components', 'html', 'preact'].includes(globalThis.storybookRenderer)) return;
 
-    // When we change the args to the button, it should not rerender
-    await channel.emit('updateStoryArgs', { storyId: id, updatedArgs: { label: 'New Text' } });
+    // When we change the args to the button, it should not remount
+    await channel.emit(UPDATE_STORY_ARGS, { storyId: id, updatedArgs: { label: 'New Text' } });
     await within(canvasElement).findByText(/New Text/);
     await expect(button).toHaveFocus();
 
-    await channel.emit('resetStoryArgs', { storyId: id });
+    await channel.emit(RESET_STORY_ARGS, { storyId: id });
     await within(canvasElement).findByText(/Click me/);
     await expect(button).toHaveFocus();
   },
