b78244e652e72f470295b39715b61d7ecad93990
Ian VanSchooten <ian.vanschooten@gmail.com>
2023-01-22 22:15:26 -0500

    Create aliases for dev

--- code/lib/builder-vite/src/plugins/externals-plugin.ts
+++ code/lib/builder-vite/src/plugins/externals-plugin.ts
@@ -1,5 +1,9 @@
+import { join } from 'node:path';
 import { init, parse } from 'es-module-lexer';
 import MagicString from 'magic-string';
+import { emptyDirSync, ensureDir, ensureFile, writeFile } from 'fs-extra';
+import { mergeAlias } from 'vite';
+import type { Alias, Plugin } from 'vite';
 import { globals } from '@storybook/preview/globals';
 
 type SingleGlobalName = keyof typeof globals;
@@ -9,6 +13,30 @@ export async function externalsPlugin() {
   return {
     name: 'storybook:externals-plugin',
     enforce: 'post',
+    async config(config, { command }) {
+      if (command !== 'serve') {
+        return undefined;
+      }
+      const newAlias = mergeAlias([], config.resolve?.alias) as Alias[];
+
+      const cachePath = join(process.cwd(), 'node_modules', '.cache', 'vite-plugin-externals');
+      await ensureDir(cachePath);
+      await emptyDirSync(cachePath);
+
+      // eslint-disable-next-line no-restricted-syntax
+      for await (const externalKey of Object.keys(globals) as Array<keyof typeof globals>) {
+        const externalCachePath = join(cachePath, `${externalKey}.js`);
+        newAlias.push({ find: new RegExp(`^${externalKey}$`), replacement: externalCachePath });
+        await ensureFile(externalCachePath);
+        await writeFile(externalCachePath, `module.exports = ${globals[externalKey]};`);
+      }
+
+      return {
+        resolve: {
+          alias: newAlias,
+        },
+      };
+    },
     async transform(code: string, id: string) {
       const globalsList = Object.keys(globals) as SingleGlobalName[];
       if (globalsList.every((glob) => !code.includes(glob))) return undefined;
@@ -41,5 +69,5 @@ export async function externalsPlugin() {
         }),
       };
     },
-  };
+  } satisfies Plugin;
 }
