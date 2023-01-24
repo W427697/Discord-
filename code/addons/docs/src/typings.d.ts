2de380835b38da1b9a1960e748e9485e031dd95a
Michael Shilman <shilman@users.noreply.github.com>
2023-01-24 15:31:06 +0800

    Fix typings

--- code/addons/docs/src/typings.d.ts
+++ code/addons/docs/src/typings.d.ts
@@ -4,6 +4,7 @@ declare module 'remark-external-links';
 declare module 'babel-plugin-react-docgen';
 declare module 'acorn-jsx';
 declare module 'vue/dist/vue';
+declare module '@storybook/mdx1-csf';
 
 declare module 'sveltedoc-parser' {
   export function parse(options: any): Promise<any>;
