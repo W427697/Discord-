c5c4dac1dd38c129748505c5eb0ef4fe917ab648
Mostafa Sherif <mosherif87@gmail.com>
2023-01-25 07:15:34 -0500

    fixes & updates as per @valentinpalkovic comments

--- code/frameworks/angular/src/client/angular-beta/utils/PropertyExtractor.ts
+++ code/frameworks/angular/src/client/angular-beta/utils/PropertyExtractor.ts
@@ -1,41 +1,11 @@
 import { CommonModule } from '@angular/common';
-import { NgModuleMeta } from '@angular/compiler-cli/src/ngtsc/metadata';
-import { NgModule, Provider } from '@angular/core';
+import { Provider } from '@angular/core';
 import { NgModuleMetadata } from '../../types';
 import { isDeclarable, isStandaloneComponent } from './NgComponentAnalyzer';
 import { isComponentAlreadyDeclared } from './NgModulesAnalyzer';
 
-/**
- * Flattens Array Deeply
- *
- * - Executes recursively
- *
- * @date `1/24/2023 - 12:58:03 PM`
- * @author `sheriffMoose`
- *
- * @param {any[]} arr - Array to be flattened
- * @returns {*} Array of imports
- */
-const flattenArray = (arr: any[]) => {
-  return (arr || [])
-    .map((item: any): any | any[] => {
-      return item instanceof Array ? flattenArray(item) : item;
-    })
-    .flat();
-};
-
-/**
- * Filter Array for Unique Values
- *
- *
- * @date `1/24/2023 - 12:58:03 PM`
- * @author `sheriffMoose`
- *
- * @param {any[]} arr - Array to be flattened
- * @returns {*} Array of imports
- */
 const uniqueArray = (arr: any[]) => {
-  return flattenArray(arr).filter((value, index, self) => self.indexOf(value) === index);
+  return arr.flat(Number.MAX_VALUE).filter((value, index, self) => self.indexOf(value) === index);
 };
 
 /**
@@ -47,16 +17,13 @@ const uniqueArray = (arr: any[]) => {
  *
  * - If ModuleWithProviders (e.g. forRoot() & forChild() ) is used, the ngModule is extracted without providers.
  *
- * @date `1/24/2023 - 12:58:03 PM`
- * @author `sheriffMoose`
- *
  * @param {NgModuleMetadata} metadata - NG Module Metadata
  * @returns {*} Array of imports
  */
 export const extractImports = (metadata: NgModuleMetadata) => {
   const imports = [CommonModule];
 
-  const modules = flattenArray(metadata.imports);
+  const modules = (metadata.imports || []).flat(Number.MAX_VALUE);
   const withProviders = modules.filter((moduleDef) => !!moduleDef?.ngModule);
   const withoutProviders = modules.filter((moduleDef) => !withProviders.includes(moduleDef));
 
@@ -76,16 +43,14 @@ export const extractImports = (metadata: NgModuleMetadata) => {
  *
  * - Use this in combination with extractImports to get all providers for a specific module
  *
- * @date `1/24/2023 - 12:58:03 PM`
- * @author `sheriffMoose`
- *
  * @param {NgModuleMetadata} metadata - NG Module Metadata
  * @returns {*} Array of providers
  */
 export const extractProviders = (metadata: NgModuleMetadata): Provider[] => {
   const providers = (metadata.providers || []) as Provider[];
 
-  const moduleProviders: Provider[] = flattenArray(metadata.imports)
+  const moduleProviders: Provider[] = (metadata.imports || [])
+    .flat(Number.MAX_VALUE)
     .filter((moduleDef) => !!moduleDef?.ngModule)
     .map((moduleDef) => moduleDef.providers || []);
 
@@ -99,9 +64,6 @@ export const extractProviders = (metadata: NgModuleMetadata): Provider[] => {
  *    - It is a component or directive or pipe
  *    - It is not already declared
  *    - It is not a standalone component
- *
- * @date `1/24/2023 - 12:58:03 PM`
- * @author `sheriffMoose`
  *
  * @param {NgModuleMetadata} metadata
  * @param {?*} storyComponent
