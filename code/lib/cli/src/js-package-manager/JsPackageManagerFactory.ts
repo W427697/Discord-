f5d8eaccae9541d5830ed45c427e3013b95282c3
Norbert de Langen <ndelangen@me.com>
2023-01-30 14:45:30 +0100

    fix issue with types after update && fix linting

--- code/lib/cli/src/js-package-manager/JsPackageManagerFactory.ts
+++ code/lib/cli/src/js-package-manager/JsPackageManagerFactory.ts
@@ -5,8 +5,7 @@ import { sync as findUpSync } from 'find-up';
 import { NPMProxy } from './NPMProxy';
 import { PNPMProxy } from './PNPMProxy';
 
-import type { JsPackageManager } from './JsPackageManager';
-import { type PackageManagerName } from './JsPackageManager';
+import type { JsPackageManager, type PackageManagerName } from './JsPackageManager';
 
 import { Yarn2Proxy } from './Yarn2Proxy';
 import { Yarn1Proxy } from './Yarn1Proxy';
