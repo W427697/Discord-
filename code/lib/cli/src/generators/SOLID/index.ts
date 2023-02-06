bebfa73908ecc20039c1a23e72cd2c1d232ff91d
Mauricio Rivera <mauriver21@gmail.com>
2023-02-06 11:30:14 -0500

    Added js solid repro template and extraAddon

--- code/lib/cli/src/generators/SOLID/index.ts
+++ code/lib/cli/src/generators/SOLID/index.ts
@@ -2,7 +2,14 @@ import { baseGenerator } from '../baseGenerator';
 import type { Generator } from '../types';
 
 const generator: Generator = async (packageManager, npmOptions, options) => {
-  await baseGenerator(packageManager, npmOptions, options, 'solid', {}, 'solid');
+  await baseGenerator(
+    packageManager,
+    npmOptions,
+    options,
+    'solid',
+    { extraAddons: ['@storybook/testing-library'] },
+    'solid'
+  );
 };
 
 export default generator;
