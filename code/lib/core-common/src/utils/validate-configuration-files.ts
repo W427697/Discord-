80790c825f11efd977b6c359d5c10e9ddae0c1d0
Yann Braga <yannbf@gmail.com>
2023-02-06 10:11:44 +0100

    undo AST main.js check in validateConfigFile

--- code/lib/core-common/src/utils/validate-configuration-files.ts
+++ code/lib/core-common/src/utils/validate-configuration-files.ts
@@ -2,7 +2,6 @@ import { dedent } from 'ts-dedent';
 import { promise as glob } from 'glob-promise';
 import path from 'path';
 import slash from 'slash';
-import { readConfig } from '@storybook/csf-tools';
 import { once } from '@storybook/node-logger';
 
 import { boost } from './interpret-files';
@@ -25,13 +24,5 @@ export async function validateConfigurationFiles(configDir: string) {
       No configuration files have been found in your configDir (${path.resolve(configDir)}).
       Storybook needs "main.js" file, please add it.
     `);
-  } else {
-    const main = await readConfig(mainConfigPath);
-    if (!main.hasDefaultExport) {
-      once.warn(dedent`
-        Your main.js is not using a default export, which is the recommended format. Please update it.
-        For more info: https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#esm-format-in-mainjs
-      `);
-    }
   }
 }
