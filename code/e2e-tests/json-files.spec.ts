3b2c3c4f18b7fef54afd712afba45bdef3db4522
Yann Braga <yannbf@gmail.com>
2023-02-06 12:44:17 +0100

    skip certain test when testing ssv6

--- code/e2e-tests/json-files.spec.ts
+++ code/e2e-tests/json-files.spec.ts
@@ -1,7 +1,9 @@
+/* eslint-disable jest/no-disabled-tests */
 import { test, expect } from '@playwright/test';
 import process from 'process';
 
 const storybookUrl = process.env.STORYBOOK_URL || 'http://localhost:8001';
+const templateName = process.env.STORYBOOK_TEMPLATE_NAME;
 
 test.describe('JSON files', () => {
   test.beforeEach(async ({ page }) => {
@@ -9,6 +11,11 @@ test.describe('JSON files', () => {
   });
 
   test('should have index.json', async ({ page }) => {
+    test.skip(
+      // eslint-disable-next-line jest/valid-title
+      templateName.includes('ssv6'),
+      'Only run this test for Sandboxes with StoryStoreV7 enabled'
+    );
     const json = await page.evaluate(() => fetch('/index.json').then((res) => res.json()));
 
     expect(json).toEqual({
