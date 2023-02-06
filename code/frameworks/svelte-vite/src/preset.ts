167056a3add98f4cd5267bd54bdae13fdebd1b73
Ben McCann <322311+benmccann@users.noreply.github.com>
2023-02-06 09:06:23 -0800

    use vite-plugin-svelte by default again

--- code/frameworks/svelte-vite/src/preset.ts
+++ code/frameworks/svelte-vite/src/preset.ts
@@ -1,3 +1,4 @@
+import { hasVitePlugins } from '@storybook/builder-vite';
 import type { PresetProperty } from '@storybook/types';
 import type { StorybookConfig } from './types';
 import { handleSvelteKit } from './utils';
@@ -14,6 +15,11 @@ export const viteFinal: NonNullable<StorybookConfig['viteFinal']> = async (confi
   // eslint-disable-next-line import/no-unresolved
   const { loadSvelteConfig } = await import('@sveltejs/vite-plugin-svelte');
   const svelteConfig = await loadSvelteConfig();
+
+  // Add svelte plugin if the user does not have a Vite config of their own
+  if (!(await hasVitePlugins(plugins, ['vite-plugin-svelte']))) {
+    plugins.push(svelte());
+  }
 
   // Add docgen plugin
   plugins.push(svelteDocgen(svelteConfig));
