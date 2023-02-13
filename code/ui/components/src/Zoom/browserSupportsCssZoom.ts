efc8ce1b6892e3b1d913eb7331d5a03ebb1316db
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-02-13 13:01:26 +0000

    checks if browser is safari

--- code/ui/components/src/Zoom/browserSupportsCssZoom.ts
+++ code/ui/components/src/Zoom/browserSupportsCssZoom.ts
@@ -3,7 +3,9 @@ import { global } from '@storybook/global';
 export function browserSupportsCssZoom(): boolean {
   try {
     // @ts-expect-error (we're testing for browser support)
-    return global.document.implementation.createHTMLDocument('').body.style.zoom !== undefined;
+    if (window.safari) {
+      return false
+    } else return global.document.implementation.createHTMLDocument('').body.style.zoom !== undefined;
   } catch (error) {
     return false;
   }
