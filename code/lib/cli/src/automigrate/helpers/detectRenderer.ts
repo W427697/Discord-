8647baab2900ea97bdc16d733f3fa169c48314a3
Yann Braga <yannbf@gmail.com>
2023-02-13 11:09:13 +0100

    add renderer detection

---
+++ code/lib/cli/src/automigrate/helpers/detectRenderer.ts
@@ -0,0 +1,32 @@
+import type { PackageJson } from '@storybook/types';
+import { rendererPackages } from '@storybook/core-common';
+import prompts from 'prompts';
+
+export const detectRenderer = async (packageJson: PackageJson) => {
+  const allDependencies = {
+    ...packageJson.dependencies,
+    ...packageJson.devDependencies,
+    ...packageJson.peerDependencies,
+  };
+
+  // Pull the viewlayer from dependencies in package.json
+  const matches = Object.keys(rendererPackages).filter((pkg) => !!allDependencies[pkg]);
+  let [rendererPackage] = matches;
+  if (matches.length > 1) {
+    const response = await prompts([
+      {
+        type: 'select',
+        name: 'rendererPackage',
+        message:
+          'Tried to detect a Storybook renderer in your project but found multiple. This could happen in monorepos, when projects contain multiple Storybook packages in package.json. Please select the correct one:',
+        choices: matches.map((type) => ({
+          title: type,
+          value: type,
+        })),
+      },
+    ]);
+    rendererPackage = response.rendererPackage;
+  }
+
+  return rendererPackage;
+};
