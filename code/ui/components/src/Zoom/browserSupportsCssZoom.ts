e2376f74f44995f8db9a29b81fee4c9130417bc2
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-02-13 13:47:03 +0000

    Update browserSupportsCssZoom.ts

--- code/ui/components/src/Zoom/browserSupportsCssZoom.ts
+++ code/ui/components/src/Zoom/browserSupportsCssZoom.ts
@@ -3,7 +3,6 @@ import { global } from '@storybook/global';
 export function browserSupportsCssZoom(): boolean {
   try {
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-    // @ts-expect-error (we're testing for browser support)
     if (!isSafari) {
       return global.document.implementation.createHTMLDocument('').body.style.zoom !== undefined;
     } else return false
