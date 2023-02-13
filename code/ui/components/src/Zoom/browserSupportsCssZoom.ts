e3e80ed7552bb3863754e072407a38e86f19e83a
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-02-13 14:12:45 +0000

    added back type exception

--- code/ui/components/src/Zoom/browserSupportsCssZoom.ts
+++ code/ui/components/src/Zoom/browserSupportsCssZoom.ts
@@ -4,8 +4,10 @@ export function browserSupportsCssZoom(): boolean {
   try {
     const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
     if (!isSafari) {
+      // @ts-expect-error (we're testing for browser support)
       return global.document.implementation.createHTMLDocument('').body.style.zoom !== undefined;
-    } else return false
+    }
+    return false;
   } catch (error) {
     return false;
   }
