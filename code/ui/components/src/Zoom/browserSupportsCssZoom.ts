03bbffa95f88e2ead339b45f21cabb6275743e7f
Flynn <88561679+FlynnFc@users.noreply.github.com>
2023-02-14 08:16:20 +0000

    Update code/ui/components/src/Zoom/browserSupportsCssZoom.ts
    Co-authored-by: Norbert de Langen <ndelangen@me.com>

--- code/ui/components/src/Zoom/browserSupportsCssZoom.ts
+++ code/ui/components/src/Zoom/browserSupportsCssZoom.ts
@@ -2,7 +2,7 @@ export function browserSupportsCssZoom(): boolean {
   try {
     // Checks if safari or firefox is being used.
     // This check can be removed when zoom becomes standard css. Currently firefox does not support it and there is a bug in safari see here:  https://developer.mozilla.org/en-US/docs/Web/CSS/zoom#browser_compatibility
-    // regex checks if there are other bronwers because "safari" is also present when using chrome or an andriod browser whilst on mac/iphone see more here : https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#which_part_of_the_user_agent_contains_the_information_you_are_looking_for
+    // regex checks if there are other browsers because "safari" is also present when using chrome or an android browser whilst on mac/iPhone see more here : https://developer.mozilla.org/en-US/docs/Web/HTTP/Browser_detection_using_the_user_agent#which_part_of_the_user_agent_contains_the_information_you_are_looking_for
     const isCompatible = /^((?!chrome|android).)*safari|firefox/i.test(navigator.userAgent);
     return isCompatible;
   } catch (error) {
