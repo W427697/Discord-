11edcabc6af9455f8afef000359e7e46ba8a7f7b
Michael Shilman <shilman@users.noreply.github.com>
2023-02-16 01:12:14 +0800

    Update deprecated v6 store APIs to use renderer instead of framework

--- code/renderers/vue/src/public-api.ts
+++ code/renderers/vue/src/public-api.ts
@@ -6,7 +6,7 @@ import type { VueRenderer } from './types';
 import { renderToCanvas, render } from './render';
 import { decorateStory } from './decorateStory';
 
-const FRAMEWORK = 'vue';
+const RENDERER = 'vue';
 
 interface ClientApi extends Addon_ClientStoryApi<VueRenderer['storyResult']> {
   configure(loader: Addon_Loadable, module: NodeModule): void;
@@ -19,10 +19,10 @@ const api = start<VueRenderer>(renderToCanvas, { decorateStory, render });
 
 export const storiesOf: ClientApi['storiesOf'] = (kind, m) => {
   return (api.clientApi.storiesOf(kind, m) as ReturnType<ClientApi['storiesOf']>).addParameters({
-    framework: FRAMEWORK,
+    renderer: RENDERER,
   });
 };
 
-export const configure: ClientApi['configure'] = (...args) => api.configure(FRAMEWORK, ...args);
+export const configure: ClientApi['configure'] = (...args) => api.configure(RENDERER, ...args);
 export const forceReRender: ClientApi['forceReRender'] = api.forceReRender;
 export const raw: ClientApi['raw'] = api.clientApi.raw;
