78c27bd73e96b9652fe901c3e77e5070e4dd8d7f
chakir qatab <javachakir@gmail.com>
2023-02-17 18:16:04 +0400

    cleanup code, remove logs

--- code/renderers/vue3/src/decorateStory.ts
+++ code/renderers/vue3/src/decorateStory.ts
@@ -1,6 +1,6 @@
 import type { ConcreteComponent, Component, ComponentOptions } from 'vue';
 import { h } from 'vue';
-import type { DecoratorFunction, StoryContext, LegacyStoryFn, Args } from '@storybook/types';
+import type { DecoratorFunction, StoryContext, LegacyStoryFn } from '@storybook/types';
 import { sanitizeStoryContextUpdate } from '@storybook/preview-api';
 
 import type { VueRenderer } from './types';
@@ -26,14 +26,13 @@ function prepare(
   }
 
   if (innerStory) {
-    console.log('--- innerStory ', innerStory);
     return {
       // Normalize so we can always spread an object
       ...normalizeFunctionalComponent(story),
       components: { ...(story.components || {}), story: innerStory },
     };
   }
-  console.log('--- story ', story);
+
   return (args) => h(story, args);
 }
 
@@ -41,8 +40,6 @@ export function decorateStory(
   storyFn: LegacyStoryFn<VueRenderer>,
   decorators: DecoratorFunction<VueRenderer>[]
 ): LegacyStoryFn<VueRenderer> {
-  console.log('---decorateStory storyFn ', storyFn);
-
   return decorators.reduce(
     (decorated: LegacyStoryFn<VueRenderer>, decorator) => (context: StoryContext<VueRenderer>) => {
       let story: VueRenderer['storyResult'] | undefined;
@@ -62,14 +59,9 @@ export function decorateStory(
       if (decoratedStory === story) {
         return story;
       }
-      console.log('~args ', context.args);
-      console.log('~story ', story);
-      console.log('~decoratedStory ', decoratedStory);
+
       const renderStoryFn = h(story, context.args);
       const StoryComponentFn = (props: any) => () => h(renderStoryFn, props);
-
-      console.log(' StoryComponentFn ', StoryComponentFn);
-      console.log(' StoryComponentFn(context.args) ', StoryComponentFn(context.args));
 
       return prepare(decoratedStory, StoryComponentFn(context.args)) as VueRenderer['storyResult'];
     },
