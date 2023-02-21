c9c35fa1089e646ce2aa7e2e66af77d45b590900
Jeppe Reinhold <jeppe@reinhold.is>
2023-02-21 23:17:31 +0100

    add stories for the DocsPage block

--- code/ui/blocks/src/examples/DocsPageParameters.stories.tsx
+++ code/ui/blocks/src/examples/DocsPageParameters.stories.tsx
@@ -2,12 +2,18 @@ import type { Meta, StoryObj } from '@storybook/react';
 
 import { EmptyExample } from './EmptyExample';
 
+/**
+ * This is the description for the component
+ */
 const meta = {
-  title: 'examples/Stories for the Stories and Primary Block',
+  title: 'examples/Stories for the DocsPage',
   component: EmptyExample,
 } satisfies Meta<typeof EmptyExample>;
 export default meta;
 
 type Story = StoryObj<typeof meta>;
 
+/**
+ * This is the description for the primary story
+ */
 export const SingleStory: Story = {};
