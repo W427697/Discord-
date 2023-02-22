c3ae79711609b6da98592a3c828f5e77a1177dbc
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2023-02-22 10:03:24 +0900

    feat(addon): add description

--- code/addons/essentials/src/index.ts
+++ code/addons/essentials/src/index.ts
@@ -4,13 +4,53 @@ import { serverRequire } from '@storybook/core-common';
 
 export interface PresetOptions {
   configDir: string;
+  /**
+   * Allow to use @storybook/addon-docs
+   * @see https://storybook.js.org/addons/@storybook/addon-docs
+   * @default true
+   */
   docs?: boolean;
+  /**
+   * Allow to use @storybook/addon-controls
+   * @see https://storybook.js.org/addons/@storybook/addon-controls
+   * @default true
+   */
   controls?: boolean;
+  /**
+   * Allow to use @storybook/addon-actions
+   * @see https://storybook.js.org/addons/@storybook/addon-actions
+   * @default true
+   */
   actions?: boolean;
+  /**
+   * Allow to use @storybook/addon-backgrounds
+   * @see https://storybook.js.org/addons/@storybook/addon-backgrounds
+   * @default true
+   */
   backgrounds?: boolean;
+  /**
+   * Allow to use @storybook/addon-viewport
+   * @see https://storybook.js.org/addons/@storybook/addon-viewport
+   * @default true
+   */
   viewport?: boolean;
+  /**
+   * Allow to use @storybook/addon-toolbars
+   * @see https://storybook.js.org/addons/@storybook/addon-toolbars
+   * @default true
+   */
   toolbars?: boolean;
+  /**
+   * Allow to use @storybook/addon-measure
+   * @see https://storybook.js.org/addons/@storybook/addon-measure
+   * @default true
+   */
   measure?: boolean;
+  /**
+   * Allow to use @storybook/addon-outline
+   * @see https://storybook.js.org/addons/@storybook/addon-outline
+   * @default true
+   */
   outline?: boolean;
 }
 
