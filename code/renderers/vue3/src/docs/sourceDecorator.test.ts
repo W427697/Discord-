207ab63e8d9cf88f6161b633d8455936abd1eb1d
chakir qatab <javachakir@gmail.com>
2023-02-24 16:00:32 +0400

    write tests for 2 main src decorator functions

--- code/renderers/vue3/src/docs/sourceDecorator.test.ts
+++ code/renderers/vue3/src/docs/sourceDecorator.test.ts
@@ -1,7 +1,13 @@
 import { describe, expect, test } from '@jest/globals';
 import type { Args } from '@storybook/types';
 
-import { generateSource, getComponentsFromTemplate } from './sourceDecorator';
+import type { ArgsType } from 'jest-mock';
+import {
+  generateSource,
+  getComponentsFromTemplate,
+  mapAttributesAndDirectives,
+  generateAttributesSource,
+} from './sourceDecorator';
 
 expect.addSnapshotSerializer({
   print: (val: any) => val,
@@ -23,72 +29,266 @@ function generateForArgs(
   return generateSource(components, args, generateArgTypes(args, slotProps), true);
 }
 
-describe('generateSource snippet Vue3', () => {
-  test('boolean true', () => {
-    expect(
-      generateForArgs({ booleanProp: true }, [], `<Component :boolean-prop='args.booleanProp' />`)
-    ).toMatchInlineSnapshot(`<Component :boolean-prop='true'/>`);
+describe('Vue3: sourceDecorator->mapAttributesAndDirective()', () => {
+  test('camelCase boolean Arg', () => {
+    expect(mapAttributesAndDirectives({ camelCaseBooleanArg: true })).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          arg: Object {
+            content: camel-case-boolean-arg,
+            loc: Object {
+              source: camel-case-boolean-arg,
+            },
+          },
+          exp: Object {
+            isStatic: false,
+            loc: Object {
+              source: true,
+            },
+          },
+          loc: Object {
+            source: :camel-case-boolean-arg='true',
+          },
+          modifiers: Array [
+            ,
+          ],
+          name: bind,
+          type: 6,
+        },
+      ]
+    `);
   });
-  test('boolean false', () => {
-    expect(
-      generateForArgs({ booleanProp: false }, [], `<Component :boolean-prop='args.booleanProp' />`)
-    ).toMatchInlineSnapshot(`<Component :boolean-prop='false'/>`);
+  test('camelCase string Arg', () => {
+    expect(mapAttributesAndDirectives({ camelCaseStringArg: 'foo' })).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          arg: Object {
+            content: camel-case-string-arg,
+            loc: Object {
+              source: camel-case-string-arg,
+            },
+          },
+          exp: Object {
+            isStatic: false,
+            loc: Object {
+              source: foo,
+            },
+          },
+          loc: Object {
+            source: camel-case-string-arg='foo',
+          },
+          modifiers: Array [
+            ,
+          ],
+          name: bind,
+          type: 6,
+        },
+      ]
+    `);
   });
-  // test('null property', () => {
-  //   expect(generateForArgs({ nullProp: null })).toMatchInlineSnapshot(
-  //     `<Component :null-prop='nullProp'/>`
-  //   );
-  // });
-  // test('string property', () => {
-  //   expect(generateForArgs({ stringProp: 'mystr' })).toMatchInlineSnapshot(
-  //     `<Component :string-prop='stringProp'/>`
-  //   );
-  // });
-  // test('number property', () => {
-  //   expect(generateForArgs({ numberProp: 42 })).toMatchInlineSnapshot(
-  //     `<Component :number-prop='numberProp'/>`
-  //   );
-  // });
-  // test('object property', () => {
-  //   expect(generateForArgs({ objProp: { x: true } })).toMatchInlineSnapshot(
-  //     `<Component :obj-prop='objProp'/>`
-  //   );
-  // });
-  // test('multiple properties', () => {
-  //   expect(generateForArgs({ a: 1, b: 2 })).toMatchInlineSnapshot(`<Component :a='a' :b='b'/>`);
-  // });
-  // test('1 slot property', () => {
-  //   expect(generateForArgs({ content: 'xyz', myProp: 'abc' }, ['content'])).toMatchInlineSnapshot(`
-  //     <Component :my-prop='myProp'>
-  //       {{ content }}
-  //     </Component>
-  //   `);
-  // });
-  // test('multiple slot property with second slot value not set', () => {
-  //   expect(generateForArgs({ content: 'xyz', myProp: 'abc' }, ['content', 'footer']))
-  //     .toMatchInlineSnapshot(`
-  //     <Component :my-prop='myProp'>
-  //       {{ content }}
-  //     </Component>
-  //   `);
-  // });
-  // test('multiple slot property with second slot value is set', () => {
-  //   expect(generateForArgs({ content: 'xyz', footer: 'foo', myProp: 'abc' }, ['content', 'footer']))
-  //     .toMatchInlineSnapshot(`
-  //     <Component :my-prop='myProp'>
-  //       <template #content>{{ content }}</template>
-  //       <template #footer>{{ footer }}</template>
-  //     </Component>
-  //   `);
-  // });
-  // // test mutil components
-  // test('multi component with boolean true', () => {
-  //   expect(generateMultiComponentForArgs({ booleanProp: true })).toMatchInlineSnapshot(`
-  //     <Component :boolean-prop='booleanProp'/>
-  //     <Component :boolean-prop='booleanProp'/>
-  //   `);
-  // });
-  // test('component is not set', () => {
-  //   expect(generateSource(null, {}, {})).toBeNull();
-  // });
+  test('boolean arg', () => {
+    expect(mapAttributesAndDirectives({ booleanarg: true })).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          arg: Object {
+            content: booleanarg,
+            loc: Object {
+              source: booleanarg,
+            },
+          },
+          exp: Object {
+            isStatic: false,
+            loc: Object {
+              source: true,
+            },
+          },
+          loc: Object {
+            source: :booleanarg='true',
+          },
+          modifiers: Array [
+            ,
+          ],
+          name: bind,
+          type: 6,
+        },
+      ]
+    `);
+  });
+  test('string arg', () => {
+    expect(mapAttributesAndDirectives({ stringarg: 'bar' })).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          arg: Object {
+            content: stringarg,
+            loc: Object {
+              source: stringarg,
+            },
+          },
+          exp: Object {
+            isStatic: false,
+            loc: Object {
+              source: bar,
+            },
+          },
+          loc: Object {
+            source: stringarg='bar',
+          },
+          modifiers: Array [
+            ,
+          ],
+          name: bind,
+          type: 6,
+        },
+      ]
+    `);
+  });
+  test('number arg', () => {
+    expect(mapAttributesAndDirectives({ numberarg: 2023 })).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          arg: Object {
+            content: numberarg,
+            loc: Object {
+              source: numberarg,
+            },
+          },
+          exp: Object {
+            isStatic: false,
+            loc: Object {
+              source: 2023,
+            },
+          },
+          loc: Object {
+            source: :numberarg='2023',
+          },
+          modifiers: Array [
+            ,
+          ],
+          name: bind,
+          type: 6,
+        },
+      ]
+    `);
+  });
+  test('camelCase boolean, string, and number Args', () => {
+    expect(
+      mapAttributesAndDirectives({
+        camelCaseBooleanArg: true,
+        camelCaseStringArg: 'foo',
+        cameCaseNumberArg: 2023,
+      })
+    ).toMatchInlineSnapshot(`
+      Array [
+        Object {
+          arg: Object {
+            content: camel-case-boolean-arg,
+            loc: Object {
+              source: camel-case-boolean-arg,
+            },
+          },
+          exp: Object {
+            isStatic: false,
+            loc: Object {
+              source: true,
+            },
+          },
+          loc: Object {
+            source: :camel-case-boolean-arg='true',
+          },
+          modifiers: Array [
+            ,
+          ],
+          name: bind,
+          type: 6,
+        },
+        Object {
+          arg: Object {
+            content: camel-case-string-arg,
+            loc: Object {
+              source: camel-case-string-arg,
+            },
+          },
+          exp: Object {
+            isStatic: false,
+            loc: Object {
+              source: foo,
+            },
+          },
+          loc: Object {
+            source: camel-case-string-arg='foo',
+          },
+          modifiers: Array [
+            ,
+          ],
+          name: bind,
+          type: 6,
+        },
+        Object {
+          arg: Object {
+            content: came-case-number-arg,
+            loc: Object {
+              source: came-case-number-arg,
+            },
+          },
+          exp: Object {
+            isStatic: false,
+            loc: Object {
+              source: 2023,
+            },
+          },
+          loc: Object {
+            source: :came-case-number-arg='2023',
+          },
+          modifiers: Array [
+            ,
+          ],
+          name: bind,
+          type: 6,
+        },
+      ]
+    `);
+  });
 });
+
+describe('Vue3: sourceDecorator->generateAttributesSource()', () => {
+  test('camelCase boolean Arg', () => {
+    expect(
+      generateAttributesSource(
+        mapAttributesAndDirectives({ camelCaseBooleanArg: true }),
+        { camelCaseBooleanArg: true },
+        [{ camelCaseBooleanArg: { type: 'boolean' } }] as ArgsType<Args>
+      )
+    ).toMatchInlineSnapshot(`:camel-case-boolean-arg='true'`);
+  });
+  test('camelCase string Arg', () => {
+    expect(
+      generateAttributesSource(
+        mapAttributesAndDirectives({ camelCaseStringArg: 'foo' }),
+        { camelCaseStringArg: 'foo' },
+        [{ camelCaseStringArg: { type: 'string' } }] as ArgsType<Args>
+      )
+    ).toMatchInlineSnapshot(`camel-case-string-arg='foo'`);
+  });
+
+  test('camelCase boolean, string, and number Args', () => {
+    expect(
+      generateAttributesSource(
+        mapAttributesAndDirectives({
+          camelCaseBooleanArg: true,
+          camelCaseStringArg: 'foo',
+          cameCaseNumberArg: 2023,
+        }),
+        {
+          camelCaseBooleanArg: true,
+          camelCaseStringArg: 'foo',
+          cameCaseNumberArg: 2023,
+        },
+        [] as ArgsType<Args>
+      )
+    ).toMatchInlineSnapshot(
+      `:camel-case-boolean-arg='true' camel-case-string-arg='foo' :came-case-number-arg='2023'`
+    );
+  });
+});
+
+describe('generateSource snippet Vue3', () => {});
