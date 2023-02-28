485eba6611dbd319a2c92786e2ea29258666b5c6
Jeppe Reinhold <jeppe@chromatic.com>
2023-02-28 15:25:08 +0100

    simplify apply in plugin

--- code/frameworks/sveltekit/src/plugins/config-overrides.ts
+++ code/frameworks/sveltekit/src/plugins/config-overrides.ts
@@ -4,7 +4,7 @@ export function configOverrides() {
   return {
     // SvelteKit sets SSR, we need it to be false when building
     name: 'storybook:sveltekit-overrides',
-    apply: (_, { command }) => command === 'build',
+    apply: 'build',
     config: () => {
       return { build: { ssr: false } };
     },
