9e5f39c33fc60103c4a22e673b97fda67e83ff16
Norbert de Langen <ndelangen@me.com>
2023-03-01 14:46:38 +0100

    add better support for workspaces for pnpm and yarn when adding storybook/dependencies in CLI

--- code/lib/cli/src/js-package-manager/Yarn1Proxy.ts
+++ code/lib/cli/src/js-package-manager/Yarn1Proxy.ts
@@ -4,6 +4,36 @@ import type { PackageJson } from './PackageJson';
 export class Yarn1Proxy extends JsPackageManager {
   readonly type = 'yarn1';
 
+  installArgs: string[] | undefined;
+
+  getInstallArgs(): string[] {
+    if (!this.installArgs) {
+      this.installArgs = [];
+
+      if (this.detectWorkspaceRoot()) {
+        this.installArgs.push('-W');
+      }
+    }
+    return this.installArgs;
+  }
+
+  detectWorkspaceRoot() {
+    const { workspaces } = this.readPackageJson();
+
+    if (Array.isArray(workspaces)) {
+      return workspaces.length > 0;
+    }
+
+    if (workspaces && typeof workspaces === 'object') {
+      if (workspaces.packages) {
+        return workspaces.packages.length > 0;
+      }
+      return false;
+    }
+
+    return false;
+  }
+
   initPackageJson() {
     return this.executeCommand('yarn', ['init', '-y']);
   }
@@ -30,7 +60,7 @@ export class Yarn1Proxy extends JsPackageManager {
   }
 
   protected runInstall(): void {
-    this.executeCommand('yarn', [], 'inherit');
+    this.executeCommand('yarn', ['install', ...this.getInstallArgs()], 'inherit');
   }
 
   protected runAddDeps(dependencies: string[], installAsDevDependencies: boolean): void {
@@ -40,13 +70,13 @@ export class Yarn1Proxy extends JsPackageManager {
       args = ['-D', ...args];
     }
 
-    this.executeCommand('yarn', ['add', ...args], 'inherit');
+    this.executeCommand('yarn', ['add', ...args, ...this.getInstallArgs()], 'inherit');
   }
 
   protected runRemoveDeps(dependencies: string[]): void {
     const args = ['--ignore-workspace-root-check', ...dependencies];
 
-    this.executeCommand('yarn', ['remove', ...args], 'inherit');
+    this.executeCommand('yarn', ['remove', ...args, ...this.getInstallArgs()], 'inherit');
   }
 
   protected runGetVersions<T extends boolean>(
