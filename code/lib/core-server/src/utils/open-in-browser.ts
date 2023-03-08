7d81483e4810e0084a0e14458632e62c8ebd049d
thtliife <9210782+thtliife@users.noreply.github.com>
2023-03-08 14:19:23 +1000

    fix: Storybook does not respect BROWSER env var
    Storybook does not respect BROWSER env var if default browser is not chromium based.
    This commit adds a check for the environment variable, and passes it to the open package as an argument if the specified browser is not chromium based.
    
    Closes #21343

--- code/lib/core-server/src/utils/open-in-browser.ts
+++ code/lib/core-server/src/utils/open-in-browser.ts
@@ -5,14 +5,29 @@ import getDefaultBrowser from '@aw-web-design/x-default-browser';
 import { dedent } from 'ts-dedent';
 
 export function openInBrowser(address: string) {
+  const browserEnvVar = process.env.BROWSER;
+  const userBrowserIsChrome =
+    browserEnvVar === 'chrome' ||
+    browserEnvVar === 'chromium' ||
+    browserEnvVar === 'brave' ||
+    browserEnvVar === 'com.brave.browser';
+
+  const openOptions = browserEnvVar ? { app: { name: browserEnvVar } } : {};
+
   getDefaultBrowser(async (err: any, res: any) => {
     try {
-      if (res && (res.isChrome || res.isChromium || res.identity === 'com.brave.browser')) {
+      if (
+        res &&
+        (res.isChrome ||
+          res.isChromium ||
+          res.identity === 'com.brave.browser' ||
+          userBrowserIsChrome)
+      ) {
         // We use betterOpn for Chrome because it is better at handling which chrome tab
         // or window the preview loads in.
         betterOpn(address);
       } else {
-        await open(address);
+        await open(address, openOptions);
       }
     } catch (error) {
       logger.error(dedent`
