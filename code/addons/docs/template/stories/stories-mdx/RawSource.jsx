5039b3b757418ad381b2e0950b0b6e148d850f22
Nathaniel Hunter <42shadow42@gmail.com>
2023-03-18 16:39:52 -0500

    Added stories to demonstrate the fix

---
+++ code/addons/docs/template/stories/stories-mdx/RawSource.jsx
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react';
+
+export const MY_VALUE_I_WANT_TO_SHOW_IN_STORY = 'Hello World!';
+
+export const RawSource = () => {
+  return <Fragment>{MY_VALUE_I_WANT_TO_SHOW_IN_STORY}</Fragment>;
+};
