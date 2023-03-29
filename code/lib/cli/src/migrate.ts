bd31273cf13c51d8843f089dd5893e823126bbba
Kasper Peulen <kasperpeulen@gmail.com>
2023-03-29 12:08:08 +0200

    Only run it in mdx-to-csf codemod

--- code/lib/cli/src/migrate.ts
+++ code/lib/cli/src/migrate.ts
@@ -6,7 +6,9 @@ export async function migrate(migration: any, { glob, dryRun, list, rename, logg
   if (list) {
     listCodemods().forEach((key: any) => logger.log(key));
   } else if (migration) {
-    await runFixes({ fixes: [bareMdxStoriesGlob] });
+    if (migration === 'mdx-to-csf') {
+      await runFixes({ fixes: [bareMdxStoriesGlob] });
+    }
     await runCodemod(migration, { glob, dryRun, logger, rename, parser });
   } else {
     throw new Error('Migrate: please specify a migration name or --list');
