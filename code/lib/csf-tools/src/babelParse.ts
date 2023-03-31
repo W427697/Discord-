bcfb7cf95469b6bbbb6079b534aa55a8e2792ee6
Federico Zivolo <5382443+FezVrasta@users.noreply.github.com>
2023-03-31 16:19:17 +0200

    fix: allow Flow syntax in stories

--- code/lib/csf-tools/src/babelParse.ts
+++ code/lib/csf-tools/src/babelParse.ts
@@ -1,11 +1,23 @@
-import * as parser from '@babel/parser';
+import * as babelParser from '@babel/parser';
 import * as recast from 'recast';
 import type { ParserOptions } from '@babel/parser';
+
+function parseWithFlowOrTypescript(source) {
+  const flowCommentPattern = /^\s*\/\/\s*@flow/;
+  const useFlowPlugin = flowCommentPattern.test(source);
+
+  const parserPlugins = useFlowPlugin ? ['flow'] : ['typescript'];
+
+  return babelParser.parse(source, {
+    sourceType: 'module',
+    plugins: parserPlugins,
+  });
+}
 
 export const parserOptions: ParserOptions = {
   sourceType: 'module',
   // FIXME: we should get this from the project config somehow?
-  plugins: ['jsx', 'typescript', 'decorators-legacy', 'classProperties'],
+  plugins: ['jsx', 'decorators-legacy', 'classProperties'],
   tokens: true,
 };
 
@@ -13,12 +25,12 @@ export const babelParse = (code: string) => {
   return recast.parse(code, {
     parser: {
       parse(source: string) {
-        return parser.parse(source, parserOptions);
+        return parseWithFlowOrTypescript(source); // Use the custom parser function here
       },
     },
   });
 };
 
 export const babelParseExpression = (code: string) => {
-  return parser.parseExpression(code, parserOptions);
+  return babelParser.parseExpression(code, parserOptions);
 };
