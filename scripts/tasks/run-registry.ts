c9d9efd331b314c1b30e6593f0c6a6167d9d1a7b
Norbert de Langen <ndelangen@me.com>
2023-04-03 15:38:49 +0200

    should say --open

--- scripts/tasks/run-registry.ts
+++ scripts/tasks/run-registry.ts
@@ -8,7 +8,7 @@ export async function runRegistry({ dryRun, debug }: { dryRun?: boolean; debug?:
   const controller = new AbortController();
 
   exec(
-    'yarn local-registry --publish',
+    'yarn local-registry --open',
     { cwd: CODE_DIRECTORY, env: { CI: 'true' } },
     { dryRun, debug, signal: controller.signal }
   ).catch((err) => {
