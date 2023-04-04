b5fbed0cdaf7ecbade378162890d85c94048987d
Tom Coleman <tom@chromatic.com>
2023-04-04 17:25:13 +1000

    Add story to demonstrate #21900

--- code/renderers/react/template/stories/decorators.stories.tsx
+++ code/renderers/react/template/stories/decorators.stories.tsx
@@ -1,11 +1,12 @@
 import type { FC } from 'react';
-import React from 'react';
+import React, { useContext, createContext } from 'react';
 import type { StoryObj, Meta } from '@storybook/react';
 
 const Component: FC = () => <p>Story</p>;
 
 export default {
   component: Component,
+  tags: ['autodocs'],
   decorators: [
     (Story) => (
       <>
@@ -25,4 +26,23 @@ export const All: StoryObj<typeof Component> = {
       </>
     ),
   ],
+};
+
+// This story will error if `parameters.docs.source.excludeDecorators` is true:
+// See https://github.com/storybookjs/storybook/issues/21900
+const TestContext = createContext<boolean>(false);
+export const Context: StoryObj<typeof Component> = {
+  // parameters: { docs: { source: { excludeDecorators: true } } },
+  decorators: [
+    (Story) => (
+      <TestContext.Provider value>
+        <Story />
+      </TestContext.Provider>
+    ),
+  ],
+  render: (args, context) => {
+    const arg = useContext(TestContext);
+    if (!arg) throw new Error('Arg not set');
+    return <p>Story</p>;
+  },
 };
