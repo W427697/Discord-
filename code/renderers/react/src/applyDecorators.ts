3f3ca00fb71100a97d97b0f1ce89d067340cdc13
Tom Coleman <tom@chromatic.com>
2023-04-04 17:29:59 +1000

    Move JSX decorator to the end of the list
    This is a bit of a hacky way to acheive `excludeDecorators` without actually excluding them.

---
+++ code/renderers/react/src/applyDecorators.ts
@@ -0,0 +1,17 @@
+import { defaultDecorateStory } from '@storybook/preview-api';
+import type { LegacyStoryFn, DecoratorFunction } from '@storybook/types';
+
+import type { ReactRenderer } from './types';
+import { jsxDecorator } from './docs/jsxDecorator';
+
+export const applyDecorators = (
+  storyFn: LegacyStoryFn<ReactRenderer>,
+  decorators: DecoratorFunction<ReactRenderer>[]
+): LegacyStoryFn<ReactRenderer> => {
+  const jsxIndex = decorators.findIndex((d) => d.originalFn === jsxDecorator);
+
+  const reorderedDecorators =
+    jsxIndex === -1 ? decorators : [...decorators.splice(jsxIndex, 1), ...decorators];
+
+  return defaultDecorateStory(storyFn, reorderedDecorators);
+};
