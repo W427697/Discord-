92dd43c456f7351471d4c87f7bda4693b69579c8
chakAs3 <javachakir@gmail.com>
2023-04-05 22:27:34 +0400

    revert render to next branch

--- code/renderers/vue3/src/decorateStory.ts
+++ code/renderers/vue3/src/decorateStory.ts
@@ -1,5 +1,5 @@
 import type { ConcreteComponent, Component, ComponentOptions } from 'vue';
-import { isVNode, h } from 'vue';
+import { h } from 'vue';
 import type { DecoratorFunction, StoryContext, LegacyStoryFn } from '@storybook/types';
 import { sanitizeStoryContextUpdate } from '@storybook/preview-api';
 
@@ -11,7 +11,6 @@ import type { VueRenderer } from './types';
   The concept is taken from Vue 3's `defineComponent` but changed from creating a `setup`
   method on the ComponentOptions so end-users don't need to specify a "thunk" as a decorator.
  */
-
 function normalizeFunctionalComponent(options: ConcreteComponent): ComponentOptions {
   return typeof options === 'function' ? { render: options, name: options.name } : options;
 }
@@ -20,7 +19,7 @@ function prepare(
   rawStory: VueRenderer['storyResult'],
   innerStory?: ConcreteComponent
 ): Component | null {
-  const story = normalizeFunctionalComponent(rawStory as ComponentOptions);
+  const story = rawStory as ComponentOptions;
 
   if (story == null) {
     return null;
@@ -47,31 +46,25 @@ export function decorateStory(
 ): LegacyStoryFn<VueRenderer> {
   return decorators.reduce(
     (decorated: LegacyStoryFn<VueRenderer>, decorator) => (context: StoryContext<VueRenderer>) => {
-      let story: VueRenderer['storyResult'] = { isNull: true };
+      let story: VueRenderer['storyResult'] | undefined;
+
       const decoratedStory: VueRenderer['storyResult'] = decorator((update) => {
         story = decorated({
           ...context,
           ...sanitizeStoryContextUpdate(update),
         });
-        const argsChanged =
-          update && update.args && Object.keys(update).length === 1 && !isVNode(story);
-        // TODO: this is a hack to avoid re-rendering the story when the args are not changed
-        // we should find a better way to do this
-        // i should get only update = { args: { ...context.args, text:... } } from the decorator and not the whole context
-        if (argsChanged) {
-          story = h(story, update.args);
-        }
         return story;
       }, context);
 
-      if (story.isNull) story = decorated(context);
+      if (!story) {
+        story = decorated(context);
+      }
 
       if (decoratedStory === story) {
         return story;
       }
-      const props = story.props ?? context.args;
-      const innerStory = () => h(story, props);
-      return prepare(decoratedStory, innerStory) as VueRenderer['storyResult'];
+
+      return prepare(decoratedStory, h(story, context.args)) as VueRenderer['storyResult'];
     },
     (context) => prepare(storyFn(context)) as LegacyStoryFn<VueRenderer>
   );
