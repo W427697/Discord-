7e7ee0a1a219bccae4281168bee87d262877b397
Evan Sosenko <razorx@evansosenko.com>
2023-04-05 22:47:31 -0700

    Implement ToolbarTextInput

--- code/addons/toolbars/src/components/ToolbarTextInput.tsx
+++ code/addons/toolbars/src/components/ToolbarTextInput.tsx
@@ -1,5 +1,6 @@
-import type { FC } from 'react';
-import React from 'react';
+import React, { type FC, useEffect, useCallback, type ChangeEventHandler } from 'react';
+import { useGlobals } from '@storybook/manager-api';
+import { Icons, TooltipMessage, WithTooltip } from '@storybook/components';
 import type { NormalizedToolbarArgTypeText } from '../types';
 
 type ToolbarTextInputProps = NormalizedToolbarArgTypeText;
@@ -8,11 +9,31 @@ export const ToolbarTextInput: FC<ToolbarTextInputProps> = ({
   id,
   name,
   description,
-  toolbar: { title },
+  toolbar: { icon, title, defaultValue, isSecret },
 }) => {
+  const [globals, updateGlobals] = useGlobals();
+  const currentValue = globals[id];
+  useEffect(() => {
+    if (currentValue == null) updateGlobals({ [id]: defaultValue });
+  });
+
+  const handleInputChange = useCallback(
+    (e) => {
+      const value = e?.target?.value;
+      if (value != null) updateGlobals({ [id]: value });
+    },
+    [updateGlobals]
+  ) as ChangeEventHandler<HTMLInputElement>;
+
   return (
-    <p>
-      {name} {title}
-    </p>
+    <WithTooltip tooltip={<TooltipMessage title={title} desc={description} />}>
+      {icon && <Icons icon={icon} />}
+      {title ? `\xa0${title}` : `\xa0${name}`}
+      <input
+        type={isSecret ? 'password' : 'text'}
+        value={currentValue}
+        onChange={handleInputChange}
+      />
+    </WithTooltip>
   );
 };
