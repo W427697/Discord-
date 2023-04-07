b09cc250843c2d9b6d427471e3bae6d92098db65
Evan Sosenko <razorx@evansosenko.com>
2023-04-06 17:20:58 -0700

    Handle defaultValue

--- code/addons/toolbars/src/components/ToolbarTextInput.tsx
+++ code/addons/toolbars/src/components/ToolbarTextInput.tsx
@@ -9,11 +9,12 @@ type ToolbarTextInputProps = NormalizedToolbarArgTypeText;
 export const ToolbarTextInput: FC<ToolbarTextInputProps> = ({
   id,
   description,
+  defaultValue,
   toolbar: { icon, title, isSecret },
 }) => {
   const [globals, updateGlobals] = useGlobals();
   const [isTooltipVisible, setIsTooltipVisible] = useState(false);
-  const currentValue = globals[id];
+  const currentValue = globals[id] ?? defaultValue;
   const hasGlobalValue = Boolean(currentValue);
 
   const handleInputChange = useCallback(
