4c37e7922267885bea49155a283289f93ec831b6
Norbert de Langen <ndelangen@me.com>
2023-04-13 11:35:05 +0200

    rename file

--- code/lib/builder-webpack5/src/preview/iframe-webpack.config.ts
+++ code/lib/builder-webpack5/src/preview/iframe-webpack.config.ts
@@ -25,7 +25,7 @@ import {
 import { toRequireContextString, toImportFn } from '@storybook/core-webpack';
 import { dedent } from 'ts-dedent';
 import type { BuilderOptions, TypescriptOptions } from '../types';
-import { createBabelLoader, createSWCLoader } from './babel-loader-preview';
+import { createBabelLoader, createSWCLoader } from './loaders';
 
 const wrapForPnP = (input: string) => dirname(require.resolve(join(input, 'package.json')));
 
