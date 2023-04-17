036e28a814613a15e0a2f3df9b419e134087e517
Kasper Peulen <kasperpeulen@gmail.com>
2023-04-17 14:13:57 +0200

    Update tests

---
+++ code/renderers/vue3/template/stories/CustomRenderOptionsArgsFromProps.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta } from '@storybook/vue3';
+import { defineComponent } from 'vue';
+import Reactivity from './Reactivity.vue';
+import * as ReactiveDecorators from './ReactiveDecorators.stories';
+
+const meta = {
+  ...ReactiveDecorators.default,
+  component: Reactivity,
+  render: (args, { argTypes }) => {
+    return defineComponent({
+      props: Object.keys(argTypes),
+      components: { Reactivity },
+      template:
+        '<div>Custom render uses options api and binds args to props: <Reactivity v-bind="$props"/></div>',
+    });
+  },
+} satisfies Meta<typeof Reactivity>;
+
+export default meta;
+
+export {
+  NoDecorators,
+  DecoratorFunctionalComponent,
+  DecoratorFunctionalComponentArgsFromContext,
+  DecoratorFunctionalComponentArgsFromProps,
+  DecoratorComponentOptions,
+  DecoratorComponentOptionsArgsFromData,
+  DecoratorComponentOptionsArgsFromProps,
+} from './ReactiveDecorators.stories';
