2bdb85d5b6f031544fcb59f7094a76c8f5333cbc
Tom Coleman <tom@chromatic.com>
2023-04-19 21:02:42 +1000

    Merge pull request #22154 from storybookjs/tom/21798-set-docs-parameters-in-index

--- code/e2e-tests/addon-backgrounds.spec.ts
+++ code/e2e-tests/addon-backgrounds.spec.ts
@@ -3,6 +3,7 @@ import process from 'process';
 import { SbPage } from './util';
 
 const storybookUrl = process.env.STORYBOOK_URL || 'http://localhost:8001';
+const templateName = process.env.STORYBOOK_TEMPLATE_NAME;
 
 test.describe('addon-backgrounds', () => {
   test.beforeEach(async ({ page }) => {
@@ -38,21 +39,34 @@ test.describe('addon-backgrounds', () => {
     await expect(sbPage.page.locator(backgroundToolbarSelector)).toBeVisible();
   });
 
-  test('button should appear for attached docs pages', async ({ page }) => {
-    const sbPage = new SbPage(page);
+  test.describe('docs pages', () => {
+    test('button should appear for attached docs pages', async ({ page }) => {
+      const sbPage = new SbPage(page);
 
-    await sbPage.navigateToStory('example/button', 'docs');
-    await expect(sbPage.page.locator(backgroundToolbarSelector)).toBeVisible();
-  });
+      await sbPage.navigateToStory('example/button', 'docs');
+      await expect(sbPage.page.locator(backgroundToolbarSelector)).toBeVisible();
+    });
 
-  test('button should appear for unattached docs pages', async ({ page }) => {
-    const sbPage = new SbPage(page);
+    test('button should appear for unattached .mdx files', async ({ page }) => {
+      // SSv6 does not support .mdx files. There is a unattached stories.mdx file
+      // at /docs/addons-docs-stories-mdx-unattached--docs, but these are functionally
+      // really attached
 
-    // We start on the introduction page by default.
-    await sbPage.page.waitForURL((url) =>
-      url.search.includes(`path=/docs/example-introduction--docs`)
-    );
+      // eslint-disable-next-line jest/no-disabled-tests
+      test.skip(
+        // eslint-disable-next-line jest/valid-title
+        templateName.includes('ssv6'),
+        'Only run this test for Sandboxes with StoryStoreV7 enabled'
+      );
 
-    await expect(sbPage.page.locator(backgroundToolbarSelector)).toBeVisible();
+      const sbPage = new SbPage(page);
+
+      // We start on the introduction page by default.
+      await sbPage.page.waitForURL((url) =>
+        url.search.includes(`path=/docs/example-introduction--docs`)
+      );
+
+      await expect(sbPage.page.locator(backgroundToolbarSelector)).toBeVisible();
+    });
   });
 });
