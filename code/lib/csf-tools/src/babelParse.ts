53d871d7cdc94d790e467cce1942c44852c4a25f
Federico Zivolo <federico.zivolo@gmail.com>
2023-04-20 17:53:52 +0200

    formatting

--- code/lib/csf-tools/src/babelParse.ts
+++ code/lib/csf-tools/src/babelParse.ts
@@ -2,17 +2,22 @@ import * as babelParser from '@babel/parser';
 import * as recast from 'recast';
 import type { ParserOptions } from '@babel/parser';
 
-function parseWithFlowOrTypescript(source: string, parserOptions: babelParser.ParserOptions) {
+function parseWithFlowOrTypescript(
+  source: string,
+  parserOptions: babelParser.ParserOptions
+) {
   const flowCommentPattern = /^\s*\/\/\s*@flow/;
   const useFlowPlugin = flowCommentPattern.test(source);
 
-  const parserPlugins: babelParser.ParserOptions['plugins'] = useFlowPlugin ? ['flow'] : ['typescript'];
+  const parserPlugins: babelParser.ParserOptions['plugins'] = useFlowPlugin
+    ? ['flow']
+    : ['typescript'];
 
   // Merge the provided parserOptions with the custom parser plugins
   const mergedParserOptions = {
     ...parserOptions,
     plugins: [...parserOptions.plugins, ...parserPlugins],
-  }
+  };
 
   return babelParser.parse(source, mergedParserOptions);
 }
@@ -28,7 +33,7 @@ export const babelParse = (code: string) => {
   return recast.parse(code, {
     parser: {
       parse(source: string) {
-        return parseWithFlowOrTypescript(source, parserOptions); 
+        return parseWithFlowOrTypescript(source, parserOptions);
       },
     },
   });
