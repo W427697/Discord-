205e5fa79de0c1a5387d91d166db96373fe5f5bc
Yann Braga <yannbf@gmail.com>
2023-04-24 22:22:41 +0200

    add error handling

--- scripts/upload-bench.ts
+++ scripts/upload-bench.ts
@@ -7,8 +7,12 @@ const uploadBench = async () => {
   // const data = {} as Record<string, any>;
   await Promise.all(
     keys.map(async (key) => {
-      const val = await loadBench({ key, rootDir: CODE_DIRECTORY });
-      console.log({ key, val });
+      try {
+        const val = await loadBench({ key, rootDir: CODE_DIRECTORY });
+        console.log({ key, val });
+      } catch (err) {
+        console.log(`Failed to load bench for the key ${key}:`, err);
+      }
     })
   );
 };
