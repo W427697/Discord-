22d9eb224cefe98a3872caccea36cad55dc61b64
Valentin Palkovic <valentin@chromatic.com>
2023-05-11 10:31:48 +0200

    Add webpack.config.js file creation to Vue-CLI and Preact-CLI templates in sandbox-templates.ts

--- code/lib/cli/src/sandbox-templates.ts
+++ code/lib/cli/src/sandbox-templates.ts
@@ -347,7 +347,8 @@ const baseTemplates = {
   },
   'vue-cli/default-js': {
     name: 'Vue-CLI (Default JS)',
-    script: 'npx -p @vue/cli vue create . --default --packageManager=yarn --force --merge',
+    script:
+      'npx -p @vue/cli vue create . --default --packageManager=yarn --force --merge && echo "module.exports = {}" > webpack.config.js',
     expected: {
       framework: '@storybook/vue3-webpack5',
       renderer: '@storybook/vue3',
@@ -359,7 +360,7 @@ const baseTemplates = {
   'vue-cli/vue2-default-js': {
     name: 'Vue-CLI (Vue2 JS)',
     script:
-      'npx -p @vue/cli vue create . --default --packageManager=yarn --force --merge --preset="Default (Vue 2)"',
+      'npx -p @vue/cli vue create . --default --packageManager=yarn --force --merge --preset="Default (Vue 2)" && echo "module.exports = {}" > webpack.config.js',
     expected: {
       framework: '@storybook/vue-webpack5',
       renderer: '@storybook/vue',
@@ -370,7 +371,8 @@ const baseTemplates = {
   },
   'preact-webpack5/default-js': {
     name: 'Preact CLI (Default JS)',
-    script: 'npx preact-cli create default {{beforeDir}} --name preact-app --yarn --no-install',
+    script:
+      'npx preact-cli create default {{beforeDir}} --name preact-app --yarn --no-install && echo "module.exports = {}" > webpack.config.js',
     expected: {
       framework: '@storybook/preact-webpack5',
       renderer: '@storybook/preact',
@@ -380,7 +382,8 @@ const baseTemplates = {
   },
   'preact-webpack5/default-ts': {
     name: 'Preact CLI (Default TS)',
-    script: 'npx preact-cli create typescript {{beforeDir}} --name preact-app --yarn --no-install',
+    script:
+      'npx preact-cli create typescript {{beforeDir}} --name preact-app --yarn --no-install && echo "module.exports = {}" > webpack.config.js',
     expected: {
       framework: '@storybook/preact-webpack5',
       renderer: '@storybook/preact',
