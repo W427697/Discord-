702bf70fc79de87d99d3cc20770306f58e347143
yang bo <yangbohnzz@gmail.com>
2023-05-12 11:45:41 +0800

    fix: typo in comment

--- code/lib/core-common/src/utils/interpret-require.ts
+++ code/lib/core-common/src/utils/interpret-require.ts
@@ -14,7 +14,7 @@ export function interopRequireDefault(filePath: string) {
       target: `node${process.version.slice(1)}`,
       format: 'cjs',
       hookIgnoreNodeModules: false,
-      // some frameworks like stylus reply 'name' property of classes of functions
+      // Some frameworks, like Stylus, rely on the 'name' property of classes or functions
       // https://github.com/storybookjs/storybook/issues/19049
       keepNames: true,
       tsconfigRaw: `{
