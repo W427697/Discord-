6498a920488050ccd50c09b9004cbfdf0f45c529
Efren Aragon <efrenaragon96@gmail.com>
2023-05-15 23:02:21 -0400

    fix: ts error on src/argTypes/docgen/typeScript folder

--- code/lib/docs-tools/src/argTypes/docgen/typeScript/createPropDef.test.ts
+++ code/lib/docs-tools/src/argTypes/docgen/typeScript/createPropDef.test.ts
@@ -8,7 +8,7 @@ function createDocgenInfo({ tsType, ...others }: Partial<DocgenInfo>): DocgenInf
     tsType,
     required: true,
     ...others,
-  };
+  } as DocgenInfo;
 }
 
 describe('type', () => {
@@ -20,7 +20,7 @@ describe('type', () => {
 
     const { type } = createTsPropDef(PROP_NAME, docgenInfo);
 
-    expect(type.summary).toBe('string');
-    expect(type.detail).toBeUndefined();
+    expect(type?.summary).toBe('string');
+    expect(type?.detail).toBeUndefined();
   });
 });
