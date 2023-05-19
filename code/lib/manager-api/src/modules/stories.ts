f45a96392c22e9b772382f1bc8b99b0e30520681
SpookyJelly <74237436+SpookyJelly@users.noreply.github.com>
2023-05-19 12:18:44 +0900

    fix: error on using useStoryPrepared hook
    sometimes, `api.getData` returns undefined. which is used by isPrepared. I added a exception for this

--- code/lib/manager-api/src/modules/stories.ts
+++ code/lib/manager-api/src/modules/stories.ts
@@ -141,6 +141,7 @@ export const init: ModuleFn<SubAPI, SubState, true> = ({
     },
     isPrepared: (storyId, refId) => {
       const data = api.getData(storyId, refId);
+      if(!data) return false;
       return data.type === 'story' ? data.prepared : true;
     },
     resolveStory: (storyId, refId) => {
