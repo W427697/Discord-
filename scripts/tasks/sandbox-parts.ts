578cbbff4ba2926d08ffecd1d80055eb8dde6578
sook <sukkyu.chung@gmail.com>
2023-05-22 20:25:56 +0900

    fix: sandbox package.json when pre-existing NODE_OPTIONS value contains whitespaces

--- scripts/tasks/sandbox-parts.ts
+++ scripts/tasks/sandbox-parts.ts
@@ -131,7 +131,7 @@ export const install: Task['run'] = async (
   }
 
   const nodeOptionsString = nodeOptions.join(' ');
-  const prefix = `NODE_OPTIONS="${nodeOptionsString}" STORYBOOK_TELEMETRY_URL="http://localhost:6007/event-log"`;
+  const prefix = `NODE_OPTIONS='${nodeOptionsString}' STORYBOOK_TELEMETRY_URL="http://localhost:6007/event-log"`;
 
   await updatePackageScripts({
     cwd,
