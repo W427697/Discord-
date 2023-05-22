9a3353d1cb5d4755b247456eab32ddb5a065cb0a
Michael Shilman <shilman@users.noreply.github.com>
2023-05-23 02:40:24 +0800

    Revert "Merge pull request #22575 from storybookjs/norbert/serverchannel-manager-fix"
    This reverts commit 2aef6938ad9f9f85d4b38972e6509fbf08da059f.

--- code/lib/builder-vite/src/codegen-set-addon-channel.ts
+++ code/lib/builder-vite/src/codegen-set-addon-channel.ts
@@ -1,6 +1,5 @@
 export async function generateAddonSetupCode() {
   return `
-    import { global } from '@storybook/global';
     import { createChannel as createPostMessageChannel } from '@storybook/channel-postmessage';
     import { createChannel as createWebSocketChannel } from '@storybook/channel-websocket';
     import { addons } from '@storybook/preview-api';
@@ -8,9 +7,10 @@ export async function generateAddonSetupCode() {
     const channel = createPostMessageChannel({ page: 'preview' });
     addons.setChannel(channel);
     window.__STORYBOOK_ADDONS_CHANNEL__ = channel;
-    
-    if (global.CONFIG_TYPE === 'DEVELOPMENT'){
-      const serverChannel = createWebSocketChannel({});
+
+    const { SERVER_CHANNEL_URL } = globalThis;
+    if (SERVER_CHANNEL_URL) {
+      const serverChannel = createWebSocketChannel({ url: SERVER_CHANNEL_URL });
       addons.setServerChannel(serverChannel);
       window.__STORYBOOK_SERVER_CHANNEL__ = serverChannel;
     }
