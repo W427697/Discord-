c68aa57cc47b713413b4b4901ed942d945c39564
SpookyJelly <74237436+SpookyJelly@users.noreply.github.com>
2023-05-23 12:56:41 +0900

    Update stories.ts
    Fixed a wrong lint.

--- code/lib/manager-api/src/modules/stories.ts
+++ code/lib/manager-api/src/modules/stories.ts
@@ -141,7 +141,9 @@ export const init: ModuleFn<SubAPI, SubState, true> = ({
     },
     isPrepared: (storyId, refId) => {
       const data = api.getData(storyId, refId);
-      if(!data) return false;
+      if(!data) {
+        return false;
+      }
       return data.type === 'story' ? data.prepared : true;
     },
     resolveStory: (storyId, refId) => {
