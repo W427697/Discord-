4063303cf6d817504ef0e28bb9066bff2187a319
Norbert de Langen <ndelangen@me.com>
2023-05-24 09:59:15 +0200

    cleanup

--- code/lib/builder-manager/src/utils/template.ts
+++ code/lib/builder-manager/src/utils/template.ts
@@ -1,12 +1,9 @@
-import path, { dirname, join } from 'path';
+import { dirname, join } from 'path';
 import fs from 'fs-extra';
 
 import { render } from 'ejs';
 
 import type { DocsOptions, Options, Ref } from '@storybook/types';
-
-const interpolate = (string: string, data: Record<string, string> = {}) =>
-  Object.entries(data).reduce((acc, [k, v]) => acc.replace(new RegExp(`%${k}%`, 'g'), v), string);
 
 export const getTemplatePath = async (template: string) => {
   return join(
@@ -17,31 +14,10 @@ export const getTemplatePath = async (template: string) => {
 };
 
 export const readTemplate = async (template: string) => {
-  // eslint-disable-next-line @typescript-eslint/no-shadow
   const path = await getTemplatePath(template);
 
   return fs.readFile(path, 'utf8');
 };
-
-export async function getManagerHeadTemplate(
-  configDirPath: string,
-  interpolations: Record<string, string>
-) {
-  const head = await fs
-    .pathExists(path.resolve(configDirPath, 'manager-head.html'))
-    .then<Promise<string> | false>((exists) => {
-      if (exists) {
-        return fs.readFile(path.resolve(configDirPath, 'manager-head.html'), 'utf8');
-      }
-      return false;
-    });
-
-  if (!head) {
-    return '';
-  }
-
-  return interpolate(head, interpolations);
-}
 
 export async function getManagerMainTemplate() {
   return getTemplatePath(`manager.ejs`);
