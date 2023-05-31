81e15e4345f28997243825d0f1eeec2487dd7b9e
Norbert de Langen <ndelangen@me.com>
2023-05-31 14:28:15 +0200

    adjust the check-deps script and a util script

--- scripts/check-dependencies.js
+++ scripts/check-dependencies.js
@@ -8,51 +8,28 @@ const checkDependencies = async () => {
   const scriptsPath = join(__dirname);
   const codePath = join(__dirname, '..', 'code');
 
-  const tasks = [];
-
   if (!existsSync(join(scriptsPath, 'node_modules'))) {
-    tasks.push(
-      spawn('yarn', ['install'], {
-        cwd: scriptsPath,
-        stdio: ['inherit', 'inherit', 'inherit'],
-      })
-    );
+    console.log('installing script dependencies');
+
+    await spawn('pnpm', ['install'], {
+      cwd: scriptsPath,
+      stdio: ['inherit', 'inherit', 'inherit'],
+    });
   }
+
   if (!existsSync(join(codePath, 'node_modules'))) {
-    tasks.push(
-      spawn('yarn', ['install'], {
-        cwd: codePath,
-        stdio: ['inherit', 'inherit', 'inherit'],
-      })
-    );
-  }
+    console.log('installing code dependencies');
 
-  if (tasks.length > 0) {
-    console.log('installing dependencies');
-
-    await Promise.all(
-      tasks.map(
-        (t) =>
-          new Promise((res, rej) => {
-            t.on('exit', (code) => {
-              if (code !== 0) {
-                rej();
-              } else {
-                res();
-              }
-            });
-          })
-      )
-    ).catch(() => {
-      tasks.forEach((t) => t.kill());
-      throw new Error('Failed to install dependencies');
-    });
-
-    // give the filesystem some time
-    await new Promise((res, rej) => {
-      setTimeout(res, 1000);
+    await spawn('yarn', ['install'], {
+      cwd: codePath,
+      stdio: ['inherit', 'inherit', 'inherit'],
     });
   }
+
+  // give the filesystem some time
+  await new Promise((res, rej) => {
+    setTimeout(res, 1000);
+  });
 };
 
 module.exports = {
