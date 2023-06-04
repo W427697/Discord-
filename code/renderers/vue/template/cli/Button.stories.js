f413b93917d250e680d62ea1e08bee347d3c6c0c
chakAs3 <javachakir@gmail.com>
2023-06-04 06:26:44 +0400

    fix story template invalide event

--- code/renderers/vue/template/cli/Button.stories.js
+++ code/renderers/vue/template/cli/Button.stories.js
@@ -8,7 +8,7 @@ export default {
   render: (args, { argTypes }) => ({
     props: Object.keys(argTypes),
     components: { MyButton },
-    template: '<my-button @onClick="onClick" v-bind="$props" />',
+    template: '<my-button  v-bind="$props" />',
   }),
   argTypes: {
     backgroundColor: { control: 'color' },
