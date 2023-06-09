54466535c8d19fb09bf617818e37ffa2367e0b41
Mateo Guzmán <20783123+mateoguzmana@users.noreply.github.com>
2023-06-09 21:56:09 +0530

    fix(manager-api): fixing store-setup type annotation for this properly

--- code/lib/manager-api/src/lib/store-setup.ts
+++ code/lib/manager-api/src/lib/store-setup.ts
@@ -2,16 +2,17 @@
 /* eslint-disable func-names */
 import { parse, stringify } from 'telejson';
 
+interface Meta {
+  _area?: string;
+  _in: (key: string) => string;
+}
+
 // setting up the store, overriding set and get to use telejson
 export default (_: any) => {
-  _.fn('set', function (key: string, data: object) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore TODO
+  _.fn('set', function (this: Meta, key: string, data: object) {
     return _.set(this._area, this._in(key), stringify(data, { maxDepth: 50 }));
   });
-  _.fn('get', function (key: string, alt: string) {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore TODO
+  _.fn('get', function (this: Meta, key: string, alt: string) {
     const value = _.get(this._area, this._in(key));
     return value !== null ? parse(value) : alt || value;
   });
