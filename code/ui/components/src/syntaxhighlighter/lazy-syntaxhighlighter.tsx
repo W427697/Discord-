711f2ea66a68753866baf1e4c5fd29d6d8b9275f
Norbert de Langen <ndelangen@me.com>
2023-06-22 18:42:46 +0200

    portal the registerLanguage api through the lazy component

--- code/ui/components/src/syntaxhighlighter/lazy-syntaxhighlighter.tsx
+++ code/ui/components/src/syntaxhighlighter/lazy-syntaxhighlighter.tsx
@@ -1,12 +1,28 @@
 import type { ComponentProps } from 'react';
 import React, { Suspense, lazy } from 'react';
 
+import type ReactSyntaxHighlighter from './syntaxhighlighter';
+
+let languages: Parameters<typeof ReactSyntaxHighlighter.registerLanguage>[] = [];
+let Comp: typeof ReactSyntaxHighlighter | null = null;
+
 const LazySyntaxHighlighter = lazy(() => import('./syntaxhighlighter'));
 const LazySyntaxHighlighterWithFormatter = lazy(async () => {
   const [{ SyntaxHighlighter }, { formatter }] = await Promise.all([
     import('./syntaxhighlighter'),
     import('./formatter'),
   ]);
+
+  if (languages.length > 0) {
+    languages.forEach((args) => {
+      SyntaxHighlighter.registerLanguage(...args);
+    });
+    languages = [];
+  }
+
+  if (Comp === null) {
+    Comp = SyntaxHighlighter;
+  }
 
   return {
     default: (props: ComponentProps<typeof LazySyntaxHighlighter>) => (
@@ -24,3 +40,13 @@ export const SyntaxHighlighter = (props: ComponentProps<typeof LazySyntaxHighlig
     )}
   </Suspense>
 );
+
+SyntaxHighlighter.registerLanguage = (
+  ...args: Parameters<typeof ReactSyntaxHighlighter.registerLanguage>
+) => {
+  if (Comp !== null) {
+    Comp.registerLanguage(...args);
+    return;
+  }
+  languages.push(args);
+};
