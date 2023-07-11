942d40c6f60c695789236493b6d4908a98cb3584
Norbert de Langen <ndelangen@me.com>
2023-07-11 15:42:14 +0200

    textarea size fix

--- code/ui/components/src/form/input/input.tsx
+++ code/ui/components/src/form/input/input.tsx
@@ -32,6 +32,8 @@ const styles = ({ theme }: { theme: Theme }): CSSObject => ({
   fontSize: theme.typography.size.s2 - 1,
   lineHeight: '20px',
   padding: '6px 10px', // 32
+  boxSizing: 'border-box',
+  height: 32,
 
   '&:focus': {
     boxShadow: `${theme.color.secondary} 0 0 0 1px inset`,
