eec3ee417c2e766dc88191b96a1ed928bb8f4f84
François de Metz <francois@2metz.fr>
2023-07-11 14:55:13 +0200

    Fix global use

--- code/frameworks/ember/src/client/preview/globals.ts
+++ code/frameworks/ember/src/client/preview/globals.ts
@@ -1,4 +1,6 @@
 import { global } from '@storybook/global';
 
-global.STORYBOOK_NAME = process.env.STORYBOOK_NAME;
-global.STORYBOOK_ENV = 'ember';
+const { window: globalWindow } = global;
+
+globalWindow.STORYBOOK_NAME = process.env.STORYBOOK_NAME;
+globalWindow.STORYBOOK_ENV = 'ember';
