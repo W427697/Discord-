a3b01c7a7bbb3982e73abca2a85dd705fe895268
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-07-12 11:50:46 +0100

    Remove old button

--- code/ui/components/src/new/Button/Button.tsx
+++ code/ui/components/src/new/Button/Button.tsx
@@ -2,66 +2,6 @@ import type { ReactNode } from 'react';
 import React, { forwardRef } from 'react';
 import { ButtonWrapper } from './styles';
 
-export type ButtonPropsOld = (
-  | React.ButtonHTMLAttributes<HTMLButtonElement>
-  | React.AnchorHTMLAttributes<HTMLAnchorElement>
-) & {
-  children: string;
-  size?: 'small' | 'medium';
-  variant?: 'primary' | 'secondary' | 'tertiary';
-  icon?: ReactNode;
-  iconOnly?: boolean;
-  onClick?: () => void;
-  disabled?: boolean;
-  active?: boolean;
-};
-
-const ButtonLink = ButtonWrapper.withComponent('a');
-
-export const ButtonOld = forwardRef<any, ButtonPropsOld>((props, ref) => {
-  const {
-    size = 'medium',
-    variant = 'primary',
-    icon,
-    iconOnly = false,
-    disabled = false,
-    active = false,
-    children,
-  } = props;
-  if ('href' in props)
-    return (
-      <ButtonLink
-        ref={ref}
-        size={size}
-        variant={variant}
-        disabled={disabled}
-        active={active}
-        iconOnly={iconOnly}
-        {...(props as React.AnchorHTMLAttributes<HTMLAnchorElement>)}
-      >
-        {icon}
-        {!iconOnly && children}
-      </ButtonLink>
-    );
-
-  return (
-    <ButtonWrapper
-      ref={ref}
-      size={size}
-      variant={variant}
-      disabled={disabled}
-      active={active}
-      iconOnly={iconOnly}
-      {...(props as React.ButtonHTMLAttributes<HTMLButtonElement>)}
-    >
-      {icon}
-      {!iconOnly && children}
-    </ButtonWrapper>
-  );
-});
-
-ButtonOld.displayName = 'ButtonOld';
-
 type PropsOf<T extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>> =
   JSX.LibraryManagedAttributes<T, React.ComponentPropsWithRef<T>>;
 
@@ -96,11 +36,3 @@ export const Button: {
 );
 
 Button.displayName = 'Button';
-
-// const Link = (props: { to: string }) => <a {...props} />;
-
-// const A = (
-//   <Button as={'a'} href='/bl'>
-//     asdf
-//   </Button>
-// );
