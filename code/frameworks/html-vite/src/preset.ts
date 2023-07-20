4c3e892973ca58a6b4df10c8c74e0d1d8c3b4967
Valentin Palkovic <valentin@chromatic.com>
2023-07-20 15:18:50 +0200

    Apply getAbsolutePath wrapper to all framework package preset files

--- code/frameworks/html-vite/src/preset.ts
+++ code/frameworks/html-vite/src/preset.ts
@@ -1,7 +1,12 @@
 import type { PresetProperty } from '@storybook/types';
+import { dirname, join } from 'path';
 import type { StorybookConfig } from './types';
 
+function getAbsolutePath<I extends string>(value: I): I {
+  return dirname(require.resolve(join(value, 'package.json'))) as any;
+}
+
 export const core: PresetProperty<'core', StorybookConfig> = {
-  builder: '@storybook/builder-vite',
-  renderer: '@storybook/html',
+  builder: getAbsolutePath('@storybook/builder-vite'),
+  renderer: getAbsolutePath('@storybook/html'),
 };
