760ad9d820d9645b71fa195e2544417210cbc2d3
Jeppe Reinhold <jeppe@reinhold.is>
2023-07-24 09:21:10 +0200

    move sort order to global git client

--- scripts/release/utils/git-client.ts
+++ scripts/release/utils/git-client.ts
@@ -1,16 +1,19 @@
 import { simpleGit } from 'simple-git';
 
-export const git = simpleGit();
+export const git = simpleGit({
+  config: [
+    /**
+     * ensures that prereleases are listed as earlier than stable releases.
+     * eg. in the following list, this config correctly puts 7.1.0 on the top instead of the bottom:
+     * 7.1.0
+     * 7.1.0-rc.2
+     * 7.1.0-rc.1
+     * See https://stackoverflow.com/a/52680984
+     */
+    'versionsort.suffix=-',
+  ],
+});
 
 export async function getLatestTag() {
-  /**
-   * ensures that prereleases are listed as earlier than stable releases.
-   * eg. in the following list, 7.1.0 would correctly be considered the latest:
-   * 7.1.0
-   * 7.1.0-rc.2
-   * 7.1.0-rc.1
-   * See https://stackoverflow.com/a/52680984
-   */
-  const gitWithSort = simpleGit({ config: ['versionsort.suffix=-'] });
-  return (await gitWithSort.tags(['v*', '--sort=-v:refname', '--merged'])).latest;
+  return (await git.tags(['v*', '--sort=-v:refname', '--merged'])).latest;
 }
