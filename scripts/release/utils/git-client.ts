aad2956eb0e9683ec6b7fa44736db2bcaae9a752
Jeppe Reinhold <jeppe@reinhold.is>
2023-07-24 09:19:06 +0200

    correctly sort git tags when getting latest

--- scripts/release/utils/git-client.ts
+++ scripts/release/utils/git-client.ts
@@ -3,5 +3,14 @@ import { simpleGit } from 'simple-git';
 export const git = simpleGit();
 
 export async function getLatestTag() {
-  return (await git.tags(['v*', '--sort=-v:refname', '--merged'])).latest;
+  /**
+   * ensures that prereleases are listed as earlier than stable releases.
+   * eg. in the following list, 7.1.0 would correctly be considered the latest:
+   * 7.1.0
+   * 7.1.0-rc.2
+   * 7.1.0-rc.1
+   * See https://stackoverflow.com/a/52680984
+   */
+  const gitWithSort = simpleGit({ config: ['versionsort.suffix=-'] });
+  return (await gitWithSort.tags(['v*', '--sort=-v:refname', '--merged'])).latest;
 }
