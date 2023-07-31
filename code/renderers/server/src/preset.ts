c0e8da0bdfa042b79a4373868edf308013b514b9
Jeppe Reinhold <jeppe@reinhold.is>
2023-07-31 10:01:34 +0200

    add support for meta tags in server indexer

--- code/renderers/server/src/preset.ts
+++ code/renderers/server/src/preset.ts
@@ -1,49 +1,10 @@
 import fs from 'fs-extra';
 import yaml from 'yaml';
-import { toId } from '@storybook/csf';
-import type { StaticMeta } from '@storybook/csf-tools';
-import type {
-  IndexedStory,
-  StoryIndexer,
-  StorybookConfig,
-  Tag,
-  StoryName,
-  ComponentTitle,
-} from '@storybook/types';
-
-// export const storyIndexers: StorybookConfig['storyIndexers'] = (existingIndexers) => {
-//   const serverIndexer: StoryIndexer['indexer'] = async (fileName) => {
-//     const json = fileName.endsWith('.json')
-//       ? await fs.readJson(fileName, 'utf-8')
-//       : yaml.parse((await fs.readFile(fileName, 'utf-8')).toString());
-//     const meta: StaticMeta = {
-//       title: json.title,
-//     };
-//     const stories: IndexedStory[] = json.stories.map((story: { name: string }) => {
-//       const id = toId(meta.title, story.name);
-//       const { name } = story;
-//       const indexedStory: IndexedStory = {
-//         id,
-//         name,
-//       };
-//       return indexedStory;
-//     });
-//     return {
-//       meta,
-//       stories,
-//     };
-//   };
-//   return [
-//     {
-//       test: /(stories|story)\.(json|ya?ml)$/,
-//       indexer: serverIndexer,
-//     },
-//     ...(existingIndexers || []),
-//   ];
-// };
+import type { StorybookConfig, Tag, StoryName, ComponentTitle } from '@storybook/types';
 
 type FileContent = {
   title: ComponentTitle;
+  tags?: Tag[];
   stories: { name: StoryName; tags?: Tag[] }[];
 };
 
@@ -60,7 +21,7 @@ export const indexers: StorybookConfig['indexers'] = (existingIndexers) => [
         exportName: story.name,
         name: story.name,
         title: content.title,
-        tags: story.tags ?? [],
+        tags: content.tags ?? story.tags ?? [],
         type: 'story',
       }));
     },
