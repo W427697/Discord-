af6d5f90ec4065baa71a7a31b294585a4c63046c
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-08-02 12:16:17 +0200

    First pass at updating all icons

--- code/ui/blocks/src/controls/options/Select.tsx
+++ code/ui/blocks/src/controls/options/Select.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 import { styled } from '@storybook/theming';
 import type { CSSObject } from '@storybook/theming';
 import { logger } from '@storybook/client-logger';
-import { Icons } from '@storybook/components';
-
+import { Icon } from '@storybook/components/experimental';
 import type { ControlProps, OptionsSelection, NormalizedOptionsConfig } from '../types';
 
 import { selectedKey, selectedKeys, selectedValues } from './helpers';
@@ -104,7 +103,7 @@ const SingleSelect: FC<SelectProps> = ({ name, value, options, onChange }) => {
 
   return (
     <SelectWrapper>
-      <Icons icon="arrowdown" />
+      <Icon.ChevronDown />
       <OptionsSelect id={controlId} value={selection} onChange={handleChange}>
         <option key="no-selection" disabled>
           {NO_SELECTION}
