277e0b2f2432b907f434f1ab2429241f72651773
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-08-03 17:46:16 +0200

    Move some styles

--- code/ui/manager/src/components/layout-new/containers/Main.tsx
+++ code/ui/manager/src/components/layout-new/containers/Main.tsx
@@ -1,7 +1,44 @@
 import type { CSSProperties, FC } from 'react';
 import React from 'react';
-import { Pane } from './Pane';
-import { Paper } from './Paper';
+import { styled } from '@storybook/theming';
+
+export const Pane = styled.div({
+  position: 'absolute',
+  boxSizing: 'border-box',
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  zIndex: 9,
+});
+
+export const Paper = styled.div<{ isFullscreen: boolean }>(
+  {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+  },
+  ({ isFullscreen, theme }) =>
+    isFullscreen
+      ? {
+          boxShadow: 'none',
+          borderRadius: 0,
+        }
+      : {
+          borderTopLeftRadius:
+            theme.appBorderRadius === 0 ? theme.appBorderRadius : theme.appBorderRadius + 1,
+          borderBottomLeftRadius:
+            theme.appBorderRadius === 0 ? theme.appBorderRadius : theme.appBorderRadius + 1,
+          overflow: 'hidden',
+          boxShadow:
+            theme.base === 'light'
+              ? '0 1px 3px 1px rgba(0, 0, 0, 0.05), 0px 0 0px 1px rgba(0, 0, 0, 0.05)'
+              : `0px 0 0px 1px ${theme.appBorderColor}`,
+          transform: 'translateZ(0)',
+        }
+);
 
 export const Main: FC<{ isFullscreen: boolean; position: CSSProperties }> = ({
   isFullscreen = false,
@@ -9,7 +46,7 @@ export const Main: FC<{ isFullscreen: boolean; position: CSSProperties }> = ({
   position = undefined,
   ...props
 }) => (
-  <Pane style={position} top {...props} role="main">
+  <Pane style={position} {...props} role="main">
     <Paper isFullscreen={isFullscreen}>{children}</Paper>
   </Pane>
 );
