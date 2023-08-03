2c8ba58d4224f9cb4326092e48080b137d5e4e69
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-08-03 09:16:04 +0200

    Update DocsPage.stories.tsx

--- code/ui/blocks/src/blocks/DocsPage.stories.tsx
+++ code/ui/blocks/src/blocks/DocsPage.stories.tsx
@@ -1,5 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { within } from '@storybook/testing-library';
 import { DocsPage } from './DocsPage';
 
 const meta = {
@@ -16,17 +15,6 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   parameters: {
     relativeCsfPaths: ['../examples/Button.stories'],
-  },
-  play: async ({ canvasElement }) => {
-    // This play function's sole purpose is to add a "chromatic ignore" region to a flaky row.
-    const canvas = within(canvasElement);
-    const sizeCell = await canvas.findByText('How large should the button be?');
-    const sizeRow = sizeCell.parentElement?.parentElement?.parentElement;
-    if (sizeRow?.nodeName === 'TR') {
-      sizeRow.setAttribute('data-chromatic', 'ignore');
-    } else {
-      throw new Error('the DOM structure changed, please update this test');
-    }
   },
 };
 export const SingleStory: Story = {
