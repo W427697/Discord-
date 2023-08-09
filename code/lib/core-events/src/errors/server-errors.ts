10dd616aafed2c1a17f505917e422683cdc6e2b8
Yann Braga <yannbf@gmail.com>
2023-08-09 10:18:25 +0200

    add more categories

--- code/lib/core-events/src/errors/server-errors.ts
+++ code/lib/core-events/src/errors/server-errors.ts
@@ -1,8 +1,32 @@
 import dedent from 'ts-dedent';
 import { StorybookError } from './storybook-error';
 
+/**
+ * If you can't find a suitable category for your error, create one
+ * based on the package name/file path of which the error is thrown.
+ * For instance:
+ * If it's from @storybook/node-logger, then NODE-LOGGER
+ * If it's from a package that is too broad, e.g. @storybook/cli in the init command, then use a combination like CLI_INIT
+ */
 export enum Category {
+  CLI = 'CLI',
   CLI_INIT = 'CLI_INIT',
+  CLI_AUTOMIGRATE = 'CLI_AUTOMIGRATE',
+  CLI_UPGRADE = 'CLI_UPGRADE',
+  CLI_ADD = 'CLI_ADD',
+  CODEMOD = 'CODEMOD',
+  CORE_SERVER = 'CORE-SERVER',
+  CSF_PLUGIN = 'CSF-PLUGIN',
+  CSF_TOOLS = 'CSF-TOOLS',
+  NODE_LOGGER = 'NODE-LOGGER',
+  TELEMETRY = 'TELEMETRY',
+  BUILDER_MANAGER = 'BUILDER-MANAGER',
+  BUILDER_VITE = 'BUILDER-VITE',
+  BUILDER_WEBPACK5 = 'BUILDER-WEBPACK5',
+  SOURCE_LOADER = 'SOURCE-LOADER',
+  POSTINSTALL = 'POSTINSTALL',
+  DOCS_TOOLS = 'DOCS-TOOLS',
+  CORE_WEBPACK = 'CORE-WEBPACK',
 }
 
 export class NxProjectDetectedError extends StorybookError {
