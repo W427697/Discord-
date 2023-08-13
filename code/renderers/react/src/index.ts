8263c61adc5d9188cc87d2f75f982ec941e6e64c
Michael Shilman <shilman@users.noreply.github.com>
2023-08-13 14:22:15 +0800

    Review feedback

--- code/renderers/react/src/index.ts
+++ code/renderers/react/src/index.ts
@@ -5,7 +5,7 @@ import './globals';
 export * from './public-api';
 export * from './public-types';
 
-export * from './testing-api';
+export * from './story-utils';
 
 // optimization: stop HMR propagation in webpack
 if (typeof module !== 'undefined') module?.hot?.decline();
