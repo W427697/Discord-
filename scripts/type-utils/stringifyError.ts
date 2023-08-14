855554428a5fa439ed70316891f0ff45c2db3d14
stilt0n <mattias.leino@arts.cornish.edu>
2023-08-14 00:25:03 -0400

    modify stringifyError to make it more general

--- scripts/type-utils/stringifyError.ts
+++ scripts/type-utils/stringifyError.ts
@@ -1,5 +1,4 @@
 /**
  * Makes typescript error handling a little less ugly
  */
-export const stringifyError = (error: unknown) =>
-  error instanceof Error ? error.message : String(error);
+export const stringifyError = (error: unknown) => (error as any)?.message ?? String(error);
