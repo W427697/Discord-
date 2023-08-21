7192433dd8d6f43b55e428f02c9edbfa678e094f
Yann Braga <yannbf@gmail.com>
2023-08-21 15:59:45 +0200

    fix types

--- code/lib/preview/src/runtime.ts
+++ code/lib/preview/src/runtime.ts
@@ -8,22 +8,22 @@ const getKeys = Object.keys as <T extends object>(obj: T) => Array<keyof T>;
 
 // Apply all the globals
 getKeys(globals).forEach((key) => {
-  (globalThis as any)[globals[key]] = values[key];
+  (global as any)[globals[key]] = values[key];
 });
 
-global.sendTelemetryError = (error) => {
+global.sendTelemetryError = (error: any) => {
   const channel = global.__STORYBOOK_ADDONS_CHANNEL__;
   channel.emit(TELEMETRY_ERROR, error);
 };
 
 // handle all uncaught StorybookError at the root of the application and log to telemetry if applicable
-global.addEventListener('error', (args) => {
+global.addEventListener('error', (args: any) => {
   const error = args.error || args;
   if (error.fromStorybook) {
     global.sendTelemetryError(error);
   }
 });
-global.addEventListener('unhandledrejection', ({ reason }) => {
+global.addEventListener('unhandledrejection', ({ reason }: any) => {
   if (reason.fromStorybook) {
     global.sendTelemetryError(reason);
   }
