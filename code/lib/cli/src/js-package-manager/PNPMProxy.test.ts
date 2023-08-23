8fd8bcacbd9c498a78c7533b78e76238353b5cb8
Marcin <marcin.strzyz@outlook.com>
2023-08-23 10:06:23 -0700

    Fix type checking.

--- code/lib/cli/src/js-package-manager/PNPMProxy.test.ts
+++ code/lib/cli/src/js-package-manager/PNPMProxy.test.ts
@@ -213,9 +213,14 @@ describe('PNPM Proxy', () => {
         .spyOn(pnpmProxy, 'writePackageJson')
         .mockImplementation(jest.fn());
 
+      const basePackageAttributes = {
+        dependencies: {},
+        devDependencies: {},
+      };
+
       jest.spyOn(pnpmProxy, 'retrievePackageJson').mockImplementation(
-        // @ts-expect-error (not strict)
-        jest.fn(() => ({
+        jest.fn(async () => ({
+          ...basePackageAttributes,
           overrides: {
             bar: 'x.x.x',
           },
@@ -228,6 +233,7 @@ describe('PNPM Proxy', () => {
       await pnpmProxy.addPackageResolutions(versions);
 
       expect(writePackageSpy).toHaveBeenCalledWith({
+        ...basePackageAttributes,
         overrides: {
           ...versions,
           bar: 'x.x.x',
