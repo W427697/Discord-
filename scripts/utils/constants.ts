18436a9e12030ad960d301c1df4d6c663f6bd241
Norbert de Langen <ndelangen@me.com>
2023-08-25 11:35:26 +0200

    Update scripts/utils/constants.ts
    Co-authored-by: Jeppe Reinhold <jeppe@chromatic.com>

--- scripts/utils/constants.ts
+++ scripts/utils/constants.ts
@@ -4,11 +4,11 @@ export const AFTER_DIR_NAME = 'after-storybook';
 export const BEFORE_DIR_NAME = 'before-storybook';
 
 export const ROOT_DIRECTORY = join(__dirname, '..', '..');
-export const CODE_DIRECTORY = join(__dirname, '..', '..', 'code');
-export const PACKS_DIRECTORY = join(__dirname, '..', '..', 'packs');
-export const REPROS_DIRECTORY = join(__dirname, '..', '..', 'repros');
-export const SANDBOX_DIRECTORY = join(__dirname, '..', '..', 'sandbox');
-export const JUNIT_DIRECTORY = join(__dirname, '..', '..', 'test-results');
+export const CODE_DIRECTORY = join(ROOT_DIRECTORY, 'code');
+export const PACKS_DIRECTORY = join(ROOT_DIRECTORY, 'packs');
+export const REPROS_DIRECTORY = join(ROOT_DIRECTORY, 'repros');
+export const SANDBOX_DIRECTORY = join(ROOT_DIRECTORY, 'sandbox');
+export const JUNIT_DIRECTORY = join(ROOT_DIRECTORY, 'test-results');
 
 export const LOCAL_REGISTRY_URL = 'http://localhost:6001';
 export const SCRIPT_TIMEOUT = 5 * 60 * 1000;
