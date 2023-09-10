4296a414db44cc8114b1f4357cbb565b9486311a
chakAs3 <javachakir@gmail.com>
2023-09-11 01:33:39 +0400

    add test story to vue3 renderer

---
+++ code/renderers/vue3/template/stories_vue3-vite-default-ts/UnionTypeProp.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/vue3';
+
+import MyComponent from './UnionTypeProp.vue';
+
+const meta: Meta = {
+  component: MyComponent,
+  argTypes: {},
+  tags: ['autodocs'],
+} satisfies Meta<typeof MyComponent>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const UnionType: Story = {
+  args: {
+    label: 'UnionType',
+    stringOrNumber: 12,
+    booleanOrNumber: true,
+  },
+};
---
+++ code/renderers/vue3/template/stories_vue3-vite-default-ts/UnionTypeProp.vue
@@ -0,0 +1,9 @@
+<template>
+  <pre>
+{{ JSON.stringify($props, null, 2)  }}
+  </pre>
+</template>
+
+<script lang="ts" setup >
+ defineProps<{label:string ,stringOrNumber?: string | number,booleanOrNumber?:boolean | number }>();
+</script>
