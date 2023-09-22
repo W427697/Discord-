35ea1dd63a7f84794f699f4cff87b4505521f735
Jeppe Reinhold <jeppe@reinhold.is>
2023-09-22 14:40:50 +0200

    add LayoutProvider to relevant stories

--- code/ui/manager/src/components/sidebar/Menu.stories.tsx
+++ code/ui/manager/src/components/sidebar/Menu.stories.tsx
@@ -9,6 +9,7 @@ import { screen, userEvent, within } from '@storybook/testing-library';
 import type { State } from '@storybook/manager-api';
 import { SidebarMenu, ToolbarMenu } from './Menu';
 import { useMenu } from '../../container/Menu';
+import { LayoutProvider } from '../layout/LayoutProvider';
 
 const fakemenu: ComponentProps<typeof TooltipLinkList>['links'] = [
   { title: 'has icon', icon: 'link', id: 'icon' },
@@ -21,6 +22,7 @@ const meta = {
   args: {
     menu: fakemenu,
   },
+  decorators: [(storyFn) => <LayoutProvider>{storyFn()}</LayoutProvider>],
 } satisfies Meta<typeof SidebarMenu>;
 export default meta;
 
