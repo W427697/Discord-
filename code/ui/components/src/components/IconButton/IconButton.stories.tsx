e7601425e05435dc76d1854a4b149c14017d4b8f
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-09-22 08:33:39 +0100

    Add the new IconButton

---
+++ code/ui/components/src/components/IconButton/IconButton.stories.tsx
@@ -0,0 +1,95 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import { IconButton } from './IconButton';
+import { Icons } from '../icon/icon';
+
+const meta: Meta<typeof IconButton> = {
+  title: 'IconButton',
+  component: IconButton,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof IconButton>;
+
+export const Base = {
+  args: { children: <Icons icon="facehappy" /> },
+};
+
+export const Types: Story = {
+  args: {
+    ...Base.args,
+  },
+  render: ({ ...args }) => (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+      <IconButton {...args} variant="solid" />
+      <IconButton {...args} variant="outline" />
+      <IconButton {...args} variant="ghost" />
+    </div>
+  ),
+};
+
+export const Active: Story = {
+  args: {
+    ...Base.args,
+    active: true,
+  },
+  render: ({ ...args }) => (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+      <IconButton {...args} variant="solid" />
+      <IconButton {...args} variant="outline" />
+      <IconButton {...args} variant="ghost" />
+    </div>
+  ),
+};
+
+export const Sizes: Story = {
+  args: {
+    ...Base.args,
+  },
+  render: ({ ...args }) => (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+      <IconButton {...args} size="small" />
+      <IconButton {...args} size="medium" />
+    </div>
+  ),
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Base.args,
+    disabled: true,
+  },
+  render: ({ ...args }) => (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+      <IconButton {...args} variant="solid" />
+      <IconButton {...args} variant="outline" />
+      <IconButton {...args} variant="ghost" />
+    </div>
+  ),
+};
+
+export const Animated: Story = {
+  args: {
+    ...Base.args,
+  },
+  render: ({ ...args }) => (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+      <IconButton {...args} onClickAnimation="glow" />
+      <IconButton {...args} onClickAnimation="rotate360" />
+      <IconButton {...args} onClickAnimation="jiggle" />
+    </div>
+  ),
+};
+
+export const WithHref: Story = {
+  args: {
+    ...Base.args,
+  },
+  render: ({ ...args }) => (
+    <div style={{ display: 'flex', alignItems: 'center', gap: '1rem' }}>
+      <IconButton {...args} onClick={() => console.log('Hello')} />
+      <IconButton {...args} as="a" href="https://storybook.js.org/" />
+    </div>
+  ),
+};
