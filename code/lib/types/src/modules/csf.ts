1c2df873304bf3f476908cb03c90b7d77e725b19
Полянский Илья Игоревич <polyanskiy_ii@albank.ru>
2023-09-29 11:44:33 +0900

    Enhance InputType

--- code/lib/types/src/modules/csf.ts
+++ code/lib/types/src/modules/csf.ts
@@ -1,8 +1,5 @@
-import type { ViewMode as ViewModeBase } from '@storybook/csf';
-
-import type { Addon_OptionsParameter } from './addons';
-
-export type {
+import type {
+  ViewMode as ViewModeBase,
   AnnotatedStoryFn,
   Args,
   ArgsEnhancer,
@@ -59,6 +56,113 @@ export type {
   StrictInputType,
   Tag,
 } from '@storybook/csf';
+
+import type { Addon_OptionsParameter } from './addons';
+
+export type ControlType =
+  | 'boolean'
+  | 'color'
+  | 'date'
+  | 'number'
+  | 'range'
+  | 'object'
+  | 'text'
+  | 'radio'
+  | 'inline-radio'
+  | 'check'
+  | 'inline-check'
+  | 'select'
+  | 'file'
+  | 'multi-select';
+
+export type InputTypeOverride = InputType & {
+  control?:
+    | ControlType
+    | {
+        type: Extract<ControlType, 'number' | 'file'>;
+        accept: string;
+      }
+    | {
+        type: Extract<ControlType, 'number' | 'range'>;
+        max: number;
+        min: number;
+        step: number;
+      }
+    | {
+        type: Extract<ControlType, 'color'>;
+        presetColors: string[];
+      }
+    | {
+        type: ControlType;
+        labels?: { [option: string]: string };
+        [key: string]: any;
+      };
+  mapping?: { [key: string]: { [option: string]: any } };
+  table?: {
+    category?: string;
+    defaultValue?: { summary: string; detail?: string };
+    subcategory?: string;
+    type?: { summary?: string; detail?: string };
+  };
+};
+
+export type {
+  AnnotatedStoryFn,
+  Args,
+  ArgsEnhancer,
+  ArgsFromMeta,
+  ArgsStoryFn,
+  ArgTypes,
+  ArgTypesEnhancer,
+  BaseAnnotations,
+  ComponentAnnotations,
+  ComponentId,
+  ComponentTitle,
+  Conditional,
+  DecoratorApplicator,
+  DecoratorFunction,
+  Globals,
+  GlobalTypes,
+  IncludeExcludeOptions,
+  InputTypeOverride as InputType,
+  LegacyAnnotatedStoryFn,
+  LegacyStoryAnnotationsOrFn,
+  LegacyStoryFn,
+  LoaderFunction,
+  Parameters,
+  PartialStoryFn,
+  PlayFunction,
+  PlayFunctionContext,
+  Renderer,
+  SBArrayType,
+  SBEnumType,
+  SBIntersectionType,
+  SBObjectType,
+  SBOtherType,
+  SBScalarType,
+  SBType,
+  SBUnionType,
+  SeparatorOptions,
+  StepFunction,
+  StepLabel,
+  StepRunner,
+  StoryAnnotations,
+  StoryAnnotationsOrFn,
+  StoryContext,
+  StoryContextForEnhancers,
+  StoryContextForLoaders,
+  StoryContextUpdate,
+  StoryFn,
+  StoryId,
+  StoryIdentifier,
+  StoryKind,
+  StoryName,
+  StrictArgs,
+  StrictArgTypes,
+  StrictGlobalTypes,
+  StrictInputType,
+  Tag,
+};
 
 type OrString<T extends string> = T | (string & {});
 
