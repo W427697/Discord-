fbde20d51951d46b10db546c6747a70f2fa75bf6
Charles de Dreuille <charles.dedreuille@gmail.com>
2023-10-09 11:31:33 +0100

    Add offset + scrollbarSize props

--- code/ui/components/src/components/ScrollArea/ScrollArea.tsx
+++ code/ui/components/src/components/ScrollArea/ScrollArea.tsx
@@ -8,38 +8,56 @@ export interface ScrollAreaProps {
   horizontal?: boolean;
   vertical?: boolean;
   className?: string;
+  offset?: number;
+  scrollbarSize?: number;
 }
 
-const ScrollAreaRoot = styled(ScrollAreaPrimitive.Root)({
-  width: '100%',
-  height: '100%',
-  overflow: 'hidden',
-  '--scrollbar-size': '6px',
-});
+const ScrollAreaRoot = styled(ScrollAreaPrimitive.Root)<{ scrollbarSize: number; offset: number }>(
+  ({ scrollbarSize, offset }) => ({
+    width: '100%',
+    height: '100%',
+    overflow: 'hidden',
+    '--scrollbar-size': `${scrollbarSize + offset}px`,
+    '--radix-scroll-area-thumb-width': `${scrollbarSize}px`,
+  })
+);
 
 const ScrollAreaViewport = styled(ScrollAreaPrimitive.Viewport)({
   width: '100%',
   height: '100%',
 });
 
-const ScrollAreaScrollbar = styled(ScrollAreaPrimitive.Scrollbar)({
+const ScrollAreaScrollbar = styled(ScrollAreaPrimitive.Scrollbar)<{
+  offset: number;
+  horizontal: boolean;
+  vertical: boolean;
+}>(({ offset, horizontal, vertical }) => ({
   display: 'flex',
   userSelect: 'none', // ensures no selection
   touchAction: 'none', // disable browser handling of all panning and zooming gestures on touch devices
   background: 'transparent',
   transition: 'all 0.2s ease-out',
-  marginRight: 8,
-  marginTop: 8,
   borderRadius: 'var(--scrollbar-size)',
 
-  '&[data-orientation="vertical"]': { width: 'var(--scrollbar-size)', height: 'calc(100% - 16px)' },
-  '&[data-orientation="horizontal"]': { flexDirection: 'column', height: 'var(--scrollbar-size)' },
-});
+  '&[data-orientation="vertical"]': {
+    width: 'var(--scrollbar-size)',
+    paddingRight: offset,
+    marginTop: offset,
+    marginBottom: horizontal && vertical ? 0 : offset,
+  },
+  '&[data-orientation="horizontal"]': {
+    flexDirection: 'column',
+    height: 'var(--scrollbar-size)',
+    paddingBottom: offset,
+    marginLeft: offset,
+    marginRight: horizontal && vertical ? 0 : offset,
+  },
+}));
 
 const ScrollAreaThumb = styled(ScrollAreaPrimitive.Thumb)(({ theme }) => ({
   flex: 1,
   background: theme.base === 'dark' ? 'rgba(255,255,255,0.2)' : 'rgba(0,0,0,0.2)',
-  borderRadius: 'var(--scrollbar-size)',
+  borderRadius: `var(--scrollbar-size)`,
   position: 'relative',
   transition: 'background 0.2s ease-out',
 
@@ -63,16 +81,28 @@ export const ScrollArea: FC<ScrollAreaProps> = ({
   children,
   horizontal = false,
   vertical = false,
+  offset = 2,
+  scrollbarSize = 6,
 }) => (
-  <ScrollAreaRoot>
+  <ScrollAreaRoot scrollbarSize={scrollbarSize} offset={offset}>
     <ScrollAreaViewport>{children}</ScrollAreaViewport>
     {horizontal && (
-      <ScrollAreaScrollbar orientation="horizontal">
+      <ScrollAreaScrollbar
+        orientation="horizontal"
+        offset={offset}
+        horizontal={horizontal}
+        vertical={vertical}
+      >
         <ScrollAreaThumb />
       </ScrollAreaScrollbar>
     )}
     {vertical && (
-      <ScrollAreaScrollbar orientation="vertical">
+      <ScrollAreaScrollbar
+        orientation="vertical"
+        offset={offset}
+        horizontal={horizontal}
+        vertical={vertical}
+      >
         <ScrollAreaThumb />
       </ScrollAreaScrollbar>
     )}
