osnoser1/storybook

use @storybook/test everywhere

code/renderers/vue3/template/stories_vue3-vite-default-ts/ReactiveArgs.stories.ts
@@ -1,7 +1,6 @@
-import { expect } from '@storybook/jest';
 import { global as globalThis } from '@storybook/global';
 import type { Meta, StoryObj, StoryFn } from '@storybook/vue3';
-import { within, userEvent } from '@storybook/testing-library';
+import { within, userEvent, expect } from '@storybook/test';
 import { UPDATE_STORY_ARGS, STORY_ARGS_UPDATED, RESET_STORY_ARGS } from '@storybook/core-events';
 
 import ReactiveArgs from './ReactiveArgs.vue';

W427697/Discord-

code/renderers/vue3/template/stories_vue3-vite-default-ts/ReactiveArgs.stories.ts
@@ -1,7 +1,7 @@
-import { expect } from '@storybook/jest';
+import { expect, within, userEvent } from '@storybook/test';
+
 import { global as globalThis } from '@storybook/global';
 import type { Meta, StoryObj, StoryFn } from '@storybook/vue3';
-import { within, userEvent } from '@storybook/testing-library';
 import { UPDATE_STORY_ARGS, STORY_ARGS_UPDATED, RESET_STORY_ARGS } from '@storybook/core-events';
 
 import ReactiveArgs from './ReactiveArgs.vue';