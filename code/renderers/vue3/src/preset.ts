02f9b36cd79b1ac77fb4b37f3752ee094e5a6d00
Norbert de Langen <ndelangen@me.com>
2023-10-31 10:01:46 +0100

    change mechanism for detecting addon docs being active

--- code/renderers/vue3/src/preset.ts
+++ code/renderers/vue3/src/preset.ts
@@ -1,23 +1,12 @@
-import type { LoadedPreset, StorybookConfig } from '@storybook/types';
+import type { StorybookConfig } from '@storybook/types';
 import { join } from 'path';
 
-const hasDocs = (presetsList: LoadedPreset[]) => {
-  return presetsList.some((preset: string | { name: string }) => {
-    const presetName = typeof preset === 'string' ? preset : preset.name;
-    return presetName.includes('@storybook/addon-docs');
-  });
-};
-
 export const previewAnnotations: StorybookConfig['previewAnnotations'] = async (input, options) => {
-  const { presetsList } = options;
-  if (!presetsList) {
-    return input;
-  }
-  const docsInList = hasDocs(presetsList);
+  const docsEnabled = Object.keys(await options.presets.apply('docs', {}, options)).length > 0;
   const result: string[] = [];
 
   return result
     .concat(input)
     .concat([join(__dirname, 'preview.mjs')])
-    .concat(docsInList ? [join(__dirname, 'preview-docs.mjs')] : []);
+    .concat(docsEnabled ? [join(__dirname, 'preview-docs.mjs')] : []);
 };
