1388c64f402878fd8bbf9355675c8ca83537da94
Jeppe Reinhold <jeppe@reinhold.is>
2023-11-02 13:49:49 +0100

    move Vite deps otimization to presets

--- code/builders/builder-vite/src/optimizeDeps.ts
+++ code/builders/builder-vite/src/optimizeDeps.ts
@@ -8,10 +8,6 @@ const INCLUDE_CANDIDATES = [
   '@emotion/core',
   '@emotion/is-prop-valid',
   '@emotion/styled',
-  '@mdx-js/react',
-  '@storybook/addon-docs > acorn-jsx',
-  '@storybook/addon-docs',
-  '@storybook/addon-essentials/docs/mdx-react-shim',
   '@storybook/channels',
   '@storybook/client-api',
   '@storybook/client-logger',
@@ -79,7 +75,6 @@ const INCLUDE_CANDIDATES = [
   'lodash/uniq',
   'lodash/upperFirst.js',
   'lodash/upperFirst',
-  'markdown-to-jsx',
   'memoizerific',
   'overlayscrollbars',
   'polished',
@@ -126,6 +121,8 @@ const asyncFilter = async (arr: string[], predicate: (val: string) => Promise<bo
   Promise.all(arr.map(predicate)).then((results) => arr.filter((_v, index) => results[index]));
 
 export async function getOptimizeDeps(config: ViteInlineConfig, options: Options) {
+  const extraOptimizeDeps = await options.presets.apply('optimizeDeps', []);
+
   const { root = process.cwd() } = config;
   const { normalizePath, resolveConfig } = await import('vite');
   const absoluteStories = await listStories(options);
@@ -136,7 +133,10 @@ export async function getOptimizeDeps(config: ViteInlineConfig, options: Options
   // This function converts ids which might include ` > ` to a real path, if it exists on disk.
   // See https://github.com/vitejs/vite/blob/67d164392e8e9081dc3f0338c4b4b8eea6c5f7da/packages/vite/src/node/optimizer/index.ts#L182-L199
   const resolve = resolvedConfig.createResolver({ asSrc: false });
-  const include = await asyncFilter(INCLUDE_CANDIDATES, async (id) => Boolean(await resolve(id)));
+  const include = await asyncFilter(
+    Array.from(new Set([...INCLUDE_CANDIDATES, ...extraOptimizeDeps])),
+    async (id) => Boolean(await resolve(id))
+  );
 
   const optimizeDeps: UserConfig['optimizeDeps'] = {
     ...config.optimizeDeps,
