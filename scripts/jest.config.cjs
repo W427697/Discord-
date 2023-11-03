3c806e093cf048ce339134859fa7771aaac29ec2
Valentin Palkovic <valentin@chromatic.com>
2023-11-03 12:20:58 +0100

    Fix tests

--- scripts/jest.config.cjs
+++ scripts/jest.config.cjs
@@ -1,1 +1,21 @@
-module.exports = {};
+module.exports = {
+  transform: {
+    '^.+\\.(t|j)sx?$': [
+      '@swc/jest',
+      {
+        jsc: {
+          parser: {
+            syntax: 'typescript',
+            tsx: false,
+            decorators: false,
+            dynamicImport: false,
+          },
+          experimental: {
+            plugins: [['jest_workaround', {}]],
+          },
+        },
+      },
+    ],
+  },
+  transformIgnorePatterns: [],
+};
