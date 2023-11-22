8d612f3c245ee9fb4810d7ae07636690e2e43c65
Kasper Peulen <kasperpeulen@gmail.com>
2023-11-22 12:33:53 +0100

    Model loaders as before each and restore mocks properly

--- code/lib/test/src/index.ts
+++ code/lib/test/src/index.ts
@@ -1,18 +1,11 @@
 import { instrument } from '@storybook/instrumenter';
-import * as spy from '@vitest/spy';
+import { type LoaderFunction } from '@storybook/csf';
 import chai from 'chai';
-import { FORCE_REMOUNT, STORY_RENDER_PHASE_CHANGED } from '@storybook/core-events';
-import { addons } from '@storybook/preview-api';
+import { global } from '@storybook/global';
 import { expect as rawExpect } from './expect';
+import { clearAllMocks, resetAllMocks, restoreAllMocks } from './spy';
 
-export * from '@vitest/spy';
-
-const channel = addons.getChannel();
-
-channel.on(FORCE_REMOUNT, () => spy.spies.forEach((mock) => mock.mockClear()));
-channel.on(STORY_RENDER_PHASE_CHANGED, ({ newPhase }) => {
-  if (newPhase === 'loading') spy.spies.forEach((mock) => mock.mockClear());
-});
+export * from './spy';
 
 export const { expect } = instrument(
   { expect: rawExpect },
@@ -32,3 +25,16 @@ export const { expect } = instrument(
 );
 
 export * from './testing-library';
+
+const resetAllMocksLoader: LoaderFunction = ({ parameters }) => {
+  if (parameters?.test?.mockReset === true) {
+    resetAllMocks();
+  } else if (parameters?.test?.clearMocks === true) {
+    clearAllMocks();
+  } else if (parameters?.test?.restoreMocks !== false) {
+    restoreAllMocks();
+  }
+};
+
+// @ts-expect-error Wwe are using this as a default loader, if the test package is used. But we don't want to get into optional peer dep shenanigans.
+global.testPackageLoaders = [resetAllMocksLoader];
