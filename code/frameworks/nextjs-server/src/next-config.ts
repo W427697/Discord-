17b74b906609041330f88654c701f0c85522af94
Tom Coleman <tom@chromatic.com>
2023-11-23 16:04:09 +1100

    Don't need to set the `x-frame-options` header any more
    As the port for the SB is the same as the manager

--- code/frameworks/nextjs-server/src/next-config.ts
+++ code/frameworks/nextjs-server/src/next-config.ts
@@ -122,19 +122,5 @@ export const withStorybook = ({
         destination: `http://localhost:${sbPort}/index.json`,
       },
     ]),
-    async headers() {
-      return [
-        ...(config.headers ? await config.headers() : []),
-        {
-          source: `/${previewPath}/:path*`,
-          headers: [
-            {
-              key: 'x-frame-options',
-              value: '',
-            },
-          ],
-        },
-      ];
-    },
   });
 };
