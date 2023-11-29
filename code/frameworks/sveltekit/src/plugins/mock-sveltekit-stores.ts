f8489ef65f6b8849ae3faf041d74c24879b337ee
Jeppe Reinhold <jeppe@reinhold.is>
2023-11-29 10:38:42 +0100

    return partial vite config instead of merged

--- code/frameworks/sveltekit/src/plugins/mock-sveltekit-stores.ts
+++ code/frameworks/sveltekit/src/plugins/mock-sveltekit-stores.ts
@@ -1,17 +1,15 @@
 import { resolve } from 'node:path';
-import { mergeConfig, type Plugin } from 'vite';
+import type { Plugin } from 'vite';
 
 export function mockSveltekitStores() {
   return {
     name: 'storybook:sveltekit-mock-stores',
-    enforce: 'post',
-    config: (config) =>
-      mergeConfig(config, {
-        resolve: {
-          alias: {
-            $app: resolve(__dirname, '../src/mocks/app/'),
-          },
+    config: () => ({
+      resolve: {
+        alias: {
+          $app: resolve(__dirname, '../src/mocks/app/'),
         },
-      }),
+      },
+    }),
   } satisfies Plugin;
 }
