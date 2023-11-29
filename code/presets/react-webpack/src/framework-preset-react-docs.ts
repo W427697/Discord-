21b933dd2feba6be9fb97a22a4b10acdd3e3a4aa
Valentin Palkovic <valentin@chromatic.com>
2023-11-29 09:46:14 +0100

    Fix exclude regex in react-docgen-loader

--- code/presets/react-webpack/src/framework-preset-react-docs.ts
+++ code/presets/react-webpack/src/framework-preset-react-docs.ts
@@ -35,7 +35,7 @@ export const webpackFinal: StorybookConfig['webpackFinal'] = async (
             options: {
               babelOptions,
             },
-            exclude: /node_modules\/.*/,
+            exclude: /node_modules/,
           },
         ],
       },
@@ -61,7 +61,7 @@ export const webpackFinal: StorybookConfig['webpackFinal'] = async (
           options: {
             babelOptions,
           },
-          exclude: /node_modules\/.*/,
+          exclude: /node_modules/,
         },
       ],
     },
