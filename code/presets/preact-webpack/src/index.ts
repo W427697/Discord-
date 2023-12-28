d88b38811772badadff8c6b4c1b8f43747da6d89
Valentin Palkovic <valentin@chromatic.com>
2023-12-28 14:45:59 +0100

    Do not apply default babel plugins for preact

--- code/presets/preact-webpack/src/index.ts
+++ code/presets/preact-webpack/src/index.ts
@@ -1,37 +1,10 @@
 import { dirname, join } from 'path';
-import type { PresetProperty } from '@storybook/types';
 import type { StorybookConfig } from './types';
 
 export * from './types';
 
 const getAbsolutePath = <I extends string>(input: I): I =>
   dirname(require.resolve(join(input, 'package.json'))) as any;
-
-export const babel: PresetProperty<'babel'> = (config) => {
-  return {
-    ...config,
-    plugins: [
-      [
-        require.resolve('@babel/plugin-transform-react-jsx'),
-        { importSource: 'preact', runtime: 'automatic' },
-      ],
-      ...(config?.plugins || []).filter((p) => {
-        const name = Array.isArray(p) ? p[0] : p;
-        if (typeof name === 'string') {
-          return !name.includes('plugin-transform-react-jsx');
-        }
-        return true;
-      }),
-    ],
-    overrides: [
-      // Transforms to apply only to first-party code:
-      {
-        exclude: '**/node_modules/**',
-        presets: [require.resolve('@babel/preset-typescript')],
-      },
-    ],
-  };
-};
 
 export const webpackFinal: StorybookConfig['webpackFinal'] = (config) => {
   return {
