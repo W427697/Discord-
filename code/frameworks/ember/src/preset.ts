b03d4830c3d8dffcbdbce60a559a22acb1338447
Valentin Palkovic <valentin@chromatic.com>
2023-12-30 12:48:56 +0100

    Add custom babel-loader to Ember.js

--- code/frameworks/ember/src/preset.ts
+++ code/frameworks/ember/src/preset.ts
@@ -1,17 +1,43 @@
 import { dirname, join } from 'path';
 import type { PresetProperty } from '@storybook/types';
+import { getVirtualModules } from '@storybook/builder-webpack5';
+import { getProjectRoot } from '@storybook/core-common';
 import type { StorybookConfig } from './types';
-import { findDistFile } from './util';
 
 const getAbsolutePath = <I extends string>(input: I): I =>
   dirname(require.resolve(join(input, 'package.json'))) as any;
 
 export const addons: PresetProperty<'addons'> = [
+  require.resolve('./server/framework-preset-babel-ember'),
   require.resolve('./server/framework-preset-ember-docs'),
 ];
 
-export const previewAnnotations: PresetProperty<'previewAnnotations'> = (entry = []) => {
-  return [...entry, findDistFile(__dirname, 'client/preview/config')];
+export const webpackFinal: StorybookConfig['webpackFinal'] = async (baseConfig, options) => {
+  const { virtualModules } = await getVirtualModules(options);
+
+  const babelOptions = await options.presets.apply('babel', {}, options);
+  const typescriptOptions = await options.presets.apply('typescript', {}, options);
+
+  return {
+    ...baseConfig,
+    module: {
+      ...baseConfig.module,
+      rules: [
+        ...(baseConfig.module?.rules ?? []),
+        {
+          test: typescriptOptions.skipCompiler ? /\.((c|m)?jsx?)$/ : /\.((c|m)?(j|t)sx?)$/,
+          use: [
+            {
+              loader: require.resolve('babel-loader'),
+              options: babelOptions,
+            },
+          ],
+          include: [getProjectRoot()],
+          exclude: [/node_modules/, ...Object.keys(virtualModules)],
+        },
+      ],
+    },
+  };
 };
 
 export const core: PresetProperty<'core', StorybookConfig> = async (config, options) => {
