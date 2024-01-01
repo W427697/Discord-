26d83b3663755e37baf3f3f35d9c10257e9c4b25
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-01-01 09:30:50 +0100

    Fix buttons in about footer

--- code/ui/.storybook/main.ts
+++ code/ui/.storybook/main.ts
@@ -65,6 +65,7 @@ const config: StorybookConfig = {
   },
   core: {
     disableTelemetry: true,
+    disableWhatsNewNotifications: false,
   },
   viteFinal: (viteConfig, { configType }) =>
     mergeConfig(viteConfig, {
