c5d86fa87f5c3776190492bcc33268161022c216
Mihai Lazari <l3m3a3@gmail.com>
2024-01-09 11:47:46 +0200

    Make export-order-loader compatible with both es and cjs

--- code/builders/builder-webpack5/src/loaders/export-order-loader.ts
+++ code/builders/builder-webpack5/src/loaders/export-order-loader.ts
@@ -1,34 +1,66 @@
-import { parse } from 'es-module-lexer';
+import { parse as parseCjs, init as initCjsParser } from 'cjs-module-lexer';
+import { parse as parseEs } from 'es-module-lexer';
 import MagicString from 'magic-string';
 import type { LoaderContext } from 'webpack';
 
-export default async function loader(this: LoaderContext<any>, source: string) {
+export default async function loader(
+  this: LoaderContext<any>,
+  source: string,
+  map: any,
+  meta: any
+) {
   const callback = this.async();
 
   try {
-    // Do NOT remove await here. The types are wrong! It has to be awaited,
-    // otherwise it will return a Promise<Promise<...>> when wasm isn't loaded.
-    const [, exports = []] = await parse(source);
+    let isEsModule = true, esImports, moduleExports, namedExportsOrder;
+    
+    try {
+      // Do NOT remove await here. The types are wrong! It has to be awaited,
+      // otherwise it will return a Promise<Promise<...>> when wasm isn't loaded.
+      const parseResult = await parseEs(source);
+      esImports = parseResult[0] || [];
+      moduleExports = parseResult[1] || [];
+    } catch {
+      esImports = [];
+      moduleExports = [];
+    }
+    
+    if (!moduleExports.length && !esImports.length) {
+      isEsModule = false;
+      try {
+        await initCjsParser();
+        moduleExports = (parseCjs(source)).exports || [];
+      } catch {
+        moduleExports = [];
+      }
+      namedExportsOrder = moduleExports.filter(
+        (e) => e !== 'default' && e !== '__esModule'
+      );
+    } else {
+      namedExportsOrder = moduleExports.map(
+        (e) => source.substring(e.s, e.e)
+      ).filter((e) => e !== 'default');
+    }
 
-    const namedExportsOrder = exports.some(
-      (e) => source.substring(e.s, e.e) === '__namedExportsOrder'
-    );
-
-    if (namedExportsOrder) {
-      return callback(null, source);
+    if (namedExportsOrder.indexOf('__namedExportsOrder') >= 0) {
+      return callback(null, source, map, meta);
     }
 
     const magicString = new MagicString(source);
-    const orderedExports = exports.filter((e) => source.substring(e.s, e.e) !== 'default');
-    magicString.append(
-      `;export const __namedExportsOrder = ${JSON.stringify(
-        orderedExports.map((e) => source.substring(e.s, e.e))
-      )};`
-    );
+    const namedExportsOrderString = JSON.stringify(namedExportsOrder);
+    if (isEsModule) {
+      magicString.append(
+        `;export const __namedExportsOrder = ${namedExportsOrderString};`
+      );
+    } else {
+      magicString.append(
+        `;module.exports.__namedExportsOrder = ${namedExportsOrderString};`
+      );
+    }
 
-    const map = magicString.generateMap({ hires: true });
-    return callback(null, magicString.toString(), map);
+    const generatedMap = magicString.generateMap({ hires: true });
+    return callback(null, magicString.toString(), generatedMap, meta);
   } catch (err) {
-    return callback(err as any);
+    return callback(null, source, map, meta);
   }
 }
