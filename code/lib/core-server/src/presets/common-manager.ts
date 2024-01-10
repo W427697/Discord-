10d3efeaca74d8357161f483fd9ca8eaea3bd6f0
Michael Shilman <shilman@users.noreply.github.com>
2024-01-10 18:32:27 +0800

    Indexer: Add metaTags and make autodocs inherit them

--- code/lib/core-server/src/presets/common-manager.ts
+++ code/lib/core-server/src/presets/common-manager.ts
@@ -14,11 +14,6 @@ const excludeTags = Object.entries(global.TAGS_OPTIONS ?? {}).reduce((acc, entry
 addons.register(STATIC_FILTER, (api) => {
   api.experimental_setFilter(STATIC_FILTER, (item) => {
     const tags = item.tags || [];
-    // very strange behavior here. Auto-generated docs entries get
-    // the tags of the primary story by default, so if that story
-    // happens to be `docs-only`, then filtering it out of the sidebar
-    // ALSO filter out the sidebar entry, which is not what we want.
-    // Here we special case it, but there should be a better solution.
-    return tags.includes('docs') || tags.filter((tag) => excludeTags[tag]).length === 0;
+    return tags.filter((tag) => excludeTags[tag]).length === 0;
   });
 });
