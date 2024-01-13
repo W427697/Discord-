337ab52695405b06442e8f825206221d693f4812
Michael Shilman <shilman@users.noreply.github.com>
2024-01-13 23:05:14 +0800

    Fix formatting

--- code/addons/docs/src/preview.ts
+++ code/addons/docs/src/preview.ts
@@ -1,13 +1,16 @@
 import type { PreparedStory } from '@storybook/types';
 import { global } from '@storybook/global';
 
-const excludeTags = Object.entries(global.TAGS_OPTIONS ?? {}).reduce((acc, entry) => {
-  const [tag, option] = entry;
-  if ((option as any).excludeFromDocsStories) {
-    acc[tag] = true;
-  }
-  return acc;
-}, {} as Record<string, boolean>);
+const excludeTags = Object.entries(global.TAGS_OPTIONS ?? {}).reduce(
+  (acc, entry) => {
+    const [tag, option] = entry;
+    if ((option as any).excludeFromDocsStories) {
+      acc[tag] = true;
+    }
+    return acc;
+  },
+  {} as Record<string, boolean>
+);
 
 export const parameters: any = {
   docs: {
