617094e01bd255908e6e4d7d5b4d4331595724df
Norbert de Langen <ndelangen@me.com>
2024-01-23 09:29:10 +0100

    cleanup

--- code/deprecated/telemetry/vitest.config.ts
+++ code/deprecated/telemetry/vitest.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig, mergeConfig } from 'vitest/config';
 import { sep, posix } from 'path';
 import { vitestCommonConfig } from '../../vitest.workspace';
