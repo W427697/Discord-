0b7037d6d2512ae1c9e0c6e3faeecccca5cb6fb9
Valentin Palkovic <valentin@chromatic.com>
2024-01-25 15:00:06 +0100

    Spike: Support new Icons component in ToolbarMenuButton

--- code/addons/toolbars/src/components/ToolbarMenuButton.tsx
+++ code/addons/toolbars/src/components/ToolbarMenuButton.tsx
@@ -1,11 +1,12 @@
 import type { FC } from 'react';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { Icons, IconButton, type IconsProps } from '@storybook/components';
+import type * as NewIcons from '@storybook/icons';
 
 interface ToolbarMenuButtonProps {
   active: boolean;
   title: string;
-  icon?: IconsProps['icon'];
+  icon?: IconsProps['icon'] | (typeof NewIcons)[keyof typeof NewIcons];
   description: string;
   onClick?: () => void;
 }
@@ -17,9 +18,20 @@ export const ToolbarMenuButton: FC<ToolbarMenuButtonProps> = ({
   description,
   onClick,
 }) => {
+  const Icon = useMemo<any>(() => {
+    if (!icon) return null;
+    if (typeof icon === 'string') {
+      return <Icons icon={icon} />;
+    }
+
+    const NewIcon = icon;
+
+    return <NewIcon />;
+  }, [icon]);
+
   return (
     <IconButton active={active} title={description} onClick={onClick}>
-      {icon && <Icons icon={icon} />}
+      {Icon && <Icon />}
       {title ? `\xa0${title}` : null}
     </IconButton>
   );
