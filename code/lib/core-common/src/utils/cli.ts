7987e76169d2340aa49330ac878a62a0c689fc99
Valentin Palkovic <valentin@chromatic.com>
2024-01-29 15:57:16 +0100

    Refactor isCorePackage function to use versions map

--- code/lib/core-common/src/utils/cli.ts
+++ code/lib/core-common/src/utils/cli.ts
@@ -97,34 +97,4 @@ export const createLogStream = async (
   });
 };
 
-const PACKAGES_EXCLUDED_FROM_CORE = [
-  '@storybook/addon-bench',
-  '@storybook/addon-console',
-  '@storybook/addon-onboarding',
-  '@storybook/addon-postcss',
-  '@storybook/addon-designs',
-  '@storybook/addon-styling',
-  '@storybook/addon-styling-webpack',
-  '@storybook/bench',
-  '@storybook/builder-vite',
-  '@storybook/csf',
-  '@storybook/design-system',
-  '@storybook/ember-cli-storybook',
-  '@storybook/eslint-config-storybook',
-  '@storybook/expect',
-  '@storybook/jest',
-  '@storybook/linter-config',
-  '@storybook/mdx1-csf',
-  '@storybook/mdx2-csf',
-  '@storybook/react-docgen-typescript-plugin',
-  '@storybook/storybook-deployer',
-  '@storybook/test-runner',
-  '@storybook/testing-library',
-  '@storybook/testing-react',
-  '@nrwl/storybook',
-  '@nx/storybook',
-];
-export const isCorePackage = (pkg: string) =>
-  pkg.startsWith('@storybook/') &&
-  !pkg.startsWith('@storybook/preset-') &&
-  !PACKAGES_EXCLUDED_FROM_CORE.includes(pkg);
+export const isCorePackage = (pkg: string) => Object.keys(versions).includes(pkg);
