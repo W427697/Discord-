9ee4b76a28b63780c0798c92089c69cfea3fb8b4
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-02-05 15:43:27 +0000

    Update StatusIcon.tsx

--- code/addons/interactions/src/components/StatusIcon.tsx
+++ code/addons/interactions/src/components/StatusIcon.tsx
@@ -22,23 +22,23 @@ export const StatusIcon: React.FC<StatusIconProps> = ({ status }) => {
 
   switch (status) {
     case CallStates.DONE: {
-      return <CheckIcon color={theme.color.positive} />;
+      return <CheckIcon color={theme.color.positive} data-testid="icon-done" />;
     }
     case CallStates.ERROR: {
-      return <StopAltIcon color={theme.color.negative} />;
+      return <StopAltIcon color={theme.color.negative} data-testid="icon-error" />;
     }
     case CallStates.ACTIVE: {
-      return <PlayIcon color={theme.color.secondary} />;
+      return <PlayIcon color={theme.color.secondary} data-testid="icon-active" />;
     }
     case CallStates.WAITING: {
       return (
-        <WarningContainer>
+        <WarningContainer data-testid="icon-waiting">
           <CircleIcon color={transparentize(0.5, '#CCCCCC')} size={6} />
         </WarningContainer>
       );
     }
     default: {
-      return null; //log warning?
+      return null;
     }
   }
 };
