d4ca82647ca4261b5442de3c3a00c10c419792a6
Norbert de Langen <ndelangen@me.com>
2024-02-07 15:48:02 +0100

    remove the types from jsdoc

--- code/lib/cli/src/autoblock/types.ts
+++ code/lib/cli/src/autoblock/types.ts
@@ -17,22 +17,22 @@ export interface Blocker<T> {
   /**
    * Check if the blocker should block.
    *
-   * @param {AutoblockOptions} options - The context.
-   * @returns {Promise<T | false>} - Return a truthy value to activate the block, return false to proceed.
+   * @param > The context.
+   * @returns > Return a truthy value to activate the block, return false to proceed.
    */
   check: (options: AutoblockOptions) => Promise<T | false>;
   /**
    * Format a message to be printed to the log-file.
-   * @param {AutoblockOptions} options - The context.
-   * @param {T} data - The data returned from the check method.
-   * @returns {string} - The string to print to the terminal.
+   * @param > The context.
+   * @param > The data returned from the check method.
+   * @returns > The string to print to the terminal.
    */
   message: (options: AutoblockOptions, data: T) => string;
   /**
    * Format a message to be printed to the log-file.
-   * @param {AutoblockOptions} options - The context.
-   * @param {T} data - The data returned from the check method.
-   * @returns {string} - The string to print to the log-file.
+   * @param > The context.
+   * @param > The data returned from the check method.
+   * @returns > The string to print to the log-file.
    */
   log: (options: AutoblockOptions, data: T) => string;
 }
