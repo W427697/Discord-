913946ad98c17e32f5c60696533bcbc13f6b088c
Jeppe Reinhold <jeppe@reinhold.is>
2024-02-08 12:43:00 +0100

    manually set vitest thread count in CI

--- scripts/vitest.config.ts
+++ scripts/vitest.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vitest/config';
 
+/**
+ * CircleCI reports the wrong number of threads to Node.js, so we need to set it manually.
+ * Script tests are running with the small resource class, which has 1 vCPU.
+ * @see https://jahed.dev/2022/11/20/fixing-node-js-multi-threading-on-circleci/
+ * @see https://vitest.dev/config/#pooloptions-threads-maxthreads
+ * @see https://circleci.com/docs/configuration-reference/#x86
+ * @see .circleci/config.yml#L187
+ */
+const threadCount = process.env.CI ? 1 : undefined;
+
 export default defineConfig({
   test: {
     clearMocks: true,
+    poolOptions: {
+      threads: {
+        minThreads: threadCount,
+        maxThreads: threadCount,
+      },
+    },
   },
 });
