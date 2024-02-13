fce6818d61b1e21d9235625e19def339c2ae04fa
Jeppe Reinhold <jeppe@reinhold.is>
2024-02-13 09:49:36 +0100

    lower fake error threshold

--- scripts/sandbox/generate.ts
+++ scripts/sandbox/generate.ts
@@ -189,7 +189,7 @@ const runGenerators = async (
           // where as others are very picky about what directories can be called. So we need to
           // handle different modes of operation.
           try {
-            if (Math.random() < 0.6) {
+            if (Math.random() < 0.2) {
               throw new Error('Blip Bloop random error when executing before-script');
             }
             if (script.includes('{{beforeDir}}')) {
@@ -227,7 +227,7 @@ const runGenerators = async (
           await remove(join(beforeDir, '.git'));
 
           try {
-            if (Math.random() < 0.6) {
+            if (Math.random() < 0.2) {
               throw new Error('Blip Bloop random error when init storybook');
             }
             await addStorybook({ baseDir, localRegistry, flags, debug, env });
@@ -244,7 +244,7 @@ const runGenerators = async (
               cause: error,
             });
           }
-          if (Math.random() < 0.6) {
+          if (Math.random() < 0.2) {
             throw new Error('Blip Bloop random error anywhere');
           }
           await addDocumentation(baseDir, { name, dirName });
