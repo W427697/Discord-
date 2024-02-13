23b8fbf79386e58717f1518b9dd7472209b781aa
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-02-13 22:45:45 +0100

    Update transform.ts

--- scripts/transform-nextjs/transform.ts
+++ scripts/transform-nextjs/transform.ts
@@ -1,29 +1,52 @@
 import chalk from 'chalk';
-// import { transformSnippets } from './transform-snippets-1';
+import { transformSnippets } from './transform-snippets-1';
 import { convertMdToMdx } from './convert-to-mdx';
 import { removecomments } from './removeComments';
-// import { transformPaths } from './transform-snippets-2';
+import prompts from 'prompts';
+import { transformPaths } from './transform-snippets-2';
 
-console.log(chalk.cyan('---------------------------'));
-console.log(chalk.cyan("-- Let's move to Next.js --"));
-console.log(chalk.cyan('---------------------------'));
-console.log(' ');
+(async () => {
+  console.log(chalk.cyan('---------------------------'));
+  console.log(chalk.cyan("-- Let's move to Next.js --"));
+  console.log(chalk.cyan('---------------------------'));
+  console.log(' ');
 
-// Convert all .md files to .mdx
-console.log(chalk.cyan('Step 1') + ' - Converting all md files into mdx files...');
-convertMdToMdx('./docs');
+  const step1 = await prompts({
+    type: 'confirm',
+    name: 'value',
+    message: 'Do you want to convert all md files into mdx files?',
+    initial: true,
+  });
 
-// Remove all comments from .mdx files
-console.log(chalk.cyan('Step 2') + ' - Converting all md files into mdx files...');
-removecomments('./docs');
+  if (step1.value === true) convertMdToMdx('./docs');
 
-// Transform all snippets into single files
-console.log(chalk.cyan('Step 3') + ' - Transforming snippets...');
-// transformSnippets();
+  const step2 = await prompts({
+    type: 'confirm',
+    name: 'value',
+    message: 'Do you want to remove all comments?',
+    initial: false,
+  });
 
-// Converting snippets imports to single paths
-console.log(chalk.cyan('Step 4') + ' - Converting snuppets paths...');
-// transformPaths();
+  if (step2.value === true) removecomments('./docs');
 
-console.log(' ');
-console.log('🤍 Done');
+  const step3 = await prompts({
+    type: 'confirm',
+    name: 'value',
+    message: 'Do you want to transform snippets?',
+    initial: false,
+  });
+
+  if (step3.value === true) transformSnippets();
+
+  const step4 = await prompts({
+    type: 'confirm',
+    name: 'value',
+    message: 'Do you want to convert snippets paths?',
+    initial: false,
+  });
+
+  if (step4.value === true) transformPaths();
+
+  console.log(' ');
+  console.log('🤍 Done');
+})();
