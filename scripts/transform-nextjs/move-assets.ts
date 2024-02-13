6956c78a73db2c2e265482227fc1b0ecd37eaa25
Charles de Dreuille <charles.dedreuille@gmail.com>
2024-02-13 23:09:16 +0100

    Update move-assets.ts

--- scripts/transform-nextjs/move-assets.ts
+++ scripts/transform-nextjs/move-assets.ts
@@ -1,23 +1,30 @@
 import fs from 'fs';
 import path from 'path';
 
-export function moveMediaFiles(sourceDirectory: string, targetDirectory: string) {
-  if (!fs.existsSync(targetDirectory)) {
-    fs.mkdirSync(targetDirectory, { recursive: true });
-  }
+export function moveMediaFiles(
+  sourceDirectory: string,
+  targetDirectory: string,
+  currentDirectory = sourceDirectory
+) {
+  const files = fs.readdirSync(currentDirectory);
 
-  const files = fs.readdirSync(sourceDirectory);
-
-  files.forEach((file: any) => {
-    const filePath = path.join(sourceDirectory, file);
+  files.forEach((file) => {
+    const filePath = path.join(currentDirectory, file);
     const stats = fs.statSync(filePath);
 
     if (stats.isDirectory()) {
-      moveMediaFiles(filePath, targetDirectory);
+      moveMediaFiles(sourceDirectory, targetDirectory, filePath);
     } else {
       const ext = path.extname(file).toLowerCase();
       if (['.png', '.jpg', '.jpeg', '.gif', '.bmp', '.webp', '.mp4'].includes(ext)) {
-        const targetPath = path.join(targetDirectory, file);
+        const relativePath = path.relative(sourceDirectory, currentDirectory);
+        const targetPathDirectory = path.join(targetDirectory, relativePath);
+
+        if (!fs.existsSync(targetPathDirectory)) {
+          fs.mkdirSync(targetPathDirectory, { recursive: true });
+        }
+
+        const targetPath = path.join(targetPathDirectory, file);
         fs.renameSync(filePath, targetPath);
       }
     }
