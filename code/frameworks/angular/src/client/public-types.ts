366949bae8870512cf39ed898bd24c2f11add8d2
Koen François <koen.francois@showpad.com>
2024-02-15 09:04:56 +0100

    feat(angular): support input signals for angular components

--- code/frameworks/angular/src/client/public-types.ts
+++ code/frameworks/angular/src/client/public-types.ts
@@ -9,7 +9,7 @@ import {
   StrictArgs,
   ProjectAnnotations,
 } from '@storybook/types';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, InputSignal } from '@angular/core';
 import { AngularRenderer } from './types';
 
 export type { Args, ArgTypes, Parameters, StrictArgs } from '@storybook/types';
@@ -21,26 +21,35 @@ export type { AngularRenderer };
  *
  * @see [Default export](https://storybook.js.org/docs/formats/component-story-format/#default-export)
  */
-export type Meta<TArgs = Args> = ComponentAnnotations<AngularRenderer, TransformEventType<TArgs>>;
+export type Meta<TArgs = Args> = ComponentAnnotations<AngularRenderer, TransformComponentType<TArgs>>;
 
 /**
  * Story function that represents a CSFv2 component example.
  *
  * @see [Named Story exports](https://storybook.js.org/docs/formats/component-story-format/#named-story-exports)
  */
-export type StoryFn<TArgs = Args> = AnnotatedStoryFn<AngularRenderer, TransformEventType<TArgs>>;
+export type StoryFn<TArgs = Args> = AnnotatedStoryFn<AngularRenderer, TransformComponentType<TArgs>>;
 
 /**
  * Story object that represents a CSFv3 component example.
  *
  * @see [Named Story exports](https://storybook.js.org/docs/formats/component-story-format/#named-story-exports)
  */
-export type StoryObj<TArgs = Args> = StoryAnnotations<AngularRenderer, TransformEventType<TArgs>>;
+export type StoryObj<TArgs = Args> = StoryAnnotations<AngularRenderer, TransformComponentType<TArgs>>;
 
 export type Decorator<TArgs = StrictArgs> = DecoratorFunction<AngularRenderer, TArgs>;
 export type Loader<TArgs = StrictArgs> = LoaderFunction<AngularRenderer, TArgs>;
 export type StoryContext<TArgs = StrictArgs> = GenericStoryContext<AngularRenderer, TArgs>;
 export type Preview = ProjectAnnotations<AngularRenderer>;
+
+/**
+ * Utility type that transforms InputSignal and EventEmitter types
+ */
+type TransformComponentType<T> = TransformInputSignalType<TransformEventType<T>>;
+
+type TransformInputSignalType<T> = {
+  [K in keyof T]: T[K] extends InputSignal<infer E> ? E : T[K];
+};
 
 type TransformEventType<T> = {
   [K in keyof T]: T[K] extends EventEmitter<infer E> ? (e: E) => void : T[K];
