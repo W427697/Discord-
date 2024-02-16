fdd5bda1fe9f5efa2a0f4f937f61380b79206d3b
Jeppe Reinhold <jeppe@reinhold.is>
2024-02-16 22:39:26 +0100

    add addon annotations to PS kitchen sink

--- test-storybooks/portable-stories-kitchen-sink/react/playwright/index.ts
+++ test-storybooks/portable-stories-kitchen-sink/react/playwright/index.ts
@@ -1,4 +1,22 @@
-import { setProjectAnnotations } from '@storybook/react'
-import sbAnnotations from '../.storybook/preview'
+import { setProjectAnnotations } from '@storybook/react';
+import sbAnnotations from '../.storybook/preview';
+import addonInteractions from '@storybook/addon-interactions/preview';
+import addonActions from '@storybook/addon-essentials/actions/preview';
+import addonBackgrounds from '@storybook/addon-essentials/actions/preview';
+import addonDocs from '@storybook/addon-essentials/docs/preview';
+import addonHighlight from '@storybook/addon-essentials/highlight/preview';
+import addonMeasure from '@storybook/addon-essentials/measure/preview';
+import addonOutline from '@storybook/addon-essentials/outline/preview';
+import addonViewport from '@storybook/addon-essentials/viewport/preview';
 
-setProjectAnnotations(sbAnnotations)
+setProjectAnnotations([
+  sbAnnotations,
+  addonInteractions, // needed for portable stories? instruments spies
+  addonActions, // needed for portable stories? adds actions from argTypes?
+  addonViewport, // needed for portable stories? sets globals?
+  addonBackgrounds, // NOT needed for portable stories
+  addonDocs, // NOT needed for portable stories
+  addonHighlight, // NOT needed for portable stories
+  addonMeasure, // NOT needed for portable stories
+  addonOutline, // NOT needed for portable stories
+]);
