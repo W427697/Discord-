80b1de6bcd71b3c516a9b00e3e850223a193c1ad
Jeppe Reinhold <jeppe@reinhold.is>
2024-02-16 11:33:44 +0100

    use fixture from @storybook/react in playwright tests

--- test-storybooks/portable-stories-react/playwright/index.tsx
+++ test-storybooks/portable-stories-react/playwright/index.tsx
@@ -1,26 +1,4 @@
-// import { beforeMount, afterMount } from '@playwright/experimental-ct-react/hooks';
 import { setProjectAnnotations } from '@storybook/react';
 import sbAnnotations from '../.storybook/preview';
-import {
-  // StorybookHooksConfig,
-  // afterMount as sbAfterMount,
-  // beforeMount as sbBeforeMount,
-  // bootstrap,
-} from './storybook-utils';
 
 setProjectAnnotations(sbAnnotations);
-
-// OPTION 1: provide a bootstrap fn that receives their beforeMount and afterMount hooks
-// bootstrap({ beforeMount: () => {}, afterMount });
-
-// OPTION 2: provide hooks for users to add to their own hooks config
-// beforeMount<StorybookHooksConfig>(async ({ hooksConfig }) => {
-//   await sbBeforeMount({ hooksConfig });
-// });
-// or inline beforeMount(sbBeforeMount);
-
-// afterMount(async () => {
-//   // some other code here
-//   await sbAfterMount();
-// });
-// or inline afterMount(sbAfterMount);
--- test-storybooks/portable-stories-react/src/stories/Button.playwright.tsx
+++ test-storybooks/portable-stories-react/src/stories/Button.playwright.tsx
@@ -1,7 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { test, expect } from '@playwright/experimental-ct-react';
-import stories from './Button.portable';
-import { mountStory, playStory } from '../../playwright/storybook-utils';
+import { createTest } from '@storybook/react/experimental_playwright';
+import { test as base, expect } from '@playwright/experimental-ct-react';
+import stories from './Button.stories.playwright';
+
+const test = createTest(base);
 
 // Basic scenario that does not need loaders nor play fn
 test('renders primary button', async ({ mount }) => {
@@ -9,44 +11,6 @@ test('renders primary button', async ({ mount }) => {
   await expect(component).toContainText('Decorator')
 });
 
-// imports => beforeMount => mount => afterMount
-
-// Alternative: beforeMount + afterMount hooks that will load and play the story
-test.only('apply story loaders from hooks', async ({ mount }) => {
-  await mount(<stories.LoaderStory/>, { hooksConfig: { storyName: 'LoaderStory' }});
+test('loads, renders and plays a story using the SB fixture', async ({ mount }) => {
+  await mount(<stories.LoaderStory/>);
 });
-
-// Alternative: standalone play fn after component is mounted. Needs beforeMount hook for loading the story
-test.skip('apply story loaders + play story', async ({ mount, page }) => {
-  await mount(<stories.LoaderStory/>, { hooksConfig: { storyName: 'LoaderStory' }});
-  await playStory('LoaderStory', page);
-});
-
-// Alternative: A single mount fn that loads and plays the story. Would need no hooks but it's tricky and we don't have it working
-test.skip('apply story loaders from wrapper fn', async ({ mount, page }) => {
-  await mountStory({ mount, page }, stories.LoaderStory);
-});
-
-// PW UNWRAP Experiment stuff
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const playTheStory = async (story: any, page: any) => {
-//   await page.evaluate(async (story: any) => {
-//     // example of getting access to the component reference via playwright shenanigans
-//     const component = await window.__pwUnwrapObject(story);
-//     component.play({ canvasElement: document.querySelector('#root') })
-//   }, story);
-// }
-
-// // Alternative: custom load and play functions coming from context
-// test.skip('with custom context properties', async ({ mount, load, play }) => {
-//   await load(stories.LoaderStory);
-//   await mount(<stories.LoaderStory/>);
-//   await play(stories.LoaderStory);
-// });
-
-// // Alternative: custom mountStory property that does everything
-// test.only('with custom mount story', async ({ mountStory }) => {
-//   await mountStory(<stories.LoaderStory/>);
-// });
-
