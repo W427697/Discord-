7f3d6ad1ea07e28493cc12158ab3c5c90b1fbb86
Jeppe Reinhold <jeppe@reinhold.is>
2024-02-19 14:18:28 +0100

    support multiple types of story references

--- code/lib/preview-api/src/modules/store/csf/portable-stories.ts
+++ code/lib/preview-api/src/modules/store/csf/portable-stories.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable no-underscore-dangle */
 import { isExportStory } from '@storybook/csf';
 import type {
@@ -124,7 +125,6 @@ export function composeStories<TModule extends Store_CSFExports>(
   globalConfig: ProjectAnnotations<Renderer>,
   composeStoryFn: ComposeStoryFn
 ) {
-  // eslint-disable-next-line @typescript-eslint/naming-convention
   const { default: meta, __esModule, __namedExportsOrder, ...stories } = storiesImport;
   const composedStories = Object.entries(stories).reduce((storiesMap, [exportsName, story]) => {
     if (!isExportStory(exportsName, meta)) {
@@ -146,9 +146,23 @@ export function composeStories<TModule extends Store_CSFExports>(
 }
 
 declare global {
-  // eslint-disable-next-line no-var, @typescript-eslint/naming-convention
-  var __pwUnwrapObject: (storyRef: any) => { type: ComposedStoryFn };
+  type WrappedStoryRef = { __pw_type: 'jsx' | 'importRef' };
+  type UnwrappedJSXStoryRef = {
+    __pw_type: 'jsx';
+    type: ComposedStoryFn;
+  };
+  type UnwrappedImportStoryRef = ComposedStoryFn;
+
+  function __pwUnwrapObject(
+    storyRef: WrappedStoryRef
+  ): UnwrappedJSXStoryRef | UnwrappedImportStoryRef;
 }
+
+const unwrapStory = async (storyRef: WrappedStoryRef) => {
+  const unwrappedStoryRef = await globalThis.__pwUnwrapObject?.(storyRef);
+  return '__pw_type' in unwrappedStoryRef ? unwrappedStoryRef.type : unwrappedStoryRef;
+};
+
 export function createPlaywrightTest<TFixture extends { extend: any }>(
   baseTest: TFixture
 ): TFixture {
@@ -156,10 +170,8 @@ export function createPlaywrightTest<TFixture extends { extend: any }>(
     mount: async ({ mount, page }: any, use: any) => {
       await use(async (storyRef: any) => {
         // load the story in the browser
-        await page.evaluate(async (innerStoryRef: any) => {
-          const unwrappedStoryRef = await globalThis.__pwUnwrapObject?.(innerStoryRef);
-
-          const story = unwrappedStoryRef.type;
+        await page.evaluate(async (wrappedStoryRef: WrappedStoryRef) => {
+          const story = await unwrapStory(wrappedStoryRef);
           return story?.load?.();
         }, storyRef);
 
@@ -167,10 +179,8 @@ export function createPlaywrightTest<TFixture extends { extend: any }>(
         const mountResult = await mount(storyRef);
 
         // play the story in the browser
-        await page.evaluate(async (innerStoryRef: any) => {
-          const unwrappedStoryRef = await globalThis.__pwUnwrapObject?.(innerStoryRef);
-          const story = unwrappedStoryRef.type;
-
+        await page.evaluate(async (wrappedStoryRef: any) => {
+          const story = await unwrapStory(wrappedStoryRef);
           const canvasElement = document.querySelector('#root');
           return story?.play?.({ canvasElement });
         }, storyRef);
