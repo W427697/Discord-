d58137115a6b218142a65b0b50f7f47914159a4f
Yann Braga <yannbf@gmail.com>
2024-02-21 10:55:25 +0100

    cleanup

--- test-storybooks/portable-stories-kitchen-sink/vue3/stories/Button.playwright.tsx
+++ test-storybooks/portable-stories-kitchen-sink/vue3/stories/Button.playwright.tsx
@@ -1,12 +1,11 @@
 import { createTest } from '@storybook/vue3/experimental-playwright';
 import { test as base } from '@playwright/experimental-ct-vue';
 
-import stories from './Button.portable';
+import stories from './Button.stories.playwright';
 
 const test = createTest(base);
 
-test('renders primary button', async ({ mount }) => {
-  // TODO: this is not working, probably the translation that Playwright does won't work
-  // as stories.CSF3Primary is not a Vue component from a Vue file
+test.skip('renders primary button', async ({ mount }) => {
+  // TODO: this is not working, probably the translation that Playwright does not work with portable stories yet
   await mount(stories.WithLoader); 
 });
