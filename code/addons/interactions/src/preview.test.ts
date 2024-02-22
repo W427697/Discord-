8ceb63664b883f8268b58438e08bceb2e1f2e00e
Kasper Peulen <kasperpeulen@gmail.com>
2024-02-22 10:27:27 +0100

    Add comments

--- code/addons/interactions/src/preview.test.ts
+++ code/addons/interactions/src/preview.test.ts
@@ -1,10 +1,10 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { fn, isMockFunction } from '@storybook/test';
 import { action } from '@storybook/addon-actions';
 
 import { traverseArgs } from './preview';
 
-test('traverseArgs', () => {
+describe('traverseArgs', () => {
   const args = {
     deep: {
       deeper: {
@@ -18,17 +18,25 @@ test('traverseArgs', () => {
   expect(args.deep.deeper.fnKey.getMockName()).toEqual('spy');
 
   const traversed = traverseArgs(args) as typeof args;
-  expect(traversed).toEqual({
-    deep: {
-      deeper: {
-        fnKey: args.deep.deeper.fnKey,
-        actionKey: args.deep.deeper.actionKey,
-      },
-    },
-    arg2: args.arg2,
-  });
 
-  expect(traversed.deep.deeper.fnKey.getMockName()).toEqual('fnKey');
+  test('The same structure is maintained', () =>
+    expect(traversed).toEqual({
+      deep: {
+        deeper: {
+          fnKey: args.deep.deeper.fnKey,
+          actionKey: args.deep.deeper.actionKey,
+        },
+      },
+      // We don't mutate frozen objects, but we do insert them back in the tree
+      arg2: args.arg2,
+    }));
+
+  test('The mock name is mutated to be the arg key', () =>
+    expect(traversed.deep.deeper.fnKey.getMockName()).toEqual('fnKey'));
+
   const actionFn = traversed.deep.deeper.actionKey;
-  expect(isMockFunction(actionFn) && actionFn.getMockName()).toEqual('actionKey');
+
+  test('Actions are wrapped in a spy', () => expect(isMockFunction(actionFn)).toBeTruthy());
+  test('The spy of the action is also matching the arg key ', () =>
+    expect(isMockFunction(actionFn) && actionFn.getMockName()).toEqual('actionKey'));
 });
