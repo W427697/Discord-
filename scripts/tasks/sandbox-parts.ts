40e0cea55b2a50d64181fa068641d51dc2cf3073
Kasper Peulen <kasperpeulen@gmail.com>
2024-02-27 12:52:21 +0100

    Loosen tsconfig a bit for storybook

--- scripts/tasks/sandbox-parts.ts
+++ scripts/tasks/sandbox-parts.ts
@@ -585,7 +585,7 @@ async function prepareAngularSandbox(cwd: string, templateName: string) {
 
   // Set tsConfig compilerOptions
 
-  const tsConfigPath = join(cwd, 'tsconfig.json');
+  const tsConfigPath = join(cwd, '.storybook', 'tsconfig.json');
   const tsConfigContent = readFileSync(tsConfigPath, { encoding: 'utf-8' });
   // This does not preserve comments, but that shouldn't be an issue for sandboxes
   const tsConfigJson = JSON5.parse(tsConfigContent);
@@ -594,6 +594,8 @@ async function prepareAngularSandbox(cwd: string, templateName: string) {
   tsConfigJson.compilerOptions.noPropertyAccessFromIndexSignature = false;
   tsConfigJson.compilerOptions.jsx = 'react';
   tsConfigJson.compilerOptions.skipLibCheck = true;
+  tsConfigJson.compilerOptions.noImplicitAny = false;
+  tsConfigJson.compilerOptions.strict = false;
 
   if (templateName === 'Angular CLI (Version 15)') {
     tsConfigJson.compilerOptions.paths = {
