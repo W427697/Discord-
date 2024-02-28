6e414d5542e39c03fa583387c28ee88f1be1bc45
Valentin Palkovic <valentin@chromatic.com>
2024-02-28 13:23:39 +0100

    Update code/lib/cli/src/automigrate/fixes/addon-postcss.ts
    Co-authored-by: Jeppe Reinhold <jeppe@chromatic.com>

--- code/lib/cli/src/automigrate/fixes/addon-postcss.ts
+++ code/lib/cli/src/automigrate/fixes/addon-postcss.ts
@@ -29,7 +29,7 @@ export const addonPostCSS: Fix<AddonPostcssRunOptions> = {
     return dedent`
       ${chalk.bold(
         'Attention'
-      )}: We've detected that you're using the following package which might be incompatible since Storybook 8:
+      )}: We've detected that you're using the following package which are incompatible with Storybook 8 and beyond:
 
       - ${chalk.cyan(`@storybook/addon-postcss`)}
       
