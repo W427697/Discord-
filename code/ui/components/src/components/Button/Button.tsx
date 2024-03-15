db4e08ae8b2aaeb840a8a9f06e68878357e40ee9
Valentin Palkovic <valentin@chromatic.com>
2024-03-15 21:16:25 +0100

    Fix theming of elements inside bars
    In Storybook 8.0, we have broken theming for elements inside bars.
    The theming variables "barTextColor", "barHoverColor" and "barSelectedColor" were mostly
    ignored and mainly other variable colors were applied.
    
    This commit mainly ensures, that the default dark and light theming doesn't change.
    If "barTextColor", "barHoverColor" or "barSelectedColor" are set by the user,
    they get normally applied to the icon and text elements inside the bar areas,
    like the topbar, bottom-bar or the tab area in the addons panel.

--- code/ui/components/src/components/Button/Button.tsx
+++ code/ui/components/src/components/Button/Button.tsx
@@ -170,6 +170,34 @@ const StyledButton = styled('button', {
     if (variant === 'ghost' && active) return theme.background.hoverable;
     return 'transparent';
   })(),
+  ...(variant === 'ghost'
+    ? {
+        '.sb-bar &': {
+          background: (() => {
+            if (active) return transparentize(0.9, theme.barTextColor);
+            return 'transparent';
+          })(),
+          color: (() => {
+            if (active) return theme.barSelectedColor;
+            return theme.barTextColor;
+          })(),
+          '&:hover': {
+            color: theme.barHoverColor,
+            background: transparentize(0.86, theme.barHoverColor),
+          },
+
+          '&:active': {
+            color: theme.barSelectedColor,
+            background: transparentize(0.9, theme.barSelectedColor),
+          },
+
+          '&:focus': {
+            boxShadow: `${rgba(theme.barHoverColor, 1)} 0 0 0 1px inset`,
+            outline: 'none',
+          },
+        },
+      }
+    : {}),
   color: (() => {
     if (variant === 'solid') return theme.color.lightest;
     if (variant === 'outline') return theme.input.color;
