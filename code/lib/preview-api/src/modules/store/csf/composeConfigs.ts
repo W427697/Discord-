e90524dde64a0479180232a3436cefacf2b50370
Kasper Peulen <kasperpeulen@gmail.com>
2024-03-18 10:37:10 +0100

    Merge pull request #26514 from storybookjs/kasper/fix-single-loaders-decorators
    Bug: Make sure loaders/decorators can be used as array
    (cherry picked from commit b016a4f37243540e9fc244b39f58069763b06df3)

--- code/lib/preview-api/src/modules/store/csf/composeConfigs.ts
+++ code/lib/preview-api/src/modules/store/csf/composeConfigs.ts
@@ -3,6 +3,7 @@ import { global } from '@storybook/global';
 
 import { combineParameters } from '../parameters';
 import { composeStepRunners } from './stepRunners';
+import { normalizeArrays } from './normalizeArrays';
 
 export function getField<TFieldType = any>(
   moduleExportList: ModuleExports[],
@@ -16,10 +17,10 @@ export function getArrayField<TFieldType = any>(
   field: string,
   options: { reverseFileOrder?: boolean } = {}
 ): TFieldType[] {
-  return getField(moduleExportList, field).reduce(
-    (a: any, b: any) => (options.reverseFileOrder ? [...b, ...a] : [...a, ...b]),
-    []
-  );
+  return getField(moduleExportList, field).reduce((prev: any, cur: any) => {
+    const normalized = normalizeArrays(cur);
+    return options.reverseFileOrder ? [...normalized, ...prev] : [...prev, ...normalized];
+  }, []);
 }
 
 export function getObjectField<TFieldType = Record<string, any>>(
