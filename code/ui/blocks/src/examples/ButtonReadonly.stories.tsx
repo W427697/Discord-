5ac51f6689eb8636e872daa67a3642c29c90344c
Valentin Palkovic <valentin@chromatic.com>
2024-03-21 16:07:28 +0100

    Remove deprecated parameters from ButtonReadonly.stories.tsx

--- code/ui/blocks/src/examples/ButtonReadonly.stories.tsx
+++ code/ui/blocks/src/examples/ButtonReadonly.stories.tsx
@@ -20,14 +20,6 @@ const meta = {
       table: { readonly: true },
     },
   },
-  parameters: {
-    // Stop *this* story from being stacked in Chromatic
-    theme: 'default',
-    // these are to test the deprecated features of the Description block
-    notes: 'These are notes for the Button stories',
-    info: 'This is info for the Button stories',
-    jsx: { useBooleanShorthandSyntax: false },
-  },
 } satisfies Meta<typeof Button>;
 
 export default meta;
