d5b573a254b68fc757c66e6f256ef643822b05f4
Norbert de Langen <ndelangen@me.com>
2024-04-02 11:19:58 +0200

    more scaffolding

--- code/lib/core-server/src/utils/save-from-controls.ts
+++ code/lib/core-server/src/utils/save-from-controls.ts
@@ -2,6 +2,7 @@ import type { CoreConfig } from '@storybook/types';
 import type { Channel } from '@storybook/channels';
 import { SAVE_STORY_REQUEST, SAVE_STORY_RESULT } from '@storybook/core-events';
 import type { OptionsWithRequiredCache } from './whats-new';
+import { readCsf, writeCsf } from '@storybook/csf-tools';
 // import { sendTelemetryError } from '../withTelemetry';
 
 interface RequestSaveStoryPayload {
@@ -21,8 +22,32 @@ export function initializeSaveFromControls(
   coreOptions: CoreConfig
 ) {
   channel.on(SAVE_STORY_REQUEST, async (data: RequestSaveStoryPayload) => {
-    // TODO: Implement the save story logic
+    try {
+      // open the story file
+      const csf = await readCsf(data.importPath, { makeTitle: () => data.name });
+      const parsed = csf.parse();
 
-    channel.emit(SAVE_STORY_RESULT, { id: data.id, success: true });
+      // find the AST node for the story
+      const found = !!parsed.stories.find((s) => s.id === data.id);
+      let node = csf.getStoryExport(data.id);
+
+      // if none can be found, create one
+      if (!found) {
+        // @ts-expect-error (TODO)
+        node = undefined;
+        throw new Error(`creation of new story: not yet implemented`);
+      }
+
+      // modify the AST node with the new args
+      // node
+
+      // save the file
+      await writeCsf(csf);
+
+      channel.emit(SAVE_STORY_RESULT, { id: data.id, success: true });
+    } catch (e) {
+      // sendTelemetryError(channel, e);
+      channel.emit(SAVE_STORY_RESULT, { id: data.id, success: false });
+    }
   });
 }
