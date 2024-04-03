e86e004b2ac99f082d17e632480b530dfbce63c0
Valentin Palkovic <valentin@chromatic.com>
2024-04-03 14:13:04 +0200

    Add new function extractProperRendererNameFromFramework

--- code/lib/core-common/src/utils/get-renderer-name.ts
+++ code/lib/core-common/src/utils/get-renderer-name.ts
@@ -1,5 +1,7 @@
 import type { Options } from '@storybook/types';
-import { getFrameworkName } from './get-framework-name';
+import { extractProperFrameworkName, getFrameworkName } from './get-framework-name';
+import { frameworkPackages } from './get-storybook-info';
+import { frameworkToRenderer } from './framework-to-renderer';
 
 /**
  * Render is set as a string on core. It must be set by the framework
@@ -15,4 +17,24 @@ export async function getRendererName(options: Options) {
   }
 
   return core.renderer;
+}
+
+/**
+ * Extracts the proper renderer name from the given framework name.
+ * @param frameworkName The name of the framework.
+ * @returns The name of the renderer.
+ * @example
+ * extractProperRendererNameFromFramework('@storybook/react') // => 'react'
+ * extractProperRendererNameFromFramework('@storybook/angular') // => 'angular'
+ * extractProperRendererNameFromFramework('@third-party/framework') // => null
+ */
+export async function extractProperRendererNameFromFramework(frameworkName: string) {
+  const extractedFrameworkName = extractProperFrameworkName(frameworkName);
+  const framework = frameworkPackages[extractedFrameworkName];
+
+  if (!framework) {
+    return null;
+  }
+
+  return frameworkToRenderer[framework as keyof typeof frameworkToRenderer];
 }
