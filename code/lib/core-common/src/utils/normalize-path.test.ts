d0a68b229ab5f32453d85fa2f79eb83e67d5c2ce
Valentin Palkovic <valentin@chromatic.com>
2024-04-03 13:51:02 +0200

    Add normalize-path helper util

---
+++ code/lib/core-common/src/utils/normalize-path.test.ts
@@ -0,0 +1,10 @@
+import { normalizePath } from './normalize-path';
+import { describe, expect, it } from 'vitest';
+
+describe('normalize-path', () => {
+  it('should normalize paths', () => {
+    expect(normalizePath('path/to/../file')).toBe('path/file');
+    expect(normalizePath('path/to/./file')).toBe('path/to/file');
+    expect(normalizePath('path\\to\\file')).toBe('path/to/file');
+  });
+});
