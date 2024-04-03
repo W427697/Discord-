f0eec0a77575cf16ca45ee12da88bc143c607ac1
Valentin Palkovic <valentin@chromatic.com>
2024-04-03 13:52:04 +0200

    Finalized filesearch in controls

---
+++ code/addons/controls/src/utils/filesearch.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from 'vitest';
+import path from 'path';
+import { searchFiles } from './filesearch';
+
+describe('filesearch', () => {
+  describe('search result', () => {
+    it('should automatically convert normal search to a glob search', async (t) => {
+      const files = await searchFiles('commonjs', path.join(__dirname, '__tests__'), 'react');
+
+      expect(files?.map((f) => f.filepath)).toEqual(['src/commonjs-default.js', 'src/commonjs.js']);
+    });
+
+    it('should work with glob search patterns', async (t) => {
+      const files = await searchFiles('**/commonjs.js', path.join(__dirname, '__tests__'), 'react');
+
+      expect(files?.map((f) => f.filepath)).toEqual(['src/commonjs.js']);
+    });
+
+    it('should ignore node_modules', async (t) => {
+      const files = await searchFiles(
+        'file-in-common.js',
+        path.join(__dirname, '__tests__'),
+        'react'
+      );
+
+      expect(files).toEqual([]);
+    });
+  });
+
+  describe('exported components', () => {
+    it('should correctly return the exported components', async (t) => {
+      const files = await searchFiles('commonjs.js', path.join(__dirname, '__tests__'), 'react');
+
+      expect(files?.flatMap((f) => f.exportedComponents)).toHaveLength(5);
+    });
+  });
+});
--- code/addons/controls/src/utils/filesearch.ts
+++ code/addons/controls/src/utils/filesearch.ts
@@ -1,58 +1,61 @@
 import { globby } from 'globby';
-import type { SupportedRenderer } from './parser';
+import path from 'path';
+import fs from 'fs';
+
+import type { SupportedRenderer } from './parser/types';
 import { getParser } from './parser';
+import { isNotNull } from './ts-utils';
 
-export interface Data {
-  searchQuery: string;
-}
-
-export interface SearchResult {
-  success: boolean;
-  files: null | Array<{
-    filepath: string;
-    searchQuery: string;
-    exportedComponents: Array<{
-      name: string;
-      default: boolean;
-    }>;
+export type SearchResult = Array<{
+  filepath: string;
+  exportedComponents: Array<{
+    name: string;
+    default: boolean;
   }>;
-  error: null | string;
-}
+}>;
 
+/**
+ * Characters that are used in glob patterns to identify search queries that are not just filenames
+ */
+const globPatternChars = ['*', '+(', '@(', '?(', '!(', '[', ']'];
+
+/**
+ * Search for files in a directory that match the search query
+ * @param searchQuery The search query. This can be a glob pattern
+ * @param cwd The directory to search in
+ * @param renderer The renderer to use for parsing the files
+ * @returns A list of files that match the search query and has exports
+ */
 export async function searchFiles(
-  data: Data,
+  searchQuery: string,
   cwd: string,
   renderer: SupportedRenderer
 ): Promise<SearchResult> {
-  try {
-    const entries = await globby(data.searchQuery, {
-      ignore: ['**/node_modules/**'],
-      gitignore: true,
-      cwd,
-      objectMode: true,
-    });
+  const hasGlobChars = globPatternChars.some((char) => searchQuery.includes(char));
 
-    const files = entries.map(async (entry) => {
-      const parser = getParser(renderer);
-      const info = await parser.parse(entry.path);
+  const globbedSearchQuery = hasGlobChars ? searchQuery : `**/${searchQuery}**`;
+
+  const entries = await globby(globbedSearchQuery, {
+    ignore: ['**/node_modules/**'],
+    gitignore: true,
+    cwd,
+    objectMode: true,
+  });
+
+  const files = entries.map(async (entry) => {
+    const parser = getParser(renderer);
+    const content = fs.readFileSync(path.join(cwd, entry.path), 'utf-8');
+
+    try {
+      const info = await parser.parse(content);
 
       return {
         filepath: entry.path,
-        searchQuery: data.searchQuery,
         exportedComponents: info.exports,
       };
-    });
-
-    return {
-      success: true,
-      files: await Promise.all(files),
-      error: null,
-    };
-  } catch (e) {
-    return {
-      success: false,
-      files: null,
-      error: 'An error occurred while searching for files',
-    };
-  }
+    } catch (e) {
+      return null;
+    }
+  });
+  return (await Promise.all(files)).filter(isNotNull);
 }
