926472884e7e75f379ddd980478f59663c1ed23c
Valentin Palkovic <valentin@chromatic.com>
2024-04-08 16:01:42 +0200

    Fix loading of experimental channel for essential addons

---
+++ code/addons/essentials/src/preset.ts
@@ -0,0 +1,10 @@
+// @ts-expect-error no types
+import { experimental_serverChannel as experimentalServerChannelControls } from '@storybook/addon-controls/dist/preset';
+
+import type { Channel } from '@storybook/channels';
+import type { Options } from '@storybook/types';
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export const experimental_serverChannel = async (channel: Channel, options: Options) => {
+  experimentalServerChannelControls(channel, options);
+};
