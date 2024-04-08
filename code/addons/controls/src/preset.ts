58c9c7818479b8eeab8d23eabf0279fc1cddfe83
Valentin Palkovic <valentin@chromatic.com>
2024-04-08 16:54:24 +0200

    Cleanup

--- code/addons/controls/src/preset.ts
+++ code/addons/controls/src/preset.ts
@@ -1,11 +1,9 @@
 import type { Options } from '@storybook/types';
 import type { Channel } from '@storybook/channels';
 import { initFileSearchChannel } from './server-channel/file-search-channel';
-import { initCreateNewStoryChannel } from './server-channel/create-new-story-channel';
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const experimental_serverChannel = async (channel: Channel, options: Options) => {
-  initCreateNewStoryChannel(channel, options);
   initFileSearchChannel(channel, options);
 
   return channel;
