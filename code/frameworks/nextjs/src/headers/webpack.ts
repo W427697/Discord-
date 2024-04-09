d1fcfd5c3b751a8d590c3bdf2ad9bff7f3162be8
Yann Braga <yannbf@gmail.com>
2024-04-09 15:11:35 +0200

    Add explanation to webpack alias

--- code/frameworks/nextjs/src/headers/webpack.ts
+++ code/frameworks/nextjs/src/headers/webpack.ts
@@ -3,6 +3,8 @@ import type { Configuration as WebpackConfig } from 'webpack';
 
 export const configureNextHeaders = (baseConfig: WebpackConfig): void => {
   const resolve = baseConfig.resolve ?? {};
+  // Use headersPath for both next/headers and @storybook/nextjs/headers imports
+  // to make sure they all serve the MJS version of the file
   const headersPath = join(
     dirname(require.resolve('@storybook/nextjs/package.json')),
     '/dist/headers/index.mjs'
