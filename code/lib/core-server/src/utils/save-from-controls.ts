9fbf59b4d22bae9c76c2a3f0603a4689619522e4
Norbert de Langen <ndelangen@me.com>
2024-04-10 09:58:26 +0200

    find the AST node successfully

--- code/lib/core-server/src/utils/save-from-controls.ts
+++ code/lib/core-server/src/utils/save-from-controls.ts
@@ -1,8 +1,10 @@
+/* eslint-disable no-underscore-dangle */
 import type { CoreConfig } from '@storybook/types';
 import type { Channel } from '@storybook/channels';
 import { SAVE_STORY_REQUEST, SAVE_STORY_RESULT } from '@storybook/core-events';
 import type { OptionsWithRequiredCache } from './whats-new';
 import { readCsf, writeCsf } from '@storybook/csf-tools';
+import { join } from 'path';
 // import { sendTelemetryError } from '../withTelemetry';
 
 interface RequestSaveStoryPayload {
@@ -13,7 +15,7 @@ interface RequestSaveStoryPayload {
   // The updated list of set args
   args: Record<string, any>;
   // The exported name of the Story -> This information doesn't exist in the index.json yet.
-  name: string;
+  name?: string;
 }
 
 export function initializeSaveFromControls(
@@ -22,32 +24,50 @@ export function initializeSaveFromControls(
   coreOptions: CoreConfig
 ) {
   channel.on(SAVE_STORY_REQUEST, async (data: RequestSaveStoryPayload) => {
+    console.log('SAVE_STORY_REQUEST', data);
+    const id = data.id.split('--')[1];
     try {
+      const location = join(process.cwd(), data.importPath);
+      console.log({ location });
       // open the story file
-      const csf = await readCsf(data.importPath, { makeTitle: () => data.name });
+      const csf = await readCsf(location, {
+        makeTitle: (userTitle: string) => userTitle,
+      });
+
       const parsed = csf.parse();
+      // console.log(parsed._stories);
 
       // find the AST node for the story
-      const found = !!parsed.stories.find((s) => s.id === data.id);
-      let node = csf.getStoryExport(data.id);
+
+      const [name, result] =
+        Object.entries(parsed._stories).find(([key, value]) => value.id.endsWith(`--${id}`)) || [];
+
+      console.log({ name, result });
+      let node;
 
       // if none can be found, create one
-      if (!found) {
-        // @ts-expect-error (TODO)
+      if (!name) {
         node = undefined;
         throw new Error(`creation of new story: not yet implemented`);
+      } else {
+        node = csf.getStoryExport(name);
       }
 
       // modify the AST node with the new args
       // node
+      console.log({ node });
 
       // save the file
-      await writeCsf(csf);
+      await writeCsf(csf, location);
 
       channel.emit(SAVE_STORY_RESULT, { id: data.id, success: true });
-    } catch (e) {
+    } catch (e: any) {
       // sendTelemetryError(channel, e);
-      channel.emit(SAVE_STORY_RESULT, { id: data.id, success: false });
+      channel.emit(SAVE_STORY_RESULT, {
+        id: data.id,
+        success: false,
+        error: e.stack || e.message || e.toString(),
+      });
     }
   });
 }
--- code/ui/.storybook/manager.tsx
+++ code/ui/.storybook/manager.tsx
@@ -1,9 +1,53 @@
 import React from 'react';
-import { addons, types } from '@storybook/manager-api';
+import { addons, types, useArgs } from '@storybook/manager-api';
 import startCase from 'lodash/startCase.js';
+import { add } from 'lodash';
+import { SAVE_STORY_REQUEST } from '@storybook/core-events';
+import { satisfies } from 'semver';
 
 addons.setConfig({
   sidebar: {
     renderLabel: ({ name, type }) => (type === 'story' ? name : startCase(name)),
   },
+});
+
+interface RequestSaveStoryPayload {
+  // The id of the request. It might be simply the story Title
+  id: string;
+  // The path of the Story
+  importPath: string;
+  // The updated list of set args
+  args: Record<string, any>;
+  // The exported name of the Story -> This information doesn't exist in the index.json yet.
+  name: string;
+}
+
+addons.register('my-addon', (api) => {
+  addons.add('my-addon/panel', {
+    type: types.PANEL,
+    title: 'My Addon',
+    render: ({ active }) => {
+      const [args] = useArgs();
+
+      return active ? (
+        <div>
+          <button
+            onClick={() => {
+              const current = api.getCurrentStoryData();
+              console.log('CLICK');
+              const payload: RequestSaveStoryPayload = {
+                args,
+                id: current.id,
+                importPath: current.importPath,
+                name: 'StatelessWithCustomEmpty',
+              };
+              api.emit(SAVE_STORY_REQUEST, payload);
+            }}
+          >
+            CLICK
+          </button>
+        </div>
+      ) : null;
+    },
+  });
 });
