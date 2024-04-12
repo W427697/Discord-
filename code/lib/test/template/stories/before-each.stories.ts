fcf2b846fe9bcb9f92908e21d5a2ada5acf30412
Kasper Peulen <kasperpeulen@gmail.com>
2024-04-12 16:13:58 +0200

    Add test

---
+++ code/lib/test/template/stories/before-each.stories.ts
@@ -0,0 +1,29 @@
+import { global as globalThis } from '@storybook/global';
+import { expect, mocked, getByRole, spyOn, userEvent } from '@storybook/test';
+
+const meta = {
+  component: globalThis.Components.Button,
+  beforeEach() {
+    spyOn(console, 'log').mockName('console.log');
+    console.log('first');
+  },
+};
+
+export default meta;
+
+export const BeforeEachOrder = {
+  beforeEach() {
+    console.log('second');
+  },
+  args: {
+    label: 'Button',
+    onClick: () => {
+      console.log('third');
+    },
+  },
+  async play({ canvasElement }) {
+    await userEvent.click(getByRole(canvasElement, 'button'));
+
+    await expect(mocked(console.log).mock.calls).toEqual([['first'], ['second'], ['third']]);
+  },
+};
