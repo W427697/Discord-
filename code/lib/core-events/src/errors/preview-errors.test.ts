2631ef79fd2b40ed3ccc00f9aba6f5c0ce2e1c69
Reuben Ellis <ethriel3695@gmail.com>
2024-04-16 09:47:35 -0600

    remove dedent from test

--- code/lib/core-events/src/errors/preview-errors.test.ts
+++ code/lib/core-events/src/errors/preview-errors.test.ts
@@ -1,5 +1,4 @@
 import { describe, it, expect } from 'vitest';
-import dedent from 'ts-dedent';
 import { UnknownArgTypesError } from './preview-errors';
 
 describe('UnknownFlowArgTypesError', () => {
@@ -9,7 +8,7 @@ describe('UnknownFlowArgTypesError', () => {
       raw: "SomeType['someProperty']",
     };
 
-    const message = dedent`There was a failure when generating ArgTypes 
+    const message = `There was a failure when generating ArgTypes 
     in Typescript for {"name":"signature","raw":"SomeType['someProperty']"}
     This type is either not supported or it is a bug in Storybook.
     If you think this is a bug, please open an issue in Github.`;
