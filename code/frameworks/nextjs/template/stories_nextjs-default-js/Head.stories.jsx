b984dd1e3013cb381d7421adb93b50b6a71daaf5
Yann Braga <yannbf@gmail.com>
2024-04-18 12:20:24 +0200

    Update sandbox stories

--- code/frameworks/nextjs/template/stories_nextjs-default-js/Head.stories.jsx
+++ code/frameworks/nextjs/template/stories_nextjs-default-js/Head.stories.jsx
@@ -22,7 +22,7 @@ export default {
 };
 
 export const Default = {
-  play: async ({ canvasElement }) => {
+  play: async () => {
     await waitFor(() => expect(document.title).toEqual('Next.js Head Title'));
     await expect(document.querySelectorAll('meta[property="og:title"]')).toHaveLength(1);
     await expect(document.querySelector('meta[property="og:title"]').content).toEqual(
