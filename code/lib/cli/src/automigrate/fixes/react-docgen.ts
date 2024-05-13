99501d211070dc9a3f8cf654ccf822ebef1a3168
Valentin Palkovic <valentin@chromatic.com>
2024-05-13 12:04:17 +0200

    Revert "Automigration: Enhance react-docgen automigration prompt"
    This reverts commit 7be94ec0e6840c380ce5a87c996d66acf42c89dc.

--- code/lib/cli/src/automigrate/fixes/react-docgen.ts
+++ code/lib/cli/src/automigrate/fixes/react-docgen.ts
@@ -62,9 +62,7 @@ export const reactDocgen: Fix<Options> = {
       For known "react-docgen" limitations, see: 
       ${chalk.yellow('https://github.com/storybookjs/storybook/issues/26606')}
       
-      Press Y to switch back to ${chalk.cyan(
-        'react-docgen-typesript'
-      )}, press N to use ${chalk.cyan('react-docgen')}
+      Would you like to switch back to ${chalk.cyan('react-docgen-typescript')} in your Storybook?
     `;
     }
   },
