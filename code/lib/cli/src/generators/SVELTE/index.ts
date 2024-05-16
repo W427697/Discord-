fb2caf85a511a23277b67f497d482300afa26593
Norbert de Langen <ndelangen@me.com>
2024-05-16 16:25:19 +0200

    We need to fix this upstream in https://www.npmjs.com/package/@storybook/addon-svelte-csf @JReinhold

--- code/lib/cli/src/generators/SVELTE/index.ts
+++ code/lib/cli/src/generators/SVELTE/index.ts
@@ -5,6 +5,9 @@ const generator: Generator = async (packageManager, npmOptions, options) => {
   await baseGenerator(packageManager, npmOptions, options, 'svelte', {
     extensions: ['js', 'ts', 'svelte'],
     extraAddons: ['@storybook/addon-svelte-csf'],
+
+    // fix for https://app.circleci.com/pipelines/github/storybookjs/storybook/76699/workflows/f2bd740a-637a-45b8-9bd2-19d18e7be387/jobs/659133
+    extraPackages: ['@storybook/client-logger'],
   });
 };
 
