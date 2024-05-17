a3f3b7de51cbd777ae8ce7c222f821688fddee47
Norbert de Langen <ndelangen@me.com>
2024-05-17 09:49:19 +0200

    remove version override, it didn't work anyway

--- code/lib/cli/src/generators/SVELTE/index.ts
+++ code/lib/cli/src/generators/SVELTE/index.ts
@@ -4,7 +4,7 @@ import type { Generator } from '../types';
 const generator: Generator = async (packageManager, npmOptions, options) => {
   await baseGenerator(packageManager, npmOptions, options, 'svelte', {
     extensions: ['js', 'ts', 'svelte'],
-    extraAddons: ['@storybook/addon-svelte-csf@4.1.3--canary.182.e67e32d.0'],
+    extraAddons: ['@storybook/addon-svelte-csf'],
   });
 };
 
