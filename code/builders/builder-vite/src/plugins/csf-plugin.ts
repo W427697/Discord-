04378b75a1d6f19df2bd16e55ab8ea04343116f5
Valentin Palkovic <valentin@chromatic.com>
2024-05-17 13:15:44 +0200

    Vite: Fix sourcemapping for CSF files

--- code/builders/builder-vite/src/plugins/csf-plugin.ts
+++ code/builders/builder-vite/src/plugins/csf-plugin.ts
@@ -32,7 +32,8 @@ export async function csfPlugin(config: Options): Promise<Plugin> {
           makeTitle,
         }).parse();
         enrichCsf(csf, csfSource, options);
-        return formatCsf(csf, { sourceMaps: true });
+        const inputSourceMap = this.getCombinedSourcemap();
+        return formatCsf(csf, { sourceMaps: true, inputSourceMap }, code);
       } catch (err: any) {
         // This can be called on legacy storiesOf files, so just ignore
         // those errors. But warn about other errors.
